715f1d4d4d2bb933884a345dc4b78e6e14b6b4c4
Wody95 <kwu0626@gmail.com>
2021-02-23 17:07:17 +0900

    2021. 02. 22 / main.swift / chance 변수 추가

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -7,8 +7,8 @@
 import Foundation
 
 class NumberBaseballGame {
-    var randomNums: [Int] = []
-
+    var randomNums: [Int] = [] // 임의의 정수 3개를 담아두는 변수
+    let chance = 9             // 남은 시도 횟수 상수
 
     // 1~9 사이의 세 개의 임의의 정수 반환
     func getRandomNums() -> [Int] {
@@ -41,8 +41,8 @@ class NumberBaseballGame {
     
     func startGame() {
         randomNums = getRandomNums()
-        var tryCnt = 9
-        var status = false
+        var tryCnt = chance    // 남은 시도횟수 상수를 참조해 횟수를 초기화
+        var status = false     // 게임의 완료 유무를 저장하는 변수 선언
         
         for _ in 1...tryCnt {
             tryCnt -= 1
@@ -65,7 +65,7 @@ class NumberBaseballGame {
             print("\(gameResult["strike"]!) 스트라이크, \(gameResult["ball"]!) 볼")
             print("남은 기회 : \(tryCnt)")
             
-            if status {
+            if status {   // 게임이 끝난 상태라면 종료
                 break
             }
         }
