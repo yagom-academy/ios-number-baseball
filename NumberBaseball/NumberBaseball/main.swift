93e3ec591e12798b32b5a8698b7dffb7d10aca8d
Neph <neph3779@gmail.com>
2021-02-24 10:16:00 +0900

    Feat : Make blueprint of Code

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -1,5 +1,31 @@
+var answer:[Int] = [0] + Array(repeating: 0, count: 3)
+var remainingChallengeOpportunity: Int = 9
 
-var n = readLine()
-if let n = n{
-print(n)
+func startGame() {
+    
 }
+
+func playGame() {
+    
+}
+
+func getUserInput() -> [Int] {
+    return []
+}
+
+func makeRandomNumber() -> [Int] {
+    return []
+}
+
+func judge(of userInput: [Int]) -> (strikeCount: Int, ballCount: Int) {
+    return (0, 0)
+}
+
+func printResult(_ result :(strikeCount: Int, ballCount: Int)) {
+    
+}
+
+startGame()
+
+
+
