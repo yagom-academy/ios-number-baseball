e12ee3b31776934a6fcbc4e48f056da48771aa65
babywalnut <anfuza@naver.com>
2021-02-25 12:26:15 +0900

    add 숫자야구-step1

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -1,10 +1,69 @@
 //
 //  NumberBaseball - main.swift
-//  Created by yagom. 
+//  Created by Henry, Bobby. 
 //  Copyright © yagom academy. All rights reserved.
 // 
 
 import Foundation
 
-print("Hello, World!")
+class Baseball {
+    
+    var gameCount: Int = 9
+    var comNums: Array<Int> = []
+
+    
+    func startGame() {
+        comNums = randomNumber()
+        
+        while gameCount > 0 {
+            let userNums: Array<Int> = randomNumber()
+            print("임의의 수 : \(userNums[0]) \(userNums[1]) \(userNums[2])")
+            
+            result(user: userNums)
+            
+            gameCount -= 1
+            print("남은 기회 : \(gameCount)")
+        }
+    }
+}
+
+
+extension Baseball {
+    
+    func randomNumber() -> Array<Int> {
+        var numArray: Array<Int> = []
+        
+        for _ in 0...2 {
+            var num: Int
+            
+            repeat {
+                num = Int.random(in: 1...9)
+            } while numArray.contains(num) == true
+            numArray.append(num)
+        }
+        
+        return numArray
+    }
+    
+    
+    func result(user: [Int]) {
+        var strikeCount: Int = 0
+        var ballCount: Int = 0
+        
+        for i in 0...2 {
+            if comNums[i] == user[i] {
+                strikeCount += 1
+                
+            }else if comNums.contains(user[i]) {
+                ballCount += 1
+            }
+        }
+        
+        print("\(strikeCount) 스트라이크 \(ballCount) 볼")
+    }
+}
+
+
+var ball: Baseball = Baseball()
+ball.startGame()
 
