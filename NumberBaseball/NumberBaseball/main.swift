e70a7b2907883276d2e2229f5d59498f0b38fe87
inwoodev <dlsdn@gmail.com>
2021-02-25 17:02:00 +0900

    result error unfixed

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -8,46 +8,40 @@ var randomValue: [Int] = []
 var gameCount = 9
 
 let strike_out = 3
-let four_ball = 4
 
 // MARK: - Main Class
 class NumberBaseball {
-    
+    init() {
+        randomValue = createRandom()
+    }
     func startGame() {
-        randomValue = createRandom()     
-
-        while count != 0 {
+        while gameCount > 0 {
             print("숫자 3개를 띄어쓰기로 구분하여 입력해주세요.")
             print("중복 숫자는 허용하지 않습니다.")
             print("입력", terminator: ":")
-            var inputNum = readLine()!.split(separator: " ").map { Int(String($0))! }
-            if inputNum.count == 3 {
-                inputNum = checkRepeat(userInput: inputNum)
-                print("임의의 수 : \(inputNum[0]) \(inputNum[1]) \(inputNum[2])")
-                count -= 1
-                let result = compare(pitch: inputNum)
+            var inputNumber = readLine()!.split(separator: " ").map { Int(String($0)) ?? -1 }
+            checkInput(userInput: inputNumber)
+            inputNumber = checkRepeat(userInput: inputNumber)
+            gameCount -= 1
+            dump(inputNumber)
+            dump(gameCount)
+            print("임의의 수 : \(inputNumber[0]) \(inputNumber[1]) \(inputNumber[2])")
+            let result = compare(pitch: inputNumber)
+            
+            if result[0] == strike_out {
+                print("사용자 승리!")
+                print("1. 게임시작")
+                print("2. 게임 종료")
+                return
                 
-                if result[0] == STRIKEOUT {
-                    print("사용자 승리!")
-                    print("1. 게임시작")
-                    print("2. 게임 종료")
-                    return
-                    
-                } else if result[1] == FOURBALL {
-                    break
-                    
-                } else {
-                    print("\(result[0]) 스트라이크, \(result[1]) 볼")
-                    print("남은 기회 : \(count)")
-                    }
-            }
-            else {
-                print("잘못입력하셨습니다.")
-                startGame()
-            }
-        }
+            } else {
+                print("\(result[0]) 스트라이크, \(result[1]) 볼")
+                print("남은 기회 : \(gameCount)")
+                }
+    }
         print("컴퓨터 승리...!")
     }
+    
     func chooseGame() {
         print("1. 게임시작")
         print("2. 게임 종료")
@@ -67,18 +61,32 @@ class NumberBaseball {
             }
         }
     }
+    
     func checkRepeat(userInput: [Int]) -> [Int] {
-        var nonRepNums: [Int] = [Int]()
-      
-        for idx in userInput {
-            if nonRepNums.contains(idx) {
+        var nonRepNumbers: [Int] = []
+        for index in userInput {
+            if nonRepNumbers.contains(index) {
                 startGame()
               
             } else {
-            nonRepNums.append(idx)
+            nonRepNumbers.append(index)
             }
         }
-        return nonRepNums
+        return nonRepNumbers
+    }
+    
+    func checkInput(userInput: [Int]) {
+        if userInput.count == 3 && !userInput.contains(-1) {
+            return
+        }
+        else {
+            printError()
+            startGame()
+        }
+    }
+    
+    func printError() {
+        print("입력이 잘못되었습니다.")
     }
 }
 
@@ -86,19 +94,18 @@ class NumberBaseball {
 extension NumberBaseball {
     
     func createRandom() -> [Int] {
-        var randomNum: [Int] = []
+        var randomPitches: [Int] = []
         
         while randomPitches.count != 3 {
-            let num = Int.random(in: 1...9)
+            let number = Int.random(in: 1...9)
             
-            if randomPitches.contains(num) {
+            if randomPitches.contains(number) {
                 continue
                 
             } else {
-                randomPitches.append(num)
+                randomPitches.append(number)
             }
         }
-        
         return randomPitches
     }
     
@@ -115,8 +122,8 @@ extension NumberBaseball {
     func checkStrike(user: [Int]) -> Int {
         var strike = 0
         
-        for (com, pitcher) in zip(randomValue, user) {
-            if com == pitcher {
+        for (computer, pitcher) in zip(randomValue, user) {
+            if computer == pitcher {
                 strike += 1
             }
         }
@@ -126,13 +133,11 @@ extension NumberBaseball {
     
     func checkBall(user: [Int]) -> Int {
         var ball = 0
-        
-        for i in 0..<randomValue.count {
-            if randomValue.contains(user[i]) && (randomValue[i] != user[i]) {
+        for index in 0..<randomValue.count {
+            if randomValue.contains(user[index]) && (randomValue[index] != user[index]) {
                 ball += 1
             }
         }
-        
         return ball
     }
 }
