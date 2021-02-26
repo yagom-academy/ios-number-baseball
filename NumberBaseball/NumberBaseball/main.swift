35598be71fccde1477ba2fd7be58f426c479d36c
eunseo5355 <eunseo5355@naver.com>
2021-02-26 18:00:50 +0900

    [Step-1] 변수명 수정, makeRandomNumber 함수에서 숫자 저장할 변수를 배열대신 세트로 대체, startGame 함수에서 while문을 for문으로 변경

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -6,59 +6,57 @@
 
 import Foundation
 
-var correctNum: [Int] = []    // 임의의 정수 3개를 담아둘 변수
+var computerNumbers: [Int] = []    // 임의의 정수 3개를 담아둘 변수
 var attemptCount: Int = 9   // 남은 시도횟수를 담아둘 변수
 
 // 1~9 사이의 세 개의 임의의 정수를 생성하여 반환하는 함수
-func makeThreeRandomNum() -> [Int] {
+func makeRandomNumber() -> [Int] {
+    var randomNumber: Set<Int> = []
     
-    var randomNum = [Int](repeating: 0, count: 3)
+    while randomNumber.count != 3 {
+        randomNumber.insert(Int.random(in: 1...9))
+    }
     
-    repeat {
-        for index in 0...2 {
-            randomNum[index] = Int.random(in: 1...9)
-        }
-    } while randomNum[0] == randomNum[1] || randomNum[1] == randomNum[2] || randomNum[0] == randomNum[2]
-    
-    return randomNum
+    return Array(randomNumber)
 }
 
 // 예상하는 숫자 3개를 전달받아 맞춰야하는 숫자 3개와 비교하여 스트라이크와 볼을 반환하는 함수
-func compareNum(_ guessNum: [Int]) -> (strikeNum: Int, ballNum: Int) {
-    var strikeNum = 0
-    var ballNum = 0
+func compareNumber(_ userNumbers: [Int]) -> (strikeNumber: Int, ballNumber: Int) {
+    var strikeNumber = 0
+    var ballNumber = 0
     
     for index in 0...2 {
-        if (correctNum[index] == guessNum[index]) {
-            strikeNum += 1
-        } else if (correctNum.contains(guessNum[index])) {
-            ballNum += 1
+        if (computerNumbers[index] == userNumbers[index]) {
+            strikeNumber += 1
+        } else if (computerNumbers.contains(userNumbers[index])) {
+            ballNumber += 1
         }
     }
-    print("임의의 수: \(guessNum[0]) \(guessNum[1]) \(guessNum[2])")
+    print("임의의 수: \(userNumbers[0]) \(userNumbers[1]) \(userNumbers[2])")
     
-    return (strikeNum,ballNum)
+    return (strikeNumber, ballNumber)
 }
 
 // 게임시작 함수
 func startGame() {
-    correctNum = makeThreeRandomNum()
+    computerNumbers = makeRandomNumber()
     
-    while attemptCount != 0 {
+    print("\(computerNumbers[0]), \(computerNumbers[1]), \(computerNumbers[2])")
+    
+    for _ in 1...attemptCount {
+        let result = compareNumber(makeRandomNumber())
         attemptCount -= 1
-        let result = compareNum(makeThreeRandomNum())
 
-        if (result.0 == 3){
+        if (result.strikeNumber == 3){
             print("사용자 승리...!")
             break
         } else if (attemptCount == 0) {
             print("컴퓨터 승리...!")
         }
         
-        print("\(result.0) 스트라이크, \(result.1) 볼")
+        print("\(result.strikeNumber) 스트라이크, \(result.ballNumber) 볼")
         print("남은 기회: \(attemptCount)")
     }
-    return
 }
 
 startGame()
