ecc08d8d67ce6cfb955d2e04124d19e4f62128d5
dacodaco <imochi@naver.com>
2021-02-26 17:41:01 +0900

    [#4]coda, yun, sunny

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -6,100 +6,114 @@
 
 import Foundation
 
-// control+ command+ e   editor-edit all in scope
+// MARK:- 컴퓨터가 만드는 var storage
 var firstRandomNumber: Int = 0
 var secondRandomNumber: Int = 0
-var thirdRandomNUmber: Int = 0
-var randomNumberArray: [Int] = [firstRandomNumber, secondRandomNumber, thirdRandomNUmber]
+var thirdRandomNumber: Int = 0
+var randomNumberArray: [Int] = [firstRandomNumber, secondRandomNumber, thirdRandomNumber]
 
-
-
-// 사용자가 추신수처럼 공을 친다는 뜻으로 chooNum
+// MARK:- 우리가 입력하는 var storage
 var InputNumber: Int = 0
 var firstInputNumber: Int = 0
 var secondInputNumber: Int = 0
 var thirdInputNumber: Int = 0
 var inputNumberArray: [Int] = [firstInputNumber, secondInputNumber, thirdInputNumber]
 
-
-// ball의 의미가 규칙의 의미와 조금 다릅니다 ㅋㅋ
+// MARK:- 게임의 승패 판정을 나타내기 위한 var storage
 var strike: Int = 0
 var ball: Int = 0
 var beforeBall: Int = 0
-
-
-// 남은 게임 횟수
 var chanceToWin: Int = 9
 
-// 함수 모음
+// MARK:- func storage
+/// 변수에 랜덤 숫자를 생성하는 함수
 func makeNumber() {
     firstRandomNumber = Int.random(in: 1...9)
     secondRandomNumber = Int.random(in: 1...9)
-    thirdRandomNUmber = Int.random(in: 1...9)
-} // 변수에 숫자를 할당하는 함수
+    thirdRandomNumber = Int.random(in: 1...9)
+}
+
+/// 세 개의 변수가 중복값이 없도록 하는 함수
 func throwBall() {
-    while ( firstRandomNumber == secondRandomNumber ) || ( firstRandomNumber == thirdRandomNUmber ) || ( secondRandomNumber == thirdRandomNUmber ) {
+    while ( firstRandomNumber == secondRandomNumber ) || ( firstRandomNumber == thirdRandomNumber ) || ( secondRandomNumber == thirdRandomNumber ) {
         makeNumber()
-    } // 28/243
-    randomNumberArray = [firstRandomNumber, secondRandomNumber, thirdRandomNUmber]
-} // 컴퓨터가 생성한 숫자가 겹치지 않도록 만들어주는 함수
+    }
+    randomNumberArray = [firstRandomNumber, secondRandomNumber, thirdRandomNumber]
+}
+
+/// 사용자가 입력한 값을 정수형으로 저장하는 함수
 func batBall() {
     print("임의의 수(세 수를 띄어쓰기 없이 입력하세요): ", terminator:"")
     guard let chooString = readLine() else { return }
     guard let a = Int(chooString) else { return }
-    InputNumber = a //123 = 1, 2, 3
-} //숫자 세개를 입력받는 함수
+    InputNumber = a
+}
+
+/// 사용자가 입력한 값을 세 개의 수로 나누어 배열에 저장하는 함수
 func changeBatNumber() {
     firstInputNumber = (InputNumber - InputNumber%100)/100 //choonum = 123, (123-23)/100 =1
     secondInputNumber = (InputNumber - firstInputNumber*100 - InputNumber%10)/10 // = 123-100 - 3 = 20/10 = 2
     thirdInputNumber = (InputNumber - firstInputNumber*100 - secondInputNumber*10) // 3
-    
     print(firstInputNumber, secondInputNumber, thirdInputNumber)
     inputNumberArray = [firstInputNumber, secondInputNumber, thirdInputNumber]
-} // 쓴 세자리 수에서 각 수를 뽑아내는 함수
-func compareIndex() {
-
-    if randomNumberArray[0] == inputNumberArray[0] { strike += 1 } //123, 132 -> [1, 2, 3], [1, 3, 2]
-    if randomNumberArray[1] == inputNumberArray[1] { strike += 1 } // 1strike
-    if randomNumberArray[2] == inputNumberArray[2] { strike += 1 }
-} // 배열 안에 같은 숫자가 같은 자리에 있는지를 검사하는 함수
-func compareNumber() {
-    if randomNumberArray.contains(firstInputNumber) { beforeBall += 1 } //123, 132
-    if randomNumberArray.contains(secondInputNumber) { beforeBall += 1 }//ball = 3
-    if randomNumberArray.contains(thirdInputNumber) { beforeBall += 1 }
-
-} //배열 안에 같은 숫자가 있는지를 검사하는 함수
-
-
-//게임 시작
-while chanceToWin > 0 {
-    
-    throwBall()
-    batBall()
-    changeBatNumber()
-    compareIndex()
-    compareNumber()
-
-    //판정
-    ball = beforeBall - strike
-    print("\(strike) 스트라이크, \(ball) 볼")
-    
-    //strike가 세 개라면 사용자 승리
-    if strike == 3 {
-        print("게임 좀 하는데? 너가 바로 숫자야구 추신수")
-        break
-    }
-    
-    // 게임 횟수 1회 차감
-    chanceToWin -= 1
-    
-    // 게임 횟수가 0이라면 컴퓨터 승리
-    if chanceToWin == 0 {
-        print("컴퓨터한테 지냐? ㅋㅋ AI가 인류를 지배한다.")
-    }
-    
-    //st, ball 값을 리셋
-    strike = 0
-    ball = 0
 }
 
+
+/// ## Strike를 판별하는 함수
+/// -----
+/// - 배열 내에 같은 수가 같은 자리에 위치한다면 strike가 하나씩 올라가는 형식으로 함수가 진행된다.
+func checkStrike() {
+    if randomNumberArray[0] == inputNumberArray[0] { strike += 1 }
+    if randomNumberArray[1] == inputNumberArray[1] { strike += 1 }
+    if randomNumberArray[2] == inputNumberArray[2] { strike += 1 }
+}
+
+/// ## Ball을 판별하는 함수
+/// -----
+/// - 배열내에 같은 숫자가 있다면 일단 beforeBall 에 1이 추가된다.
+/// - 만약 그 숫자가 strike판정이 되는 경우, beforeBall 값에서 1을 빼 주어야 할 것이다.
+/// - 따라서 ball = beforeBall - strike로 변수를 설정해 ball 값을 결정지었다.
+func checkBall() {
+    if randomNumberArray.contains(firstInputNumber) { beforeBall += 1 }
+    if randomNumberArray.contains(secondInputNumber) { beforeBall += 1 }
+    if randomNumberArray.contains(thirdInputNumber) { beforeBall += 1 }
+    ball = beforeBall - strike
+}
+
+///
+/// ## 게임을 시작하는 함수
+/// -------
+/// - 게임을 시작하면 정해둔 함수를 차례대로 수행합니다.
+/// - 스트라이크와 볼 값을 출력합니다.
+/// - 남은 게임 횟수를 차감하고, 3strike이 되지 않는 한 게임 횟수가 0이 될때까지 반복합니다.
+func startGame() {
+    
+    for gameCount in 1...9 {
+        throwBall()
+        batBall()
+        changeBatNumber()
+        checkStrike()
+        checkBall()
+
+        print("\(strike) 스트라이크, \(ball) 볼")
+        print("남은 기회: \(9-gameCount)")
+        
+        //strike가 세 개라면 사용자가 승리하는 조건문
+        if strike == 3 {
+            print("사용자 승리!")
+            break
+        }
+        
+        // 게임횟수가 9번이 되면 컴퓨터가 승리하는 조건문
+        if gameCount == 9 {
+            print("컴퓨터 승리...!")
+        }
+        
+        //st, ball 값을 리셋
+        strike = 0
+        beforeBall = 0
+        ball = 0
+    }
+}
+
+startGame()
