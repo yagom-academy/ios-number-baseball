707e3a6cde1c34b9ebfd307982dc7ebd6255e310
dacodaco <imochi@naver.com>
2021-02-27 18:26:07 +0900

    Revert "git merge master"
    This reverts commit 43d64bf2395071110c8b06a0cae8a26fd97de4fd.
    
    NumberBaseball/NumberBaseball/main.swift

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -20,11 +20,65 @@ var thirdInputNumber: Int = 0
 var inputNumberArray: [Int] = [firstInputNumber, secondInputNumber, thirdInputNumber]
 
 // MARK:- 게임의 승패 판정을 나타내기 위한 var storage
+var strike: Int = 0
+var ball: Int = 0
+var beforeBall: Int = 0
+var chanceToWin: Int = 9
+
+// MARK:- func storage
+/// 변수에 랜덤 숫자를 생성하는 함수
+func makeNumber() {
+    firstRandomNumber = Int.random(in: 1...9)
+    secondRandomNumber = Int.random(in: 1...9)
+    thirdRandomNumber = Int.random(in: 1...9)
+}
+
+/// 세 개의 변수가 중복값이 없도록 하는 함수
+func throwBall() {
+    while ( firstRandomNumber == secondRandomNumber ) || ( firstRandomNumber == thirdRandomNumber ) || ( secondRandomNumber == thirdRandomNumber ) {
+        makeNumber()
+    }
+    randomNumberArray = [firstRandomNumber, secondRandomNumber, thirdRandomNumber]
+}
+
+/// 사용자가 입력한 값을 정수형으로 저장하는 함수
+func batBall() {
+    print("임의의 수(세 수를 띄어쓰기 없이 입력하세요): ", terminator:"")
+    guard let chooString = readLine() else { return }
+    guard let a = Int(chooString) else { return }
+    InputNumber = a
+}
+
+/// 사용자가 입력한 값을 세 개의 수로 나누어 배열에 저장하는 함수
+func changeBatNumber() {
+    firstInputNumber = (InputNumber - InputNumber%100)/100 //choonum = 123, (123-23)/100 =1
+    secondInputNumber = (InputNumber - firstInputNumber*100 - InputNumber%10)/10 // = 123-100 - 3 = 20/10 = 2
+    thirdInputNumber = (InputNumber - firstInputNumber*100 - secondInputNumber*10) // 3
+    print(firstInputNumber, secondInputNumber, thirdInputNumber)
+    inputNumberArray = [firstInputNumber, secondInputNumber, thirdInputNumber]
+}
 
 
+/// ## Strike를 판별하는 함수
+/// -----
+/// - 배열 내에 같은 수가 같은 자리에 위치한다면 strike가 하나씩 올라가는 형식으로 함수가 진행된다.
+func checkStrike() {
+    if randomNumberArray[0] == inputNumberArray[0] { strike += 1 }
+    if randomNumberArray[1] == inputNumberArray[1] { strike += 1 }
+    if randomNumberArray[2] == inputNumberArray[2] { strike += 1 }
+}
 
-
-
+/// ## Ball을 판별하는 함수
+/// -----
+/// - 배열내에 같은 숫자가 있다면 일단 beforeBall 에 1이 추가된다.
+/// - 만약 그 숫자가 strike판정이 되는 경우, beforeBall 값에서 1을 빼 주어야 할 것이다.
+/// - 따라서 ball = beforeBall - strike로 변수를 설정해 ball 값을 결정지었다.
+func checkBall() {
+    if randomNumberArray.contains(firstInputNumber) { beforeBall += 1 }
+    if randomNumberArray.contains(secondInputNumber) { beforeBall += 1 }
+    if randomNumberArray.contains(thirdInputNumber) { beforeBall += 1 }
+    ball = beforeBall - strike
+}
 
 ///
 /// ## 게임을 시작하는 함수
@@ -34,67 +88,6 @@ var inputNumberArray: [Int] = [firstInputNumber, secondInputNumber, thirdInputNu
 /// - 남은 게임 횟수를 차감하고, 3strike이 되지 않는 한 게임 횟수가 0이 될때까지 반복합니다.
 func startGame() {
     
-    //MARK:- 함수 내 변수 선언
-    var strike: Int = 0
-    var ball: Int = 0
-    var beforeBall: Int = 0
-    var chanceToWin: Int = 9
-    
-    //MARK:- 함수 내 메서드 생성
-    /// ## Strike를 판별하는 함수
-    /// -----
-    /// - 배열 내에 같은 수가 같은 자리에 위치한다면 strike가 하나씩 올라가는 형식으로 함수가 진행된다.
-    func checkStrike() {
-        if randomNumberArray[0] == inputNumberArray[0] { strike += 1 }
-        if randomNumberArray[1] == inputNumberArray[1] { strike += 1 }
-        if randomNumberArray[2] == inputNumberArray[2] { strike += 1 }
-    }
-
-    /// ## Ball을 판별하는 함수
-    /// -----
-    /// - 배열내에 같은 숫자가 있다면 일단 beforeBall 에 1이 추가된다.
-    /// - 만약 그 숫자가 strike판정이 되는 경우, beforeBall 값에서 1을 빼 주어야 할 것이다.
-    /// - 따라서 ball = beforeBall - strike로 변수를 설정해 ball 값을 결정지었다.
-    func checkBall() {
-        if randomNumberArray.contains(firstInputNumber) { beforeBall += 1 }
-        if randomNumberArray.contains(secondInputNumber) { beforeBall += 1 }
-        if randomNumberArray.contains(thirdInputNumber) { beforeBall += 1 }
-        ball = beforeBall - strike
-    }
-    
-    /// 변수에 랜덤 숫자를 생성하는 함수
-    func makeNumber() {
-        firstRandomNumber = Int.random(in: 1...9)
-        secondRandomNumber = Int.random(in: 1...9)
-        thirdRandomNumber = Int.random(in: 1...9)
-    }
-
-    /// 세 개의 변수가 중복값이 없도록 하는 함수
-    func throwBall() {
-        while ( firstRandomNumber == secondRandomNumber ) || ( firstRandomNumber == thirdRandomNumber ) || ( secondRandomNumber == thirdRandomNumber ) {
-            makeNumber()
-        }
-        randomNumberArray = [firstRandomNumber, secondRandomNumber, thirdRandomNumber]
-    }
-
-    /// 사용자가 입력한 값을 정수형으로 저장하는 함수
-    func batBall() {
-        print("임의의 수(세 수를 띄어쓰기 없이 입력하세요): ", terminator:"")
-        guard let inputString = readLine() else { return }
-        guard let stringStorage = Int(inputString) else { return }
-        InputNumber = stringStorage
-    }
-
-    /// 사용자가 입력한 값을 세 개의 수로 나누어 배열에 저장하는 함수
-    func changeBatNumber() {
-        firstInputNumber = (InputNumber - InputNumber % 100) / 100 //choonum = 123, (123-23)/100 =1
-        secondInputNumber = (InputNumber - firstInputNumber * 100 - InputNumber % 10) / 10 // = 123-100 - 3 = 20/10 = 2
-        thirdInputNumber = (InputNumber - firstInputNumber * 100 - secondInputNumber * 10) // 3
-        print(firstInputNumber, secondInputNumber, thirdInputNumber)
-        inputNumberArray = [firstInputNumber, secondInputNumber, thirdInputNumber]
-    }
-
-    //MARK:- 실질적인 게임 진행 과정
     for gameCount in 1...9 {
         throwBall()
         batBall()
@@ -117,8 +110,10 @@ func startGame() {
         }
         
         //st, ball 값을 리셋
+        strike = 0
+        beforeBall = 0
+        ball = 0
     }
 }
 
 startGame()
-
