d2a9744d1df8e834abbfd626a56f5ba2f51181fc
blanche37 <wkddbsghk16@gmail.com>
2021-05-26 20:37:59 +0900

    feat: 전역변수 추가

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -1,10 +1,15 @@
 //
 //  NumberBaseball - main.swift
-//  Created by yagom. 
+//  Created by shapiro,yun
 //  Copyright © yagom academy. All rights reserved.
-// 
+//
 
 import Foundation
 
-print("Hello, World!")
-
+var computerNumbers: Array<UInt> = []
+var numberOfTry: UInt = 9
+var ballCount: UInt = 0
+var strikeCount: UInt = 0
+var randomNumber: UInt = 0
+var isUserWin: Bool = false
+var isComputerWin: Bool = false
