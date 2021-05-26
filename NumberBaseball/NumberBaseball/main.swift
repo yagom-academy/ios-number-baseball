08f1c4d85a69200108276235678910d5a490365a
soll4u <dasollpark@gmail.com>
2021-05-27 00:20:19 +0900

    1~9 사이의 세 개의 임의의 정수를 생성하여 반환하는 함수 추가

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -1,10 +1,30 @@
 //
 //  NumberBaseball - main.swift
-//  Created by yagom. 
+//  Created by yagom.
+//  Edited by Soll & Seunggi
 //  Copyright © yagom academy. All rights reserved.
-// 
+//
 
 import Foundation
 
-print("test")
+var comX = 0, comY = 0, comZ = 0
+var myX, myY, myZ: Int
+var count: Int = 9
 
+func makeThreeRandomNo() -> (Int, Int, Int) {
+    var tempArray = [Int]()
+    
+    while tempArray.count < 3 {
+        let tempNo: Int = Int.random(in: 1...9)
+        if !tempArray.contains(tempNo){
+            tempArray.append(tempNo)
+        }
+    }
+    comX = tempArray[0]
+    comY = tempArray[1]
+    comZ = tempArray[2]
+    
+    return (comX, comY, comZ)
+}
+
+print(makeThreeRandomNo())
