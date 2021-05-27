c50e7bedfae8499aa9351869e3818086115a2d8d
brian2931 <sem789456@gmail.com>
2021-05-27 20:40:31 +0900

    feat: Add playGame function

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -1,2 +1,45 @@
 import Foundation
 
+func playGame() {
+    let answer:[Int] = generateAnswer()
+    var trialNo:Int = 9
+    
+    while trialNo > 0 {
+        let userInput:[Int] = getUserInput()
+
+        if compareAnswer(answer: answer, userInput: userInput) == 1 {
+            didWin()
+//            chooseOption()
+            return
+        }
+        trialNo -= 1
+    }
+
+    didWin()
+//    chooseOption()
+}
+
+
+func generateAnswer() -> [Int] {
+    
+}
+
+func getUserInput() -> [Int] {
+    
+}
+
+func compareAnswer(answer: [Int], userInput: [Int]) -> Int {
+    
+}
+
+func didWin() {
+    
+}
+
+//func chooseOption() {
+//
+//}
+
+func checkError() {
+    
+}
