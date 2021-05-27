3eba7fc68fa55911c645592cb52e5ca014f022cb
YesCoach <sbsii1020@gmail.com>
2021-05-27 10:39:55 +0900

    feat: 함수 getRandomNumbers, compareNumbers 생성

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -6,5 +6,35 @@
 
 import Foundation
 
-print("Hello, World!")
+let computerNumber: Set<Int>
+var gameCount = 9
 
+func getRandomNumbers() -> [Int] {
+    var randomNumbers = Set<Int>()
+    while randomNumbers.count < 3 {
+        randomNumbers.insert(Int.random(in: 0...9))
+    }
+    //let randomNumber = Array(randomNumbers) -> 변수 선언해주는 방법이 좋은지
+    return Array(randomNumbers)
+}
+
+func compareNumbers(computer: [Int], user: [Int]) -> (strike: Int, ball: Int) {
+    var strike: Int = 0
+    var ball: Int = 0
+    var diffNumbers = [Int]() // 스트라이크가 아닌 수의 배열
+    
+    for index in 0...2 {
+        if computer[index] == user[index] {
+            strike += 1
+        } else {
+            diffNumbers.append(computer[index])
+        }
+    }
+    
+    for index in diffNumbers {
+        if user.contains(index){
+            ball += 1
+        }
+    }
+    return (strike, ball)
+}
