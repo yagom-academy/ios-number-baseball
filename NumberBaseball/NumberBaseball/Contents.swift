55a9f49c05073acdcc8bfda679a2b0c9b0aeb140
taeyoung-Kim-KR <kty72301@naver.com>
2021-05-27 13:01:35 +0900

    Feat. 컨텐츠 모델 구현

---
+++ NumberBaseball/NumberBaseball/Contents.swift
@@ -0,0 +1,17 @@
+//
+//  Contents.swift
+//  NumberBaseball
+//
+//  Created by 김태영 on 2021/05/27.
+//
+
+import Foundation
+
+struct Contents {
+    static let randomNumber = "임의의 수 : "
+    static let strike = " 스트라이크, "
+    static let ball = "볼"
+    static let win = "사용자승리!!"
+    static let lose = "컴퓨터승리...!"
+    static let chance = "남은 기회 : "
+}
