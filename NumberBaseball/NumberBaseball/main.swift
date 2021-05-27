fa050504308daf7c77a69816385ffa8e158bb6a5
ictechgy <ictechgy@gmail.com>
2021-05-27 19:47:47 +0900

    feat: 기본적인 로직 및 로직에 필요한 함수 프로토타입 작성
    * generateThreeRandomNumbers(): 랜덤한 정수를 생성하여 반환하는 함수
    * changeNumbersToString(numbers:): 정수 배열을 문자열로 나열하여 반환하는 함수
    * getStrikesAndBallsNumber(numbersToCheck:): 정수 배열을 받아 스트라이크와 볼의 개수를 판별하는 함수
    * printResult(numberOfStrikes:numberOfBalls:): 스트라이크와 볼 개수를 출력해주는 함수

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -6,5 +6,40 @@
 
 import Foundation
 
-print("Hello, World!")
+func generateThreeRandomNumbers() -> [Int] {
+    
+    
+    return []
+}
+
+func changeNumbersToString(numbers: [Int]) -> String {
+    return numbers.map { element in
+        "\(element)"
+    }.joined(separator: " ")
+}
+
+func getStrikesAndBallsNumber(numbersToCheck numberList: [Int]) -> (strike: Int, ball: Int) {
+    
+    return (0, 0)
+}
+
+func printResult() {
+    
+}
+
+var answerRandomNumbers: [Int]
+var attemptsRemaining: Int = 9
+var attemptRandomNumbers: [Int]
+
+answerRandomNumbers = generateThreeRandomNumbers()
+
+while attemptsRemaining > 0 {
+    attemptRandomNumbers = generateThreeRandomNumbers()
+    let stringSequence = changeNumbersToString(numbers: attemptRandomNumbers)
+    print("임의의 수 : \(stringSequence)")
+    attemptsRemaining -= 1
+    
+    
+    print("남은 기회 : \(attemptsRemaining)")
+}
 
