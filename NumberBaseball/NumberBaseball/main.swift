de66b16832da01a5f5466fefe493eb0a8a3bf83b
산체스 <xognsl87@gmail.com>
2021-05-27 21:00:03 +0900

    첫번째 commit
    초기 계획은 함수별 commit을 하려고 했지만, step1 함수를 전부 구현하다보니
    한번에 pull request 함
    while loop 안에 if문 들여쓰기 수정 안됨.
    승부 결정시 코멘트 출력 구현함

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -6,5 +6,53 @@
 
 import Foundation
 
-print("Hello, World!")
+func generateRandom3() -> [Int] {
+    var randomSet = Set<Int>()
+    
+    while randomSet.count < 3 {
+        let number = Int.random(in: 1...9)
+        randomSet.insert(number)
+    }
+     return Array(randomSet)
+    
+}
+
+func compare(_ answer: [Int], _ userInput: [Int]) -> (Int,Int) {
+    var strike = 0
+    var ball = 0
+    
+    for i in 0...2 {
+        strike += answer[i] == userInput[i] ? 1 : 0
+    }
+    for i in 0...2 {
+        ball += answer.contains(userInput[i]) && answer[i] != userInput[i] ? 1 : 0
+    }
+    
+    return (strike, ball)
+}
+
+func starStep1Game() {
+    var remainingChances = 9
+    let answer = generateRandom3()
+    var userInput = generateRandom3()
+    
+    while remainingChances > 0 {
+        let strikeAndBall = compare(answer, userInput)
+        print("임의의 수 : \(userInput[0]) \(userInput[1]) \(userInput[2])")
+        print("\(strikeAndBall.0) 스트라이크,\(strikeAndBall.1) 볼")
+        
+        if strikeAndBall.0 == 3 {
+            return
+        }
+        
+        remainingChances -= 1
+        print("남은 기회 : \(remainingChances)")
+        userInput = generateRandom3()
+    }
+    return
+    
+}
+starStep1Game()
+
+
 
