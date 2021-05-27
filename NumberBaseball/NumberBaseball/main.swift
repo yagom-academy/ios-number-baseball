809d2b66a4929f26516f83a18b4894120d9a8319
YesCoach <sbsii1020@gmail.com>
2021-05-27 11:49:42 +0900

    fix : Bug 추가

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -13,31 +13,37 @@ func getRandomNumbers() -> [Int] {
     while randomNumbers.count < 3 {
         randomNumbers.insert(Int.random(in: 0...9))
     }
-    //let randomNumber = Array(randomNumbers) -> 변수 선언해주는 방법이 좋은지
     return Array(randomNumbers)
 }
+
+//func getStrike(_ computer: Int, _ user: Int){
+//    if computer == user {
+//
+//    } else {
+//
+//    }
+//}
 
 func compareNumbers(_ computer: [Int], _ user: [Int]) -> (strike: Int, ball: Int) {
     var strike: Int = 0
     var ball: Int = 0
-    var diffNumbers = [Int]() // 스트라이크가 아닌 수의 배열
+    var tempNumbers = [Int]()   // 스트라이크가 아닌 숫자의 배열 생성, 임시변수
     
     for index in 0...2 {
+//        getStrike(computer[index], user[index])
         if computer[index] == user[index] {
             strike += 1
         } else {
-            diffNumbers.append(computer[index])
+            tempNumbers.append(computer[index])
         }
     }
     
-    for index in diffNumbers {
+    for index in tempNumbers {
         if user.contains(index){
             ball += 1
         }
     }
-    
-    print("\(strike) 스트라이크, \(ball) 볼")
-    
+//    tempNumbers = []
     return (strike, ball)
 }
 
@@ -48,11 +54,20 @@ func startGame() {
         var userNumbers = getRandomNumbers()
         print("임의의 수 : \(userNumbers)")
         
-        compareNumbers(computerNumbers, userNumbers)
-        
+        var result = compareNumbers(computerNumbers, userNumbers)
+        if result.strike == 3 {
+            print("사용자 승리!")
+            break
+        }
         gameCount -= 1
+        if gameCount == 0 {
+            print("컴퓨터의 승리...!")
+        }
+        print("\(result.strike) 스트라이크, \(result.ball) 볼")
         print("남은 기회 : \(gameCount)")
+        
     }
+    print("\(computerNumbers)")
 }
 
 startGame()
