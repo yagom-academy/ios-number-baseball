cd49494ff3ccfb1dcc561a583b806e0be2c3cad4
ellen <jcrescent61@gmail.com>
2021-05-27 23:01:36 +0900

    docs: Step1 구현완료했습니다.

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -6,5 +6,59 @@
 
 import Foundation
 
-print("Hello, World!")
+// MARK: --- Step 1
+func makeRandomNumbers() -> Array<Int> {
+    var numbers = Array<Int>()
 
+    while numbers.count < 3 {
+        let randomNumber = Int.random(in: 1...9)
+    
+        if !numbers.contains(randomNumber) {
+            numbers.append(randomNumber)
+        }
+    }
+
+    return numbers
+}
+
+func playOneIning(com: Array<Int>, user: Array<Int>) -> (Int, Int) {
+    var strike: Int = 0
+    var ball: Int = 0
+
+    for index in 0..<3 {
+        if com[index] == user[index] {
+            strike += 1
+        } else if com.contains(user[index]) {
+            ball += 1
+        }
+    }
+    
+    return (strike, ball)
+}
+
+func startGame() {
+    var comPlayer = Array<Int>()
+    var ining: Int = 9
+
+    while ining > 0 {
+        comPlayer = makeRandomNumbers()
+        let autoPlayer = makeRandomNumbers()
+    
+        let (strike, ball) = playOneIning(com: comPlayer, user: autoPlayer)
+
+        print("임의의 수 : \(autoPlayer[0]) \(autoPlayer[1]) \(autoPlayer[2])")
+        print("\(strike) 스트라이크, \(ball) 볼")
+
+        ining -= 1
+        print("남은 기회 : \(ining)")
+
+        if strike > 2 {
+            print("사용자 승리!")
+            break
+        } else if ining == 0 {
+           print("컴퓨터 승리...!")
+        }
+    }
+}
+
+startGame()
