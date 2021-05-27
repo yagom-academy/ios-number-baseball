6e4590f53c24fee24017288af72b1245d4ca2942
soo941226 <rhrnakrnakr@naver.com>
2021-05-27 22:16:36 +0900

    ADD some functions for step1

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -1,10 +1,32 @@
-//
-//  NumberBaseball - main.swift
-//  Created by yagom. 
-//  Copyright © yagom academy. All rights reserved.
-// 
+var randomNumbers = makeRandomNum()
+var userNumbers = makeRandomNum()
+var count : Int = 9
 
-import Foundation
 
-print("Hello, World!")
+func makeRandomNum() -> [Int] {
+    var array = [Int]()
+    
+    while array.count <= 2 {
+        let randomNumber = Int.random(in: 1...9)
+        appendUniqueValue(to: &array, randomNumber)
+    }
 
+    return array
+}
+
+func appendUniqueValue(to: inout [Int],_ value:Int) -> Void {
+    let isContain = to.contains(value)
+    if isContain == false {
+        to.append(value)
+
+    }
+}
+
+func compareNumbers() -> String {
+    userNumbers = makeRandomNum()
+    
+    return ""
+}
+
+print(randomNumbers)
+print(userNumbers)
