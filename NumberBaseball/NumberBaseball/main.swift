59abd5b7c4a4408be92cb917470f87a90421dda8
soll4u <dasollpark@gmail.com>
2021-05-27 18:46:12 +0900

    feat: 컴퓨터와 유저의 수를 비교해 볼과 스트라이크를 계산하는 함수

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -6,11 +6,7 @@
 //
 import Foundation
 
-//var comX = 0, comY = 0, comZ = 0
-//var myX, myY, myZ: Int
 var count: Int = 9
-var userX = 0, userY = 0, userZ = 0
-
 
 func makeThreeRandomNo() -> [Int] {
     var tempArray = [Int]()
@@ -21,25 +17,26 @@ func makeThreeRandomNo() -> [Int] {
             tempArray.append(tempNo)
         }
     }
-    let comX = tempArray[0]
-    let comY = tempArray[1]
-    let comZ = tempArray[2]
-    
-    return [comX, comY, comZ]
+    return tempArray
 }
+
 let comArray = makeThreeRandomNo()
 let userArray = makeThreeRandomNo()
 var strike = 0, ball = 0
 print(comArray,userArray)
 
-for (indexCom, valueCom) in comArray.enumerated(){
-    for (indexUser, valueUser) in userArray.enumerated(){
-        if indexCom == indexUser && valueCom == valueUser {
-            strike += 1
-        }else if valueCom == valueUser {
-            ball += 1
+func compareComUser(_ com: [Int], _ user: [Int]) {
+    for (indexCom, valueCom) in com.enumerated(){
+        for (indexUser, valueUser) in user.enumerated(){
+            if indexCom == indexUser && valueCom == valueUser {
+                strike += 1
+            }else if valueCom == valueUser {
+                ball += 1
+            }
         }
     }
 }
+
+compareComUser(comArray, userArray)
 
 print(strike, ball)
