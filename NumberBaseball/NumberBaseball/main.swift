7ef29df759b5ac8c49d834ee7ab4f6361e07c4a7
KimWanKi <seoulkwk95@gmail.com>
2021-05-28 11:31:17 +0900

    feat/refactor : checkGameOver 함수 구현 / oneMatch, gameStart 함수 수정

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -14,61 +14,69 @@ func generateRandomNumbers() -> [Int] {
     return Array(randomNumbers)
 }
 
-func printNumbers(_ numbers: [Int]){
+func printNumbers(_ numbers: [Int]) {
     print("임의의 수 : \(numbers.map{String($0)}.joined(separator: " "))")
 }
 
 func checkStrike(userNumbers: [Int], to computerNumbers: [Int], index: Int) -> Int {
-    if(userNumbers[index] == computerNumbers[index]){
+    if userNumbers[index] == computerNumbers[index] {
         return 1
-    }else {
+    } else {
         return 0
     }
 }
 
-func checkTotalCalls(userNumbers: [Int], computerNumbers: [Int])-> Int {
+func checkTotalCalls(userNumbers: [Int], computerNumbers: [Int]) -> Int {
     return Set(userNumbers).intersection(Set(computerNumbers)).count
 }
 
-func oneMatch(userNumbers: [Int],vs computerNumbers: [Int]) -> (Int, Int) {
-    let totalCall = checkTotalCalls(userNumbers: userNumbers, computerNumbers: computerNumbers)
+func oneMatch(_ user: [Int],_ computer: [Int]) -> Bool {
+    var userWin = false
+    let totalCall = checkTotalCalls(userNumbers: user, computerNumbers: computer)
     if totalCall == 0 {
         print("0 스트라이크, 0 볼")
-        return (0, 0)
+        return userWin
     }
     var strike = 0
-    for i in 0..<3{
-        strike += checkStrike(userNumbers: userNumbers, to: computerNumbers, index: i)
+    for i in 0..<3 {
+        strike += checkStrike(userNumbers: user, to: computer, index: i)
     }
     let ball = totalCall - strike
     print("\(strike) 스트라이크, \(ball) 볼")
     
     if strike == 3 {
         printWinner(name: "사용자")
+        userWin = true
     }
-    return (strike, ball)
+    return userWin
 }
 
-func printWinner(name: String){
+func printWinner(name: String) {
     print("\(name) 승리...!")
 }
 
-
-func gameStart(){
-    let computerNumbers = generateRandomNumbers()
-    printNumbers(computerNumbers)
-    var gameCount: Int = 9
-    var userNumbers = [Int]()
-    var callResult: (strike: Int, ball: Int)
-    while gameCount > 0 {
-        userNumbers = generateRandomNumbers()
-        printNumbers(userNumbers)
-        callResult = oneMatch(userNumbers: userNumbers, vs: computerNumbers)
-        gameCount -= 1
+func checkGameOver(gameCount: Int, userWin: Bool) {
+    if gameCount > 0 && !userWin {
         print("남은 기회 : \(gameCount)")
     }
-    printWinner(name: "컴퓨터")
-    
+    else if gameCount == 0 {
+        print("남은 기회 : \(gameCount)")
+        printWinner(name: "컴퓨터")
+    }
+}
+
+func gameStart() {
+    let computerNumbers = generateRandomNumbers()
+    var gameCount: Int = 9
+    var userNumbers = [Int]()
+    var userWin = false
+    while gameCount > 0 && !userWin {
+        userNumbers = generateRandomNumbers()
+        printNumbers(userNumbers)
+        userWin = oneMatch(userNumbers, computerNumbers)
+        gameCount -= 1
+        checkGameOver(gameCount: gameCount, userWin: userWin)
+    }
     return
 }
 
