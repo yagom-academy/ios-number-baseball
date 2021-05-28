6b7da8da8bc1d17714370b8bada2265f13a01bc2
taelee <ritmakid@gmail.com>
2021-05-28 11:18:33 +0900

    refactor: isAnswer 함수에서 답인지 아닌지만 판단하고 출력하는 기능은 상위 스코프로 이동

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -25,8 +25,6 @@ func makeRandomNumbers() -> [Int] {
 
 func isAnswer(_ guessNumbers: [Int], _ answerNumbers: [Int]) -> Bool {
     if guessNumbers == answerNumbers {
-        print("3 스트라이크, 0 볼")
-        print("사용자 승리!")
         return true
     }
     return false
@@ -71,6 +69,8 @@ func gameStart(_ tryRound: Int, _ answerNumbers: [Int] ) {
     for i in 1...tryRound {
         let guessNumbers = makeRandomNumbers()
         if isAnswer(guessNumbers, answerNumbers) {
+            print("3 스트라이크, 0 볼")
+            print("사용자 승리!")
             break
         }
         let (strike, ball) = getStrikeAndBall(guessNumbers, answerNumbers)
