67f95b43fcee4051406efaa5dd4f00035e4efee2
Ldoy <leedoy7989@gmail.com>
2021-05-28 20:01:39 +0900

    작성 : 스텝2 함수 구현

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -350,58 +350,21 @@ import Foundation
 
 
 
+let ruleLiteral = """
+    숫자 3개로 띄어쓰기를 구분하여 입력해주세요.
+    중복 숫자는 허용하지 않습니다.
+    입력 :\(" ")
+    """
 
-func endGame() -> String {
-    return "종료"
+func endGame() {
 }
 
-var answer = Set<Int>()
-
-func generateAnswer() -> [Int] {
-    
-    while answer.count < 3 {
-        
-    let randomNumber = Int.random(in: 1...9)
-    answer.insert(randomNumber)
-        
-    }
-    
-    let answer = Array(answer)
-    
-    return answer
-}
-
-
-func judgeAnswer(user input: Int, com answer: [Int]) {
-// 스타트 게임에서
-// let userNums = userNum?.compactMap { $0.wholeNumberValue }
-    
-    
-    
-}
-
-let numbers: Set = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-
-
-func startGame() {
-    
-    let answer = generateAnswer()
-    
-    let userNum = readLine()
-    
-    
-    
-    
-}
-
-
-//any 써도 됨??
-func showOption() -> String? {
+func selectOption() -> String? {
     
     let optionList = """
         1. 게임시작
         2. 게임종료
-        원하는 기능을 선택해 주세요 :
+        원하는 기능을 선택해 주세요 :\(" ")
         """
         
     print(optionList, terminator: "")
@@ -412,17 +375,98 @@ func showOption() -> String? {
 }
 
 
+func generateAnswer() -> [Int] {
+    
+    var answer = Set<Int>()
 
-//any해도 됨? return 안해도 됨?
+    while answer.count < 3 {
+        
+    let randomNumber = Int.random(in: 1...9)
+    answer.insert(randomNumber)
+        
+    }
+    
+    let answers = Array(answer)
+    
+    return answers
+}
 
-func selectOption(option num: String?) -> Any {
+
+
+
+
+func filterUserInput(num : String?) -> [Int?] {
+    
+    if let userNums = num {
+        let userNumSet: Set = Set(userNums.split(separator: " ", maxSplits: 2).map { Int($0) })
+        let basicNum: Set = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        
+        let userNumLast = userNumSet.intersection(basicNum).map { $0 }
+        
+        switch userNumLast.count {
+        case 3:
+            return userNumLast
+        default:
+            print("입력이 잘못되었습니다.")
+            print(ruleLiteral, terminator: "")
+            
+        }
+    }
+    return [0]
+}
+
+
+
+func judgeAnswer(user num: [Int?]) -> String {
+    var strike = 0
+    var ball = 0
+    let numRange = 0...2
+
+    for index in numRange {
+        if num[index] == num[index] {
+            strike += 1
+        } else if num.contains(num[index] ?? 0) {
+            ball += 1
+        }
+    }
+
+    if strike == 3 {
+        return "사용자 승리...!"
+    }
+    
+    return "\(strike) 스트라이크, \(ball) 볼"
+}
+
+
+func startGame() -> Any {
+    
+    let comanswer = generateAnswer()
+    
+    var leftchance = 9
+
+    for _ in 1...leftchance {
+        let userInput = readLine()
+        let userNum = filterUserInput(num: userInput)
+        let judgeAnswer = judgeAnswer(user: userNum)
+        
+        switch judgeAnswer {
+        case "사용자 승리...!":
+            return selectOption()
+
+        default:
+            print("남은 기회 : \(leftchance - 1)", terminator: "")
+            leftchance -= 1
+        }
+    }
+    
+    print("컴퓨터의 승리...!")
+    return selectOption()
+}
+
+
+func writeNumber(option num: String?) -> Any {
     switch num {
     case "1" :
-        let ruleLiteral = """
-            숫자 3개로 띄어쓰기를 구분하여 입력해주세요.
-            중복 숫자는 허용하지 않습니다.
-            입력 :
-            """
         print(ruleLiteral, terminator: "")
         
         return startGame()
@@ -432,12 +476,13 @@ func selectOption(option num: String?) -> Any {
         
     default :
         print("입력이 잘못되었습니다.")
-        return showOption() ?? ""
+        selectOption()
     }
+    return 0
 }
 
 
-let a = selectOption(option: "a")
-print(a)
+let doyi = selectOption()
+let b = writeNumber(option: doyi)
 
-
+print(b)
