a19e7ce767f5416c893959a71bc4620b6d32af17
Jiwon Yoon <jiwonyoon@Jiwonui-MacBookPro.local>
2021-05-30 03:30:53 +0900

    Refactor - Step 2
    - 코드컨벤션을 수정했습니다.
    - 출력문을 묶어 출력해주는 함수들을 추가하였습니다.
    - nil 병합연산자를 바인딩함수를 추가함으로서 줄이도록 노력하였습니다.
    - 변수/상수 선언시 타입을 명시하여 type inferencing 을 피했습니다.

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -1,73 +1,83 @@
+//
+//  NumberBaseball - main.swift
+//  Created by yagom.
+//  Copyright © yagom academy. All rights reserved.
+//
+
 import Foundation
 
 func createThreeRandomNumbers() -> [Int] {
-    var randomSet = Set<Int>()
+    var randomSet: Set = Set<Int>()
     
     while randomSet.count < 3 {
-        let number = Int.random(in: 1...9)
+        let number: Int = Int.random(in: 1...9)
         randomSet.insert(number)
     }
      return Array(randomSet)
     
 }
 
-func compareAnswers(answerNumbers: [Int], userInputNumbers: [Int]) -> (strikes: Int,balls: Int) {
-    var strike = 0
-    var ball = 0
+func compareAnswers(answerNumbers: [Int], userInputNumbers: [Int]) -> (strike: Int, ball: Int) {
+    var strike: Int = 0
+    var ball: Int = 0
     
-    for i in 0...2 {
-        strike += answerNumbers[i] == userInputNumbers[i] ? 1 : 0
+    for index in 0..<answerNumbers.count {
+        strike += answerNumbers[index] == userInputNumbers[index] ? 1 : 0
         
-        ball += answerNumbers.contains(userInputNumbers[i])
-            && answerNumbers[i] != userInputNumbers[i] ? 1 : 0
+        ball += answerNumbers.contains(userInputNumbers[index])
+            && answerNumbers[index] != userInputNumbers[index] ? 1 : 0
     }
-   
     
     return (strike, ball)
 }
 
-func printGameStatus(strikes:Int, balls:Int, remainingChances: inout Int) {
-    if strikes == 3 {print("사용자 승리!")}
-    else if remainingChances == 0 {print("컴퓨터 승리!")}
-    
-    print("\(strikes) 스트라이크,\(balls) 볼")
-    print("남은 기회 : \(remainingChances)")
-    if strikes == 3 {remainingChances = 0}
-}
-
-func startStep1Game() {
-    var remainingChances = 9
-    let answers = createThreeRandomNumbers()
-    
-    while remainingChances > 0 {
-        let userInputs = createThreeRandomNumbers()
-        let strikeAndBall = compareAnswers(answerNumbers: answers, userInputNumbers: userInputs)
-        print("임의의 수 : \(userInputs[0]) \(userInputs[1]) \(userInputs[2])")
-        remainingChances -= 1
-
-        printGameStatus(strikes: strikeAndBall.strikes,
-                        balls: strikeAndBall.balls,
-                        remainingChances: &remainingChances)
-        
+func printGameStatus(strike: Int, ball: Int, remainingChances: inout Int) {
+    if strike == 3 {
+        print("사용자 승리!")
     }
-    return
+    else if remainingChances == 0 {
+        print("컴퓨터 승리!")
+    }
     
+    print("\(strike) 스트라이크,\(ball) 볼")
+    print("남은 기회 : \(remainingChances)")
+    
+    if strike == 3 {
+        remainingChances = 0
+    }
 }
 
-startStep1Game()
+func printInvalidInput() {
+    print("입력이 잘못되었습니다.")
+}
+
+func printMenu() {
+    print("1. 게임 시작")
+    print("2. 게임 종료")
+    print("원하는 기능을 선택해 주세요 : ", terminator: "")
+}
+
+func printGuidance() {
+    print("숫자 3개를 띄어쓰기로 구분하여 입력해주세요.")
+    print("중복숫자는 허용하지 않습니다.")
+    print("입력 : ", terminator: "")
+}
 
 func receiveMenu() -> String {
-    let input = readLine() ?? "3"
-    return input
+    let input: String? = readLine()
+    guard let validInput = input else{
+        return "0"
+    }
+    return validInput
 }
 
-func printAndRunMenu(){
-    print("1. 게임 시작 \n2. 게임 종료\n원하는 기능을 선택해주세요 :", terminator: "")
-    var input = receiveMenu()
+func printAndRunMenu() {
+    printMenu()
+    var input: String = receiveMenu()
     
     while input != "1" && input != "2" {
-        print("입력이 잘못되었습니다.")
-        print("1. 게임 시작 \n2. 게임 종료\n원하는 기능을 선택해주세요 :", terminator: "")
+        printInvalidInput()
+        printMenu()
         input = receiveMenu()
     }
     
@@ -79,41 +89,45 @@ func printAndRunMenu(){
 }
 
 func hasDuplicate(numbers:[Int]) -> Bool {
-    let toSet = Set(numbers)
-    return toSet.count != numbers.count
+    return Set(numbers).count != numbers.count
+}
+
+func receiveUserGuesses() -> String {
+    let input: String? = readLine()
+    guard let validInput = input else{
+        return " "
+    }
+    return validInput
+}
+
+func receiveValidInputNumbers() -> [Int] {
+    var input: String = receiveUserGuesses()
+    var convertedInput: [Int] = input.split(separator:" ").map {Int($0) ?? 0}
+    while convertedInput.count != 3 || hasDuplicate(numbers: convertedInput) ||
+            convertedInput.filter({$0 > 0 && $0 < 10}).count != 3 {
+        printInvalidInput()
+        printGuidance()
+        input = receiveUserGuesses()
+        convertedInput = input.split(separator:" ").map {Int($0) ?? 0}
+    }
+    return convertedInput
 }
 
 func startStep2Game() {
-    let answerNumbers = createThreeRandomNumbers()
-    var remainingChances = 9
+    let answerNumbers: [Int] = createThreeRandomNumbers()
+    var remainingChances: Int = 9
 
     while remainingChances > 0  {
-        print("숫자 3개를 띄어쓰기로 구분하여 입력해주세요.\n중복숫자는 허용하지 않습니다.\n입력 : ",terminator: "")
-        let inputArray = receiveValidInputNumbers()
+        printGuidance()
+        let inputArray: [Int] = receiveValidInputNumbers()
         remainingChances -= 1
-        let strikeAndBall = compareAnswers(answerNumbers: answerNumbers, userInputNumbers: inputArray)
-        printGameStatus(strikes: strikeAndBall.strikes,
-                        balls: strikeAndBall.balls,
+        let strikeAndBall: (strike: Int, ball: Int) = compareAnswers(answerNumbers: answerNumbers, userInputNumbers: inputArray)
+        printGameStatus(strike: strikeAndBall.strike,
+                        ball: strikeAndBall.ball,
                         remainingChances: &remainingChances)
     }
     
     return
-    
-}
-
-func receiveValidInputNumbers() -> [Int] {
-    
-    var input = readLine() ?? " "
-    var check = input.split(separator:" ").map {Int($0) ?? 0}
-    while check.count != 3 || hasDuplicate(numbers: check) ||
-            check.filter({$0 > 0 && $0 < 10}).count != 3 {
-        print("입력이 잘못되었습니다.")
-        print("숫자 3개를 띄어쓰기로 구분하여 입력해주세요.\n중복숫자는 허용하지 않습니다.\n입력 : ",terminator: "")
-        input = readLine() ?? " "
-        check = input.split(separator:" ").map {Int($0) ?? 0}
-    }
-    return check
 }
 
 printAndRunMenu()
-
