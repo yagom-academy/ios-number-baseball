f5890900947a2e8b0e7b45db4d5cec20ae20ef75
blanche37 <wkddbsghk16@gmail.com>
2021-05-30 16:31:54 +0900

    style: inputUserNumbers()변경

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -7,34 +7,66 @@
 import Foundation
 
 var computerNumbers: Array<UInt> = []
-var numberOfTry: UInt = 9
-var ballCount: UInt = 0
-var strikeCount: UInt = 0
-var randomNumber: UInt = 0
+let numberOfTry: UInt = 9
+var playCount: UInt = 0
+typealias BaseballCount = (strike: UInt, ball: UInt)
+var baseballCount: BaseballCount = BaseballCount(0,0)
 var isUserWin: Bool = false
 var isComputerWin: Bool = false
 
-func makeRandomNumber() {
-    randomNumber = UInt.random(in: 1...9)
+func printMenu() {
+    print("1. 게임 시작")
+    print("2. 게임 종료")
+    print("원하는 기능을 선택해 주세요 : ", terminator: " ")
 }
 
-func makeUserNumbers() {
+func selectMenu() {
+    printMenu()
+    guard let menu = readLine() else {
+        return
+    }
+    switch menu {
+    case "1":
+        print("숫자 3개를 띄어쓰기로 구분하여 입력해주세요")
+        startGame()
+    case "2":
+        return
+    default:
+        print("입력이 잘못되었습니다")
+        selectMenu()
+    }
+}
+
+func generateRandomNumber() -> UInt {
+    let randomNumber = UInt.random(in: 1...9)
+    return randomNumber
+}
+
+func printUserNumbers() {
     print("숫자 3개를 띄어쓰기로 구분하여 입력해주세요")
     print("중복 숫자는 허용하지 않습니다.")
     print("입력: ", terminator:" ")
-    let userNumber = readLine()!
-    let intArr = userNumber.split(separator: " ").map { UInt($0)! }
-    if checkUserNumberRange(userNumbers: intArr) && checkUserNumberCount(userNumbers: intArr) && checkDuplicatedNumber(userNumbers: intArr) {
-        compareNumber(userNumbers: intArr)
-    } else {
-        makeUserNumbers()
+}
+
+func generateComputerNumbers() {
+    computerNumbers = generateRandomNumbers()
+    print(computerNumbers)
+}
+
+func generateRandomNumbers() -> Array<UInt> {
+    var randomNumbers: Array<UInt> = []
+    while randomNumbers.count < 3 {
+        let randomNumber = generateRandomNumber()
+        if randomNumbers.contains(randomNumber) == false {
+            randomNumbers.append(randomNumber)
+        }
     }
+    return randomNumbers
 }
 
 func checkDuplicatedNumber(userNumbers: Array<UInt>) -> Bool {
     let ConvertedNumbers = Set(userNumbers)
-    if(userNumbers.count != ConvertedNumbers.count)
-    {
+    if(userNumbers.count != ConvertedNumbers.count) {
         print("입력이 잘못되었습니다")
         return false
     }
@@ -59,83 +91,74 @@ func checkUserNumberCount(userNumbers: Array<UInt>) -> Bool {
     return true
 }
 
-func makeComputerNumbers() {
-    while computerNumbers.count < 3 {
-        makeRandomNumber()
-        if computerNumbers.contains(randomNumber) == false {
-            computerNumbers.append(randomNumber)
+func inputUserNumbers() {
+    var intArr: Array<UInt> = []
+    
+    guard let userNumber = readLine() else {
+        return inputUserNumbers()
+    }
+   
+    let split = userNumber.components(separatedBy: " ")
+    
+    for i in split {
+        guard UInt(i) != nil else {
+            print("입력이 잘못되었습니다")
+            return inputUserNumbers()
         }
     }
-    print(computerNumbers)
     
+    intArr = userNumber.split(separator: " ").map { UInt($0)! }
+    
+    if checkUserNumberRange(userNumbers: intArr) && checkUserNumberCount(userNumbers: intArr) && checkDuplicatedNumber(userNumbers: intArr) {
+        compareNumber(with: intArr)
+    } else {
+        inputUserNumbers()
+    }
 }
 
-func printResult() {
+func printComparedNumberResult() {
     if isUserWin == true {
         print("유저 승리...!")
     } else if isComputerWin == true {
         print("컴퓨터 승리...!")
     }
-    print("\(strikeCount) 스트라이크, \(ballCount) 볼")
-    print("남은 기회 : \(numberOfTry)")
+    print("\(baseballCount.strike) 스트라이크, \(baseballCount.ball) 볼")
+    print("남은 기회 :\(numberOfTry-playCount)")
 }
 
-func checkGameOver () {
-    if strikeCount == 3{
+func checkGameOver() -> Bool {
+    if baseballCount.strike == 3 {
         isUserWin = true
-        return
-    } else if numberOfTry == 0{
+        return false
+    } else if numberOfTry == playCount {
         isComputerWin = true
-        return
+        return false
+    } else{
+        return true
     }
 }
 
 func resetCount() {
-    ballCount = 0
-    strikeCount = 0
+    baseballCount = (0,0)
 }
 
-func compareNumber(userNumbers: Array<UInt>) {
-    for i in 0...userNumbers.count-1 {
+func compareNumber(with userNumbers: Array<UInt>) {
+    printUserNumbers()
+    for i in 0..<userNumbers.count {
         if userNumbers[i] == computerNumbers[i] {
-            strikeCount += 1
+            baseballCount.strike += 1
         } else if computerNumbers.contains(userNumbers[i]) {
-            ballCount += 1
+            baseballCount.ball += 1
         }
     }
-    numberOfTry -= 1
-}
-
-func printMenu() {
-    print("1. 게임 시작")
-    print("2. 게임 종료")
-    print("원하는 기능을 선택해 주세요 : ", terminator: " ")
-}
-
-
-func selectMenu() {
-    printMenu()
-    guard let menu = readLine() else {
-        return
-    }
-    switch menu {
-    case "1":
-        print("숫자 3개를 띄어쓰기로 구분하여 입력해주세요")
-        startGame()
-    case "2":
-        return
-    default:
-        print("입력이 잘못되었습니다")
-        selectMenu()
-    }
+    playCount += 1
 }
 
 func startGame() {
-    makeComputerNumbers()
-    while isComputerWin == false && isUserWin == false {
-        makeUserNumbers()
-        checkGameOver()
-        printResult()
+    generateComputerNumbers()
+    while checkGameOver() {
+        inputUserNumbers()
+        printComparedNumberResult()
         resetCount()
     }
 }
