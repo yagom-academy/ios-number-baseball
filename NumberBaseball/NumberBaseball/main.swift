2577a969d600f2163774df9aaf4adadc01e54730
shpairo711 <shapiro7@ajou.ac.kr>
2021-05-30 13:38:47 +0900

    refactor: 리뷰 받은 내용 수정

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -7,87 +7,87 @@
 import Foundation
 
 var computerNumbers: Array<UInt> = []
-var numberOfTry: UInt = 9
-var ballCount: UInt = 0
-var strikeCount: UInt = 0
-var randomNumber: UInt = 0
+let numberOfTry: UInt = 9
+var playCount: UInt = 0
+typealias BaseballCount = (strike: UInt, ball: UInt)
+var baseballCount: BaseballCount = BaseballCount(0,0)
 var isUserWin: Bool = false
 var isComputerWin: Bool = false
 
-func makeRandomNumber() {
-    randomNumber = UInt.random(in: 1...9)
+func generateRandomNumber() -> UInt {
+    let randomNumber = UInt.random(in: 1...9)
+    return randomNumber
 }
 
-func printUserNumbers(userNumbers: Array<UInt>) {
-    let convertedNumber = userNumbers.map{"\($0)"}.reduce("") {
+func generateUserNumbers(userNumbers: Array<UInt>) {
+    let convertedNumber = userNumbers.map{"\($0)"}.reduce(""){
         return $0 + " " + $1
     }
-    print("임의의 수 : \(convertedNumber)")
+    print("임의의 수 :\(convertedNumber)")
 }
 
-func makeComputerNumbers() {
-    while computerNumbers.count < 3 {
-        makeRandomNumber()
-        if computerNumbers.contains(randomNumber) == false {
-            computerNumbers.append(randomNumber)
+func generateComputerNumbers() {
+    computerNumbers = generateRandomNumbers()
+}
+
+func generateRandomNumbers() -> Array<UInt> {
+    var randomNumbers: Array<UInt> = []
+    while randomNumbers.count < 3 {
+        let randomNumber = generateRandomNumber()
+        if randomNumbers.contains(randomNumber) == false {
+            randomNumbers.append(randomNumber)
         }
     }
+    return randomNumbers
 }
 
-func makeUserNumbers() -> Array<UInt> {
-    var userNumbers: Array<UInt> = []
-    while userNumbers.count < 3 {
-        makeRandomNumber()
-        if userNumbers.contains(randomNumber) == false {
-            userNumbers.append(randomNumber)
-        }
-    }
-    return userNumbers
+func generateUserNumbers() -> Array<UInt> {
+    generateRandomNumbers()
 }
 
-func printResult() {
+func printComparedNumberResult() {
     if isUserWin == true {
         print("유저 승리...!")
     } else if isComputerWin == true {
         print("컴퓨터 승리...!")
     }
-    print("\(strikeCount) 스트라이크, \(ballCount) 볼")
-    print("남은 기회 : \(numberOfTry)")
+    print("\(baseballCount.strike) 스트라이크, \(baseballCount.ball) 볼")
+    print("남은 기회 : \(numberOfTry-playCount)")
 }
 
-func checkGameOver () {
-    if strikeCount == 3{
+func checkGameOver() -> Bool {
+    if baseballCount.strike == 3 {
         isUserWin = true
-        return
-    } else if numberOfTry == 0{
+        return false
+    } else if numberOfTry == playCount {
         isComputerWin = true
-        return
+        return false
+    } else{
+        return true
     }
 }
 
 func resetCount() {
-    ballCount = 0
-    strikeCount = 0
+    baseballCount = (0,0)
 }
 
-func compareNumber(userNumbers: Array<UInt>) {
-    printUserNumbers(userNumbers: userNumbers)
-    for i in 0...userNumbers.count-1 {
+func compareNumber(with userNumbers: Array<UInt>) {
+    generateUserNumbers(userNumbers: userNumbers)
+    for i in 1..<userNumbers.count{
         if userNumbers[i] == computerNumbers[i] {
-            strikeCount += 1
+            baseballCount.strike += 1
         } else if computerNumbers.contains(userNumbers[i]) {
-            ballCount += 1
+            baseballCount.ball += 1
         }
     }
-    numberOfTry -= 1
+    playCount += 1
 }
 
 func startGame() {
-    makeComputerNumbers()
-    while isComputerWin == false && isUserWin == false {
-        compareNumber(userNumbers: makeUserNumbers())
-        checkGameOver()
-        printResult()
+    generateComputerNumbers()
+    while checkGameOver() {
+        compareNumber(with: generateUserNumbers())
+        printComparedNumberResult()
         resetCount()
     }
 }
