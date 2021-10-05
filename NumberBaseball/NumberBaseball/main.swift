166be6b3352456ab56c7a5c55f60a63fbb19f58f
zoe <kny0651@naver.com>
2021-10-05 22:58:05 +0900

    feat: 전역변수와 3개의 랜덤숫자를 만드는 함수 생성

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -6,5 +6,27 @@
 
 import Foundation
 
-print("Hello, World!")
+// 변수 - 남은 시도횟수, (볼), 스트라이크 횟수, 임의의 정수 3개를 담을 변수
+
+var remainingAttempts: Int = 9
+
+var ballCount: Int
+
+var strikeCount: Int
+
+var winningNumbers: Set<Int>
+
+var randomNumbers: Set<Int>
+
+
+// 임의의 3개 정수 만들고 반환
+
+func makeRandomNumbers() {
+    var numbers: Set<Int> = []
+    
+    while numbers.count < 3 {
+        let number: Int = Int.random(in: 1...9)
+        numbers.insert(number)
+    }
+}
 
