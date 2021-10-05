73429ba73ab83b0e5f7752e0a9dfd218ebd94768
ICS-Asan <carpediem1015@gmail.com>
2021-10-05 21:18:20 +0900

    feat: 스트라이크 횟수 판별하는 함수 추가
    함수의 분리를 위해 두 배열의 같은 자리수를 비교하고 스트라이크 횟수를 변경하는 함수, 이 과정을 3번 반복하는 함수를 분리하여 작성

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -6,5 +6,21 @@
 
 import Foundation
 
-print("Hello, World!")
+var strikeCount = 0
+
+var array1 = [3, 2, 1]
+var array2 = [3, 2, 1]
+
+func strikeCounter(a: Int) {
+    if array1[a] == array2[a] {
+        strikeCount += 1
+    }
+}
+
+func readStrike() {
+    for time in 0...2 {
+        strikeCounter(a: time)
+    }
+    print("\(strikeCount) 스트라이크")
+}
 
