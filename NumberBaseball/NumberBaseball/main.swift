3a0c487a45750691016619332bfbf2e8d3f976c1
forestjae <forestsj30@gmail.com>
2021-10-05 23:31:19 +0900

    feat: 두 숫자세트 비교 후 중복되는 숫자의 개수를 반환하는 함수 추가

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -19,14 +19,30 @@ var winningNumbers: Set<Int>
 var randomNumbers: Set<Int>
 
 
+
+func isSame(first: Set<Int>, second: Set<Int>) -> Int {
+    let winningSet = first.intersection(second).count
+    switch winningSet {
+    case 0:
+        print("0 스트라이크, 0 볼")
+    case 1:
+        print("1 스트라이크, 0볼 혹은 0 스트라이크, 1 볼")
+    case 2:
+        print("2 스트라이크, 0볼 혹은 1 스트라이크, 1 볼 혹은 0 스트라이크 2 볼")
+    case 3:
+    default:
+    }
+    
+}
+
 // 임의의 3개 정수 만들고 반환
 
 func makeRandomNumbers() {
-    var numbers: Set<Int> = []
+    var numbers: [Int] = []
     
     while numbers.count < 3 {
         let number: Int = Int.random(in: 1...9)
-        numbers.insert(number)
+        numbers.append(number)
     }
 }
 
