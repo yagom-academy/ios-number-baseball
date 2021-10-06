d2f08306ca9de8c8832ace0bc5c323e52890d096
hoBahk <qudgh849@gmail.com>
2021-10-06 20:05:07 +0900

    fix: startGame 함수에서 승패 결정 로직 변경, refactor: 변수명 수정
    playOneRound() 함수의 상수 string을 RandomNumberToString
    returnResult함수에서 변수 result를 사용하지 않고 return 값에 String으로 직접 전달

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -11,56 +11,54 @@ var tryCount: Int = 9
 var strikeCount: Int = 0
 var ballCount: Int = 0
 
-func generatedRandomNumbers() -> Array<Int>{
+func generatedRandomNumbers() -> Array<Int> {
     var numbers: Set<Int> = Set<Int>()
-    while numbers.count < 3{
+    
+    while numbers.count < 3 {
         numbers.insert(Int.random(in: 1...9))
     }
     return Array(numbers)
 }
 
-func returnResult(of numbers: [Int]) -> String{
-    var result: String = " "
+func returnResult(of numbers: [Int]) -> String {
     
-    for index in 0...2{
-          judgeStrikeOrBall(of: numbers[index], at: index)
-      }
-    
-    result = "\(strikeCount) 스트라이크, \(ballCount) 볼"
-    
-    return result
+    for index in 0...2 {
+        judgeStrikeOrBall(of: numbers[index], at: index)
+    }
+
+    return "\(strikeCount) 스트라이크, \(ballCount) 볼"
 }
 
-
-func judgeStrikeOrBall(of number: Int, at index: Int){
-    if number == computerNumbers[index]{
+func judgeStrikeOrBall(of number: Int, at index: Int) {
+    if number == computerNumbers[index] {
         strikeCount += 1
     }else if computerNumbers.contains(number) {
         ballCount += 1
     }
 }
 
-
-func playOneRound(){
+func playOneRound() {
     let randomNumbers = generatedRandomNumbers()
-    let string: String = randomNumbers.reduce(""){String($0) + " " + String($1)}
+    let randomNumberToRandomNumber: String = randomNumbers.reduce(""){String($0) + " " + String($1)}
+    
     tryCount -= 1
+    strikeCount = 0
+    ballCount = 0
+    
     let message = """
-    임의의 수 : \(string)
+    임의의 수 : \(randomNumberToRandomNumber)
     \(returnResult(of: randomNumbers))
     남은 기회 : \(tryCount)
     """
     print(message)
 }
 
-func startGame(){
+func startGame() {
     while tryCount > 0, strikeCount != 3 {
         playOneRound()
-        strikeCount = 0
-        ballCount = 0
     }
     
-    if tryCount < 0 {
+    if tryCount == 0, strikeCount != 3 {
         print("컴퓨터 승리!")
     }else {
         print("사용자 승리!")
@@ -69,13 +67,4 @@ func startGame(){
 
 startGame()
 
-/*
-func input(){
-    print("입력 : ", terminator: " ")
-    guard let input = readLine() else{
-        return
-    }
-    
-    let splitedInput = input.filter{$0 != " "}
-}
-*/
+
