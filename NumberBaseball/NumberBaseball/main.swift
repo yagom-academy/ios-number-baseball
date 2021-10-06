7aaad394a9c4ed8ee8b97d56ce5418b23c075657
youngwooJang <duddn5906@naver.com>
2021-10-06 11:45:47 +0900

    refactor: 코딩 컨벤션 개선

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -4,73 +4,55 @@
 //  Copyright © yagom academy. All rights reserved.
 //
 
-
-/* Step 1
- 임의의 정수를 담아둘 3개의 변수 생성
- 남은 시도횟수를 담아둘 변수 생성 (초기값 9)
- func generateRandomNumber() -> Set[Int] {임의의 수 생성}
- func giveGameResult() {정수 비교, 스트라이크, 볼 결과 반환}
- func startGame() {}
- */
-
-/* Step 2
- func selectMenu() {1.게임시작 2.게임종료}
- func inputNumber() {}
- 비교 기능 구현
- 중복성 검증, 1~9까지 수만 유효
- */
 import Foundation
 
-
-let keyNumbers = generateRandomThreeNumbers()
-
 func runNumberGame() {
-    var remainNumber = 9
-    var trialNumbers: [Int]
+    let numLife = 9
+    let keyNumbers = generateThreeRandomNumbers()
+    var userTryNumbers: [Int]
     
-    while true {
-        remainNumber -= 1
-        trialNumbers = generateRandomThreeNumbers()
-        let (numStrike, numBall) = giveGameResult(trialNumbers: trialNumbers)
-        print("입력된 수: \(keyNumbers[0]) \(keyNumbers[1]) \(keyNumbers[2])")
-        print("임의의 수: \(trialNumbers[0]) \(trialNumbers[1]) \(trialNumbers[2])")
+    for numAttempt in 1...numLife {
+        userTryNumbers = generateThreeRandomNumbers()
+        
+        let (numStrike, numBall) = computeStrikeAndBall(keyNumbers: keyNumbers, userTryNumbers: userTryNumbers)
+        
+        print("임의의 수: \(keyNumbers[0]) \(keyNumbers[1]) \(keyNumbers[2])")
+        print("임의의 수: \(userTryNumbers[0]) \(userTryNumbers[1]) \(userTryNumbers[2])")
         print("\(numStrike) 스트라이크, \(numBall) 볼")
+        
         if numStrike == 3 {
             print("사용자 승리...!")
-            break
+            return
         }
-        print("남은 기회: \(remainNumber)")
-        if remainNumber <= 0 {
-            print("컴퓨터 승리...!")
-            break
-        }
+        print("남은 기회: \(numLife-numAttempt)")
     }
+    print("컴퓨터 승리...!")
 }
 
-func generateRandomThreeNumbers() -> [Int] {
-    var randomNumbers = [Int]()
+func generateThreeRandomNumbers() -> [Int] {
+    var randomNumbers: [Int]
     
-    while randomNumbers.count < 3 {
-        let randomNumber = Int.random(in: 1...9)
-        if randomNumbers.contains(randomNumber) == false {
-            randomNumbers.append(randomNumber)
+    while true {
+        randomNumbers = [Int.random(in: 1...9), Int.random(in: 1...9), Int.random(in: 1...9)]
+        
+        let deduplicatedNumbers = Set(randomNumbers)
+        
+        if randomNumbers.count == deduplicatedNumbers.count {
+            return randomNumbers
         }
     }
-    return randomNumbers
 }
 
-func giveGameResult(trialNumbers: [Int]) -> (Int, Int) {
+func computeStrikeAndBall(keyNumbers: [Int], userTryNumbers: [Int]) -> (Int, Int) {
     var numStrike = 0
     var numBall = 0
     
-    for idx in 0..<trialNumbers.count {
-        if trialNumbers[idx] == keyNumbers[idx] {
+    for idx in 0..<userTryNumbers.count {
+        if userTryNumbers[idx] == keyNumbers[idx] {
             numStrike += 1
-        } else if keyNumbers.contains(trialNumbers[idx]) {
+        } else if keyNumbers.contains(userTryNumbers[idx]) {
             numBall += 1
         }
     }
     return (numStrike, numBall)
 }
-
-runNumberGame()
