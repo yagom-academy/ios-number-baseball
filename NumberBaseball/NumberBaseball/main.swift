248f1d3fd2887981123bb8d54a62146eca657010
yeha <dev.yeha@gmail.com>
2021-10-06 15:15:51 +0900

    Feat : checkBallCount 함수 추가

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -12,7 +12,25 @@ func createRandomNumbers() -> [Int] {
     return threeNumbers
 }
 
+func checkBallCount(answerNumbers: [Int], userNumbers: [Int]) -> [Int] {
+    guard answerNumbers != userNumbers else {
+        return [3, 0]
+    }
+    var strikeCount: Int = 0
+    var ballCount: Int = 0
+
+    for index in 0...2 {
+        let matchIndex = answerNumbers.firstIndex(of: userNumbers[index])
+        if matchIndex == nil {
+            continue
+        } else if matchIndex == index {
+            strikeCount += 1
+        } else {
+            ballCount += 1
+        }
+    }
+    return [strikeCount, ballCount]
+}
+
 var randomAnswerNumbers = createRandomNumbers()
 var gameCount = 9
-
-
