acc050ba200c542b615c7bc3663c9cdef6cb0469
vayne77 <sky891@naver.com>
2021-10-06 13:22:29 +0900

    feat: makeDeduplicatedRandomNumbers 함수 추가

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -6,3 +6,13 @@ var trialCount: Int = 9
 var strike: Int = 0
 var ball: Int = 0
 var isGameOver: Bool = false
+
+func makeDeduplicatedRandomNumbers() -> [Int] {
+    var deduplicatedNumbers: Set<Int> = []
+    
+    while deduplicatedNumbers.count < 3 {
+        let randomNumber = Int.random(in: 1...9)
+        deduplicatedNumbers.insert(randomNumber)
+    }
+    return Array(deduplicatedNumbers)
+}
