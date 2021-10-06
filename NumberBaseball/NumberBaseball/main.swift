66c9c2631b09494d71728884cc8f452cb34a905b
junbangg <wnstjr4620@naver.com>
2021-10-06 16:59:07 +0900

    feat: 난수를 생성하는 함수 구현

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -6,5 +6,21 @@
 
 import Foundation
 
-print("Hello, World!")
+//MARK: - Step 1
+/// 1. 변수 설정
+/// 2. 난수 3개 생성하는 함수 구현
+///   - 중복된 숫자가 없어야한다
+///   - 1~9 사이의 숫자여야 한다
+
+func generateRandomNumbers() -> Set<Int> {
+    var randomNumbers: Set<Int> = []
+    
+    while randomNumbers.count < 3 {
+        randomNumbers.insert(Int.random(in: 1..<10))
+    }
+    
+    return randomNumbers
+}
+
+
 
