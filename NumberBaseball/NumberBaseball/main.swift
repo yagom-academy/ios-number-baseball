1f34faaaadda086c78f1be24e95eb71a3dad9799
ICS-Asan <carpediem1015@gmail.com>
2021-10-07 22:15:37 +0900

    feature: separateInput 함수 추가

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -138,3 +138,8 @@ func inputPlayerNums() {
         return print(defaultErrorMessage)
     }
 }
+
+func separateInput(input: String) -> [String] {
+    return input.components(separatedBy: " ")
+}
+
