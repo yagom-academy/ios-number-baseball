47d8a10622abb1bb4230af6250787c93a77ee17d
zoe <kny0651@naver.com>
2021-10-08 01:02:04 +0900

    refactor: 기능이 중복되는 함수 통합, 승패 판정함수 추가
    프로젝트가 확장되는것에 대비하여 매직넘버를 없애고 전역상수로 선언했습니다.

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -7,70 +7,68 @@
 import Foundation
 
 var remainingAttempts: Int = 9
-var targetNumbers: [Int] = generateRandomNumbers()
+var targetNumbers: [Int] = []
+let numbersToGenerate = 3
 
 func generateRandomNumbers() -> [Int] {
     var radomNumbers: Set<Int> = []
     
-    while radomNumbers.count < 3 {
+    while radomNumbers.count < numbersToGenerate {
         let number: Int = Int.random(in: 1...9)
         radomNumbers.insert(number)
     }
     return Array(radomNumbers)
 }
 
-func countMatchingNumbers(target: [Int], player: [Int]) -> Int {
-    let target = Set<Int>(target)
-    let player = Set<Int>(player)
-    let count = target.intersection(player).count
-    return count
-}
-
-func calculateStrikeCount(target: [Int], player: [Int]) -> Int {
-    var count = 0
-    for index in 0...2 {
-        count += target[index] == player[index] ? 1 : 0
-    }
-    return count
-}
-
-func calculateStrikeAndBall(targetNumbers: [Int], playerNumbers: [Int]) -> (Int, Int) {
-    let total: Int = countMatchingNumbers(target: targetNumbers, player: playerNumbers)
-    let strikes: Int = calculateStrikeCount(target: targetNumbers, player: playerNumbers)
-    let balls = total - strikes
+func calculateStrikeAndBall(target: [Int], player: [Int]) -> (Int, Int) {
+    var strikeCount = 0
+    var ballCount = 0
     
-    return (strikes, balls)
+    for index in 0..<numbersToGenerate {
+        if target[index] == player[index] {
+            strikeCount += 1
+        } else if target.contains(player[index]){
+            ballCount += 1
+        }
+    }
+    return (strikeCount, ballCount)
 }
 
 func playInning() -> Int {
-    let playerNumbers = generateRandomNumbers()
-    print("임의의 수 : \(playerNumbers[0]) \(playerNumbers[1]) \(playerNumbers[2])")
-    
-    let (strikeCount, ballCount) = calculateStrikeAndBall(targetNumbers: targetNumbers, playerNumbers: playerNumbers)
+    let playerNumbers: [Int] = generateRandomNumbers()
+    let numbersToPrint: String = playerNumbers.reduce("") {
+        $0 + String($1) + " "
+    }
+    print("임의의 수 : \(numbersToPrint)" )
+
+    let (strikeCount, ballCount) = calculateStrikeAndBall(target: targetNumbers, player: playerNumbers)
     print("\(strikeCount) 스트라이크, \(ballCount) 볼")
-    
     return strikeCount
 }
 
 func printRemainingAttempts() {
     remainingAttempts -= 1
     print("남은 기회: \(remainingAttempts)")
-    
-    guard remainingAttempts != 0 else {
-        print("컴퓨터 승리...!")
-        return
-    }
 }
 
 func startGame() {
+    targetNumbers = generateRandomNumbers()
     while remainingAttempts > 0 {
         let strikeCount = playInning()
         
-        if strikeCount == 3 {
-            print("사용자 승리!")
-            return
+        if strikeCount == numbersToGenerate {
+            break
         }
         printRemainingAttempts()
+    }
+    checkGameResult()
+}
+
+func checkGameResult() {
+    if remainingAttempts == 0 {
+        print("컴퓨터 승리...!")
+    } else {
+        print("사용자 승리!")
     }
 }
 
