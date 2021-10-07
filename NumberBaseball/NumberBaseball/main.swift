c07b7e0078f280ca608ba4d0868b1ea699242a5b
namu <simjh27@gmail.com>
2021-10-07 18:56:41 +0900

    refactor: String 상수들을 열거형에 정리
    * GameMessage 열거형으로 기존 String 변수들을 이동
    * 일관되지 않은 함수 내부 개행 수정

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -18,49 +18,55 @@ func generateRandomNumbers() -> [Int] {
         let number = Int.random(in: randomNumberRange)
         randomNumbers.insert(number)
     }
+    
     return Array(randomNumbers)
 }
 
 func judgeStrikeAndBall(with userNumbers: [Int]) -> (strike: Int, ball: Int) {
     var ballCount = 0
     var strikeCount = 0
+    
     for index in 0..<userNumbers.count {
         ballCount += computerNumbers.contains(userNumbers[index]) ? 1 : 0
     }
     for index in 0..<userNumbers.count {
         strikeCount += computerNumbers[index] == userNumbers[index] ? 1 : 0
     }
+    
     ballCount -= strikeCount
+    
     return (strikeCount, ballCount)
 }
 
+enum GameMessage: String {
+    case userWin = "사용자 승리!"
+    case computerWin = "컴퓨터 승리...!"
+    case generatedUserNumber = "임의의 수 : "
+    case leftTrial = "남은 기회 : "
+    case strikeCount = " 스트라이크, "
+    case ballCount = " 볼"
+}
+
 func startGame() {
-    computerNumbers = generateRandomNumbers()
     let strikesForUserWin = 3
-    let userWinMessage = "사용자 승리!"
-    let computerWinMessage = "컴퓨터 승리...!"
-    let generatedUserNumberMessage = "임의의 수 : "
-    let leftTrialMessage = "남은 기회 : "
-    let strikeCountMessage = " 스트라이크, "
-    let ballCountMessage = " 볼"
-    
     var strikeCount = 0
     var ballCount = 0
-        
+
+    computerNumbers = generateRandomNumbers()
+    
     while tryCount != 0 && strikeCount != strikesForUserWin {
         tryCount -= 1
         let userNumbers = generateRandomNumbers()
         (strikeCount, ballCount) = judgeStrikeAndBall(with: userNumbers)
-        print(generatedUserNumberMessage, userNumbers.map{String($0)}.joined(separator: " "))
-        print(strikeCount, strikeCountMessage, ballCount, ballCountMessage)
-        print(leftTrialMessage, tryCount)
+        print(GameMessage.userWin.rawValue, userNumbers.map{String($0)}.joined(separator: " "))
+        print(strikeCount, GameMessage.strikeCount, ballCount, GameMessage.ballCount)
+        print(GameMessage.leftTrial, tryCount)
     }
     
     if strikeCount == strikesForUserWin {
-        print(userWinMessage)
+        print(GameMessage.userWin)
     } else {
-        print(computerWinMessage)
+        print(GameMessage.computerWin)
     }
 }
 startGame()
-
