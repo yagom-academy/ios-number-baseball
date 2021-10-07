9c06743edbe2e6d862d4b851c413ce5abfc65eac
horeng2 <huski.a.a@gmail.com>
2021-10-07 15:21:04 +0900

    Edit : startGame 함수 내 if문을 printVictoryMessage 함수로 분리 구현

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -40,6 +40,14 @@ func checkBallCount(answerNumbers: [Int], userNumbers: [Int]) -> [Int] {
 var randomAnswerNumbers = createRandomNumbers()
 var gameCount = 9
 
+func printVictoryMessage(gameChance gameCount: Int, strikeScore strikeCount: Int) {
+    if gameCount == 0 && strikeCount < 3 {
+        print("컴퓨터 승리…!")
+    } else if strikeCount == 3 {
+        print("사용자 승리!")
+    }
+}
+
 func startGame() {
     while gameCount > 0 {
         gameCount -= 1
@@ -51,11 +59,8 @@ func startGame() {
         print("임의의 수 : \(userNumbers[0]) \(userNumbers[1]) \(userNumbers[2])")
         print("\(strikeCount) 스트라이크, \(ballCount) 볼")
         print("남은 기회 : \(gameCount)")
-        if gameCount == 0 && strikeCount < 3 {
-            print("컴퓨터 승리…!")
-        } else if strikeCount == 3 {
-            print("사용자 승리!")
-        }
+        
+        printVictoryMessage(gameChance: gameCount, strikeScore: strikeCount)
     }
 }
 
