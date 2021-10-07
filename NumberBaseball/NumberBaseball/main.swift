fbf19274fe9ced78eb4573dbe8b67f807aa895d4
smart8612 <smart8612@gmail.com>
2021-10-07 23:46:54 +0900

    refactor: 함수 기능 세분화 및 게임 로직 수정

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -9,11 +9,26 @@ import Foundation
 var targetNumbers: [Int] = []
 var randomNumbers: [Int] = []
 var tryCount = 9
+var isUserWin = false
 
-func convertIntArrayToString(_ array: [Int]) -> String {
-    let resultString: String = array.map { String($0) }.joined(separator: " ")
+func playNumberBaseballGame() {
+    while tryCount > 0 && isUserWin == false {
+        let strikeCount = playRound()
+        
+        isUserWin = isThreeStrike(count: strikeCount)
+        printTryCount()
+    }
+    printWinner()
+}
+
+func playRound() -> Int {
+    let randomNumbers = createRandomNumbers()
+    printRandomNumbers(numbers: randomNumbers)
     
-    return resultString
+    let score = compare(randomNumbers, with: targetNumbers)
+    printStrikeAndBall(score: score)
+    
+    return score.strike
 }
 
 func createRandomNumbers(range: ClosedRange<Int> = 1...9, count: Int = 3) -> [Int] {
@@ -28,22 +43,15 @@ func createRandomNumbers(range: ClosedRange<Int> = 1...9, count: Int = 3) -> [In
     return resultNumbers.shuffled()
 }
 
-targetNumbers = createRandomNumbers()
-
-func playRound() -> Int {
+func printRandomNumbers(numbers: [Int]) {
     print("임의의 수 : ", terminator: "")
-    let randomNumbers = createRandomNumbers()
-    
-    print(convertIntArrayToString(randomNumbers))
-    
-    let score = compare(randomNumbers, with: targetNumbers)
-    printStrikeAndBall(score: score)
-    
-    return score.strike
+    print(convertIntArrayToString(numbers))
 }
 
-func printStrikeAndBall(score: (strike: Int, ball: Int)) {
-    print("\(score.strike) 스트라이크, \(score.ball) 볼")
+func convertIntArrayToString(_ array: [Int]) -> String {
+    let resultString: String = array.map { String($0) }.joined(separator: " ")
+    
+    return resultString
 }
 
 func compare(_ randomNumbers: [Int], with targetNumbers: [Int]) -> (strike: Int, ball: Int) {
@@ -60,23 +68,28 @@ func compare(_ randomNumbers: [Int], with targetNumbers: [Int]) -> (strike: Int,
     return score
 }
 
-func playGame(tryCount: Int) {
-    for currentTryCount in 1...tryCount {
-        let gameScore = playRound()
-        
-        printGameResult(score: gameScore, currentTryCount: currentTryCount, tryCount: tryCount)
-    }
+func printStrikeAndBall(score: (strike: Int, ball: Int)) {
+    print("\(score.strike) 스트라이크, \(score.ball) 볼")
 }
 
-func printGameResult(score: Int, currentTryCount: Int, tryCount: Int) {
-    let isWin = (score == 3)
-    let remainCount = tryCount - currentTryCount
-    
-    if isWin {
+func printWinner() {
+    if isUserWin {
         print("사용자 승리!")
-        return
-    } else if currentTryCount == tryCount {
-        print("남은 기회 : \(remainCount)")
+    } else {
         print("컴퓨터 승리...!")
     }
 }
+
+func printTryCount() {
+    if isUserWin == false {
+        tryCount = tryCount - 1
+        print("남은 기회 : \(tryCount)")
+    }
+}
+
+func isThreeStrike(count: Int) -> Bool {
+    return count == 3
+}
+
+targetNumbers = createRandomNumbers()
+playNumberBaseballGame()
