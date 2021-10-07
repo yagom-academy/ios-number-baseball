30294e668f3cf6f71899a5824280238fa00b4ed3
ICS-Asan <carpediem1015@gmail.com>
2021-10-08 00:31:31 +0900

    refactor: 플레이어 숫자 입력에 관련된 함수들 재배치, presentPlayerNums 함수의 지역변수명 수정

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -9,16 +9,16 @@ import Foundation
 let digitsOfGame = 3
 
 var randomTargetNums: [Int] = generateUniqueRandomNums(from: 1, to: 9)
-var randomPlayerNums: [Int] = [Int]()
+var playerNums: [Int] = [Int]()
 
 var remainedRounds = 9
 var strikeCounts = 0
 var ballCounts = 0
 
 func judgeStrikeAndBall(at: Int) {
-    if randomTargetNums[at] == randomPlayerNums[at] {
+    if randomTargetNums[at] == playerNums[at] {
         strikeCounts += 1
-    } else if Set(randomTargetNums).contains(randomPlayerNums[at]) {
+    } else if Set(randomTargetNums).contains(playerNums[at]) {
         ballCounts += 1
     }
 }
@@ -38,89 +38,6 @@ func generateUniqueRandomNums(from start: Int, to end: Int) -> [Int] {
     }
     
     return Array(uniqueRandomNums)
-}
-
-func presentPlayerNums() {
-    randomPlayerNums = generateUniqueRandomNums(from: 1, to: 9)
-    
-    var playerNums: [String] = []
-    for order in 0..<digitsOfGame {
-        playerNums.append(String(randomPlayerNums[order]))
-    }
-    
-    print("임의의 수 : \(playerNums.joined(separator: " "))")
-}
-
-func decreaseRemainedRounds() {
-    remainedRounds -= 1
-}
-
-func presentRoundResult() {
-    print("\(strikeCounts) 스트라이크 \(ballCounts) 볼")
-    print("남은 기회 : \(remainedRounds)")
-}
-
-func gameResult() {
-    if strikeCounts == digitsOfGame {
-        print("플레이어 승리...!")
-    } else if remainedRounds == 0 {
-        print("컴퓨터 승리...!")
-    }
-}
-
-func resetStrikeAndBallCounts() {
-    strikeCounts = 0
-    ballCounts = 0
-}
-
-func playBaseballGame() {
-    repeat {
-        resetStrikeAndBallCounts()
-        presentPlayerNums()
-        countStrikeAndBall()
-        decreaseRemainedRounds()
-        presentRoundResult()
-    } while remainedRounds > 0 && strikeCounts < digitsOfGame
-    gameResult()
-}
-
-//MARK: -메뉴구현
-let defaultErrorMessage = "입력이 잘못되었습니다"
-
-func presentMenu() {
-    print("""
-    1. 게임시작
-    2. 게임종료
-    """)
-}
-
-func selectMenu() -> String {
-    print("원하는 기능을 선택해주세요 : ", terminator: "")
-    let selectedMenu = readLine() ?? ""
-    return selectedMenu
-}
-
-func operateMenu(menu: String) {
-    switch menu {
-    case "1":
-        playBaseballGame()
-    case "2":
-        break
-    default:
-        print(defaultErrorMessage)
-        break
-    }
-}
-
-func runBaseballGame() {
-    var selectedMenu: String
-    
-    repeat {
-        presentMenu()
-        selectedMenu = selectMenu()
-        operateMenu(menu: selectedMenu)
-    } while selectedMenu != "2"
-    
 }
 
 //MARK: -플레이어 숫자 입력
@@ -180,7 +97,7 @@ func isValidInput(input: [String]) -> Bool {
 
 func returnResultOfGeneration(isSuccess: Bool, input: [String]) {
     if isSuccess {
-        randomPlayerNums = input.compactMap { Int($0) }
+        playerNums = input.compactMap { Int($0) }
     } else {
         print(defaultErrorMessage)
     }
@@ -194,5 +111,90 @@ func generatePlayerNums() {
         let separatedPlayerNums = separateInput(input: inputtedPlayerNums)
         isValid = isValidInput(input: separatedPlayerNums)
         returnResultOfGeneration(isSuccess: isValid, input: separatedPlayerNums)
-    } while randomPlayerNums.isEmpty
+    } while !isValid
 }
+
+func presentPlayerNums() {
+    var convertedPlayerNums: [String] = []
+    for order in 0..<digitsOfGame {
+        convertedPlayerNums.append(String(playerNums[order]))
+    }
+    
+    print("임의의 수 : \(convertedPlayerNums.joined(separator: " "))")
+}
+
+func decreaseRemainedRounds() {
+    remainedRounds -= 1
+}
+
+func presentRoundResult() {
+    print("\(strikeCounts) 스트라이크 \(ballCounts) 볼")
+    print("남은 기회 : \(remainedRounds)")
+}
+
+func gameResult() {
+    if strikeCounts == digitsOfGame {
+        print("플레이어 승리...!")
+    } else if remainedRounds == 0 {
+        print("컴퓨터 승리...!")
+    }
+}
+
+func resetStrikeAndBallCounts() {
+    strikeCounts = 0
+    ballCounts = 0
+}
+
+func playBaseballGame() {
+    remainedRounds = 9
+    repeat {
+        resetStrikeAndBallCounts()
+        generatePlayerNums()
+        presentPlayerNums()
+        countStrikeAndBall()
+        decreaseRemainedRounds()
+        presentRoundResult()
+    } while remainedRounds > 0 && strikeCounts < digitsOfGame
+    gameResult()
+}
+
+//MARK: -메뉴구현
+let defaultErrorMessage = "입력이 잘못되었습니다"
+
+func presentMenu() {
+    print("""
+    1. 게임시작
+    2. 게임종료
+    """)
+}
+
+func selectMenu() -> String {
+    print("원하는 기능을 선택해주세요 : ", terminator: "")
+    let selectedMenu = readLine() ?? ""
+    return selectedMenu
+}
+
+func operateMenu(menu: String) {
+    switch menu {
+    case "1":
+        playBaseballGame()
+    case "2":
+        break
+    default:
+        print(defaultErrorMessage)
+        break
+    }
+}
+
+func runBaseballGame() {
+    var selectedMenu: String
+    
+    repeat {
+        presentMenu()
+        selectedMenu = selectMenu()
+        operateMenu(menu: selectedMenu)
+    } while selectedMenu != "2"
+    
+}
+
+runBaseballGame()
