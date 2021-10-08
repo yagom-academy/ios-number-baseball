22334bd6b8fff13e340a70b4fe0c4b20c43d9cd7
Quokkaaa <rktrkdml@gmail.com>
2021-10-08 16:13:03 +0900

    refactor: 변수명과 함수명 수정

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -17,15 +17,15 @@ import Foundation
 
 private func generateRandomNumbers() -> Set<Int> {
     var randomNumbers: Set<Int> = []
-    
+
     while randomNumbers.count < 3 {
         randomNumbers.insert(Int.random(in: 1..<10))
     }
-    
+
     return randomNumbers
 }
 
-private func calculateStrikeBall(from computerNumbers: Set<Int>, and playerNumbers: Set<Int>) -> [Int] {
+private func calculateStrikeBall(from computerNumbers: Set<Int>, and playerNumbers: Set<Int>) -> (Int, Int) {
     var ballCount = computerNumbers.intersection(playerNumbers).count
     var strikeCount = 0
     let computerNumbers = Array(computerNumbers)
@@ -38,42 +38,51 @@ private func calculateStrikeBall(from computerNumbers: Set<Int>, and playerNumbe
         }
     }
     
-    return [strikeCount, ballCount]
+    return (strikeCount, ballCount)
 }
 
 private func checkWin(by strikeCount: Int) -> Bool {
     return strikeCount == 3
 }
 
-private func createStringForConsole(with numbers: Set<Int>) -> String {
+private func createPlayerNumbersString(with numbers: Set<Int>) -> String {
     let numbers = Array(numbers)
-    var string = "임의의 수 :"
-    
+    var playerNumbersString = "임의의 수 :"
+
     for number in numbers {
-        string += " \(number)"
+        playerNumbersString += " \(number)"
     }
-    
-    return string
+
+    return playerNumbersString
 }
 
 private func startNumberBaseballGame() {
     var chance = 9
     let computerNumbers = generateRandomNumbers()
-    
+    var playerHasWon = false
+
     while chance > 0 {
         let playerNumbers = generateRandomNumbers()
-        print(createStringForConsole(with: playerNumbers))
-        let strikeBall = calculateStrikeBall(from: computerNumbers, and: playerNumbers)
-        print("\(strikeBall[0]) 스트라이크, \(strikeBall[1]) 볼")
-        
-        if checkWin(by: strikeBall[0]){
-            print("사용자 승리...!")
-            return
+        print(createPlayerNumbersString(with: playerNumbers))
+        let (strikeCount, ballCount) = calculateStrikeBall(from: computerNumbers, and: playerNumbers)
+        print("\(strikeCount) 스트라이크, \(ballCount) 볼")
+
+        if checkWin(by: strikeCount) {
+            playerHasWon = true
+            break
         } else {
             chance -= 1
             print("남은 기회 : \(chance)")
         }
     }
-    print("컴퓨터 승리...!")
+    
+    if playerHasWon {
+        print("사용자 승리...!")
+    }else {
+        print("컴퓨터 승리...!")
+    }
 }
+
 startNumberBaseballGame()
+
+
