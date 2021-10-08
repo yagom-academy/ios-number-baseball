81f3b90124414b47590f778ff2effbe2074902ca
yeahg-dev <yeahg.dev@gmail.com>
2021-10-08 21:03:46 +0900

    feat: 사용자 메뉴를 출력하고 메뉴를 입력받는 함수 생성

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -89,6 +89,28 @@ func startGame() {
     }
 }
 
-startGame()
+func selectMenu(){
+    print("""
+    1. 게임시작
+    2. 게임종료
+    원하는 기능을 선택해주세요 :
+    """, terminator: " ")
+    
+    guard let menu = readLine() else {
+        selectMenu()
+        return
+    }
+    
+    switch menu {
+    case "1":
+        startGame()
+    case "2":
+        return
+    default:
+        print("입력이 잘못되었습니다")
+        selectMenu()
+    }
+}
+selectMenu()
 
 
