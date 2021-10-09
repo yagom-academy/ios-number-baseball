7ac8ce632e69f58ade5d5e9e6a219b3bb526f84f
JT <dd3557@naver.com>
2021-10-09 16:51:58 +0900

    feat: 메뉴 선택 함수 추가

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -1,59 +1,57 @@
-////
-////  NumberBaseball - main.swift
-////  Created by yagom.
-////  Copyright © yagom academy. All rights reserved.
-////
 //
-//import Foundation
+//  NumberBaseball - main.swift
+//  Created by yagom.
+//  Copyright © yagom academy. All rights reserved.
 //
-//func makeRandomNumbers() -> [Int] {
-//    var randomNumbers: Set<Int> = []
-//    while randomNumbers.count < 3 {
-//        let randomNumber: Int = Int.random(in: 1...9)
-//        randomNumbers.insert(randomNumber)
-//    }
-//    return Array(randomNumbers)
-//}
-//
-//var computerNumbers: [Int] = makeRandomNumbers()
-//
-//func compareComputerNumbers(with userNumbers: [Int]) -> (strikeCount: Int, ballCount: Int) {
-//    var ballCount: Int = .zero
-//    var strikeCount: Int = .zero
-//
-//    for index in 0...2 where computerNumbers[index] == userNumbers[index] {
-//        strikeCount += 1
-//    }
-//
-//    for userNumber in userNumbers where computerNumbers.contains(userNumber) {
-//       ballCount += 1
-//    }
-//
-//    ballCount -= strikeCount
-//
-//    return (strikeCount, ballCount)
-//}
-//
-//func startGame() {
-//    var chancesLeft: Int = 9
-//    var isUserWin: Bool = false
-//    while chancesLeft > 0, isUserWin == false {
-//        let userNumbers: [Int] = makeRandomNumbers()
-//        print("임의의 수 : \(userNumbers[0]) \(userNumbers[1]) \(userNumbers[2])")
-//        let result: (strikeCount: Int, ballCount: Int) = compareComputerNumbers(with: userNumbers)
-//        print("\(result.strikeCount) 스트라이크, \(result.ballCount) 볼")
-//        chancesLeft -= 1
-//        print("남은 기회 : \(chancesLeft)")
-//        isUserWin = result.strikeCount == 3
-//    }
-//    if isUserWin {
-//        print("사용자 승리...!")
-//    } else {
-//        print("컴퓨터 승리...!")
-//    }
-//}
-//
-//startGame()
+
+import Foundation
+
+func makeRandomNumbers() -> [Int] {
+    var randomNumbers: Set<Int> = []
+    while randomNumbers.count < 3 {
+        let randomNumber: Int = Int.random(in: 1...9)
+        randomNumbers.insert(randomNumber)
+    }
+    return Array(randomNumbers)
+}
+
+var computerNumbers: [Int] = makeRandomNumbers()
+
+func compareComputerNumbers(with userNumbers: [Int]) -> (strikeCount: Int, ballCount: Int) {
+    var ballCount: Int = .zero
+    var strikeCount: Int = .zero
+
+    for index in 0...2 where computerNumbers[index] == userNumbers[index] {
+        strikeCount += 1
+    }
+
+    for userNumber in userNumbers where computerNumbers.contains(userNumber) {
+       ballCount += 1
+    }
+
+    ballCount -= strikeCount
+
+    return (strikeCount, ballCount)
+}
+
+func startGame() {
+    var chancesLeft: Int = 9
+    var isUserWin: Bool = false
+    while chancesLeft > 0, isUserWin == false {
+        let userNumbers: [Int] = makeRandomNumbers()
+        print("임의의 수 : \(userNumbers[0]) \(userNumbers[1]) \(userNumbers[2])")
+        let result: (strikeCount: Int, ballCount: Int) = compareComputerNumbers(with: userNumbers)
+        print("\(result.strikeCount) 스트라이크, \(result.ballCount) 볼")
+        chancesLeft -= 1
+        print("남은 기회 : \(chancesLeft)")
+        isUserWin = result.strikeCount == 3
+    }
+    if isUserWin {
+        print("사용자 승리...!")
+    } else {
+        print("컴퓨터 승리...!")
+    }
+}
 
 func getInputFromUserAndCheck() -> [Int] {
     topLoop: while true {
@@ -90,9 +88,23 @@ func getInputFromUserAndCheck() -> [Int] {
     }
 }
 
-getInputFromUserAndCheck()
+func menuHandler() {
+    print("""
+    1. 게임시작
+    2. 게임종료
+    """)
+    guard let menuNumber = readLine() else {
+        print("입력이 잘못되었습니다")
+        return
+    }
+    switch menuNumber {
+    case "1":
+        startGame()
+    case "2":
+        break
+    default:
+        break
+    }
+}
 
-//print("""
-//1. 게임시작
-//2. 게임종료
-//""")
+menuHandler()
