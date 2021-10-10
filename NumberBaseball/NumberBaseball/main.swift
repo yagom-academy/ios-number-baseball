a79ceaac5fd673385edef623084c91a5d40cd0e6
GOSARI <goeunrhdms@gmail.com>
2021-10-10 20:36:19 +0900

    feat: 사용자 타입 제거 및 함수 chooseMenu 기능 추가

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -11,96 +11,85 @@ var randomNumberAmount = 3
 var computerNumbers: [Int] = []
 var userNumbers: [Int] = []
 
-class BaseballGame {
-  
-
-    func extractRandomNumber(randomNumberAmount:Int = randomNumberAmount) -> [Int] {
-        computerNumbers = []
-        let randomNumberRange = 1...9
-
-        while computerNumbers.count != randomNumberAmount {
-            computerNumbers.append(Int.random(in: randomNumberRange))
-            computerNumbers = Array(Set(computerNumbers))
-        }
-       
-        return computerNumbers
+func extractRandomNumber(randomNumberAmount:Int = randomNumberAmount) -> [Int] {
+    let randomNumberRange = 1...9
+    var randomNumbers: [Int] = []
+    
+    while randomNumbers.count != randomNumberAmount {
+        randomNumbers.append(Int.random(in: randomNumberRange))
+        randomNumbers = Array(Set(randomNumbers))
     }
     
-    func extractUserNumber(){
-        
-        userNumbers = []
-        
-        guard let userInput = readLine() , userInput != "" else{
-            fatalError("입력이 잘못되었습니다.")
-        }
-        let userinputs = userInput.split(separator: " ")
-        
-        userNumbers = userinputs.compactMap{ Int($0) }
-        
-    }
-    
-    func countStrikeAndBall() -> (strike: Int, ball:Int) {
-        var sameNumbers:[Int] = []
-        
-        for eachNumber in computerNumbers{
-            userNumbers.contains(eachNumber) ? sameNumbers.append(eachNumber) : nil
-        }
-        let strikeCount:Int = sameNumbers.filter{ userNumbers.firstIndex(of: $0) == computerNumbers.firstIndex(of: $0) }.count
-        let ballCount:Int = sameNumbers.count - strikeCount
-        
-        chancesLeft -= 1
-        
-        return (strike : strikeCount, ball: ballCount)
-    }
-
-    init() {
-        extractRandomNumber()
-
-        while chancesLeft > 0{
-            let users = extractRandomNumber()
-            let mappedUsers = users.map{ String($0) }
-            let joinedUserNumber = mappedUsers.joined(separator: " ")
-            let strikeAndBall = countStrikeAndBall()
-            print("임의의 수 : \(joinedUserNumber)")
-            print("\(strikeAndBall.strike) 스트라이크, \(strikeAndBall.ball) 볼")
-            print("남은 기회 : \(chancesLeft)")
-            (chancesLeft == 0 && strikeAndBall.strike != 3) ? print("컴퓨터승리...!") : strikeAndBall.strike == 3 ? chancesLeft = 0 : nil
-        }
-    }
+    return randomNumbers
 }
-class MenuHandler{
-    enum menuNumbers:Int{
-        case one = 1
-        case two = 2
+
+func inputUserNumber() -> [Int] {
+    
+    var inputNumbers: [Int] = []
+    
+    print("숫자 3개를 띄어쓰기로 구분하여 입력해주세요.")
+    print("중복 숫자는 허용하지 않습니다.")
+    print("입력 : ", terminator: "")
+    
+    guard let userInput = readLine() , userInput != "" else{
+        fatalError("입력이 잘못되었습니다.")
     }
-    func chooseMenu(){
-        print("1. 게임시작")
-        print("2. 게임종료")
-        print("원하는 기능을 선택해주세요 : ", terminator: "")
-        guard let menuChosen = readLine() , menuChosen != "" else{
-            fatalError("입력이 잘못되었습니다.")
-        }
-        guard let menuChosenInt = Int(menuChosen) else{
-            fatalError("입력이 잘못되었습니다.")
-        }
+    let userinputs = userInput.split(separator: " ")
+    
+    inputNumbers = userinputs.compactMap{ Int($0) }
+    
+    return inputNumbers
+}
+
+func countStrikeAndBall() -> (strike: Int, ball:Int) {
+    var sameNumbers:[Int] = []
+    
+    for eachNumber in computerNumbers{
+        userNumbers.contains(eachNumber) ? sameNumbers.append(eachNumber) : nil
+    }
+    let strikeCount:Int = sameNumbers.filter{ userNumbers.firstIndex(of: $0) == computerNumbers.firstIndex(of: $0) }.count
+    let ballCount:Int = sameNumbers.count - strikeCount
+    chancesLeft -= 1
+    
+    return (strike : strikeCount, ball: ballCount)
+}
+
+func userWin() {
+    print("유저승리!!!")
+    chooseMenu()
+}
+
+func startGame() {
+    computerNumbers = extractRandomNumber()
+
+    while chancesLeft > 0 {
+        userNumbers = inputUserNumber()
         
-        switch menuNumbers (rawValue:menuChosenInt) {
-        case .one : BaseballGame()
-        case .two : break
-        case .none : break
-        }
+        let strikeAndBall = countStrikeAndBall()
+        
+        print("\(strikeAndBall.strike) 스트라이크, \(strikeAndBall.ball) 볼")
+        print("남은 기회 : \(chancesLeft)")
+        strikeAndBall.strike == 3 ? userWin() : nil
+        (chancesLeft == 0 && strikeAndBall.strike != 3) ? print("컴퓨터승리...!") : strikeAndBall.strike == 3 ? chancesLeft = 0 : nil
     }
 }
 
+func chooseMenu(){
+    print("1. 게임시작")
+    print("2. 게임종료")
+    print("원하는 기능을 선택해주세요 : ", terminator: "")
+    guard let menuChosen = readLine() , menuChosen != "" else{
+        fatalError("입력이 잘못되었습니다.")
+    }
+    guard let menuChosenInt = Int(menuChosen) else{
+        fatalError("입력이 잘못되었습니다.")
+    }
+    if menuChosenInt == 1 {
+        startGame()
+    } else if menuChosenInt != 2 {
+        print("입력이 잘못되었습니다")
+        chooseMenu()
+    }
+}
 
-
-BaseballGame().extractUserNumber()
-
-
-
-
-
-
-
-
-
+chooseMenu()
