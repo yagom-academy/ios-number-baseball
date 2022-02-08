3ee3d1f895170ee7668baf1b72f3b2993fc651c8
Mino777 <jomino7777@gmail.com>
2022-02-08 21:48:12 +0900

    feat: 볼과 스트라이크 결과 반환 함수 및 결과 출력 구현

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -19,6 +19,43 @@ func generateExtractedNumbers() -> [Int] {
     return extractedNumbers.map{$0}
 }
 
+func getBallStrikeResult(computerNumbers: [Int], userNumbers: [Int]) -> (Int, Int) {
+    var strike = 0
+    var ball = 0
+    
+    let computerNumbersSet = Set(computerNumbers)
+    
+    for (idx, userNum) in userNumbers.enumerated() {
+        if computerNumbers[idx] == userNum {
+            strike += 1
+        } else if computerNumbersSet.contains(userNum) {
+            ball += 1
+        }
+    }
+    
+    return (strike, ball)
+}
+
 func startGame() {
     let randomNumbers = generateExtractedNumbers()
+    var cnt = 9
+    
+    while cnt > 0 {
+        let userNumbers = generateExtractedNumbers()
+        print("임의의 수 : \(userNumbers.map { String($0) }.joined(separator: " "))")
+        
+        let (strike, ball) = getBallStrikeResult(computerNumbers: randomNumbers, userNumbers: userNumbers)
+        cnt -= 1
+        
+        print("\(strike) 스트라이크, \(ball) 볼")
+        print("남은 기회 : \(cnt)")
+        
+        if strike == 3 {
+            print("사용자 승리!")
+            return
+        }
+    }
+    print("컴퓨터 승리...!")
 }
+
+startGame()
