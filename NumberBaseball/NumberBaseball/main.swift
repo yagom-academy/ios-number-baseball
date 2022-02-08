d7128f8d5d75075ad736689809da80240a654cd0
sery <nicesery@naver.com>
2022-02-08 20:24:40 +0900

    feat: 렌덤한 3개의 수 생성함수 구현

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -6,5 +6,25 @@
 
 import Foundation
 
-print("Hello, World!")
+var playerNum: Set<Int> = Set()
+var computerNum: Set<Int> = Set()
+var matchCount: Int = 9
+
+func generateComputerNum() -> Set<Int> {
+    var nums: Set<Int> = Set()
+    while nums.count <= 3 {
+        nums.insert(Int.random(in: 1...9))
+    }
+    return nums
+}
+
+func validatePlayerNum(from userInput) {
+    
+}
+
+func main() {
+    print("임의의 수 : ", terminator: "")
+    guard let input = readLine() else { return }
+    let playerInput = input.split(separator: " ")
+}
 
