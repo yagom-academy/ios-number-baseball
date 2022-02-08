581109b3fb785cd8e2c2799548710e8239a598cc
Roy-wonji <shuwj81@daum.net>
2022-02-08 23:36:12 +0900

    feat: generateRandomNumbers() 함수  구현 및  변수 생성
    * 컴퓨터가 제시할 임의의 정수 3개를 담아둘 변수
    *  남은 시도횟수를 담아둘 변수

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -1,10 +1,17 @@
 //
 //  NumberBaseball - main.swift
-//  Created by yagom. 
+//  Created by Roy and 쿼카 .
 //  Copyright © yagom academy. All rights reserved.
 // 
 
 import Foundation
 
-print("Hello, World!")
-
+func generateRandomNumbers() -> Set<Int> {
+    var randomNumbers: Set<Int> = []
+    while randomNumbers.count < 3 {
+        randomNumbers.insert(Int.random(in: 1...9))
+    }
+    return randomNumbers
+}
+var remainingChance = 9
+var computerNumbers = generateRandomNumbers()
