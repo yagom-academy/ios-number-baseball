cc4e08f71136000f8c90e73cf3f950f8107c03d1
saafaaari <111dltldnjs@naver.com>
2022-02-08 23:46:33 +0900

    feat : 1~9사이에 중복되지 않는 랜덤한 수를 반환하는 기능 추가

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -6,5 +6,15 @@
 
 import Foundation
 
-print("Hello, World!")
+var winningNumber: Set<Int> = Set<Int>()
+var gameCount: Int = 9
+
+func randomInt(to: Int, from: Int) -> Int { Int.random(in: to...from) }
+func makeNonOverlappingNumber() -> Set<Int> {
+    var nonOverlappingNumber: Set<Int> = Set<Int>()
+    while nonOverlappingNumber.count < 3 {
+        nonOverlappingNumber.insert(randomInt(to: 1, from: 9))
+    }
+    return nonOverlappingNumber
+}
 
