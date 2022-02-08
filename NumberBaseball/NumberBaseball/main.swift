0ead7077196e2bafc25a816ee3982773f755e3f5
sery <nicesery@naver.com>
2022-02-08 22:04:53 +0900

    feat: main 함수 구현

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -12,47 +12,63 @@ var matchCount: Int = 9
 
 func generateComputerNums() -> Set<Int> {
     var nums: Set<Int> = Set()
-    while nums.count <= 3 {
+    while nums.count < 3 {
         nums.insert(Int.random(in: 1...9))
     }
     return nums
 }
 
-func generatePlayerNums() {
-    playerNums = Set()
-    
-    if playerNums.count != 3 {
-        playerNums.insert(Int.random(in: 1...9))
+func generatePlayerNums() -> Set<Int> {
+    var nums: Set<Int> = Set()
+    while nums.count < 3 {
+        nums.insert(Int.random(in: 1...9))
     }
+    return nums
 }
 
-/*
-func validatePlayerNum(from userInput) {
-    
-}
-*/
-
-func main() {
-    print("임의의 수 : ", terminator: "")
-    generatePlayerNums()
-    print(playerNums)
-    //guard let input = readLine() else { return }
-    //let playerInput = input.split(separator: " ")
-}
-
-func matchNums() -> String {
+func matchNums() {
     var strikeNums: Int = 0
     var ballNums: Int = 0
     
 }
-
-func matchBallNums() -> Int {
-    var matchPlayerNums: Set<Int> = Set()
+func matchStrikeNums() -> Int {
+    var matchStrikeCount: Int = 0
     
-    for index in 0...computerNums.count {
-        matchPlayerNums = playerNums.filter { $0 == computerNums["\(index)"] }
+    for (playerNum, computerNum) in zip(playerNums, computerNums) {
+        if playerNum == computerNum {
+            matchStrikeCount += 1
+        }
     }
-    
-    return matchPlayerNums.count
+    return matchStrikeCount
 }
 
+func matchBallNums() -> Int {
+    var matchBallCount: Int = 0
+    
+    for playerNum in playerNums {
+        if computerNums.contains(playerNum) {
+            matchBallCount += 1
+        }
+    }
+    
+    return matchBallCount
+}
+
+func main() {
+    
+    while matchCount > 0 {
+        print("임의의 수 : ", terminator: "")
+        
+        computerNums = generateComputerNums()
+        playerNums = generatePlayerNums()
+        print(playerNums)
+        matchCount -= 1
+        let matchStrikeCount = matchStrikeNums()
+        let matchBallCount = matchBallNums()
+        
+        print("\(matchStrikeCount) 스트라이크, \(matchBallCount) 볼")
+        
+    }
+}
+
+main()
