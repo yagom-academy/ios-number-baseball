32c2fc5b84e1125445fca2284d8e364106234acf
Tiana <97kth01@naver.com>
2022-02-08 22:53:18 +0900

    feat: 컴퓨터와 유저 랜덤 값 비교 함수 구현

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -4,13 +4,40 @@
 //  Copyright © yagom academy. All rights reserved.
 // 
 
-var randomThreeNumbers: Set<Int> = []
 var tryCount: Int = 9
 
-func makeThreeRandomNumbers() {
+func makeThreeRandomNumbers() -> Set<Int> {
     var randomNumber: Int
-    while(randomThreeNumbers.count < 3) {
+    var randomNumberSet: Set<Int> = []
+    while(randomNumberSet.count < 3) {
         randomNumber = Int.random(in: 1...9)
-        randomThreeNumbers.insert(randomNumber)
+        randomNumberSet.insert(randomNumber)
     }
+    return randomNumberSet
 }
+
+var computerRandomNumbers:[Int] = Array(makeThreeRandomNumbers())
+var userRandomNumbers:[Int] = Array(makeThreeRandomNumbers())
+
+func compareUserAndComputer() {
+    var strike = 0
+    var ball = 0
+    if computerRandomNumbers[0] == userRandomNumbers[0] {
+        strike += 1
+    } else {
+        ball += 1
+    }
+    if computerRandomNumbers[1] == userRandomNumbers[1] {
+        strike += 1
+    } else {
+        ball += 1
+    }
+    if computerRandomNumbers[2] == userRandomNumbers[2] {
+        strike += 1
+    } else {
+        ball += 1
+    }
+    print(strike, ball)
+}
+
+compareUserAndComputer()
