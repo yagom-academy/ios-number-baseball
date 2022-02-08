cc7ed0b4c90b3b6919d98eeea64ca58b78babd7e
kimkyunghun3 <kimkyunghun3@naver.com>
2022-02-09 00:11:08 +0900

    feat: 스트라이크와 볼 값 획득함수 정의

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -5,6 +5,8 @@
 // 
 
 var tryCount: Int = 9
+var strike = 0
+var ball = 0
 
 func makeThreeRandomNumbers() -> Set<Int> {
     var randomNumber: Int
@@ -18,6 +20,7 @@ func makeThreeRandomNumbers() -> Set<Int> {
 
 var computerRandomNumbers:[Int] = Array(makeThreeRandomNumbers())
 var userRandomNumbers:[Int] = Array(makeThreeRandomNumbers())
+
 
 func compareUserAndComputer() {
     var strike = 0
@@ -40,4 +43,38 @@ func compareUserAndComputer() {
     print(strike, ball)
 }
 
-compareUserAndComputer()
+func findNotStrike() -> Array<Int> {
+    var result: [Int] = []
+    for (usernumber, userindex) in userRandomNumbers.enumerated() {
+        if let numberNotStrike = compareForStrike(userNumber: usernumber, index: userindex) {
+            result.append(numberNotStrike)
+        }
+    }
+    return result
+}
+
+func compareForStrike(userNumber: Int, index: Int) -> Int? {
+    for (computernumber, computerindex) in computerRandomNumbers.enumerated() {
+        if computernumber == userNumber && computerindex == index {
+            strike += 1
+        }
+        if userRandomNumbers.last == computernumber {
+            return userNumber
+        }
+    }
+    return nil
+}
+
+func findBall(numbersNotStrike:[Int]) { // 매개변수가 스트라이크는 아니지만 볼 일수도 있는 숫자들
+    for number in numbersNotStrike {
+        ballcount(number: number)
+    }
+}
+
+func ballcount(number:Int) {
+    for computerNumber in computerRandomNumbers {
+        if number == computerNumber {
+            ball += 1
+        }
+    }
+}
