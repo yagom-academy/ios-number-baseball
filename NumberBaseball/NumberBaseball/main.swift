912d2a7618147802b739ed6a3c60fcdd51882130
Llingo <llingo@kakao.com>
2022-02-08 21:07:04 +0900

    feat: 볼 매치 함수 구현

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -6,11 +6,11 @@
 
 import Foundation
 
-var playerNum: Set<Int> = Set()
-var computerNum: Set<Int> = Set()
+var playerNums: Set<Int> = Set()
+var computerNums: Set<Int> = Set()
 var matchCount: Int = 9
 
-func generateComputerNum() -> Set<Int> {
+func generateComputerNums() -> Set<Int> {
     var nums: Set<Int> = Set()
     while nums.count <= 3 {
         nums.insert(Int.random(in: 1...9))
@@ -18,13 +18,41 @@ func generateComputerNum() -> Set<Int> {
     return nums
 }
 
+func generatePlayerNums() {
+    playerNums = Set()
+    
+    if playerNums.count != 3 {
+        playerNums.insert(Int.random(in: 1...9))
+    }
+}
+
+/*
 func validatePlayerNum(from userInput) {
     
 }
+*/
 
 func main() {
     print("임의의 수 : ", terminator: "")
-    guard let input = readLine() else { return }
-    let playerInput = input.split(separator: " ")
+    generatePlayerNums()
+    print(playerNums)
+    //guard let input = readLine() else { return }
+    //let playerInput = input.split(separator: " ")
+}
+
+func matchNums() -> String {
+    var strikeNums: Int = 0
+    var ballNums: Int = 0
+    
+}
+
+func matchBallNums() -> Int {
+    var matchPlayerNums: Set<Int> = Set()
+    
+    for index in 0...computerNums.count {
+        matchPlayerNums = playerNums.filter { $0 == computerNums["\(index)"] }
+    }
+    
+    return matchPlayerNums.count
 }
 
