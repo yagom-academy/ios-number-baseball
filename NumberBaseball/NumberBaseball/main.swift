7036904e59e43135e4e36824ad2daae4556d706c
malrang <ruehd7924@gmail.com>
2022-02-09 15:09:04 +0900

    feat: 사용자 번호와 정답 번호 비교 기능 추가

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -6,10 +6,8 @@
 
 import Foundation
 
-var winningNumber: Set<Int> = Set<Int>()
-var gameCount: Int = 9
-
 func randomInt(to: Int, from: Int) -> Int { Int.random(in: to...from) }
+
 func makeNonOverlappingNumber() -> Set<Int> {
     var nonOverlappingNumber: Set<Int> = Set<Int>()
     while nonOverlappingNumber.count < 3 {
@@ -18,3 +16,39 @@ func makeNonOverlappingNumber() -> Set<Int> {
     return nonOverlappingNumber
 }
 
+func checkScore(answerNumber: [Int], playerNumber: [Int]) -> (Int, Int) {
+    var ballScore: Int = 0
+    var strikeScore: Int = 0
+    var checkBallNumber: [Int] = []
+    for i in 0...answerNumber.count - 1 {
+        if answerNumber == playerNumber {
+            strikeScore = 3
+            return (strikeScore, ballScore)
+        }
+        if answerNumber[i] == playerNumber[i] {
+            strikeScore += 1
+        } else {
+            checkBallNumber.append(playerNumber[i])
+        }
+    }
+    for i in checkBallNumber {
+        if answerNumber.contains(i) {
+            ballScore += 1
+        }
+    }
+    return (strikeScore, ballScore)
+}
+
+func startGame(answerNumber: [Int]) {
+    var gameCount: Int = 9
+    while gameCount > 0 {
+        var playerNumber = Array(makeNonOverlappingNumber())
+        checkScore(answerNumber: answerNumber, playerNumber: playerNumber)
+        gameCount -= 1
+    }
+}
+
+var answerNumber: [Int] = Array(makeNonOverlappingNumber())
+//var gameCount: Int = 9
+
+startGame(answerNumber: answerNumber)
