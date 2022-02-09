b7330d39e92fb9aaa6e9097bf0fd14555577cb95
kch1285 <kch1285@naver.com>
2022-02-09 14:34:18 +0900

    feat: 난수 생성 함수 구현

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -1,10 +1,28 @@
 //
 //  NumberBaseball - main.swift
-//  Created by yagom. 
+//  Created by Chuuny & Doogie.
 //  Copyright © yagom academy. All rights reserved.
 // 
 
 import Foundation
 
-print("Hello, World!")
 
+var randomNumber: [String] = []
+var userNumber: [String] = []
+var maxRange = 8
+
+func makeRandomNumber(_ list: [String]) {
+    var numberList = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    for _ in 1...3 {
+        let randomInt = Int.random(in: 0...maxRange)
+        randomNumber.append(String(numberList[randomInt]))
+        numberList.remove(at: randomInt)
+        maxRange = maxRange - 1
+    }
+//    if Set(list).count != 3 {
+//        return makeRandomNumber(<#T##list: [String]##[String]#>)
+//    }
+}
+
+
+print("임의의 수 : \(userNumber[0]) \(userNumber[1]) \(userNumber[2])")
