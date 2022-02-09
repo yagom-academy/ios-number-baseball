ed2e622126c4da0107ca7e46d29aba87353334ff
kch1285 <kch1285@naver.com>
2022-02-09 16:17:36 +0900

    feat: 볼 카운트 체크 기능 구현, 기회에 따른 출력 기능 수정 및 함수화 chore: 코드 정리

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -6,38 +6,55 @@
 
 import Foundation
 
-var userNumber: [String] = []
-var chance = 9
-
 func makeRandomNumber() -> [String] {
     var randomNumber: [String] = []
     var numberList = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     numberList.shuffle()
-    randomNumber.append(String(numberList[0]))
-    randomNumber.append(String(numberList[1]))
-    randomNumber.append(String(numberList[2]))
+    for i in 1...3 {
+        randomNumber.append(String(numberList[i - 1]))
+    }
     
     return randomNumber
 }
 
-func checkNumber(_ userNumbers: [String], randomNumbers: [String]) -> (Int, Int) {
+func checkNumber(_ userNumbers: [String]) -> (Int, Int) {
     var strike = 0
     var ball = 0
     
     for i in 0..<userNumbers.count {
-        if userNumbers[i] == randomNumbers[i] {
-            strike += 1
+        if randomNumbers.contains(userNumbers[i]) {
+            ball += 1
         }
     }
-    return (0, 0)
+    
+    for i in 0..<userNumbers.count {
+        if userNumbers[i] == randomNumbers[i] {
+            strike += 1
+            ball -= 1
+        }
+    }
+    
+    return (strike, ball)
 }
 
-let randomNumber = makeRandomNumber()
+let randomNumbers = makeRandomNumber()
+var chance = 9
 
-while chance > 0 {
-    let userNumber = makeRandomNumber()
-    print("임의의 수 : \(userNumber[0]) \(userNumber[1]) \(userNumber[2])")
-//    print("\(checkNumber(userNumber).0) 스트라이크, \(checkNumber(userNumber).1) 볼")
-    chance -= 1
-    print("남은 기회 \(chance)")
+func playGame() {
+    while chance > 0 {
+        let userNumber = makeRandomNumber()
+        print("임의의 수 : \(userNumber[0]) \(userNumber[1]) \(userNumber[2])")
+        print("\(checkNumber(userNumber).0) 스트라이크, \(checkNumber(userNumber).1) 볼")
+        chance -= 1
+        print("남은 기회 \(chance)")
+        
+        if userNumber == randomNumbers {
+            print("사용자 승리...!")
+            return
+        }
+    }
+    
+    print("컴퓨터 승리...!")
 }
+
+playGame()
