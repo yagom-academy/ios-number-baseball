d7f0c4be3b6e524c3368625377b3632ccf22ac6d
marisol <marisol.develop@gmail.com>
2022-02-09 10:40:24 +0900

    feat: checkStrike, startGame 함수 구현

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -9,6 +9,7 @@ import Foundation
 
 var numbersByComputer: [Int] = []
 var numberOfChance: Int = 9
+var numbersByUser: [Int] = []
 
 func returnRandomNumbers() -> [Int] {
     var tempSet = Set<Int>()
@@ -19,11 +20,24 @@ func returnRandomNumbers() -> [Int] {
     return Array(tempSet)
 }
 
+func returnResult() -> String {
+    return "\(checkStrike()) 스트라이크, 볼"
+}
+
+func checkStrike() -> Int {
+    var count: Int = 0
+    for i in 0...2 {
+        if numbersByUser[i] == numbersByComputer[i] {
+            count += 1
+        }
+    }
+    return count
+}
 /*
-func returnResult(numbers: [Int]) -> String {
+func checkBall() -> Int {
     
 }
-*/
+ */
 
 func startGame() {
     
@@ -31,4 +45,16 @@ func startGame() {
 
 numbersByComputer = returnRandomNumbers()
 
+while(numberOfChance > 0) {
+    numbersByUser = returnRandomNumbers()
+    print(numbersByUser)
+    let gameResult = returnResult()
+    print(gameResult)
+    if gameResult == "3 스트라이크, 0 볼" {
+        print("사용자 승리!")
+        break
+    }
+    numberOfChance -= 1
+    print("남은 기회 : \(numberOfChance)")
+}
 
