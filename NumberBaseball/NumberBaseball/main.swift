923ecc8bef18bac350a92d378396148b0a0f7605
Llingo <llingo@kakao.com>
2022-02-09 16:27:43 +0900

    refactor: generateRandomThreeNums 함수 재사용성 향상

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -9,9 +9,8 @@ import Foundation
 var playerNums: Set<Int> = Set()
 var computerNums: Set<Int> = Set()
 var matchCount: Int = 9
-var matchStrikeCount: Int = 0
 
-func generateComputerNums() -> Set<Int> {
+func generateRandomThreeNums() -> Set<Int> {
     var nums: Set<Int> = Set()
     while nums.count < 3 {
         nums.insert(Int.random(in: 1...9))
@@ -19,16 +18,8 @@ func generateComputerNums() -> Set<Int> {
     return nums
 }
 
-func generatePlayerNums() -> Set<Int> {
-    var nums: Set<Int> = Set()
-    while nums.count < 3 {
-        nums.insert(Int.random(in: 1...9))
-    }
-    return nums
-}
-
-func matchNums() -> String {
-    matchStrikeCount = matchStrikeNums()
+func displayMatchScore() -> String {
+    let matchStrikeCount = matchStrikeNums()
     let matchBallCount = matchBallNums()
     
     return "\(matchStrikeCount) 스트라이크, \(matchBallCount - matchStrikeCount) 볼"
@@ -57,22 +48,23 @@ func setToString(set: Set<Int>) -> String {
     return stringArray.joined(separator: " ")
 }
 
-func startGame() {
-    computerNums = generateComputerNums()
+func playGame() {
+    computerNums = generateRandomThreeNums()
     
     while matchCount > 0 {
         print("임의의 수 : ", terminator: "")
         
-        playerNums = generatePlayerNums()
+        playerNums = generateRandomThreeNums()
         print(setToString(set: playerNums))
         
-        if matchStrikeCount == 3 {
+        if matchStrikeNums() == 3 {
             print("사용자 승리!")
             break
         } else if matchCount == 1 {
             print("컴퓨터 승리...!")
         }
-        print(matchNums())
+        
+        print(displayMatchScore())
         matchCount -= 1
         
         print("남은 기회 : \(matchCount)")
@@ -80,7 +72,7 @@ func startGame() {
 }
 
 func main() {
-    startGame()
+    playGame()
 }
 
 main()
