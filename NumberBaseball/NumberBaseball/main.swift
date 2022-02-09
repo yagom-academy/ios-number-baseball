726a58c6b84c0f8182ce230e3faf32b2eb60b388
minseong <msb0987@naver.com>
2022-02-09 14:50:40 +0900

    feat: 세 개의 정수를 비교 후 볼과 스트라이크 결과를 반환하는 함수추가

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -12,3 +12,25 @@ func createThreeRamdomNumber() -> [Int] {
     return Array(threeRandomNumber)
 }
 
+func compare(userInputNumber: [Int]) {
+    var strikeCount: Int = 0
+    var ballCount: Int = 0
+
+    for index in 0..<3 {
+        if computerThreeRandomNumber[index] == userInputNumber[index] {
+            strikeCount += 1
+        } else if computerThreeRandomNumber.contains(userInputNumber[index]) {
+            ballCount += 1
+        }
+    }
+    
+//    print("\(strikeCount) 스트라이크, \(ballCount) 볼")
+//    print("남은 기회 : \(remainingTime)")
+    
+    remainingTime -= 1
+    
+    if remainingTime == 0 {
+        print("컴퓨터 승리!")
+    }
+}
+
