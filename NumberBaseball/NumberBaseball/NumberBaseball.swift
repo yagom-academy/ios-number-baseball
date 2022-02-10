032382918c17d1610504b331d52ed61ad198b971
JoSH0318 <jsh9870@naver.com>
2022-02-10 19:35:56 +0900

    ♻️[[REFACTOR] : 함수 추가 및 세분화, 함수 위치 변경, 함수명 변경, 변수명 변경
    checkGameResult 함수로 추가, 세분화
    printScoreInformation 함수로 추가, 세분화
    checkScoreCondition 으로 함수명 변경
    randomNum -> randomNumber 로 이름 변경
    함수 위치 변경

--- NumberBaseball/NumberBaseball/NumberBaseball.swift
+++ NumberBaseball/NumberBaseball/NumberBaseball.swift
@@ -10,29 +10,26 @@ import Foundation
 var computerNumbers: [Int] = []
 var playerNumbers: [Int] = []
 var roundCount: Int = 9
-var strike = 0
-var ball = 0
+var strike: Int = 0
+var ball: Int = 0
 
 func startGame() {
     while roundCount > 0 {
         saveComputerNumbers()
         savePlayerNumbers()
-        checkTotalStatus()
-        if roundCount == 0 {
-            print("컴퓨터 승리🤣")
-        } else if roundCount > 0 && strike == 3 {
-            print("유저 승리❤️")
-            break
-        }
+        checkScoreCondition()
+        checkGameResult()
         resetValue()
     }
 }
 
-func resetValue() {
-    computerNumbers.removeAll()
-    playerNumbers.removeAll()
-    strike = 0
-    ball = 0
+func checkGameResult() {
+    if roundCount == 0 {
+        print("컴퓨터 승리🤣")
+    } else if strike == 3 {
+        print("유저 승리❤️")
+        roundCount = 0
+    }
 }
 
 func generateRandomNumber() -> Int {
@@ -45,43 +42,41 @@ func saveComputerNumbers() {
     }
 }
 
+func compareComputerNumbers() {
+    let randomNumber = generateRandomNumber()
+    if computerNumbers.contains(randomNumber) == false {
+        computerNumbers.append(randomNumber)
+    }
+}
+
 func savePlayerNumbers() {
     while playerNumbers.count < 3 {
         comparePlayerNumbers()
     }
 }
 
+func comparePlayerNumbers() {
+    let randomNumber = generateRandomNumber()
+    if playerNumbers.contains(randomNumber) == false {
+        playerNumbers.append(randomNumber)
+    }
+}
+
 func printRandomNumbers() {
-    print("\n컴퓨터 임의의 수 : ", terminator: "")
-    computerNumbers.forEach{ print($0, terminator: " ") }
+    print("\n컴퓨터 임의의 수 : ", terminator: "") // 테스트를 위한 텍스트, 삭제해야하는 줄
+    computerNumbers.forEach{ print($0, terminator: " ") } // 테스트를 위한 텍스트, 삭제해야하는 줄
     print("\n유저 임의의 수 : ", terminator: "")
     playerNumbers.forEach{ print($0, terminator: " ") }
 }
 
-func compareComputerNumbers() {
-    let randomNum = generateRandomNumber()
-    if computerNumbers.contains(randomNum) == false {
-        computerNumbers.append(randomNum)
-    }
-}
-
-func comparePlayerNumbers() {
-    let randomNum = generateRandomNumber()
-    if playerNumbers.contains(randomNum) == false {
-        playerNumbers.append(randomNum)
-    }
-}
-
-func checkTotalStatus() {
+func checkScoreCondition() {
     let sameNumbers = computerNumbers.filter{ playerNumbers.contains($0) }
     for element in 0..<3 {
         checkStrikeCondition(element: element)
     }
     checkBallCondition(sameNumbers: sameNumbers.count)
     printRandomNumbers()
-    print("\n\(strike) 스트라이크, \(ball) 볼")
-    roundCount -= 1
-    print("남은 기회 : \(roundCount)")
+    printScoreInformation()
 }
 
 func checkStrikeCondition(element: Int) {
@@ -94,3 +89,15 @@ func checkBallCondition(sameNumbers: Int) {
     ball = sameNumbers - strike
 }
 
+func printScoreInformation() {
+    print("\n\(strike) 스트라이크, \(ball) 볼")
+    roundCount -= 1
+    print("남은 기회 : \(roundCount)")
+}
+
+func resetValue() {
+    computerNumbers.removeAll()
+    playerNumbers.removeAll()
+    strike = 0
+    ball = 0
+}
