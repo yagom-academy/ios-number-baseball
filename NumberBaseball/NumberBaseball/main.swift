d46a4de1a78a102f694b0faf82e5266acc5bef6e
Mino777 <jomino7777@gmail.com>
2022-02-10 21:16:17 +0900

    feat: add printMenu()

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -10,6 +10,12 @@ let winnerGoalCount = 3
 var strikeCount = 0
 var ballCount = 0
 
+func printMenu() {
+    print("1. 게임시작")
+    print("2. 게임종료")
+    print("원하는 기능을 선택해주세요 : ", terminator: "")
+}
+
 func startGame() {
     let randomNumbers = generatedThreeRandomNumbers()
     var matchCount = 9
