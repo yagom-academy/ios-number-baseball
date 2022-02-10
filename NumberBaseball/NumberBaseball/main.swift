074955abcf2e830f1e2f2a8768d268f3686e9bb5
marisol <marisol.develop@gmail.com>
2022-02-10 11:30:04 +0900

    refactor: generateRandomNumbers 함수 로직 수정, startGame 함수 내 임의의 수 출력 로직 수정, judgeGameResult로 함수명 수정

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -13,79 +13,51 @@ var numbersByUser: [Int] = []
 var strikeCount: Int = 0
 var ballCount: Int = 0
 
-func returnRandomNumbers() -> [Int] {
-    var numberSet = Set<Int>()
+func generateRandomNumbers() -> [Int] {
+    var numberArray: [Int] = []
+    var numberPool: [Int] = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     
-    while(numberSet.count < 3) {
-        numberSet.insert(Int.random(in: 1...9))
+    for _ in 1...3 {
+        numberArray.append(numberPool[Int.random(in: 0..<numberPool.count)])
+        numberPool = numberPool.filter({ !numberArray.contains($0) })
     }
-    
 
-    return Array(numberSet)
+    return numberArray
 }
-/* old one
-func returnResult() -> String {
-    var strikeCount: Int = 0
-    var ballCount: Int = 0
-    
-    for i in 0...2 {
-        if numbersByUser[i] == numbersByComputer[i] {
-            strikeCount += 1
-        } else if numbersByComputer.contains(numbersByUser[i]) {
-            ballCount += 1
-        }
-    }
-    return "\(strikeCount) 스트라이크, \(ballCount) 볼"
-}
-*/
 
-func returnResult() {
+func judgeGameResult() {
     strikeCount = 0
     ballCount = 0
     
-    for i in 0...2 {
-        if numbersByUser[i] == numbersByComputer[i] {
+    for index in 0...2 {
+        if numbersByUser[index] == numbersByComputer[index] {
             strikeCount += 1
-        } else if numbersByComputer.contains(numbersByUser[i]) {
+        } else if numbersByComputer.contains(numbersByUser[index]) {
             ballCount += 1
         }
     }
-    
-    /*
-    if strikeCount == 3 {
-        print("사용자 승리!!")
-        return
-    }
-     */
 }
 
 func startGame() {
-    numbersByComputer = returnRandomNumbers()
+    numbersByComputer = generateRandomNumbers()
     
     while(numberOfChance > 0 && strikeCount < 3) {
-        numbersByUser = returnRandomNumbers()
+        numbersByUser = generateRandomNumbers()
+        let convertedNumbers = numbersByUser.map({ String($0) })
+        print("임의의 수 :", convertedNumbers.joined(separator: " "))
+        judgeGameResult()
         
-        /*
-        print("임의의 수 : ", terminator: "")
-        numbersByUser.forEach {
-            print($0, terminator: " ")
+        if strikeCount == 3 {
+            print("사용자 승리!")
+            print("\(strikeCount) 스트라이크, \(ballCount) 볼")
+            break
         }
-        print()
-        */
-        
-        print("임의의 수 : \(numbersByUser[0]) \(numbersByUser[1]) \(numbersByUser[2])")
-        returnResult()
-        
-        //if strikeCount == 3 {
-        //    print("사용자 승리!")
-        //    break
-        //}
-        
-        if (numberOfChance - 1) == 0 {
+
+        if (numberOfChance - 1) == .zero {
             print("컴퓨터 승리...!")
+            print("\(strikeCount) 스트라이크, \(ballCount) 볼")
         }
         
-        print("\(strikeCount) 스트라이크, \(ballCount) 볼")
         numberOfChance -= 1
         print("남은 기회 : \(numberOfChance)")
     }
