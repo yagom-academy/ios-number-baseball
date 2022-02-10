b07da0ea2fee80a26a450df6d1873a6009fd2969
GoMyungju171 <5211616@likelion.org>
2022-02-10 12:32:22 +0900

    Feat: 게임 종료 함수 생성

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -1,15 +1,9 @@
-//
-//  NumberBaseball - main.swift
-//  Created by yagom. 
-//  Copyright © yagom academy. All rights reserved.
-// 
-
 import Foundation
 
 
 func makeRandomNum() -> Set<Int> {
     var randomSet: Set<Int> = []
-    
+
     while randomSet.count < 3 {
         let randomNumberInSet = Int.random(in: 1...9)
         randomSet.insert(randomNumberInSet)
@@ -28,7 +22,7 @@ func changeArray() -> Array<Int> {
 func checkStrikeBall(answerNumbers: Array<Int>, randomNumbers: Array<Int>) -> (Int, Int) {
     var strikeCount = 0
     var ballCount = 0
-    
+
     for index in 0...2 {
         if answerNumbers[index] == randomNumbers[index] {
             strikeCount += 1
@@ -40,31 +34,39 @@ func checkStrikeBall(answerNumbers: Array<Int>, randomNumbers: Array<Int>) -> (I
 }
 
 
+func printResult(randomNumbers: Array<Int>, strike: Int, ball: Int, tryCount: Int) {
+    print("임의의 수 : \(randomNumbers[0]) \(randomNumbers[1]) \(randomNumbers[2])")
+    print("\(strike) 스트라이크, \(ball) 볼")
+    print("남은 기회 : \(tryCount)")
+}
+
+
+func finishGame(strike: Int, tryCount: Int) -> Int {
+    var endCount = tryCount
+
+    if strike == 3 {
+        print("사용자 승리...!")
+        endCount = 0
+    } else if tryCount == 0 {
+        print("컴퓨터 승리...!")
+    }
+    return endCount
+}
+
+
 func startGame() {
     var answerNumbers: Array<Int> = []
     var randomNumbers: Array<Int> = []
     var tryCount = 9
-    
+
     answerNumbers = changeArray()
     while tryCount != 0 {
         randomNumbers = changeArray()
         let (strike, ball) = checkStrikeBall(answerNumbers: answerNumbers, randomNumbers: randomNumbers)
         tryCount -= 1
         printResult(randomNumbers: randomNumbers, strike: strike, ball: ball, tryCount: tryCount)
-        if strike == 3 {
-            print("사용자 승리...!")
-            break
-        } else if tryCount == 0 {
-            print("컴퓨터 승리...!")
-        }
+        tryCount = finishGame(strike: strike, tryCount: tryCount)
     }
-}
-
-
-func printResult(randomNumbers: Array<Int>, strike: Int, ball: Int, tryCount: Int) {
-    print("임의의 수 : \(randomNumbers[0]) \(randomNumbers[1]) \(randomNumbers[2])")
-    print("\(strike) 스트라이크, \(ball) 볼")
-    print("남은 기회 : \(tryCount)")
 }
 
 
