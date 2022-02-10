1e589b4b1822e55800664c9db458437714625499
dudu <whqtkf12@naver.com>
2022-02-10 17:25:54 +0900

    refactor: 가독성을 위한 함수위치 조정

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -10,47 +10,6 @@ let winnerGoalCount = 3
 var strikeCount = 0
 var ballCount = 0
 
-func generateRandomNumber(numberRange: ClosedRange<Int> = (1...9)) -> Int {
-    return Int.random(in: numberRange)
-}
-
-func generatedThreeRandomNumbers() -> [Int] {
-    var extractedNumbers: Set<Int> = []
-    let limitCount = 3
-    
-    while extractedNumbers.count != limitCount {
-        extractedNumbers.insert(generateRandomNumber())
-    }
-    
-    return Array(extractedNumbers)
-}
-
-func increaseBallOrStrike(targetNumbers: [Int], userNumberIndex: Int, userNumber: Int) {
-    if targetNumbers[userNumberIndex] == userNumber {
-        strikeCount += 1
-    } else if targetNumbers.contains(userNumber) {
-        ballCount += 1
-    }
-}
-
-func judgeBallOrStrike(targetNumbers: [Int], userNumbers: [Int]) {
-    for (userNumberIndex, userNumber) in userNumbers.enumerated() {
-        increaseBallOrStrike(targetNumbers: targetNumbers, userNumberIndex: userNumberIndex, userNumber: userNumber)
-    }
-}
-
-func showGameResult() {
-    if strikeCount == winnerGoalCount {
-        print("사용자 승리!")
-    } else {
-        print("컴퓨터 승리...!")
-    }
-}
-
-func convertIntArrayToString(intArray: [Int]) -> String {
-    intArray.map { String($0) }.joined(separator: " ")
-}
-
 func startGame() {
     let randomNumbers = generatedThreeRandomNumbers()
     var matchCount = 9
@@ -71,6 +30,47 @@ func startGame() {
         print("남은 기회 : \(matchCount)")
     }
     showGameResult()
+}
+
+func generatedThreeRandomNumbers() -> [Int] {
+    var extractedNumbers: Set<Int> = []
+    let limitCount = 3
+    
+    while extractedNumbers.count != limitCount {
+        extractedNumbers.insert(generateRandomNumber())
+    }
+    
+    return Array(extractedNumbers)
+}
+
+func generateRandomNumber(numberRange: ClosedRange<Int> = (1...9)) -> Int {
+    return Int.random(in: numberRange)
+}
+
+func judgeBallOrStrike(targetNumbers: [Int], userNumbers: [Int]) {
+    for (userNumberIndex, userNumber) in userNumbers.enumerated() {
+        increaseBallOrStrike(targetNumbers: targetNumbers, userNumberIndex: userNumberIndex, userNumber: userNumber)
+    }
+}
+
+func increaseBallOrStrike(targetNumbers: [Int], userNumberIndex: Int, userNumber: Int) {
+    if targetNumbers[userNumberIndex] == userNumber {
+        strikeCount += 1
+    } else if targetNumbers.contains(userNumber) {
+        ballCount += 1
+    }
+}
+
+func showGameResult() {
+    if strikeCount == winnerGoalCount {
+        print("사용자 승리!")
+    } else {
+        print("컴퓨터 승리...!")
+    }
+}
+
+func convertIntArrayToString(intArray: [Int]) -> String {
+    intArray.map { String($0) }.joined(separator: " ")
 }
 
 startGame()
