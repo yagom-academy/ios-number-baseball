c2ef4f63071ada4b294afd0ef46cf397114b4685
malrang <ruehd7924@gmail.com>
2022-02-10 19:05:06 +0900

    refactor :  숫자 야구게임 step1 기능을 전체적으로 수정함
    randomInt 함수 기능 makeRandomNumber 로 병합
    
    makeNonOverlappingNumber 에서 makeRandomNumber 으로 네이밍 변경
    
    makeRandomNumber 함수에 파라미터를 추가하여 유연한 함수로 수정함
    함수의 반환 값을 Set타입에서 Array로 변경
    중복값 제거 방식을 변경하여 원하는만큼만 반복해 중복되지 않는 값을 만들어내도록 수정함
    
    게임의 스코어를 가져오는 getScore 함수 추가
    
    checkScore 함수에서 튜플 반환값 추가
    
    CheckScore 함수 내부의 print구문 삭제
    
    printGameResult 게임결과를 출력하는 함수 추가
    
    gameStart 함수 내부에서 while 반복문을 repeat-while 로 변경

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -6,49 +6,61 @@
 
 import Foundation
 
-func randomInt(to: Int, from: Int) -> Int { Int.random(in: to...from) }
-
-func makeNonOverlappingNumber() -> Set<Int> {
-    var nonOverlappingNumber: Set<Int> = Set<Int>()
-    while nonOverlappingNumber.count < 3 {
-        nonOverlappingNumber.insert(randomInt(to: 1, from: 9))
+func makeRandomNumber(to: Int, from: Int, count: Int) -> [Int]? {
+    var nonOverlappingNumber: [Int] = []
+    var range: Set<Int> = Set<Int>(to...from)
+    while nonOverlappingNumber.count < count {
+        guard let randomNumber: Int = range.randomElement() else { return nil }
+        nonOverlappingNumber.append(randomNumber)
+        range.remove(randomNumber)
     }
     return nonOverlappingNumber
 }
 
-func checkScore(answerNumber: [Int], playerNumber: [Int]) {
-    var ballScore: Int = 0
-    var strikeScore: Int = 0
-    
-    for i in 0...answerNumber.count - 1 {
-        if answerNumber[i] == playerNumber[i] {
-            strikeScore += 1
-        } else if answerNumber.contains(playerNumber[i]) {
-            ballScore += 1
-        }
+func getScore(problemNumber: [Int], solutionNumber: [Int]) -> (strikeScore: Int, ballScore: Int) {
+    var score: (strikeScore: Int, ballScore: Int) = (strikeScore: 0, ballScore: 0)
+
+    for i in 0..<problemNumber.count {
+        score.strikeScore += checkScore(problemNumber: problemNumber, solutionNumber: solutionNumber, index: i).strike
+        score.ballScore += checkScore(problemNumber: problemNumber, solutionNumber: solutionNumber, index: i).ball
     }
-    print("\n\(strikeScore) 스트라이트, \(ballScore) 볼")
+    return score
+}
+
+func checkScore(problemNumber: [Int], solutionNumber: [Int], index: Int) -> (strike: Int, ball: Int) {
+    if problemNumber[index] == solutionNumber[index] {
+        return (strike: 1, ball: 0)
+    } else if problemNumber.contains(solutionNumber[index]) {
+        return (strike: 0, ball: 1)
+    } else {
+        return (strike: 0, ball: 0)
+    }
+}
+
+func printGameResult(solutionNumber: [Int], playerStrikeScore: Int, playerBallScore: Int, gameCount: Int) {
+    print("임의의 수 : ", terminator: "")
+    solutionNumber.forEach{ print($0, terminator: " ") }
+
+    if playerStrikeScore == 3 {
+        print("\n사용자 승리!", terminator: "")
+
+    } else if gameCount == 0 {
+        print("\n컴퓨터 승리...!", terminator: "")
+    }
+    print("\n(playerStrikeScore) 스트라이크,(playerBallScore) 볼", terminator: "")
+    print("\n남은 기회 : (gameCount)")
 }
 
 func startGame() {
-    let computerNumbers: [Int] = Array(makeNonOverlappingNumber())
+    var playerScore: (strikeScore: Int, ballScore: Int)
+    guard let computerNumbers = makeRandomNumber(to: 1, from: 9, count: 3) else { return }
     var gameCount: Int = 9
-
-    while true {
-        let playerNumbers = Array(makeNonOverlappingNumber())
-        if gameCount == 0 {
-            print("컴퓨터 승리...!")
-            break
-        } else if computerNumbers != playerNumbers {
-            gameCount -= 1
-            checkScore(answerNumber: computerNumbers, playerNumber: playerNumbers)
-            print("임의의 수 : ", terminator: "")
-            playerNumbers.forEach{ print($0, terminator: " ") }
-            print("\n남은 게임 횟수 : \(gameCount)")
-        } else {
-            print("3 스트라이크, 0 볼\n사용자 승리!")
-            break
-        }
-    }
+    repeat {
+        gameCount -= 1
+        guard let playerNumbers = makeRandomNumber(to: 1, from: 9, count: 3) else { return }
+        playerScore = getScore(problemNumber: playerNumbers, solutionNumber: computerNumbers)
+        printGameResult(solutionNumber: playerNumbers, playerStrikeScore: playerScore.strikeScore, playerBallScore: playerScore.ballScore, gameCount: gameCount)
+    } while gameCount > Int.zero && playerScore.strikeScore != 3
 }
+
 startGame()
