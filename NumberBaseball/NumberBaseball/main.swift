29269688e1e7f4ad7fed50ab1d90353f751ca4e7
Tiana <97kth01@naver.com>
2022-02-10 17:45:42 +0900

    refactor: 전역변수를 지역변수로 수정, 랜덤 수 생성하는 함수 반환값 수정

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -4,75 +4,87 @@
 //  Copyright © yagom academy. All rights reserved.
 // 
 
+var computerRandomNumbers:[Int] = makeThreeRandomNumbers()
 var tryCount: Int = 9
-var strike = 0
-var ball = 0
-var computerRandomNumbers:[Int] = Array(makeThreeRandomNumbers())
-var userRandomNumbers:[Int] = Array(makeThreeRandomNumbers())
 
 func startGame() {
-    while tryCount > 0 && strike < 3 {
-        strike = 0
-        ball = 0
-        userRandomNumbers = Array(makeThreeRandomNumbers())
+    var userStrikeCount = 0
+    var userBallCount = 0
+    var userRandomNumbers: [Int]
+    while tryCount > 0 && userStrikeCount < 3 {
+        userStrikeCount = 0
+        userBallCount = 0
+        userRandomNumbers = makeThreeRandomNumbers()
         print("임의의 수 : ", terminator: "")
         for userNumber in userRandomNumbers {
             print(userNumber, terminator: " ")
         }
         print()
-        checkBall(notStrikeNumbers: findNotStrike())
+        (userStrikeCount, userBallCount) = checkBall(notStrikeNumbers: findNotStrike(userStrikeCount: userStrikeCount, userNumbers: userRandomNumbers), userBallCount: userBallCount)
         tryCount -= 1
-        if strike == 3 {
+        if userStrikeCount == 3 {
             print("사용자 승리...!")
         }
         if tryCount == 0 {
             print("컴퓨터 승리...!")
         }
-        print("\(strike) 스트라이크, \(ball) 볼")
+        print("\(userStrikeCount) 스트라이크, \(userBallCount) 볼")
         print("남은 기회 : \(tryCount)")
     }
 }
 
-func makeThreeRandomNumbers() -> Set<Int> {
+func makeThreeRandomNumbers() -> [Int] {
     var randomNumbers: Set<Int> = []
     while(randomNumbers.count < 3) {
         randomNumbers.insert(Int.random(in: 1...9))
     }
-    return randomNumbers
+    return Array(randomNumbers)
 }
 
-func findNotStrike() -> [Int] {
+func findNotStrike(userStrikeCount: Int, userNumbers: [Int]) -> ([Int], Int) {
     var notStrikeNumbers: [Int] = []
-    for (userIndex, userNumber) in userRandomNumbers.enumerated() {
-        if let notStrikeNumber = countStrike(userNumber: userNumber, userIndex: userIndex) {
-            notStrikeNumbers.append(notStrikeNumber)
+    var strikeCount = userStrikeCount //var
+    for (userIndex, userNumber) in userNumbers.enumerated() {
+        if let (notStrikeNumber, strikeCount2) = countStrike(userNumber: userNumber, userIndex: userIndex, userStrikeCount: strikeCount) {
+            strikeCount = strikeCount2
+            if let notStrikeNum = notStrikeNumber {
+                notStrikeNumbers.append(notStrikeNum)
+            }
+            
         }
     }
-    return notStrikeNumbers
+    return (notStrikeNumbers, strikeCount)
 }
 
-func countStrike(userNumber: Int, userIndex: Int) -> Int? {
+func countStrike(userNumber: Int, userIndex: Int, userStrikeCount: Int) -> (Int?, Int)? {
+    var strikeCount = userStrikeCount
     for (computerIndex, computerNumber) in computerRandomNumbers.enumerated() {
         if computerNumber == userNumber && computerIndex == userIndex {
-            strike += 1
-            return nil
+            strikeCount += 1
+            return (nil, strikeCount)
         }
     }
-    return userNumber
+    return (userNumber, strikeCount)
 }
 
-func checkBall(notStrikeNumbers:[Int]) {
-    for checkNumber in notStrikeNumbers {
-        countBall(userNumber: checkNumber)
+func checkBall(notStrikeNumbers: ([Int], Int), userBallCount: Int) -> (Int, Int) {
+    var ballCount = userBallCount
+    let strikeCount = notStrikeNumbers.1
+    for checkNumber in notStrikeNumbers.0 {
+        ballCount = countBall(userNumber: checkNumber, userBallCount: ballCount)
     }
+    return (strikeCount, ballCount)
 }
 
-func countBall(userNumber:Int) {
+func countBall(userNumber: Int, userBallCount: Int) -> Int {
+    var ballCount = userBallCount
     for computerNumber in computerRandomNumbers {
         if userNumber == computerNumber {
-            ball += 1
+            ballCount += 1
         }
     }
+    return ballCount
 }
 
+print(computerRandomNumbers)
 startGame()
