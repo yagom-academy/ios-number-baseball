9f4363a86b7d9c27b7db86ed4141253e4681765b
vayne77 <sky891@naver.com>
2022-02-10 21:23:27 +0900

    refactor: 네이밍 수정 및 stratGame() 함수 로직 개선

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -3,54 +3,58 @@ import Foundation
 var computerNumbers: [Int] = []
 var remainingTime: Int = 9
 
-func createDeduplicatedRadnomNumbers() -> [Int] {
-    var deduplicatedRandomNumbers: Set<Int> = []
+func createUniqueRadnomNumbers() -> [Int] {
+    var uniqueRandomNumbers: Set<Int> = []
     let numberOfDigits: Int = 3
     let randomOfRange: ClosedRange<Int> = 1...9
     
-    while deduplicatedRandomNumbers.count < numberOfDigits {
-        deduplicatedRandomNumbers.insert(Int.random(in: randomOfRange))
+    while uniqueRandomNumbers.count < numberOfDigits {
+        uniqueRandomNumbers.insert(Int.random(in: randomOfRange))
     }
-    return Array(deduplicatedRandomNumbers)
+    return Array(uniqueRandomNumbers)
 }
 
-func compare(computerNumber: [Int], playerNumber: [Int]) -> (Int, Int) {
+func compare(computerNumber: [Int], playerNumber: [Int]) -> (strikeCount: Int, ballCount: Int) {
     var strikeCount: Int = 0
     var ballCount: Int = 0
     let iterationOfRange: Range<Int> = 0..<3
     
     for index in iterationOfRange {
-        if computerNumbers[index] == playerNumber[index] {
+        if computerNumber[index] == playerNumber[index] {
             strikeCount += 1
-        } else if computerNumbers.contains(playerNumber[index]) {
+        } else if computerNumber.contains(playerNumber[index]) {
             ballCount += 1
         }
     }
-    return (strikeCount, ballCount)
+    return (strikeCount: strikeCount, ballCount: ballCount)
 }
 
 func printWinner(strikeCount: Int) {
     if strikeCount == 3 {
         print("사용자 승리!")
-        return
     } else if remainingTime == 0 {
         print("컴퓨터 승리...!")
     }
 }
 
 func startGame() {
+    computerNumbers = createUniqueRadnomNumbers()
+    
     repeat {
-        computerNumbers = createDeduplicatedRadnomNumbers()
-        let playerNumbers: [Int] = createDeduplicatedRadnomNumbers()
-        let strikeBallOfResult: (strikeCount: Int, ballCount: Int) = compare(computerNumber: computerNumbers, playerNumber: playerNumbers)
+        let playerNumbers: [Int] = createUniqueRadnomNumbers()
+        let strikeBallOfResult = compare(computerNumber: computerNumbers, playerNumber: playerNumbers)
         remainingTime -= 1
         
-        print("임의의 수 : \(computerNumbers[0]) \(computerNumbers[1]) \(computerNumbers[2])")
+        print("임의의 수 : \(playerNumbers[0]) \(playerNumbers[1]) \(playerNumbers[2])")
         
         printWinner(strikeCount: strikeBallOfResult.strikeCount)
         
         print("\(strikeBallOfResult.strikeCount) 스트라이크, \(strikeBallOfResult.ballCount) 볼")
         print("남은 기회 : \(remainingTime)")
+        
+        if strikeBallOfResult.strikeCount == 3 {
+            return
+        }
         
     } while remainingTime != .zero
 }
