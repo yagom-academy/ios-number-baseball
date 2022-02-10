67bcc307c2da3f53e072891541a06a4673ba33ae
dnwhd0112 <dnwhd0112@naver.com>
2022-02-10 14:00:03 +0900

    feat : 점수획득 함수 추가
    getScore 볼, 스트라이크판별 함수 추가

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -6,11 +6,32 @@
 
 import Foundation
 
-func createThreeRandomNumber() -> Set<Int> {
-    var returnSet = Set<Int>()
+func createThreeRandomNumbers() -> [Int] {
+    var returnNumbers: Int[] = []
     
-    while returnSet.count < 3 {
-        returnSet.insert(Int.random(in: 1...9))
+    while returnNumbers.count < 3 {
+        returnNumbers.insert(Int.random(in: 1...9))
     }
-    return returnSet
+    
+    return returnNumbers
+}
+
+func getScore(answerNumbers : Array<Int>, guessNumbers : Array<Int>) -> (strike: int, ball: int) {
+    var strike = 0
+    var ball = 0
+    
+    for answerIndex in 0..<answerNumbers.count {
+        for guessIndex in 0..<guessNumbers.count {
+            //스트라이크: 순서와 숫자가 모두 일치할때
+            if answerNumbers[answerIndex] == guessNumbers[guessIndex] && answerIndex == guessIndex {
+                strike += 1
+            }
+            //ball: 숫자는 일치하는대 순서는 다를때
+            else if answerNumbers[answerIndex] == guessNumbers[guessIndex] && answerIndex != guessIndex {
+                ball += 1
+            }
+        }
+    }
+    
+    return (strike, ball)
 }
