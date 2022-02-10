830b3cbd3a076a9c3644b09d827e79fa2cdd8701
GoMyungju171 <5211616@likelion.org>
2022-02-10 16:44:00 +0900

    refactor: strike, ball 을 strikeCount, ballCount 로 수정

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -34,17 +34,17 @@ func checkStrikeBall(answerNumbers: Array<Int>, randomNumbers: Array<Int>) -> (I
 }
 
 
-func printResult(randomNumbers: Array<Int>, strike: Int, ball: Int, tryCount: Int) {
+func printResult(randomNumbers: Array<Int>, strikeCount: Int, ballCount: Int, tryCount: Int) {
     print("임의의 수 : \(randomNumbers[0]) \(randomNumbers[1]) \(randomNumbers[2])")
-    print("\(strike) 스트라이크, \(ball) 볼")
+    print("\(strikeCount) 스트라이크, \(ballCount) 볼")
     print("남은 기회 : \(tryCount)")
 }
 
 
-func finishGame(strike: Int, tryCount: Int) -> Int {
+func finishGame(strikeCount: Int, tryCount: Int) -> Int {
     var endCount = tryCount
 
-    if strike == 3 {
+    if strikeCount == 3 {
         print("사용자 승리...!")
         endCount = 0
     } else if tryCount == 0 {
@@ -62,10 +62,10 @@ func startGame() {
     answerNumbers = changeArray()
     while tryCount != 0 {
         randomNumbers = changeArray()
-        let (strike, ball) = checkStrikeBall(answerNumbers: answerNumbers, randomNumbers: randomNumbers)
+        let (strikeCount, ballCount) = checkStrikeBall(answerNumbers: answerNumbers, randomNumbers: randomNumbers)
         tryCount -= 1
-        printResult(randomNumbers: randomNumbers, strike: strike, ball: ball, tryCount: tryCount)
-        tryCount = finishGame(strike: strike, tryCount: tryCount)
+        printResult(randomNumbers: randomNumbers, strikeCount: strikeCount, ballCount: ballCount, tryCount: tryCount)
+        tryCount = finishGame(strikeCount: strikeCount, tryCount: tryCount)
     }
 }
 
