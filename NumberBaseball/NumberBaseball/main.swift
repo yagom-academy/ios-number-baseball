de6af98c3ac67975b4e3bd716333748026e9cfe7
doogie <chris_97@naver.com>
2022-02-10 11:48:48 +0900

    feat: 임의의 숫자 추출 방법 변경, 승리 여부 확인 함수 추가 refactor: 네이밍 수정, 들여쓰기 2회 제한에 따른 코드 수정

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -2,59 +2,69 @@
 //  NumberBaseball - main.swift
 //  Created by Chuuny & Doogie.
 //  Copyright © yagom academy. All rights reserved.
-// 
+//
 
 import Foundation
 
-func makeRandomNumber() -> [String] {
-    var randomNumber: [String] = []
-    var numberList = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    numberList.shuffle()
-    for i in 1...3 {
-        randomNumber.append(String(numberList[i - 1]))
-    }
-    
-    return randomNumber
-}
-
-func checkNumber(_ userNumbers: [String]) -> (Int, Int) {
-    var strike = 0
-    var ball = 0
-    
-    for i in 0..<userNumbers.count {
-        if randomNumbers.contains(userNumbers[i]) {
-            ball += 1
-        }
-    }
-    
-    for i in 0..<userNumbers.count {
-        if userNumbers[i] == randomNumbers[i] {
-            strike += 1
-            ball -= 1
-        }
-    }
-    
-    return (strike, ball)
-}
-
-let randomNumbers = makeRandomNumber()
 var chance = 9
 
-func playGame() {
-    while chance > 0 {
-        let userNumber = makeRandomNumber()
-        print("임의의 수 : \(userNumber[0]) \(userNumber[1]) \(userNumber[2])")
-        print("\(checkNumber(userNumber).0) 스트라이크, \(checkNumber(userNumber).1) 볼")
+func makeRandomNumbers() -> [String] {
+    var randomNumbers: Set<String> = []
+
+    while randomNumbers.count < 3 {
+        let numbers = Int.random(in: 1...9)
+        randomNumbers.insert(String(numbers))
+    }
+
+    return Array(randomNumbers)
+}
+
+func calculateCounts(userNumbers: [String], randomNumbers: [String]) -> (strikeCounts: Int, ballCounts: Int) {
+    var strikeCounts = 0
+    var ballCounts = 0
+
+    for number in userNumbers {
+        ballCounts += randomNumbers.filter { $0.contains(number) }.count
+    }
+
+    for (index, number) in userNumbers.enumerated() {
+        strikeCounts += randomNumbers.enumerated().filter { $0.element == number && $0.offset == index }.count
+    }
+
+    ballCounts -= strikeCounts
+
+    return (strikeCounts, ballCounts)
+}
+
+func checkVictory(userNumbers: [String], randomNumbers: [String]) -> Bool {
+    if userNumbers == randomNumbers {
+        print("사용자 승리...!")
+        return true
+    }
+    else {
         chance -= 1
-        print("남은 기회 \(chance)")
-        
-        if userNumber == randomNumbers {
-            print("사용자 승리...!")
+    }
+
+    return false
+}
+
+func playGame() {
+    chance = 9
+    let randomNumbers = makeRandomNumbers()
+    while chance > 0 {
+        let userNumbers = makeRandomNumbers()
+
+        print("임의의 수 : \(userNumbers[0]) \(userNumbers[1]) \(userNumbers[2])")
+        if checkVictory(userNumbers: userNumbers, randomNumbers: randomNumbers) {
             return
         }
+        else if chance == 0 {
+            print("컴퓨터 승리...!")
+        }
+        print("\(calculateCounts(userNumbers: userNumbers, randomNumbers: randomNumbers).strikeCounts) 스트라이크, \(calculateCounts(userNumbers: userNumbers, randomNumbers: randomNumbers).ballCounts) 볼")
+
+        print("남은 기회 \(chance)")
     }
-    
-    print("컴퓨터 승리...!")
 }
 
 playGame()
