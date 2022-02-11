0cef00775f360eb17bfa4e7c60f9128f8a5dc074
kimkyunghun3 <kimkyunghun3@naver.com>
2022-02-11 11:53:45 +0900

    refactor: 함수 단위 쪼개기

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -9,45 +9,87 @@ import Foundation
 var computerRandomNumbers:[Int] = [0, 0, 0]
 var tryCount: Int = 9
 
+func menuChoice() {
+    var userNum: String?
+    while userNum != "2" {
+        print("1. 게임시작")
+        print("2. 게임종료")
+        print("원하는 기능을 선택해주세요 :", terminator: " ")
+        userNum = readLine()
+        if let userNum2 = userNum {
+            checkUserChoice(userNum: userNum2)
+        }
+    }
+}
+
+func checkUserChoice(userNum: String) {
+    if userNum == "1" {
+        startGame()
+    } else if userNum == "2" {
+        return
+    } else {
+        print("입력이 잘못 되었습니다.")
+    }
+}
+
 func startGame() {
     var userStrikeCount: Int = 0
     var userBallCount: Int = 0
     computerRandomNumbers = makeThreeRandomNumbers()
     tryCount = 9
     while tryCount > 0 && userStrikeCount < 3 {
+        let userFinalNumber = secondMenuChoice()
         userStrikeCount = 0
         userBallCount = 0
-        let userFinalNumber = secondMenuChoice()
-        (userStrikeCount, userBallCount) = checkBall(notStrikeNumbers: findNotStrike(userStrikeCount: userStrikeCount, userNumbers: userFinalNumber), userBallCount: userBallCount)
+        (userStrikeCount, userBallCount) = checkBall(notStrikeNumbers: checkStrike(userStrikeCount: userStrikeCount, userNumbers: userFinalNumber), userBallCount: userBallCount)
         tryCount -= 1
-        if userStrikeCount == 3 {
-            print("사용자 승리...!")
+        printResult(userStrikeCount: userStrikeCount, userBallCount: userBallCount)
+    }
+}
+
+func secondMenuChoice() -> [Int] {
+    var flag:Bool = true
+    var returnNumber: [Int] = []
+    while flag == true {
+        print("숫자 3개를 띄어쓰기로 구분하여 입력해주세요.")
+        print("중복 숫자는 허용하지 않습니다.")
+        print("입력 ", terminator: "")
+        let input1 = checkUserInput()
+        if checkUserNumberFormat(userInput: input1).isEmpty {
+        } else {
+            returnNumber = checkUserNumberFormat(userInput: input1)
+            flag = false
         }
-        if tryCount == 0 {
-            print("컴퓨터 승리...!")
-        }
-        print("\(userStrikeCount) 스트라이크, \(userBallCount) 볼")
-        print("남은 기회 : \(tryCount)")
+    }
+    return returnNumber
+}
+
+func printResult(userStrikeCount: Int, userBallCount: Int) {
+    print("\(userStrikeCount) 스트라이크, \(userBallCount) 볼")
+    print("남은 기회 : \(tryCount)")
+    if userStrikeCount == 3 {
+        print("사용자 승리...!")
+    }
+    if tryCount == 0 {
+        print("컴퓨터 승리...!")
     }
 }
 
 func makeThreeRandomNumbers() -> [Int] {
     var randomNumbers: Set<Int> = []
-    while(randomNumbers.count < 3) {
+    while randomNumbers.count < 3 {
         randomNumbers.insert(Int.random(in: 1...9))
     }
     return Array(randomNumbers)
 }
 
-func findNotStrike(userStrikeCount: Int, userNumbers: [Int]) -> ([Int], Int) {
+func checkStrike(userStrikeCount: Int, userNumbers: [Int]) -> ([Int], Int) {
     var notStrikeNumbers: [Int] = []
     var strikeCount = userStrikeCount
     for (userIndex, userNumber) in userNumbers.enumerated() {
         if let (notStrikeNumber, strikeCount2) = countStrike(userNumber: userNumber, userIndex: userIndex, userStrikeCount: strikeCount) {
             strikeCount = strikeCount2
-            if let notStrikeNum = notStrikeNumber {
-                notStrikeNumbers.append(notStrikeNum)
-            }
+            notStrikeNumbers = makeNotStrikeNumbers(notStrikeNumber: notStrikeNumber)
         }
     }
     return (notStrikeNumbers, strikeCount)
@@ -62,6 +104,14 @@ func countStrike(userNumber: Int, userIndex: Int, userStrikeCount: Int) -> (Int?
         }
     }
     return (userNumber, strikeCount)
+}
+
+func makeNotStrikeNumbers(notStrikeNumber: Int?) -> [Int] {
+    var notStrikeNumbers: [Int] = []
+    if let notStrikeNum = notStrikeNumber {
+        notStrikeNumbers.append(notStrikeNum)
+    }
+    return notStrikeNumbers
 }
 
 func checkBall(notStrikeNumbers: ([Int], Int), userBallCount: Int) -> (Int, Int) {
@@ -83,42 +133,6 @@ func countBall(userNumber: Int, userBallCount: Int) -> Int {
     return ballCount
 }
 
-func menuChoice() {
-    var userNum: String?
-    while userNum != "2" {
-        print("1. 게임시작")
-        print("2. 게임종료")
-        print("원하는 기능을 선택해주세요 :", terminator: " ")
-        userNum = readLine()
-        if let userNum2 = userNum {
-            if userNum2 == "1" {
-                startGame()
-            } else if userNum2 == "2" {
-                return
-            } else {
-                print("입력이 잘못 되었습니다.")
-            }
-        }
-    }
-}
-
-func secondMenuChoice() -> [Int] {
-    var flag:Bool = true
-    var returnNumber: [Int] = []
-    while flag == true {
-        print("숫자 3개를 띄어쓰기로 구분하여 입력해주세요.")
-        print("중복 숫자는 허용하지 않습니다.")
-        print("입력 ", terminator: "")
-        let input1 = checkUserInput()
-        if checkUserNumberFormat(userInput: input1).isEmpty {
-        } else {
-            returnNumber = checkUserNumberFormat(userInput: input1)
-            flag = false
-        }
-    }
-    return returnNumber
-}
-
 func checkUserInput() -> [String] {
     var userThreeNumbers: [String] = []
     guard let userNumbers = readLine() else {
@@ -132,36 +146,50 @@ func checkUserInput() -> [String] {
 }
 
 func checkUserNumberFormat(userInput: [String]) -> [Int] {
-    if userInput.isEmpty {
-        return []
-    }
-    if userInput.count != 5 {
-        return []
-    }
-    if userInput[1] != " " || userInput[3] != " " {
-        return []
-    }
-    let S: String = userInput.filter{ $0 != " " }.reduce("") { $0 + String($1) }
-    if S.count != 3 {
-        return []
-    }
-    guard let _ = Int(S) else {
+    if !isStringFormat(userInput: userInput),
+       !isThreeDigits(userInput: userInput) {
         return []
     }
     if Set(userInput).count != 4 {
         return []
     }
-    var result: [String] = []
-    for a in S {
-        result.append(String(a))
+    return makeIntArry(userInput: userInput)
+}
+
+func isStringFormat(userInput: [String]) -> Bool {
+    if userInput.isEmpty {
+        return false
     }
+    if userInput.count != 5 {
+        return false
+    }
+    if userInput[1] != " " || userInput[3] != " " {
+        return false
+    }
+    return true
+}
+
+func isThreeDigits(userInput: [String]) -> Bool {
+    let S: String = userInput.filter{ $0 != " " }.reduce("") { $0 + String($1) }
+    if S.count != 3 , S.contains("0") {
+        return false
+    }
+    guard let _ = Int(S) else {
+        return false
+    }
+    return true
+}
+
+func makeIntArry(userInput: [String]) -> [Int] {
     var result2: [Int] = []
-    for n in result {
-        if let num = Int((n)) {
-            if num == 0 {
-                return []
-            }
+    for n in userInput {
+        if n == " " {
+            return []
+        }
+        if let num = Int(String(n)) {
             result2.append(num)
+        } else {
+            return []
         }
     }
     return result2
