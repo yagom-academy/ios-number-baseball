d98051963ff8d18a358fe13c786ec93930963875
vayne77 <sky891@naver.com>
2022-02-13 15:28:33 +0900

    refactor: 코드 개행 개선 및 네이밍 수정

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -1,20 +1,23 @@
 import Foundation
 
-func createUniqueRadnomNumbers() -> [Int] {
+let wrongInputErrorMessage = "입력이 잘못되었습니다"
+
+func createUniqueRandomNumbers() -> [Int] {
     var uniqueRandomNumbers: Set<Int> = []
-    let numberOfDigits: Int = 3
-    let randomOfRange: ClosedRange<Int> = 1...9
+    let numberOfDigits = 3
+    let randomOfRange = 1...9
     
     while uniqueRandomNumbers.count < numberOfDigits {
         uniqueRandomNumbers.insert(Int.random(in: randomOfRange))
     }
+    
     return Array(uniqueRandomNumbers)
 }
 
 func compare(computerNumber: [Int], playerNumber: [Int]) -> (strikeCount: Int, ballCount: Int) {
-    var strikeCount: Int = 0
-    var ballCount: Int = 0
-    let iterationOfRange: Range<Int> = 0..<3
+    var strikeCount = 0
+    var ballCount = 0
+    let iterationOfRange = 0..<3
     
     for index in iterationOfRange {
         if computerNumber[index] == playerNumber[index] {
@@ -23,6 +26,7 @@ func compare(computerNumber: [Int], playerNumber: [Int]) -> (strikeCount: Int, b
             ballCount += 1
         }
     }
+    
     return (strikeCount: strikeCount, ballCount: ballCount)
 }
 
@@ -47,39 +51,39 @@ func inputPlayerNumbers() -> String? {
 }
 
 func checkInputAvailability() -> [Int] {
-    let numberOfRange: ClosedRange<Int> = 1...9
+    let numberOfRange = 1...9
     
     while true {
         guard let playerNumbers = inputPlayerNumbers(), playerNumbers.isEmpty == false else {
-            print("입력이 잘못되었습니다")
+            print(wrongInputErrorMessage)
             continue
         }
         
-        let playerInputNumberList: [String] = playerNumbers.components(separatedBy: " ")
+        let playerInputNumbers: [String] = playerNumbers.components(separatedBy: " ")
         
-        guard playerInputNumberList.count == 3 else {
-            print("입력이 잘못되었습니다")
+        guard playerInputNumbers.count == 3 else {
+            print(wrongInputErrorMessage)
             continue
         }
         
-        guard let fistInputNumber: Int = Int(playerInputNumberList[0]),
-              let secondInputNumber: Int = Int(playerInputNumberList[1]),
-              let thirdInputNumber: Int = Int(playerInputNumberList[2]) else {
-                  print("입력이 잘못되었습니다")
+        guard let fistInputNumber = Int(playerInputNumbers[0]),
+              let secondInputNumber = Int(playerInputNumbers[1]),
+              let thirdInputNumber = Int(playerInputNumbers[2]) else {
+                  print(wrongInputErrorMessage)
                   continue
               }
         
         guard (numberOfRange).contains(fistInputNumber),
               (numberOfRange).contains(secondInputNumber),
               (numberOfRange).contains(thirdInputNumber) else {
-                  print("입력이 잘못되었습니다")
+                  print(wrongInputErrorMessage)
                   continue
               }
         
-        guard playerInputNumberList[0] != playerInputNumberList[1],
-              playerInputNumberList[0] != playerInputNumberList[2],
-              playerInputNumberList[1] != playerInputNumberList[2] else {
-                  print("입력이 잘못되었습니다")
+        guard playerInputNumbers[0] != playerInputNumbers[1],
+              playerInputNumbers[0] != playerInputNumbers[2],
+              playerInputNumbers[1] != playerInputNumbers[2] else {
+                  print(wrongInputErrorMessage)
                   continue
               }
         
@@ -89,9 +93,9 @@ func checkInputAvailability() -> [Int] {
 
 
 func startProgram() {
-    var isgameOver: Bool = false
+    var isGameOver = false
     
-    while isgameOver == false {
+    while isGameOver == false {
         print("1.게임시작\n2.게임종료\n원하는 기능을 선택해주세요 : ", terminator: "")
         
         let playerInput = readLine()
@@ -100,32 +104,31 @@ func startProgram() {
         case "1":
             startGame()
         case "2":
-            isgameOver = true
+            isGameOver = true
         default:
-            print("입력이 잘못되었습니다")
+            print(wrongInputErrorMessage)
         }
     }
 }
 
 func startGame() {
-    let computerNumbers: [Int] = createUniqueRadnomNumbers()
-    var remainingTime: Int = 9
+    let computerNumbers: [Int] = createUniqueRandomNumbers()
+    var remainingTime = 9
     
     repeat {
-        let checkedPlayerInputNumberList: [Int] = checkInputAvailability()
+        let checkedPlayerInputNumbers: [Int] = checkInputAvailability()
         
-        let strikeBallOfResult = compare(computerNumber: computerNumbers, playerNumber: checkedPlayerInputNumberList)
+        let gameResult = compare(computerNumber: computerNumbers, playerNumber: checkedPlayerInputNumbers)
         remainingTime -= 1
         
-        printWinner(strikeCount: strikeBallOfResult.strikeCount, remainingTime: remainingTime)
+        printWinner(strikeCount: gameResult.strikeCount, remainingTime: remainingTime)
         
-        print("\(strikeBallOfResult.strikeCount) 스트라이크, \(strikeBallOfResult.ballCount) 볼")
+        print("\(gameResult.strikeCount) 스트라이크, \(gameResult.ballCount) 볼")
         print("남은 기회 : \(remainingTime)")
         
-        if strikeBallOfResult.strikeCount == 3 {
+        if gameResult.strikeCount == 3 {
             return
         }
-        
     } while remainingTime != .zero
 }
 
