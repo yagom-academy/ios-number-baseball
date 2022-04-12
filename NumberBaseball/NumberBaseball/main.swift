4459ab9d94cee67e8adeff64c13563e8fa578521
unchain123 <unchain123@kakao.com>
2022-04-12 16:06:23 +0900

    feat: make function dicide

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -8,7 +8,6 @@ import Foundation
 
 var initialComputerInput: Set<Int> = Set<Int>()
 var verifiedComputerArray: Array<Int> = Array<Int>()
-var initalUserInput: Set<Int> = Set<Int>()
 var verifiedUserArray: Array<Int> = Array<Int>()
 
 var remainingChance = 9
@@ -22,6 +21,7 @@ func generateRandomNumbersForComputer() {
 }
 
 func generateRandomNumbersForUser() {
+    var initalUserInput: Set<Int> = Set<Int>()
     while initalUserInput.count < 3 {
         let number: Int = Int.random(in: 1...9)
         initalUserInput.insert(number)
@@ -29,4 +29,23 @@ func generateRandomNumbersForUser() {
     verifiedUserArray = Array(initalUserInput)
 }
 
+func decide() {
+    var strikeCount = 0
+    var ballCount = 0
+    for element in 0...2 {
+        if verifiedComputerArray[element] == verifiedUserArray[element] {
+            strikeCount += 1
+            continue
+        }
+        if verifiedUserArray.contains(verifiedComputerArray[element]) {
+            ballCount += 1
+        }
+    }
+    
+    if strikeCount == 3 {
+        print("\(strikeCount) 스트라이크, \(ballCount) 볼 - 사용자 승리!")
+    } else {
+        print("\(strikeCount) 스트라이크, \(ballCount) 볼")
+    }
+}
 
