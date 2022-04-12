476601db5b514cc338e27fe284b81b04c8b35434
iilim <42.4.ilim@gmail.com>
2022-04-13 02:12:05 +0900

    feat: step1 기능 완성

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -6,26 +6,7 @@
 
 import Foundation
 
-
-
-/*
- 0. 게임 시작
- 1. 길이가 3인 정수 배열 선언, 난수 카운터
- 2. 시행횟수 선언 후 9 할당
- 3. 난수 생성후 정수 배열에서 검색
-     1. 같은 원소 있으면 다시 난수 생성
-     2. 없으면 추가 후 난수 카운터 1증가
- 4. 난수 카운터가 3이 아닌 경우 다시 난수 생성으로
- 5. 임의의 수 생성 후 배열과 비교
- 6. 배열과 일치하는게 있으면 일단 ball, 순서까지 동일하면 strike, 하나도 일치하지 않으면 nothing
- 7. strike 갯수 알아보고 3이면 플레이어 승 아니면 시행횟수 0 될때까지 게임 다시 진행
- 8. 시행 횟수 0에 도달 하면 컴퓨터 승
- 11. 게임 종료
- */
-
-
-var randomNumbers = [Int]()
-var index = 0
+var computerRandomNumbers = [Int]()
 var totalTrialNumber = 9
 
 func makeRandomNumbers() -> Array<Int> {
@@ -33,26 +14,45 @@ func makeRandomNumbers() -> Array<Int> {
     while randomNumbers.count < 3 {
         randomNumbers.insert(Int.random(in: 1...9))
     }
-    
     return Array(randomNumbers)
 }
 
-
-func checkStrikeBall() -> (Int, Int) {
+func checkStrikeBall(numbers: Array<Int>) -> (Int, Int) {
     var strike = 0
     var ball = 0
-    let numbers = makeRandomNumbers()
     
     for numberindex in (0...numbers.count - 1) {
-        if numbers.contains(numbers[numberindex]) && index == numberindex {
+        if computerRandomNumbers.contains(numbers[numberindex]) && computerRandomNumbers[numberindex] == numbers[numberindex] {
             strike += 1
-        } else if numbers.contains(numbers[numberindex]) && index != numberindex{
+        } else if computerRandomNumbers.contains(numbers[numberindex]) && computerRandomNumbers[numberindex] != numbers[numberindex] {
             ball += 1
         }
-        
-        index += 1
     }
-    
     return (strike, ball)
 }
+
+func printResult(result : (Int, Int)) {
+    print("\(result.0) 스트라이크, \(result.1) 볼")
+    if result.0 == 3 {
+        print("사용자 승리")
+        return
+    } else {
+        print("남은 기회 : \(totalTrialNumber)")
+    }
+    if totalTrialNumber == 0 {
+        print("컴퓨터 승리")
+    }
+}
+
+func startGame() {
+    computerRandomNumbers = makeRandomNumbers()
+    while totalTrialNumber > 0 {
+        let numbers = makeRandomNumbers()
+        print("임의의 수 : \(numbers)")
+        totalTrialNumber -= 1
+        printResult(result: checkStrikeBall(numbers: numbers))
+    }
+}
+
+startGame()
 
