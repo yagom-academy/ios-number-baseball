8825e44e0ba6b321f4c38f898d7dc717947325e9
Judy <joo00ee@naver.com>
2022-04-12 21:43:41 +0900

    feat: complete STEP1

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -16,18 +16,15 @@ var randomNumberAnswer: Array<Int> = {
     return Array(result)
 }()
 
-func checkStrikeAndBall(_ userStringInput: Array<String>) -> [Int]{
+func checkStrikeAndBall(_ randomNumbers: Array<Int>) -> [Int] {
     var countList: [Int] = [0,0]
-    var userInput = userStringInput.map({Int($0)})
+    var randomInput = randomNumbers
     for index in 0...2{
-        if userInput[index] == randomNumberAnswer[index]{
+        if randomNumbers[index] == randomNumberAnswer[index]{
             countList[0] += 1
-            userInput[index] = 0
+            randomInput[index] = 0
         }
-    }
-    
-    for index in 0...2{
-        if randomNumberAnswer.contains(userInput[index] ?? 0){
+        if randomNumberAnswer.contains(randomInput[index]) {
             countList[1] += 1
         }
     }
@@ -35,21 +32,41 @@ func checkStrikeAndBall(_ userStringInput: Array<String>) -> [Int]{
     return countList
 }
 
-while !isEnd{
+func createRandomNumbers() -> [Int] {
+    var result = Set<Int>()
+    while result.count < 3{
+        result.insert(Int.random(in: 1...9))
+    }
+    return Array(result)
+}
+
+func printWinner(_ winner: String){
+    isEnd = true
+    print("\n\(winner) 승리...!", terminator: " ")
+}
+
+while !isEnd {
     print("임의의 수 :", terminator: " ")
-    guard var userInput = readLine()?.components(separatedBy: " ") else{
-        break
-    }
-    let countList: [Int] = checkStrikeAndBall(userInput)
-    if countList[0] == 3 {
-        print("유저 승리..!")
-        isEnd = true
-    }else{
-        
-    }
-    //출력코드
-    break
+    let randomNumbers = createRandomNumbers()
     
+    for randomNumber in randomNumbers{
+        print(randomNumber, terminator: " ")
+    }
+    
+    let countList: [Int] = checkStrikeAndBall(randomNumbers)
+    
+    if countList[0] == 3 {
+        printWinner("유저")
+    }
+    
+    challenge -= 1
+    
+    if challenge == 0 {
+        printWinner("컴퓨터")
+    }
+   
+    print("\n\(countList[0]) 스트라이크, \(countList[1]) 볼")
+    print("남은 기회 : \(challenge)")
 }
 
 
