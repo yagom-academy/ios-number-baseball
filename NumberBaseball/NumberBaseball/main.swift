4be79d58759a151f3f0eed3576f3ce6ad93127c2
baard <schy2503@naver.com>
2022-04-12 18:01:15 +0900

    feat: 스트라이크 및 볼 카운트하는 함수 생성

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -12,6 +12,28 @@ func generateNumbers() -> Set<Int> {
     while randomNumber.count < 3 {
         randomNumber.insert(Int.random(in: 1...9))
     }
-    
     return randomNumber
 }
+
+func checkStrikeOrBall( answerNumber: Array<Int>, count: Int) {
+    let suggestNumber = Array<Int>(generateNumbers())
+    var strikeCount = 0
+    var ballCount = 0
+    
+    if answerNumber[0] == suggestNumber[0] {
+        strikeCount += 1
+    } else if suggestNumber.contains(answerNumber[0]) {
+        ballCount += 1
+    }
+    if answerNumber[1] == suggestNumber[1] {
+        strikeCount += 1
+    } else if suggestNumber.contains(answerNumber[1]) {
+        ballCount += 1
+    }
+    if answerNumber[2] == suggestNumber[2] {
+        strikeCount += 1
+    } else if suggestNumber.contains(answerNumber[2]) {
+        ballCount += 1
+    }
+}
+
