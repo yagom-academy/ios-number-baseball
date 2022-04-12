6b85ef83e88c8da9eec85dbd31a2fb1861f53b63
OldStar <tyr2293@naver.com>
2022-04-12 21:27:33 +0900

    FEAT: 컴퓨터 초기값 설정 및 사용자 입력값 반환 함수 작성

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -6,5 +6,23 @@
 
 import Foundation
 
-print("Hello, World!")
+var computerNumber = Set<Int>()
 
+while computerNumber.count < 3 {
+    computerNumber.insert(Int.random(in: 1...9))
+}
+
+var remainingNumber = 9
+var userNumber = Set<Int>()
+
+func makeRandomNumber() -> Set<Int> {
+    while userNumber.count < 3 {
+        userNumber.insert(Int.random(in: 1...9))
+    }
+    
+    return userNumber
+}
+
+makeRandomNumber()
+let userNumberList = Array(userNumber)
+let computerNumberList = Array(computerNumber)
