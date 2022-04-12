e18310989dd22f55b6272d2d67b84652918bf6da
finnn1 <tgss3535@gmail.com>
2022-04-12 19:44:40 +0900

    feat, fix: 랜덤한 숫자 생성하는 함수 추가, 전역변수 타입 수정

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -7,15 +7,27 @@ import Foundation
 
 func startGame() {
     
-    var threeRandomNumbers: Int
+    var threeRandomNumbers: [Int]
     var chance = 9
     var result: String
+    
+    threeRandomNumbers = peakThreeRandomNumbers()
+    
 }
 
-
-
-
-
-
-
-
+func peakThreeRandomNumbers() -> [Int] {
+    var list: [Int] = []
+    
+    while true {
+        list.append(Int.random(in: 1...9))
+        
+        list = Array(Set(list))
+        
+        if list.count == 3 {
+            print(list)
+            break
+        }
+    }
+    
+    return list
+}
