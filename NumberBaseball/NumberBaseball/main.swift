d4eab907b417a25bb1fdd9eb428be36fc47c847a
박서현 <psh@bagseohyeon-ui-MacBook-Pro.local>
2022-04-12 20:36:18 +0900

    feat:난수 생성 및 중복 숫자 검색하는 함수 구현

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -6,5 +6,32 @@
 
 import Foundation
 
-print("Hello, World!")
 
+
+/*
+ 0. 게임 시작
+ 1. 길이가 3인 정수 배열 선언, 난수 카운터
+ 2. 시행횟수 선언 후 9 할당
+ 3. 난수 생성후 정수 배열에서 검색
+     1. 같은 원소 있으면 다시 난수 생성
+     2. 없으면 추가 후 난수 카운터 1증가
+ 4. 난수 카운터가 3이 아닌 경우 다시 난수 생성으로
+ 5. 임의의 수 생성 후 배열과 비교
+ 6. 배열과 일치하는게 있으면 일단 ball, 순서까지 동일하면 strike, 하나도 일치하지 않으면 nothing
+ 7. strike 갯수 알아보고 3이면 플레이어 승 아니면 시행횟수 0 될때까지 게임 다시 진행
+ 8. 시행 횟수 0에 도달 하면 컴퓨터 승
+ 11. 게임 종료
+ */
+
+
+var randomNumbers = [Int]()
+var randomNumberCounter = 0
+var totalTrialNumber = 9
+
+
+func searchDuplicatedNumber(randomNumbers: Array<Int>) {
+    for _ in (0...randomNumbers.count - 1) {
+        
+    }
+
+}
