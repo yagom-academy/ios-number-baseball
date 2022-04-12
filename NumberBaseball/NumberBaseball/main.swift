c56d941d5717e6a8ede43b2769b99bdca7b07625
unchain123 <unchain123@kakao.com>
2022-04-12 14:31:59 +0900

    feat : make function generateRandomNumbers

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -6,5 +6,16 @@
 
 import Foundation
 
-print("Hello, World!")
+var initialComputerInput: Set<Int> = Set<Int>()
+var verifiedComputerArray: Array<Int> = Array<Int>()
+var initalUserInput: Set<Int> = Set<Int>()
+var verifiedUserArray: Array<Int> = Array<Int>()
+
+func generateRandomNumbers() {
+    while initialComputerInput.count < 3 {
+        let number: Int = Int.random(in: 1...9)
+        initialComputerInput.insert(number)
+    }
+    verifiedComputerArray = Array(initialComputerInput)
+}
 
