c0c402c0d9a4f237053d2f06633978630e7642e4
starting1231 <starting1231@naver.com>
2022-04-13 09:56:42 +0900

    refactor: 함수안에 있는 공백제거, 전역변수 userNumber 제거, 변수위치 조정, compareNumber함수 안에 있는 numberBaseball() 제거
    - numberBaseball 함수는 사용자의 입력을 받기 때문에 제출을 위해 제거함.

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -5,26 +5,19 @@
 // 
 
 var randomArray: Array<Int> = []
+var inputArray: Array<Int> = []
 var count: Int = 9
-
-func insertRandomNumber() -> Set<Int> {
-    
-    var randomSet: Set<Int> = []
-    
-    while randomSet.count != 3 {
-        randomSet.insert(Int.random(in: 1...9))
-    }
-    
-    return randomSet
-}
-
-
-
 var strike: Int = 0
 var ball: Int = 0
 
-var inputArray: Array<Int> = []
-var userNumber: Array<Int?> = []
+
+func insertRandomNumber() -> Set<Int> {
+    var randomSet: Set<Int> = []
+    while randomSet.count != 3 {
+        randomSet.insert(Int.random(in: 1...9))
+    }
+    return randomSet
+}
 
 func numberBaseball() {
     let userNumber = readLine()?.split(separator: " ").map {Int($0) ?? 0}
@@ -34,32 +27,24 @@ func numberBaseball() {
 }
 
 func compareNumber() {
-    
     while strike != 3 && count != 0 {
-        
         strike = 0
         count -= 1
         print("임의의 수 : ", terminator: "")
-        numberBaseball()
-        
         for number in 0...2 {
             if randomArray[number] == inputArray[number] {
                 strike += 1
             }
         }
-        
         let intersectionOfArrays = Set(randomArray).intersection(inputArray)
         ball = intersectionOfArrays.count - strike
-        
         if strike == 3 {
             print("사용자 승리...!")
         } else if count == 0 {
             print("컴퓨터 승리...!")
         }
-        
         print("\(strike) 스트라이크, \(ball) 볼")
         print("남은 기회 : \(count)")
-        
     }
 }
 
