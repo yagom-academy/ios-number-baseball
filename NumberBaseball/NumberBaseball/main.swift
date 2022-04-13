2e1ec6fc2ba707aba48034597b2449d1ee54bf82
bard <schy2503@naver.com>
2022-04-13 21:42:20 +0900

    refactor: magic number,  옵셔널 값,  중복된 코드 제거, 스트라이크 및 볼 카운트 if문 , 결과 출력 코드 위치 수정

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -8,44 +8,41 @@ var repeatCheck = true
 
 func generateThreeNonOverlappingRandomNumbers() -> Set<Int> {
     var randomNumbers = Set<Int>()
+    let countNumber = 3
+    let randomNumberRange = 1...9
     
-    while randomNumbers.count < 3 {
-        randomNumbers.insert(Int.random(in: 1...9))
+    while randomNumbers.count < countNumber {
+        randomNumbers.insert(Int.random(in: randomNumberRange))
     }
     return randomNumbers
 }
 
 func countStrikeOrBall(answerNumber: Array<Int>, tryCount: Int) -> [String : Int] {
     let suggestNumber = Array<Int>(generateThreeNonOverlappingRandomNumbers())
+    let arrayRange = 0...2
     var strikeCount = 0
     var ballCount = 0
+    var StringSuggestNumber: String = String()
     
-    if answerNumber[0] == suggestNumber[0] {
-        strikeCount += 1
-    } else if suggestNumber.contains(answerNumber[0]) {
-        ballCount += 1
+    for arrayLocation in arrayRange {
+        StringSuggestNumber += String(suggestNumber[arrayLocation])
     }
-    if answerNumber[1] == suggestNumber[1] {
-        strikeCount += 1
-    } else if suggestNumber.contains(answerNumber[1]) {
-        ballCount += 1
-    }
-    if answerNumber[2] == suggestNumber[2] {
-        strikeCount += 1
-    } else if suggestNumber.contains(answerNumber[2]) {
-        ballCount += 1
+    StringSuggestNumber.insert(" ", at: StringSuggestNumber.index(StringSuggestNumber.startIndex, offsetBy: 1))
+    StringSuggestNumber.insert(" ", at: StringSuggestNumber.index(StringSuggestNumber.startIndex, offsetBy: 3))
+    
+    for arrayLocation in arrayRange {
+        if answerNumber[arrayLocation] == suggestNumber[arrayLocation] {
+            strikeCount += 1
+        } else if suggestNumber.contains(answerNumber[arrayLocation]) {
+            ballCount += 1
+        }
     }
     
     print("""
-          임의의 수 : \(suggestNumber)
+          임의의 수 : \(StringSuggestNumber)
           \(strikeCount) 스트라이크, \(ballCount) 볼
           남은기회 : \(tryCount)
           """)
-    if strikeCount == 3 {
-        print("사용자 승리...!")
-    }else if tryCount == 0 {
-        print("컴퓨터 승리...!")
-    }
     
     var strikeBallCount: [String: Int] = [:]
     strikeBallCount["strike"] = strikeCount
@@ -55,22 +52,26 @@ func countStrikeOrBall(answerNumber: Array<Int>, tryCount: Int) -> [String : Int
 }
 
 func gameStart() {
-    var answerNumber = Set<Int>()
+    var answerNumber = Array<Int>()
     var tryCount = 9
     
-    while answerNumber.count < 3 {
-        answerNumber.insert(Int.random(in: 1...9))
-    }
+    answerNumber = Array<Int>(generateThreeNonOverlappingRandomNumbers())
+    
     while repeatCheck {
         tryCount -= 1
-        let strikeBallCount = countStrikeOrBall(answerNumber: Array<Int>(answerNumber), tryCount: tryCount)
+        let strikeBallCount = countStrikeOrBall(answerNumber: answerNumber, tryCount: tryCount)
         checkGameOver(strikeTryCount: strikeBallCount)
     }
 }
 
 func checkGameOver(strikeTryCount: [String : Int]) {
-    if strikeTryCount["strike"] == 3 || strikeTryCount["trycount"] ?? 0 < 1{
+    if strikeTryCount["strike"] == 3 || strikeTryCount["trycount"] == 0 {
         repeatCheck = false
+    }
+    if strikeTryCount["strike"] == 3 {
+        print("사용자 승리...!")
+    } else if strikeTryCount["trycount"] == 0 {
+        print("컴퓨터 승리...!")
     }
 }
 
