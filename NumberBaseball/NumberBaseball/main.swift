6252da52a92cec1ab09ea5bf0ab1ffef92812fb4
Hugh-github <tnghk4822@gmail.com>
2022-04-13 12:09:28 +0900

    refactor: 중복되지 않는 임의의 수 3개를 추출하기 위한 숫자의 범위를 제공하는 상수 추가(기존의 파라미터 대체 및 기존 전역변수 제거)

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -6,7 +6,6 @@
 
 import Foundation
 
-var candidateNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 let totalAnswerNumbers = 3
 
@@ -14,9 +13,10 @@ var answerNumbers: [Int] = []
 
 var remainingOpportunity = 9
 
-func makeRandomNumbers(in numbers: [Int]) -> [Int] {
+func makeRandomNumbers() -> [Int] {
+    let candidateNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     var result: [Int] = []
-    result = numbers.shuffled()
+    result = candidateNumbers.shuffled()
     result.removeSubrange(0...result.count - totalAnswerNumbers - 1)
     return result
 }
