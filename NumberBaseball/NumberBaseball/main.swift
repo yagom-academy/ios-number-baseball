7d43fe829b7827d6d8aa70254fc7ec81d8e7b0f6
웡빙 <wb277@naver.com>
2022-04-14 18:12:06 +0900

    refactor: 변수 네이밍 수정, feat: 메서드 추가

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -14,10 +14,10 @@ func startGame() {
     var ballScore: Int
     var isSuccess: Bool = false
     
-    // computerThreeNumbers = makingThreeRandomNumbers()
+    computerThreeNumbers = generatedRandomNumbers()
     
     while isSuccess == false && givenChance > 0 {
-    
+        
         userThreeNumbers = generatedRandomNumbers()
         
         resultOfStrikeAndBall = calculateResult(computer: computerThreeNumbers, user: userThreeNumbers)
@@ -27,7 +27,7 @@ func startGame() {
         print("임의의 수 : \(userThreeNumbers[0]) \(userThreeNumbers[1]) \(userThreeNumbers[2])")
         print("\(strikeScore) 스트라이크, \(ballScore) 볼")
         
-        isSuccess = checkStrike(strikeScore, 3)
+        isSuccess = checkStrike(strikeScore)
         givenChance -= 1
         print("남은 기회 : \(givenChance)")
         
@@ -40,45 +40,48 @@ func startGame() {
     if givenChance == .zero {
         print("컴퓨터 승리...!")
     }
-
 }
 
 func generatedRandomNumbers(range: ClosedRange<Int> = 1...9, count: Int = 3) -> [Int] {
+    
     var list: [Int] = []
     
-    while true {
+    while list.count != count {
+        
         list.append(Int.random(in: range))
-        
         list = Array(Set(list))
-        
-        if list.count == count { break }
     }
     
     return list
 }
 
 func calculateResult(computer: [Int], user: [Int]) -> [Int] {
-    
-    var resultStrikeAndBall: [Int] = [0, 0]
 
+    var resultStrikeAndBall: [Int] = [0, 0]
+    
     for userIndex in 0...2 {
         guard let computerIndex = computer.firstIndex(of: user[userIndex]) else { continue }
-        
-        if computerIndex == userIndex {
-            resultStrikeAndBall[0] += 1
-        } else {
-            resultStrikeAndBall[1] += 1
-        }
+        addScore(to: &resultStrikeAndBall, computer: computerIndex, user: userIndex)
     }
     
     return resultStrikeAndBall
 }
 
-func checkStrike(_ strike: Int, strikeCount: Int) -> Bool {
+func checkStrike(_ strike: Int, _ strikeCount: Int = 3) -> Bool {
+    
     if strike == strikeCount {
         return true
     } else {
         return false
+    }
+}
+
+func addScore(to resultStrikeAndBall: inout [Int], computer: Array<Int>.Index, user: Array<Int>.Index) {
+    
+    if computer == user {
+        resultStrikeAndBall[0] += 1
+    } else {
+        resultStrikeAndBall[1] += 1
     }
 }
 
