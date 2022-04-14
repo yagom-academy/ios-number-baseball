a3ceafc40fcd28989223a8b0ad627c6a77225488
ic_6_user_28 <toskw@naver.com>
2022-04-14 17:27:02 +0900

    chore: 띄어쓰기 및 오타수정

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -23,7 +23,7 @@ func makeRandomThreeNumbers() -> [String] {
 func printMenu() {
     print("1. 게임시작")
     print("2. 게임종료")
-    print("원하는 기능을 선택해주세요:",terminator: " ")
+    print("원하는 기능을 선택해주세요 :",terminator: " ")
     choiceMenu()
 }
 
@@ -40,15 +40,15 @@ func selectMenu(menuNumber: String) -> () {
         gameStart()
     case "2":
         break
-    default: print("입력이 잘못되었습니다")
+    default: print("입력이 잘못되었습니다.")
         printMenu()
     }
 }
 
 func inputUserNumbers() {
-    print("숫자 3개를 띄어쓰기로 구분하여 입력해주세요")
-    print("중복숫자는 허용하지 않습니다")
-    print("입력:",terminator: " ")
+    print("숫자 3개를 띄어쓰기로 구분하여 입력해주세요.")
+    print("중복숫자는 허용하지 않습니다.")
+    print("입력 :",terminator: " ")
     let userinputNumbers = readLine()?.components(separatedBy: " ")
     if let userNumbers = userinputNumbers {
         printResult(userInput: userNumbers)
@@ -70,7 +70,7 @@ func printResult(userInput: [String]) {
 
 func printLife() {
     if userLife > 0 {
-        print("남은기회: \(userLife)")
+        print("남은기회 : \(userLife)")
     }
 }
 
