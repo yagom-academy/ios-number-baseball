27ed846a0635b036a7712d155f46a3ac9451cfd6
Judy <joo00ee@naver.com>
2022-04-14 18:32:52 +0900

    refactor: split nested statements

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -39,21 +39,29 @@ func createRandomNumbers() -> [Int] {
     return Array(result)
 }
 
-func getUserInput(){
-    while true{
-        guard let userInputNumber = readLine()?.components(separatedBy: " ") else {
-            print("입력이 잘못되었습니다\n숫자 3개를 띄어쓰기로 구분하여 입력해주세요.\n중복 숫자는 허용하지 않습니다.")
-            continue
-        }
-        
-        if !(validateUserInput(userInput: userInputNumber).isEmpty){
-            userInput = validateUserInput(userInput: userInputNumber)
-            break
-        }
+func checkUserInput() -> Bool {
+    guard let userInputNumber = readLine()?.components(separatedBy: " ") else {
+        print("입력이 잘못되었습니다\n숫자 3개를 띄어쓰기로 구분하여 입력해주세요.\n중복 숫자는 허용하지 않습니다.")
+        return true
+    }
+    
+    if !(validateUserInput(userInput: userInputNumber).isEmpty) {
+        userInput = validateUserInput(userInput: userInputNumber)
+        return false
+    } else {
+        return true
     }
 }
 
-func validateUserInput(userInput Input: [String]) -> [Int]{
+func getUserInput() {
+    var isUserInputValid = true
+    while isUserInputValid {
+        print("입력 :", terminator: " ")
+        isUserInputValid = checkUserInput()
+    }
+}
+
+func validateUserInput(userInput Input: [String]) -> [Int] {
     var checkOverlapSet = Set<String>()
     var stringCount: Int = 0
     
@@ -73,67 +81,60 @@ func validateUserInput(userInput Input: [String]) -> [Int]{
         print("입력이 잘못되었습니다\n숫자 3개를 띄어쓰기로 구분하여 입력해주세요.\n중복 숫자는 허용하지 않습니다.")
         return []
     }
-    /*
-     @숫자를 잘못 입력하거나 범위를 벗어난 경우,
-     @숫자가 중복된 경우 -> set에 넣고 count == 3
-     @갯수가 잘못된 경우, 띄어쓰기 안했을 때도 -> count == 3
-     @숫자가 아닌 값 -> Int 변환이 안되면
-     이 경우 게임을 진행할 수 있는 횟수를 차감하지 않습니다.
-     */
-    //validation Code
-    return intTypeInput
-}
 
-func printRandomNumber(_ numbers: [Int]){
-    for number in numbers {
-        print(number, terminator: " ")
-    }
+    return intTypeInput
 }
 
 func printWinner(_ winner: String) {
     isEnd = true
-    print("\n\(winner) 승리...!")
+    print("\(winner) 승리...!")
 }
 
+func checkWinner(strikeCount: Int) {
+    if strikeCount == 3 {
+        printWinner("유저")
+        return
+    }
 
+    challenge -= 1
+    if challenge == 0 {
+        printWinner("컴퓨터")
+    }
+}
 
-func playGame(){
+func playGame() {
     randomNumberAnswer = createRandomNumbers()
     while !isEnd {
-        print("임의의 수 :", terminator: " ")
-        
         getUserInput()
-        printRandomNumber(userInput)
 
         let strikeCount: Int = checkStrikeCount()
         let ballCount: Int = checkBallCount()
         
-        if strikeCount == 3 {
-            printWinner("유저")
-            break
-        }
+        checkWinner(strikeCount: strikeCount)
         
-        challenge -= 1
-        if challenge == 0 {
-            printWinner("컴퓨터")
-        }
-        
-        print("\n\(strikeCount) 스트라이크, \(ballCount) 볼 \n남은 기회 : \(challenge)")
+        print("\(strikeCount) 스트라이크, \(ballCount) 볼 \n남은 기회 : \(challenge)")
+    }
+}
+
+func getMeneInput() -> Bool {
+    let menuInput = readLine()
+
+    if menuInput == "1" {
+        playGame()
+        return true
+    } else if menuInput == "2" {
+        return false
+    } else {
+        print("입력이 잘못되었습니다.")
+        return true
     }
 }
 
 func startGame() {
-    while true {
-        print("1. 게임시작 \n2. 게임 종료 \n원하는 기능을 선택해주세요 : ",terminator: "")
-        
-        let menuInput = readLine()
-        if menuInput == "1" {
-            playGame()
-        } else if menuInput == "2" {
-            break
-        } else {
-            print("입력이 잘못되었습니다.")
-        }
+    var canPlay = true
+    while canPlay {
+        print("1. 게임시작 \n2. 게임 종료 \n원하는 기능을 선택해주세요 : ", terminator: "")
+        canPlay = getMeneInput()
     }
 }
 
