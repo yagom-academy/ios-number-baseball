1d5385e6b8359df2046444bc3efa7bd5e2977159
yusw10 <yusw10@naver.com>
2022-04-14 15:44:37 +0900

    feat : separate startgame about playgame and startGame for mainMenu

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -8,15 +8,15 @@ import Foundation
 
 var challenge: Int = 9
 var isEnd: Bool = false
-var randomComputerInput: [Int] = []
+var userInput: [Int] = []
 var randomNumberAnswer: [Int] = []
 
 func checkStrikeCount() -> Int {
     var strikeCount = 0
     
-    for index in 0...2 where randomComputerInput[index] == randomNumberAnswer[index] {
+    for index in 0...2 where userInput[index] == randomNumberAnswer[index] {
             strikeCount += 1
-        randomComputerInput[index] = 0
+        userInput[index] = 0
     }
     
     return strikeCount
@@ -24,7 +24,7 @@ func checkStrikeCount() -> Int {
 
 func checkBallCount() -> Int {
     var ballCount = 0
-    for index in 0...2 where randomNumberAnswer.contains(randomComputerInput[index]) {
+    for index in 0...2 where randomNumberAnswer.contains(userInput[index]) {
         ballCount += 1
     }
     return ballCount
@@ -39,8 +39,28 @@ func createRandomNumbers() -> [Int] {
     return Array(result)
 }
 
+func getUserInput() -> [Int] {
+    return [1,2,3]
+}
+
+func validateUserInput(userInput Input: [Int]) -> Bool{
+    /*
+     숫자를 잘못 입력하거나 범위를 벗어난 경우,
+     갯수가 잘못된 경우,
+     숫자가 아닌 값을 입력한 경우 등 잘못 입력에 대해 알려줍니다.
+     이 경우 게임을 진행할 수 있는 횟수를 차감하지 않습니다.
+     */
+    //validation Code
+    return true
+}
+
+func validateMenuInput(selectedMenuNumber menu: Int) -> Bool{
+    var tmpResult:[Int] = []
+    return true
+}
+
 func printRandomNumber(_ numbers: [Int]){
-    for number in numbers{
+    for number in numbers {
         print(number, terminator: " ")
     }
 }
@@ -51,13 +71,17 @@ func printWinner(_ winner: String) {
 }
 
 
-func startGame(){
+
+func playGame(){
     randomNumberAnswer = createRandomNumbers()
     while !isEnd {
         print("임의의 수 :", terminator: " ")
         
-        randomComputerInput = createRandomNumbers()
-        printRandomNumber(randomComputerInput)
+        userInput = getUserInput()
+        if validateUserInput(userInput: userInput){
+            
+        }
+        printRandomNumber(userInput)
         
         let strikeCount: Int = checkStrikeCount()
         let ballCount: Int = checkBallCount()
@@ -72,6 +96,21 @@ func startGame(){
         }
         
         print("\n\(strikeCount) 스트라이크, \(ballCount) 볼 \n남은 기회 : \(challenge)")
+    }
+}
+
+func startGame() {
+    while true {
+        print("1. 게임시작 \n2. 게임 종료 \n원하는 기능을 선택해주세요 : ",terminator: "")
+        
+        let menuInput = readLine()
+        if menuInput == "1" {
+            playGame()
+        } else if menuInput == "2" {
+            break
+        } else {
+            print("입력이 잘못되었습니다.")
+        }
     }
 }
 
