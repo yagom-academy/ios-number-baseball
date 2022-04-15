ac75acb0bab0275c876a1da5ff9bbd1c8187645b
apwierk2451 <apwierk2451@gmail.com>
2022-04-15 14:46:54 +0900

    docs: 피드백 반영 및 개선

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -6,10 +6,45 @@
 
 import Foundation
 
-var computerNumbers: [String] = makeRandomThreeNumbers()
+var computerNumbers: [String] = []
 var userLife: Int = 9
 var strikeCount: Int = 0
-var ballCount: Int = 0
+var ballCounts: Int = 0
+printMenu()
+
+func printMenu() {
+    print("1. 게임시작")
+    print("2. 게임종료")
+    print("원하는 기능을 선택해주세요 :",terminator: " ")
+    inputMenu()
+}
+
+func inputMenu() {
+    let choiceMenuNumber = readLine()
+    if let number = choiceMenuNumber {
+        selectMenu(menuNumber: number)
+    }
+}
+
+func selectMenu(menuNumber: String) {
+    switch menuNumber {
+    case "1":
+        gameStart()
+    case "2":
+        break
+    default:
+        print("입력이 잘못되었습니다.")
+        printMenu()
+    }
+}
+
+func gameStart() {
+    computerNumbers = makeRandomThreeNumbers()
+    userLife = 9
+    while userLife > 0 {
+        inputUserNumbers()
+    }
+}
 
 func makeRandomThreeNumbers() -> [String] {
     var randomNumbers: Set<Int> = []
@@ -17,51 +52,27 @@ func makeRandomThreeNumbers() -> [String] {
         let randomNumber = Int.random(in: 1...9)
         randomNumbers.insert(randomNumber)
     }
-    return Array(randomNumbers).map{ String( $0 ) }
-}
-
-func printMenu() {
-    print("1. 게임시작")
-    print("2. 게임종료")
-    print("원하는 기능을 선택해주세요 :",terminator: " ")
-    choiceMenu()
-}
-
-func choiceMenu() {
-    let choiceMenuNumber = readLine()
-    if let number = choiceMenuNumber {
-        selectMenu(menuNumber: number)
-    }
-}
-
-func selectMenu(menuNumber: String) -> () {
-    switch menuNumber {
-    case "1":
-        gameStart()
-    case "2":
-        break
-    default: print("입력이 잘못되었습니다.")
-        printMenu()
-    }
+    return Array(randomNumbers).map{ String($0) }
 }
 
 func inputUserNumbers() {
     print("숫자 3개를 띄어쓰기로 구분하여 입력해주세요.")
     print("중복숫자는 허용하지 않습니다.")
     print("입력 :",terminator: " ")
-    let userinputNumbers = readLine()?.components(separatedBy: " ")
-    if let userNumbers = userinputNumbers {
-        printResult(userInput: userNumbers)
-    }
+    let userinputNumbers = readLine()
+    guard let userName = userinputNumbers else { return }
+    printResult(userInput: userName.components(separatedBy: " "))
 }
 
 func printResult(userInput: [String]) {
     if mapNumbers(userInput: userInput) && userInput.count == 3 {
         userLife -= 1
-        strikeCount = countStrikes(computerNumbers: computerNumbers, userInputNumbers: userInput)
-        ballCount = countBall(computerNumbers: computerNumbers, userInputNumbers: userInput) - strikeCount
-        print("\(strikeCount) 스트라이크, \(ballCount) 볼")
-        decideWinner(strike: strikeCount, ball: ballCount, life: userLife)
+        strikeCount = countStrikes(computerNumbers: computerNumbers,
+                                   userInputNumbers: userInput)
+        ballCounts = countBall(computerNumbers: computerNumbers,
+                              userInputNumbers: userInput)
+        print("\(strikeCount) 스트라이크, \(ballCounts) 볼")
+        decideWinner(strike: strikeCount, ball: ballCounts, life: userLife)
         printLife()
     } else {
         print("입력이 잘못되었습니다.")
@@ -78,42 +89,45 @@ func mapNumbers(userInput: [String]) -> Bool {
     let result: [Int] = userInput.compactMap {
         Int(String($0))
     }
-    return checkCommonNumbers(userInput: result)
+    return checkCommonNumbers(userInputNumbers: result)
 }
 
-func checkCommonNumbers(userInput: [Int]) -> Bool {
-    let checkedNumbers: Set<Int> = Set(userInput)
-    guard checkedNumbers.count == 3 && checkedNumbers.contains(0) == false else { return false }
+func checkCommonNumbers(userInputNumbers: [Int]) -> Bool {
+    let checkedNumbers: Set<Int> = Set(userInputNumbers)
+    guard checkedNumbers.count == 3 && checkedNumbers.contains(0) == false else {
+        return false
+    }
     return true
 }
 
 func countStrikes(computerNumbers: [String], userInputNumbers: [String]) -> Int {
     var strikeCount: Int = 0
-    if computerNumbers[0] == userInputNumbers[0] {
-        strikeCount += 1
-    }
-    if computerNumbers[1] == userInputNumbers[1] {
-        strikeCount += 1
-    }
-    if computerNumbers[2] == userInputNumbers[2] {
-        strikeCount += 1
+    for computerNumber in 0...computerNumbers.count - 1 {
+        strikeCount += countStrike(computerNumbers: computerNumbers,
+                                   userInputNumbers: userInputNumbers,
+                                   count: computerNumber)
     }
     return strikeCount
-    
+}
+
+func countStrike(computerNumbers: [String], userInputNumbers: [String], count: Int) -> Int {
+    guard computerNumbers[count] == userInputNumbers[count] else { return 0 }
+    return 1
 }
 
 func countBall(computerNumbers: [String], userInputNumbers: [String]) -> Int {
     var commonNumbersCount: Int = 0
-    if computerNumbers.contains(userInputNumbers[0]) {
-        commonNumbersCount += 1
+    for computerNumber in 0...computerNumbers.count - 1 {
+        commonNumbersCount += countCommonNumber(computerNumbers: computerNumbers,
+                                                userInputNumbers: userInputNumbers,
+                                                count: computerNumber)
     }
-    if computerNumbers.contains(userInputNumbers[1]) {
-        commonNumbersCount += 1
-    }
-    if computerNumbers.contains(userInputNumbers[2]) {
-        commonNumbersCount += 1
-    }
-    return commonNumbersCount
+    return commonNumbersCount - strikeCount
+}
+
+func countCommonNumber(computerNumbers: [String], userInputNumbers: [String], count: Int) -> Int {
+    guard computerNumbers.contains(userInputNumbers[count]) else { return 0 }
+    return 1
 }
 
 
@@ -125,14 +139,5 @@ func decideWinner(strike: Int, ball: Int, life: Int) {
     if userLife == 0 {
         print("컴퓨터 승리...!")
         printMenu()
-    }
-}
-
-printMenu()
-func gameStart() {
-    computerNumbers = makeRandomThreeNumbers()
-    userLife = 9
-    while userLife > 0 {
-        inputUserNumbers()
     }
 }
--- README.md
+++ README.md
@@ -12,3 +12,28 @@
 
 ![](https://i.imgur.com/Fd4pPRJ.png)
 
+- if let 말고도 옵셔널 처리하는 방법에는 무엇이 있나요? 각각은 어떤 상황에 적절할까요?
+1. ! 강제 언랩핑
+
+2. if 문 사용
+(ex. if name != nil { print(name) } )
+
+3. 옵셔널 바인딩 사용
+
+if let
+guard let
+
+4. Nil-Coalescing
+ 
+ -  반환 값이 없을 때 괄호를 표기한 것이 좋을까요?
+헷갈리지 않기 위해 제거하는 것이 좋겠네요
+
+- components 말고도 split이라는 메서드도 있는데요. 둘은 어떤 차이가 있을까요?
+- 위 코드를 split으로 구현한다면 어떻게 구현될까요?
+components는 String 타입으로 값이 반환되고, split은 subString 타입으로 값이 반환됩니다.
+또한, components는 foundation이 import되어있을 때만 사용 가능합니다.
+
+- 옵셔널 체이닝 사용하고 if let을 통해 다시 옵셔널 제거를 시도하네요. readLine()을 guard로 옵셔널을 제거하고 이후 코드를 진행시키는 것은 어떤가요?
+- if let과 guard let의 차이는 무엇일까요?
+if let 은 바인딩한 값을 scope 안에서 지역변수로써 사용 가능하고 guard let은 함수안에서 사용하여 바인딩한 값을 return 하여 전역 변수로 활용 가능 합니다.
+
