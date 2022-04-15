3145184ac2b6fffc32ec6441c02e6eff8def28ea
bard <schy2503@naver.com>
2022-04-15 15:31:51 +0900

    refactor: 출력부분을 함수로 빼내서 분리, 몽키테스틀 진행해 공백오류 제거, 검증 함수 세분화

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -12,42 +12,63 @@ let arrayExtent = 0...2
 let gameOverStrikeCount = 3
 let gameOverTryCount = 0
 
-func selectMenu() {
+func printMenu() {
     print("1. 게임시작")
     print("2. 게임종료")
     print("원하는 기능을 선택해주세요 : ", terminator: "")
-    let userSelectMenu = readLine()
+}
+
+func selectMenu() {
+    printMenu()
+    let userSelectMenu = readLine()?.trimmingCharacters(in: .whitespaces)
     
     if userSelectMenu == "1" {
         repeatCheck = true
         gameStart()
         selectMenu()
-    } else if userSelectMenu == "2" {
-        print("", terminator: "")
+    }
+    else if userSelectMenu == "2" {
+        
     } else {
         print("입력이 잘못되었습니다")
         selectMenu()
     }
 }
 
-func userInputNumbers() -> String {
+func printGameRule() {
     print("숫자 3개를 띄어쓰기로 구분하여 입력해주세요")
     print("중복 숫자는 허용하지 않습니다.")
     print("입력 : ", terminator: "")
-    let userInput = readLine() ?? ""
+}
+
+func userInputNumbers() -> String {
+    printGameRule()
+    let userInput = readLine()?.trimmingCharacters(in: .whitespaces) ?? ""
     
-    if confirmInputNumbers(InputNumbers: userInput) {
+    if isValidate(inputNumbers: userInput) {
         return userInput
-    }else{
+    } else{
         print("입력이 잘못되었습니다")
         return userInputNumbers()
     }
 }
 
-func confirmInputNumbers(InputNumbers : String) -> Bool {
-    if InputNumbers.isEmpty || InputNumbers.split(separator: " ").count != 3 || InputNumbers.filter({$0.isNumber }).count != 3 || Set(InputNumbers).count != 4 {
+func isNumbersCountThree(numbers: String) -> Bool {
+    return numbers.filter({$0.isNumber }).count != 3
+}
+
+func validateOverlappingNumbers(numbers: String) -> Bool {
+    return Set(numbers).count != 4
+}
+
+func isZeroInNumbers(numbers: String) -> Bool {
+    return numbers.contains("0")
+}
+
+func isValidate(inputNumbers: String) -> Bool {
+    if  isNumbersCountThree(numbers: inputNumbers) || validateOverlappingNumbers(numbers: inputNumbers) || isZeroInNumbers(numbers: inputNumbers) {
         return false
-    }else{
+    } else{
         return true
     }
 }
@@ -62,25 +83,22 @@ func threeNonOverlappingRandomNumbers() -> Set<Int> {
 }
 
 func countStrikeOrBall(computerNumbers: [Int], tryCount: Int, userSuggestNumbers: String) -> (Int, Int) {
-    var userSuggestThreeNumbers = [Int]()
-    let userSuggestNumbersSplit = userSuggestNumbers.components(separatedBy: " ")
-    userSuggestThreeNumbers = userSuggestNumbersSplit.map{Int($0) ?? 0}
+    let splitedUserSuggestNumbers = userSuggestNumbers.components(separatedBy: " ")
+    let splitedUserSuggestThreeNumbers = splitedUserSuggestNumbers.map({(numbers: String) -> Int in return Int(numbers) ?? 0})
     var strikeCount = 0
     var ballCount = 0
     
     for index in arrayExtent {
-        if computerNumbers[index] == userSuggestThreeNumbers[index] {
+        if computerNumbers[index] == splitedUserSuggestThreeNumbers[index] {
             strikeCount += 1
-        } else if userSuggestThreeNumbers.contains(computerNumbers[index]) {
+        } else if splitedUserSuggestThreeNumbers.contains(computerNumbers[index]) {
             ballCount += 1
         }
     }
     
     print("\(strikeCount) 스트라이크, \(ballCount) 볼")
     print("남은기회 : \(tryCount)")
-    let strikeTryCount: (Int, Int) = (strikeCount, tryCount)
-    
-    return strikeTryCount
+    return (strikeCount, tryCount)
 }
 
 func gameStart() {
@@ -107,3 +125,6 @@ func checkGameOver(strikeTryCount: (Int, Int)) {
 }
  
 selectMenu()
+
+
+
