4c77bd7eeb5526931ff5af105de513a527338e83
MinsuJeon <chso294@naver.com>
2022-04-15 15:58:06 +0900

    refactor: 매직 스트링을 심볼릭 스트링으로 변환, 변수명 수정, 함수 순서 변경
    - 매직 스트링 사용을 피하기 위하여 "입력이 잘못되었습니다"를 inputError라는 상수에 할당
    - selectMenu함수를 executeNumberBaseballGame으로 수정
    - mainMenu 변수명을 userInput으로 수정
    - 한줄로 입력된 게임시작, 게임종료, 원하는 기능 선택 창을 """을 이용하여 여러 줄로 한번에 표현 및, terminator를 ""에서 " "로 수정
    - userInput의 값을 판단하는 switch문을 performUserSelect함수로 빼내어 두 번 들여쓰기를 지양
    - performUserSelect 함수 내 case "2"의 경우 break로 인하여 이중 종료되는 것을 종료 문구 사용으로 대체
    - generateRandomNumber 함수 내 while 조건문의 부등호 방향이 '>='로 잘못되어 '<='로 수정
    
    - compareNumber 함수 내 사용자승리, 컴퓨터승리, 남은기회를 판단한는 조건문을 printResult함수로 빼내어 두 번 들여쓰기를 지양
    - inputUserNumber 함수 내 while 문 내 유효성 검사 출력 문구를 """를 사용하여 여러 줄로 한번에 표현 및, terminator를 ""에서 " "로 수
    - compareNumber 함수 내 striekCount를 0으로 초기화하는 코드를 checkStrike 함수 내로 위치 변경
    - compareNumber 함수 내 스트라이크와 볼을 출력하는 코드를 checkBall 함수 내로 위치 변경
    - compareNumber 함수 내 remainingChances를 하나씩 차감한 코드를 printReuslt 함수 내로 위치 변경

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -10,19 +10,65 @@ var inputArray: Array<Int> = []
 var remainingChances: Int = 9
 var strikeCount: Int = 0
 var ballCount: Int = 0
-let requiredNumberOfElements: Int = 3
-let minimumNumber: Int = 1
-let maximumNumber: Int = 9
 let goalOfStrikeCount: Int = 3
 let gameOverCount: Int = 0
 
+let requiredNumberOfElements: Int = 3
+
+let minimumNumber: Int = 1
+let maximumNumber: Int = 9
+
+let inputError: String = "입력이 잘못되었습니다"
+
+func executeNumberBaseballGame() {
+    var userInput: String = ""
+    
+    while userInput != "2" {
+        print("""
+        1. 게임시작
+        2. 게임종료
+        원하는 기능을 선택해주세요 :
+        """, terminator: " ")
+        
+        userInput = readLine() ?? ""
+        
+        performUserSelect(from: userInput)
+
+        }
+}
+
+func performUserSelect(from userInput: String) {
+    switch userInput {
+    case "1" :
+        startGame()
+    case "2" :
+        print("게임을 종료합니다")
+    default :
+        print(inputError)
+    }
+}
+
+func startGame() {
+    randomArray = generateRandomNumber()
+    compareNumber()
+}
+
 func generateRandomNumber() -> Array<Int> {
     var randomSet: Set<Int> = []
-    while randomSet.count >= requiredNumberOfElements {
+    while randomSet.count <= requiredNumberOfElements {
         randomSet.insert(Int.random(in: minimumNumber...maximumNumber))
     }
     
     return Array(randomSet)
+}
+
+func compareNumber() {
+    while strikeCount < goalOfStrikeCount && remainingChances > gameOverCount {
+        inputUserNumber()
+        checkStrike()
+        checkBall()
+        printResult()
+    }
 }
 
 func inputUserNumber() {
@@ -31,7 +77,11 @@ func inputUserNumber() {
     inputArray = []
     
     while inputArray.count < requiredNumberOfElements {
-        print("숫자 3개를 띄어쓰기로 구분하여 입력해주세요.", "중복 숫자는 허용하지 않습니다.", "입력 : ", separator: "\n", terminator: "")
+        print("""
+        숫자 \(requiredNumberOfElements)개를 띄어쓰기로 구분하여 입력해주세요.
+        중복 숫자는 허용하지 않습니다.
+        입력 :
+        """, terminator: " ")
         
         userNumber = readLine()?.split(separator: " ").map{Int($0) ?? 0}
         
@@ -42,38 +92,14 @@ func inputUserNumber() {
         if Set(unwrappedUserNumber).count >= requiredNumberOfElements {
             inputArray = unwrappedUserNumber
         } else {
-            print("입력이 잘못되었습니다")
+            print(inputError)
         }
     }
-}
-
-func compareNumber() {
-    while strikeCount < goalOfStrikeCount && remainingChances > gameOverCount {
-        strikeCount = 0
-        remainingChances -= 1
-        
-        inputUserNumber()
-        checkStrike()
-        checkBall()
-        
-        print("\(strikeCount) 스트라이크, \(ballCount) 볼", separator: "\n")
-        
-        if strikeCount == goalOfStrikeCount {
-            print("사용자 승리!")
-        } else if remainingChances == gameOverCount {
-            print("남은 기회 : \(remainingChances)", "컴퓨터 승리...!", separator: "\n")
-        } else {
-            print("남은 기회 : \(remainingChances)")
-        }
-    }
-}
-
-func startGame() {
-    randomArray = generateRandomNumber()
-    compareNumber()
 }
 
 func checkStrike() {
+    strikeCount = 0
+    
     for number in 0..<requiredNumberOfElements {
         let sameDigitNumber = (randomArray[number] - inputArray[number] == 0) ? 1 : 0
         strikeCount += sameDigitNumber
@@ -83,25 +109,22 @@ func checkStrike() {
 func checkBall() {
     let intersectionOfArrays = Set(randomArray).intersection(inputArray)
     ballCount = intersectionOfArrays.count - strikeCount
+    print("\(strikeCount) 스트라이크, \(ballCount) 볼", separator: "\n")
 }
 
-func selectMenu() {
-    var mainMenu: String = ""
+func printResult() {
+    remainingChances -= 1
     
-    while mainMenu != "2" {
-        print("1. 게임시작", "2. 게임종료", "원하는 기능을 선택해주세요 : ", separator: "\n", terminator: "")
-        
-        mainMenu = readLine() ?? ""
-        
-        switch mainMenu {
-        case "1" :
-            startGame()
-        case "2" :
-            break
-        default :
-            print("입력이 잘못되었습니다")
-        }
+    if strikeCount == goalOfStrikeCount {
+        print("사용자 승리!")
+    } else if remainingChances == gameOverCount {
+        print("""
+            남은 기회 : \(remainingChances)
+            컴퓨터 승리...!
+        """)
+    } else {
+        print("남은 기회 : \(remainingChances)")
     }
 }
 
-selectMenu()
+executeNumberBaseballGame()
