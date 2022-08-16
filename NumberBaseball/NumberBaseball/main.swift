9091d0a8a5d4537916e505dc8866f3edd69aa9a2
jsa0224 <94514250+jsa0224@users.noreply.github.com>
2022-08-16 18:34:44 +0900

    feat: matchIndex 메서드 추가 & matchNumberArray 메서드 수정

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -9,6 +9,8 @@ import Foundation
 var computerRandomNumberArray: Set<Int> = []
 var numberOfAttempts = 9
 var otherComputerRandomNumberArray: Set<Int> = []
+var strike: Int = 0
+var ball: Int = 0
 
 func presentRandomNumber() -> Set<Int> {
     var randomNumArray: Set<Int> = []
@@ -21,10 +23,28 @@ func presentRandomNumber() -> Set<Int> {
 func putNumberInArray() {
     computerRandomNumberArray = presentRandomNumber()
     otherComputerRandomNumberArray = presentRandomNumber()
+    print(computerRandomNumberArray)
+    print(otherComputerRandomNumberArray)
 }
 
-func matchNumberArray(_ computerArray: Set<Int>, _ otherComputerArray: Set<Int>) {
-    
+func matchIndex(_ x: Int, _ y: Int) -> Bool {
+    if x == y {
+        return true
+    } else {
+        return false
+    }
+}
+
+func matchNumberArray(_ computerArray: Set<Int>, _ otherComputerArray: Set<Int>) -> Int {
+    for i in 0...2 {
+        if matchIndex(Int(computerArray[i]), Int(otherComputerArray[i])) == true {
+            strike += 1
+        } else if computerArray.contains(otherComputerArray[i]) {
+            ball += 1
+        }
+    }
+    return ball
 }
 
 putNumberInArray()
+print(matchNumberArray(computerRandomNumberArray, otherComputerRandomNumberArray))
