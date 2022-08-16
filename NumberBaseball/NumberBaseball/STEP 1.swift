2e8df44276579c24251fb93136680d89702ab283
Gundy <korea1880@naver.com>
2022-08-16 17:12:35 +0900

    feat: STEP 1-4 startGame 함수 구현

--- NumberBaseball/NumberBaseball/STEP 1.swift
+++ NumberBaseball/NumberBaseball/STEP 1.swift
@@ -30,5 +30,31 @@ func compareNumbers(myNumbers: Array<Int>) {
             ball += 1
         }
     }
-    print("\(strike) 스트라이크, \(ball) 볼")
+    remainCount -= 1
+    printResult(strike: strike, ball: ball)
+}
+
+func printResult(strike: Int, ball: Int) {
+    print("""
+          \(strike) 스트라이크, \(ball) 볼
+          남은 기회: \(remainCount)
+          """)
+    if strike == 3 {
+        remainCount = 0
+        print("사용자 승리!")
+    } else if remainCount == 0 {
+        print("컴퓨터 승리...!")
+    }
+}
+
+func startGame() {
+    var isLoop = true
+    while isLoop {
+        let myNumbers: Array<Int> = makeThreeRandomNumbers()
+        print("임의의 수 : \(myNumbers.description.trimmingCharacters(in: ["[", "]"]))")
+        compareNumbers(myNumbers: myNumbers)
+        if remainCount == 0 {
+            isLoop = false
+        }
+    }
 }
