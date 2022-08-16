a06407eb0fd6355ef2e7c4fa9be9daeda4bb4936
jeongmin <lj845854@gmail.com>
2022-08-16 22:51:16 +0900

    feat: decideBallCount 함수 선언

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -14,3 +14,22 @@ func generateAnswer() -> [Int] {
     array.shuffle()
     return array[...2].map(){Int($0)}
 }
+
+func decideBallCount(comparing answer: [Int], with userNumbers: [Int]) -> [Int] {
+    var strikeCount = 0
+    var ballCount = 0
+    
+    for index in 0...2 {
+        if userNumbers[index] == answer[index] {
+            strikeCount += 1
+        } else if answer.contains(userNumbers[index]) {
+            ballCount += 1
+        }
+    }
+    return [strikeCount, ballCount]
+}
+
+
+
+add: error에 Inputvalueerror 추가
+
