f0c91f35ae28234d33f71e7abd25f2302129d591
flameyg <kioswe92@gmail.com>
2022-08-16 20:55:10 +0900

    220816_Step1_Complete

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -6,5 +6,39 @@
 
 import Foundation
 
-print("Hello, World!")
+var randomNumber = Set<Int>()
+
+while randomNumber.count < 3 {
+    randomNumber.insert(Int.random(in: 1...9))
+}
+
+gameStart()
+
+func insert() {
+    print("임의의 수 : ", terminator: "")
+    let userNumber = readLine()!.split(separator: " ").map{Int($0)!}
+    var strike = 0
+    var ball = 0
+    
+    userNumber.forEach {
+        if randomNumber.contains($0) { ball += 1 }
+    }
+    strike = zip(randomNumber, userNumber).compactMap {
+        $0.0 == $0.1 ? true : nil
+    }.count
+    ball -= strike
+    
+    print("\(strike) 스트라이크, \(ball) 볼")
+}
+
+func gameStart() {
+    for round in (0...8).reversed() {
+        insert()
+        if round == 0 {
+            print("컴퓨터 승리...!")
+            break
+        }
+        print("남은 기회 : \(round)")
+    }
+}
 
