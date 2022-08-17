e4f74f9452f2d2f3747c7127fe6bb66bb1b20e15
lnhk98 <hozzang666@gmai.com>
2022-08-17 11:07:50 +0900

    feat: 번호 생성 함수, 게임 결과 보여주는 함수 정의

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -6,5 +6,26 @@
 
 import Foundation
 
-print("Hello, World!")
+let numbers = [Int]()
+var times = 9
 
+func generateNumbers() -> [Int] {
+    return Array((1...9).shuffled()[..<3])
+}
+
+print(generateNumbers())
+
+func showGameResult() {
+    var strike = 0
+    var ball = 0
+    let myNumbers = readLine()?.split(separator: " ").map { Int($0) }
+    for number, myNumber in zip(numbers, myNumbers) {
+        if numbers == myNumbers {
+            strike += 1
+        } else {
+            if numbers.contains(myNumber) {
+                ball += 1
+            }
+        }
+    }
+}
