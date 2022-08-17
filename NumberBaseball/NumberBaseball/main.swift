46f62ebae5bda8ffd611cf885bfb3e0429190d5b
lnhk98 <hozzang666@gmai.com>
2022-08-17 12:23:45 +0900

    feat: judge 함수 생성 및 기능 분리

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -1,46 +1,44 @@
-//
-//  NumberBaseball - main.swift
-//  Created by yagom. 
-//  Copyright © yagom academy. All rights reserved.
-// 
-
 import Foundation
 
-let numbers = Array((1...9).shuffled()[..<3])
-var times = 9
+let randomNumbers = Array((1...9).shuffled()[..<3])
+var tryCount = 9
 
 func generateNumbers() -> [Int] {
     return Array((1...9).shuffled()[..<3])
 }
 
-print(generateNumbers())
+func judge(_ myNumbers: [Int]) -> (Int, Int) {
+    var (strike, ball) = (0, 0)
+    
+    for (number, myNumber) in zip(randomNumbers, myNumbers) {
+        if number == myNumber {
+            strike += 1
+        } else if randomNumbers.contains(myNumber) {
+            ball += 1
+        }
+    }
+    
+    return (strike, ball)
+}
 
-func showGameResult() {
-    while times >= 1 {
-        var strike = 0
-        var ball = 0
+func showResult() {
+    while tryCount >= 1 {
+        var (strike, ball) = (0, 0)
         let myNumbers = generateNumbers()
         
-        times -= 1
+        tryCount -= 1
         
-        for (number, myNumber) in zip(numbers, myNumbers) {
-            print("number : \(number), myNumber : \(myNumber)")
-            if numbers == myNumbers {
-                strike += 1
-            } else if numbers.contains(myNumber) {
-                ball += 1
-            }
-        }
-        
-        print("임의의 수 : \(myNumbers)")
+        print("임의의 수 :\(myNumbers.reduce("") { "\(String($0)) \(String($1))" })")
         print("\(strike) 스트라이크, \(ball) 볼")
+        (strike, ball) = judge(myNumbers)
+        
         if strike == 3 {
             print("사용자 승리!")
             return
         } else {
-            times != 0 ? print("남은 기회 : \(times)") : print("컴퓨터 승리...!")
+            tryCount != 0 ? print("남은 기회 : \(tryCount)") : print("컴퓨터 승리...!")
         }
     }
 }
 
-showGameResult()
+showResult()
