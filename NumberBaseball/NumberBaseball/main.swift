9e4f559c750694978b8f66ba2d54b3e74ed5e1b4
yjjem <marksim0606@gmail.com>
2022-08-17 13:01:34 +0900

    [#1] compareNumbers() func added

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -6,40 +6,44 @@
 
 import Foundation
 
-var computer: [Int] = []
+var computerNumbers: [Int] = []
 var remaining = 9
 var remainingNumbers = Array(1...9)
 
 func generateNumber() {
-    while computer.count < 3 {
+    while computerNumbers.count < 3 {
         let number = Int(arc4random_uniform(UInt32(remainingNumbers.count)))
         var selectedNumber = remainingNumbers[number]
         if selectedNumber != 0 {
-            computer.append(selectedNumber)
+            computerNumbers.append(selectedNumber)
         }
         remainingNumbers[number] = 0
     }
-    print(computer)
+    print(computerNumbers)
 }
 
-func getUserBall() {
+func getUserBall() -> [Int?] {
     let userNumberList = readLine()?.split(separator: " ").map({ number in
         return Int(number)
     })
-    
+    return  userNumberList ?? []
 }
 
-func result() {
-    let user = getUserBall()
+func compareNumbers() {
+    let userNumbers = getUserBall()
     var strike = 0
     var ball = 0
-    for (idx, com) in computer.enumerated() {
-        if com == user[idx] {
+    for (idx, computerNum) in computerNumbers.enumerated() {
+        if computerNum == userNumbers[idx] {
             strike += 1
         } else {
             ball += 1
         }
     }
+    print("\(strike),\(ball)")
 }
 
-result()
+
+generateNumber()
+getUserBall()
+compareNumbers()
