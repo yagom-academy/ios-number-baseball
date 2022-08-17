73616e14470187a08bd09368b9fdb2f541c08dfc
yuvinrho <yvrho@naver.com>
2022-08-17 13:05:35 +0900

    refactor: showResult 함수 오류수정

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -1,7 +1,6 @@
 import Foundation
 
 let randomNumbers = Array((1...9).shuffled()[..<3])
-var tryCount = 9
 
 func generateNumbers() -> [Int] {
     return Array((1...9).shuffled()[..<3])
@@ -9,7 +8,7 @@ func generateNumbers() -> [Int] {
 
 func judge(_ myNumbers: [Int]) -> (Int, Int) {
     var (strike, ball) = (0, 0)
-    
+
     for (number, myNumber) in zip(randomNumbers, myNumbers) {
         if number == myNumber {
             strike += 1
@@ -17,26 +16,28 @@ func judge(_ myNumbers: [Int]) -> (Int, Int) {
             ball += 1
         }
     }
-    
+
     return (strike, ball)
 }
 
 func showResult() {
+    var tryCount = 9
+
     while tryCount >= 1 {
         var (strike, ball) = (0, 0)
         let myNumbers = generateNumbers()
-        
+
         tryCount -= 1
-        
-        print("임의의 수 :\(myNumbers.reduce("") { "\(String($0)) \(String($1))" })")
-        print("\(strike) 스트라이크, \(ball) 볼")
+
         (strike, ball) = judge(myNumbers)
-        
+        print("임의의 수 :(myNumbers.reduce("") { "(String($0)) (String($1))" })")
+        print("(strike) 스트라이크, (ball) 볼")
+
         if strike == 3 {
             print("사용자 승리!")
             return
         } else {
-            tryCount != 0 ? print("남은 기회 : \(tryCount)") : print("컴퓨터 승리...!")
+            tryCount != 0 ? print("남은 기회 : (tryCount)") : print("컴퓨터 승리...!")
         }
     }
 }
