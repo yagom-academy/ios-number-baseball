9cad880c0b112554ffac7575bd94fbb751a7ca79
HyosungP <hyosing92@gmail.com>
2022-08-17 12:56:21 +0900

    feat : 컴퓨터, 사용자 정수 저장공간 생성 및 세 개의 랜덤숫자 생성 함수 구현

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -6,5 +6,30 @@
 
 import Foundation
 
-print("Hello, World!")
+// 컴퓨터가 제시할 임의의정수 3개
+var computerNumbers: [Int] = []
+var userNumbers: [Int] = []
+// 남은 시도횟수를 담아둘 변수
+var tryNumber: Int = 0
+
+
+// 1-9사이의 세개의 임의의 정수를 생성하여 반환하는 함수
+// 중복X
+func generateThreeRandomNumbers() -> [Int] {
+    var numbers: Set<Int> = []
+    
+    while numbers.count < 3 {
+        let num = Int.random(in: 1...9)
+        numbers.update(with: num)
+    }
+    
+    return Array(numbers)
+}
+
+
+computerNumbers = generateThreeRandomNumbers()
+userNumbers = generateThreeRandomNumbers()
+
+//서로비교하는 함수.
+
 
