c9f7c661651e36002760adeb55f9bcd233f05ca0
lxodud <xodud1998223@naver.com>
2022-08-17 10:22:26 +0900

    feat: Creat Random Number 함수 구현

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -4,7 +4,38 @@
 //  Copyright © yagom academy. All rights reserved.
 // 
 
-import Foundation
+func creatRandomNumber() -> [Int] {
+    var randNumber: [Int] = []
+    var count: Int = 0
+    
+    while count < 3 {
+        var temp: Int = Int.random(in: 1...9)
+        
+        if !randNumber.contains(temp) {
+            randNumber.append(temp)
+            count += 1
+        }
+    }
+    return randNumber
+}
 
-print("Hello, World!")
+func compareThreeNumber(a: [Int], b: [Int]) -> Bool {
+    var count: Int = 0
+    
+    for element in a {
+        if b.contains(element) {
+            count += 1
+        }
+    }
+    if count == 3 {
+        return true
+    } else {
+        return false
+    }
+}
+
+func numberBaseBall() {
+    
+}
+
 
