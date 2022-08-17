bbb04b1bb668b87a7fb29e8a2a691c917e730e3d
LimKiWoong <lkw4670@naver.com>
2022-08-17 13:16:33 +0900

    feat : zip을 사용해서 숫자를 서로 묶고 스트라이크를 비교하는 기능 추가

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -11,7 +11,8 @@ var computerNumbers: [Int] = []
 var userNumbers: [Int] = []
 // 남은 시도횟수를 담아둘 변수
 var tryNumber: Int = 0
-
+var ballCount : Int = 0
+var strikeCount : Int = 0
 
 // 1-9사이의 세개의 임의의 정수를 생성하여 반환하는 함수
 // 중복X
@@ -26,10 +27,19 @@ func generateThreeRandomNumbers() -> [Int] {
     return Array(numbers)
 }
 
-
 computerNumbers = generateThreeRandomNumbers()
 userNumbers = generateThreeRandomNumbers()
 
-//서로비교하는 함수.
+func foundStrike() {
+    var pairNumbers = zip(userNumbers, computerNumbers)
+    
+    for pairNumber in pairNumbers {
+        if pairNumber.0 == pairNumber.1 {
+            strikeCount += 1
+        }
+        print(pairNumber.0)
+        print(pairNumber.1)
+    }
+}
 
-
+foundStrike()
