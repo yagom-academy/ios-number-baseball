14db9ebafef6213a4a55b398fae94fba8740931b
dylan-yoon <d.dylany21@gmail.com>
2022-08-17 10:07:47 +0900

    feat: createThreeRandomInt함수 생성

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -6,5 +6,14 @@
 
 import Foundation
 
-print("Hello, World!")
+var computerNumbers: [Int] = [Int]()
+var trialNumber: Int = 9
 
+func createThreeRandomInt() -> [Int] {
+    var result: Set<Int> = Set<Int>()
+    while result.count < 3 {
+        result.insert(Int.random(in: 1...9))
+    }
+    
+    return Array(result)
+}
