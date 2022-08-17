6504effa8a7625bdfa5412411fcb009dad9df324
lxodud <xodud1998223@naver.com>
2022-08-18 08:55:03 +0900

    refactor: 변수명 및 코딩 컨벤션 수정

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -5,37 +5,36 @@
 // 
 
 func creatRandomNumber() -> [Int] {
-    var randomNumber: [Int] = []
+    var randomNumbers: [Int] = []
     var count: Int = 0
     
     while count < 3 {
-        let temp: Int = Int.random(in: 1...9)
+        let randomNumber: Int = Int.random(in: 1...9)
         
-        if !randomNumber.contains(temp) {
-            randomNumber.append(temp)
+        if !randomNumbers.contains(randomNumber) {
+            randomNumbers.append(randomNumber)
             count += 1
         }
     }
-    return randomNumber
+    return randomNumbers
 }
 
-func checkBall(from a: [Int], to b: [Int]) -> Int {
+func checkBall(comparing: [Int], with: [Int]) -> Int {
     var ballCount: Int = 0
     
-    for (index, value)  in b.enumerated() {
-        if a.contains(value) && b[index] != a[index] {
+    for (index, value)  in with.enumerated() {
+        if comparing.contains(value) && with[index] != comparing[index] {
             ballCount += 1
         }
     }
-    
     return ballCount
 }
 
-func checkStrike(from a: [Int], to b: [Int]) -> Int {
+func checkStrike(comparing: [Int], with: [Int]) -> Int {
     var strikeCount: Int = 0
     
-    for (index, _) in a.enumerated() {
-        if a[index] == b[index] {
+    for (index, _) in comparing.enumerated() {
+        if comparing[index] == with[index] {
             strikeCount += 1
         }
     }
@@ -49,15 +48,16 @@ func startNumberBaseBall() {
     while lifeCount != 0 {
         let userInputNumber: [Int] = creatRandomNumber()
         print("임의의 수 : \(userInputNumber[0]) \(userInputNumber[1]) \(userInputNumber[2])")
-        let strikeCount: Int = checkStrike(from: randomNumber, to: userInputNumber)
+        let strikeCount: Int = checkStrike(comparing: randomNumber, with: userInputNumber)
         if strikeCount == 3 {
             print("사용자 승리!")
             break
         }
         
-        let ballCount: Int = checkBall(from: randomNumber, to: userInputNumber)
+        let ballCount: Int = checkBall(comparing: randomNumber, with: userInputNumber)
         print("\(strikeCount) 스트라이크, \(ballCount) 볼")
         lifeCount -= 1
+        print("남은기회 : \(lifeCount)")
     }
     if lifeCount == 0 {
         print("컴퓨터 승리...!")
