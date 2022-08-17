94134e3c7a294334cf8d90ef3e6b4d686988e80d
jin <pearlisrich@naver.com>
2022-08-17 12:04:05 +0900

    #2 step 1 컴퓨터 변수 생성 및 함수 구현

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -6,5 +6,20 @@
 
 import Foundation
 
-print("Hello, World!")
+var computer: [Int] = []
+var remaining = 9
+var numbers = Array(1...9)
 
+func generateNumber() {
+    for _ in 0...2 {
+        let number = arc4random_uniform(UInt32(numbers.count))
+        computer.append(numbers[Int(number)])
+        numbers.remove(at: Int(number))
+    }
+    print(computer)
+}
+
+for _ in 0...4 {
+    generateNumber()
+    computer = []
+}
