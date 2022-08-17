32f324d8aa6a93312583b91b215075f89602c4d5
wonbi <wonbi@Wonbiui-MacBookAir.local>
2022-08-17 18:09:15 +0900

    feat: 게임시작 함수 구현

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -6,25 +6,55 @@
 
 import Foundation
 
-var computerNumbers: Set<Int> = []
-var gameCounter: Int = 0
-var userNumbers: Set<Int> = []
+var computerNumbers: Array<Int> = []
+var gameCounter: Int = 9
+var userNumbers: Array<Int> = []
 
-func makeThreeNumbers() -> Set<Int>{
+func makeThreeNumbers() -> Array<Int> {
     var generatedNumbers: Set<Int> = []
-    while generatedNumbers.count < 3{
+    while generatedNumbers.count < 3 {
         generatedNumbers.update(with: Int.random(in: 1...9))
     }
-    return generatedNumbers
+    return generatedNumbers.map { $0 }
 }
 
-func gameStart(){
-    computerNumbers = makeThreeNumbers()
-    gameCounter = 9
-    userNumbers = makeThreeNumbers()
-    print(computerNumbers)
-    print(userNumbers)
+func gameStart() {
+    var strikeCounter: Int = 0
+    var ballCounter: Int = 0
+    gameCounter -= 1
+    print("임의의 수 : \(userNumbers.map { String($0) }.joined(separator: " "))")
+    computerNumbers.enumerated().forEach { (index,computerNumber) in
+        if userNumbers[index] == computerNumber {
+            strikeCounter += 1
+        } else if userNumbers.contains(computerNumber) {
+            ballCounter += 1
+        }
+    }
+    print("\(strikeCounter) 스트라이크, \(ballCounter) 볼")
+    if gameCounter < 1 {
+        print("컴퓨터 승리...!")
+        return
+    } else {
+    print("남은 기회 : \(gameCounter)")
+    }
 }
 
-
+computerNumbers = makeThreeNumbers()
+userNumbers = makeThreeNumbers()
+gameStart()
+userNumbers = makeThreeNumbers()
+gameStart()
+userNumbers = makeThreeNumbers()
+gameStart()
+userNumbers = makeThreeNumbers()
+gameStart()
+userNumbers = makeThreeNumbers()
+gameStart()
+userNumbers = makeThreeNumbers()
+gameStart()
+userNumbers = makeThreeNumbers()
+gameStart()
+userNumbers = makeThreeNumbers()
+gameStart()
+userNumbers = makeThreeNumbers()
 gameStart()
