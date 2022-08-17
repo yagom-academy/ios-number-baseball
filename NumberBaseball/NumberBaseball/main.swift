13b0215fda13b63d18cf2fd18d6a8f7bad0685e3
wonbi <wonbi@Wonbiui-MacBookAir.local>
2022-08-17 19:23:15 +0900

    refactor: STEP1 코드 리팩토링2

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -9,7 +9,6 @@ import Foundation
 var computerNumbers: Array<Int> = []
 var gameCounter: Int = 9
 var userNumbers: Array<Int> = []
-var gameStatus: Bool = true
 
 func makeThreeNumbers() -> Array<Int> {
     var generatedNumbers: Set<Int> = []
@@ -31,51 +30,23 @@ func checkStrikeAndBall(numbers: [Int]) -> [Int] {
     }
     return [strikeCounter, ballCounter]
 }
-//=====================================================================
-func winnerChecker(strike: Int) {
-    if strike == 3 {
-        print("유저 승리...!")
-    } else {
-        print("컴퓨터 승리...!")
-    }
-}
 
 func gameStart() {
-    if gameCounter == 0 || !gameStatus { return }
-    gameCounter -= 1
     let gameResult = checkStrikeAndBall(numbers: userNumbers)
+    gameCounter -= 1
     print("임의의 수 : \(userNumbers.map{ String($0) }.joined(separator: " "))")
     print("\(gameResult[0]) 스트라이크, \(gameResult[1]) 볼")
-
-    if gameCounter == 0 {
-        winnerChecker(strike: gameResult[0])
+    if gameResult[0] == 3 {
+        print("유저 승리...!")
+    } else if gameCounter <= 0 {
+        print("컴퓨터 승리...!")
     } else {
         print("남은 기회 : \(gameCounter)")
     }
 }
-//=====================================================================
-// 더 나은 코드를 위하여..!
-//
-//func gameStart() {
-//    if gameCounter < 1 || !gameStatus { return }
-//    gameCounter -= 1
-//    let gameResult = checkStrikeAndBall(numbers: userNumbers)
-//    print("임의의 수 : \(userNumbers.map{ String($0) }.joined(separator: " "))")
-//    print("\(gameResult[0]) 스트라이크, \(gameResult[1]) 볼")
-//    if gameResult[0] == 3 {
-//        print("유저 승리...!")
-//        gameStatus = false
-//        return
-//    } else if gameCounter == 0 {
-//        print("컴퓨터 승리...!")
-//        gameStatus = false
-//        return
-//    }
-//    print("남은 기회 : \(gameCounter)")
-//}
 
 computerNumbers = makeThreeNumbers()
-for _ in 1...10 {
+for _ in 1...9 {
     userNumbers = makeThreeNumbers()
     gameStart()
 }
