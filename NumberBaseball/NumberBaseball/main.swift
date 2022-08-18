549d8f5e3c0e878220b05419f5cc567d79f017ee
chacha <hyhy0429_@nvaer.com>
2022-08-18 17:50:44 +0900

    게임실행함수 while조건문 추가

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -1,55 +1,64 @@
 
-import Foundation
+var chance: Int = 9
+var gameNumbers: [Int] = makeRandomThreeNumbers()
 
-var trialRoundsCount : Int = 9
-var gameNumber : Array<Int> = makeRandomGameNumber()
-
-func makeRandomGameNumber() -> Array<Int> {
-    var randomGameNumbers : Set<Int> = []
+func makeRandomThreeNumbers() -> [Int] {
+    var randomGameNumbers: Set<Int> = []
     while randomGameNumbers.count < 3 {
         randomGameNumbers.insert(Int.random(in: 1...9))
     }
     return Array(randomGameNumbers)
 }
 
-func compareNumber(userGameNumber: Array<Int>) {
-//    let userGameNumbers = makeRandomGameNumber()
+func compareNumber(userGameNumber: [Int], strikeCount: Int, ballCount: Int) {
     var strikeCount = 0
     var ballCount = 0
-    for i in 0...2 {
-        if gameNumber[i] == userGameNumber[i] { // 자리수가 같고, 숫자도 같으면 스트라이크카운트 + 1
+    for index in 0...2 {
+        if gameNumbers[index] == userGameNumber[index] {
             strikeCount += 1
-        }
-        if gameNumber[i] != userGameNumber[i] && gameNumber.contains(userGameNumber[i]) {
+        } else if gameNumbers[index] != userGameNumber[index] && gameNumbers.contains(userGameNumber[index]) {
             ballCount += 1
-        }  // 자리수와 숫자가 같지않고, 숫자를 포함하면 볼카운트 + 1
-    }
-    if strikeCount == 3 {
-        print("사용자 승리...")
-        return
-    }
-    print("\(strikeCount) 스트라이크, \(ballCount) 볼")
-}
-
-// 게임실행함수에서 비교하는 함수를 호출했을 때, 스트라이크 개수가 3개면 게임을 멈춰야하는데 임의의수가 계속 생성된다.
-
-func playNumberBaseballGame () {
- 
-    for _ in 1...9 {
-        let userGameNumber : Array<Int> = makeRandomGameNumber()
-        
-        print("임의의 수 : \(userGameNumber[0]) \(userGameNumber[1]) \(userGameNumber[2])")
-        compareNumber(userGameNumber: userGameNumber)
-        trialRoundsCount -= 1
-        
-        if trialRoundsCount == 0 {
-            print("컴퓨터승리...!")
-        }else{
-            print("남은기회 : \(trialRoundsCount) 회")
         }
     }
-    
+//    if strikeCount == 3 {
+//        print("사용자 승리...")
+//        return
+//    }
+//    print("\(strikeCount) 스트라이크, \(ballCount) 볼")
 }
 
-playNumberBaseballGame()
+//func playNumberBaseballGame(chane: Int) {
+//    for _ in 1...chance {
+//        let userGameNumber : Array<Int> = makeRandomThreeNumbers()
+//        print("임의의 수 : \(userGameNumber[0]) \(userGameNumber[1]) \(userGameNumber[2])")
+//        compareNumber(userGameNumber: userGameNumber, strikeCount: 0, ballCount: 0)
+////        print("\(strikeCount) 스트라이크, \(ballCount) 볼")
+//        chance -= 1
+//        if chance == 0 {
+//            print("컴퓨터승리...!")
+//        } else {
+//            print("남은기회 : \(chance) 회")
+//        }
+//    }
+//}
+
+func playNumberBaseballGame(chance: Int) {
+    var strikeCount: Int = 0
+    var ballCount: Int = 0
+    var chance: Int = chance
+    while 0 < chance || strikeCount != 3 {
+        let userGameNumber : Array<Int> = makeRandomThreeNumbers()
+        print("임의의 수 : \(userGameNumber[0]) \(userGameNumber[1]) \(userGameNumber[2])")
+        compareNumber(userGameNumber: userGameNumber, strikeCount: 0, ballCount: 0)
+        print("\(strikeCount) 스트라이크, \(ballCount) 볼")
+        chance -= 1
+        if chance == 0 {
+            print("컴퓨터승리...!")
+        } else {
+            print("남은기회 : \(chance) 회")
+        }
+    }
+}
+
+playNumberBaseballGame(chance: 9)
 
