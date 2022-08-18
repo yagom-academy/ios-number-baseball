84be2173eb7fb43038ef8bc33cad5224a8d2911b
wonbi <wonbi@Wonbiui-MacBookAir.local>
2022-08-18 13:27:42 +0900

    feat: 사용자 메뉴 출력 입력함수, 게임 숫자 입력받는 함수 구현

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -44,9 +44,40 @@ func gameStart() {
         print("남은 기회 : \(gameCounter)")
     }
 }
+//
+//computerNumbers = makeThreeNumbers()
+//for _ in 1...9 {
+//    userNumbers = makeThreeNumbers()
+//    gameStart()
+//}
 
-computerNumbers = makeThreeNumbers()
-for _ in 1...9 {
-    userNumbers = makeThreeNumbers()
-    gameStart()
+func gameMenuSelect() -> String {
+    print("1. 게임시작", "2. 게임종료", "원하는 기능을 선택해주세요 : ", separator: "\n", terminator: "")
+    guard let inputedMenuValue = readLine(), let inputedMenuNumber = Int(inputedMenuValue), 1...2 ~= inputedMenuNumber else { return "3" }
+    return inputedMenuValue
 }
+
+func reciveNumbers() {
+    print("숫자 3개를 띄어쓰기로 구분하여 입력해주세요.", "중복 숫자는 허용하지 않습니다.", "입력 : ", separator: "\n" , terminator: "")
+    guard let recivedValue = readLine(), !recivedValue.isEmpty else { return }
+    let recivedNumbers = Set(recivedValue.components(separatedBy: " ")).compactMap({ Int($0) })
+    guard recivedNumbers.count == 3, recivedNumbers.filter({ $0 < 10 }).count == 3 else { return }
+    print(recivedNumbers)
+//    guard recivedNumbers.count == 3 else { return nil }
+//
+}
+
+//while true {
+//    let gameMenuSelecter = gameMenuSelect()
+//    if gameMenuSelecter == "1" {
+//        print("게임시작")
+//        break
+//    } else if gameMenuSelecter == "2" {
+//        print("게임종료")
+//        break
+//    }
+//    print("입력이 잘못되었습니다.")
+//}
+//
+
+reciveNumbers()
