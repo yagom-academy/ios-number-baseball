b06c91a733024e214ab736aa842e491bbc9f045f
jonghancha <jonghancha@gmail.com>
2022-08-19 00:26:39 +0900

    feat: STEP1 함수 구현 - checkBallStrike - startGame - 변수 flag, menuNum, 함수 equalIndexNumber 삭제

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -9,10 +9,8 @@ import Foundation
 var comNumArray: Array<Int> = []
 var tempNumArray: Array<Int> = []
 var tryCount: Int = 9
-var menuNum: Int
 var strikeCount: Int = 0
 var ballCount: Int = 0
-var flag: Bool = false
 
 func createRandomNumbers() -> [Int] {
     var numbersSet: Set<Int> = []
@@ -20,51 +18,47 @@ func createRandomNumbers() -> [Int] {
     while numbersSet.count < 3 {
         numbersSet.insert(Int.random(in: 1...9))
     }
-
     return Array(numbersSet)
 }
 
-func containNumber(i: Int) -> Bool {
-    flag = comNumArray.contains(tempNumArray[i])
-    return flag
-}
-
-func equalIndexNumber(tempNumArray: Array<Any>) {
-    for (index, number) in array.enumerated() {
-
-    }
-}
-
-
-/*-------------------------------------*/
-
-comNumArray = createRandomNumbers()
-
-while tryCount > 0 {
-    strikeCount = 0
-    ballCount = 0
-    tempNumArray = createRandomNumbers()
-    print("기준 숫자 : \(comNumArray)")
-    print("비교 숫자 : \(tempNumArray)")
-    
-    for num in 0...2 {        //비교할 숫자 3개 각각 확인하기
-        if containNumber(i: num) {
-            equalIndexNumber(i: num)
+func checkBallStrike() {
+    for (index,number) in tempNumArray.enumerated() {
+        guard comNumArray.contains(number) else { continue }
+        if comNumArray[index] == number {
+            strikeCount += 1
         } else {
-            print("꽝")
+            ballCount += 1
         }
     }
-    print("\(strikeCount) 스트라이크, \(ballCount) 볼")
-    if strikeCount == 3 {
-        print("사용자 승리...!")
-        break
-    }
-    tryCount -= 1
-    print("남은 기회 : \(tryCount) \n")
 }
 
-if strikeCount != 3 {
-    print("컴퓨터 승리...!")
+func startGame() {
+    comNumArray = createRandomNumbers()
+    while tryCount > 0 {
+        strikeCount = 0
+        ballCount = 0
+        tempNumArray = createRandomNumbers()
+        
+        print("임의의 수 : \(tempNumArray)")
+        
+        checkBallStrike()
+        
+        print("\(strikeCount) 스트라이크, \(ballCount) 볼")
+        
+        if strikeCount == 3 {
+            print("사용자 승리...!")
+            break
+        }
+        tryCount -= 1
+        
+        print("남은 기회 : \(tryCount) \n")
+    }
+
+    if strikeCount != 3 {
+        print("컴퓨터 승리...!")
+    }
 }
+
+startGame()
 
 
