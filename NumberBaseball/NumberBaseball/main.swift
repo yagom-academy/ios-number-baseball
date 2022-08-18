6270c97fb8e6a9fc51d51ba6a012757a3a00cf74
oneStar92 <onestar7984@gmail.com>
2022-08-18 11:20:20 +0900

    refactor: answer, remainCount를 전역변수로 변경.
    answer가 전역변수로 변경되어서 decideStrikeCountAndBallCount함수의 parameter로 answer를 전달받지 않아도 되기에 해당부분의 네이밍을 수정했다.

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -2,7 +2,9 @@
 //  NumberBaseball - main.swift
 //  Created by yagom. 
 //  Copyright © yagom academy. All rights reserved.
-// 
+//
+var answer = [Int]()
+var remainCount = 9
 
 func fetchRandomNumbers() -> [Int] {
     var numberSet = Set<Int>()
@@ -15,7 +17,9 @@ func fetchRandomNumbers() -> [Int] {
     return randomNumbers
 }
 
-func decideStrikeCountAndBallCount(comparing answer: [Int], with userNumbers: [Int]) -> (Int, Int) {
+answer = fetchRandomNumbers()
+
+func decideStrikeCountAndBallCount(about userNumbers: [Int]) -> (Int, Int) {
     var strikeCount = 0
     var ballCount = 0
     
@@ -30,8 +34,6 @@ func decideStrikeCountAndBallCount(comparing answer: [Int], with userNumbers: [I
 }
 
 func playBall() {
-    let answer = fetchRandomNumbers()
-    var remainCount = 9
     var userNumbers = [Int]()
     
     while remainCount > 0 {
@@ -40,7 +42,7 @@ func playBall() {
         let numbersToString = userNumbers.map({String($0)}).joined(separator: " ")
         guidance += "임의의 수 : \(numbersToString)\n"
 
-        let (strikeCount, ballCount) = decideBallCount(comparing: answer, with: userNumbers)
+        let (strikeCount, ballCount) = decideStrikeCountAndBallCount(about: userNumbers)
         guidance += "\(strikeCount) 스트라이크, \(ballCount) 볼\n"
         
         if strikeCount == 3 {
