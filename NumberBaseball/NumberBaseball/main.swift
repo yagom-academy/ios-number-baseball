eb615fbd858ae3da87b8c715bc52be6eac60137a
junho <junhoL1046@gmail.com>
2022-08-18 10:26:46 +0900

    refactor: createThreeRandomInt 함수명 변경 외
    createThreeRandomInt -> createThreeRandomNumbers
    playGame 함수 기능 일부를 isWin함수를 만들어 분리함

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -8,8 +8,9 @@ import Foundation
 
 playGame()
 
-func createThreeRandomInt() -> [Int] {
+func createThreeRandomNumbers() -> [Int] {
     var result: Set<Int> = Set<Int>()
+    
     while result.count < 3 {
         result.insert(Int.random(in: 1...9))
     }
@@ -37,26 +38,32 @@ func determineStrikesBallsInTwoNumbers(_ computerNumbers: [Int], _ userNumbers: 
     return (strike: strike, ball: ball)
 }
 
+func isWin(strike: Int, trialNumber: Int) -> Bool {
+    if strike == 3 {
+        print("사용자 승리")
+        return true
+    } else if trialNumber == 0 {
+        print("컴퓨터 승리")
+        return true
+    } else {
+        print("남은 기회 : \(trialNumber)")
+        return false
+    }
+}
+
 func playGame() {
     var trialNumber: Int = 9
-    let computerNumbers = createThreeRandomInt()
+    let computerNumbers = createThreeRandomNumbers()
     
-    while true {
+    while trialNumber > 0 {
         trialNumber -= 1
-        let userNumbers: [Int] = createThreeRandomInt()
+        let userNumbers: [Int] = createThreeRandomNumbers()
         print("임의의 수 : \(userNumbers.map { String($0) }.joined(separator: " "))")
-        let result = determineStrikesBallsInTwoNumbers(computerNumbers,
-                                                       userNumbers)
+        let result = determineStrikesBallsInTwoNumbers(computerNumbers, userNumbers)
         print("\(result.strike) 스트라이크, \(result.ball) 볼")
         
-        if result.strike == 3 {
-            print("사용자 승리")
+        if isWin(strike: result.strike, trialNumber: trialNumber) {
             break
-        } else if trialNumber == 0 {
-            print("컴퓨터 승리")
-            break
-        } else {
-            print("남은 기회 : \(trialNumber)")
         }
     }
 }
