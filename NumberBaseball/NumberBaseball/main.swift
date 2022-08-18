05039e263c696cd7023d81e4db0ad2b7868eac03
yjjem <marksim0606@gmail.com>
2022-08-18 10:54:01 +0900

    [#1] 비교함수 구현 및 리펙토링

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -6,31 +6,36 @@
 
 import Foundation
 
-var computerNumbers: [Int] = []
-var remaining = 9
-var remainingNumbers = Array(1...9)
 
-func generateNumber() {
-    while computerNumbers.count < 3 {
+func generateNumber() -> [Int] {
+    var remainingNumbers = Array(1...9)
+    var randomNumbers: [Int] = []
+    while randomNumbers.count < 3 {
         let number = Int(arc4random_uniform(UInt32(remainingNumbers.count)))
         var selectedNumber = remainingNumbers[number]
         if selectedNumber != 0 {
-            computerNumbers.append(selectedNumber)
+            randomNumbers.append(selectedNumber)
         }
         remainingNumbers[number] = 0
     }
-    print(computerNumbers)
+    return randomNumbers
 }
 
-func getUserBall() -> [Int?] {
-    let userNumberList = readLine()?.split(separator: " ").map({ number in
-        return Int(number)
-    })
-    return  userNumberList ?? []
+func getUserBall(userNumbers: [Int]) {
+//    let userNumberList = readLine()?.split(separator: " ").map({ number in
+//        return Int(number)
+//    })
+    print("임의의 수 : ", terminator: "")
+    for num in userNumbers {
+        print(num, terminator: " ")
+    }
 }
 
-func compareNumbers() {
-    let userNumbers = getUserBall()
+func compareNumbers() -> Int {
+    remaining -= 1
+    let userNumbers = generateNumber()
+    getUserBall(userNumbers: userNumbers)
+    
     var strike = 0
     var ball = 0
     for (idx, computerNum) in computerNumbers.enumerated() {
@@ -40,10 +45,16 @@ func compareNumbers() {
             ball += 1
         }
     }
-    print("\(strike),\(ball)")
+    print(" ")
+    print("\(strike) 스트라이크, \(ball) 볼")
+    print("남은 기회: \(remaining)")
+    
+    return remaining
+}
+var remaining = 9
+let computerNumbers = generateNumber()
+
+while remaining > 0 {
+    remaining = compareNumbers()
 }
 
-
-generateNumber()
-getUserBall()
-compareNumbers()
