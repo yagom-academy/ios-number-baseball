f3897a4283306d087bc81b0727144bec8bc19671
hashswim <ss991117@naver.com>
2022-08-18 19:50:20 +0900

    feat: 게임시작 함수 추가

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -1,13 +1,13 @@
 //
 //  NumberBaseball - main.swift
-//  Created by yagom.
+//  Created by Dragon, Aaron.
 //  Copyright © yagom academy. All rights reserved.
 //
 
 func compareStikeBall(randomNumber: Array<Int>, userNumber: Array<Int>) -> Bool {
     var strike = 0
     var ball = 0
-    
+
     userNumber.forEach {
         if randomNumber.contains($0) {
             ball += 1
@@ -17,43 +17,41 @@ func compareStikeBall(randomNumber: Array<Int>, userNumber: Array<Int>) -> Bool 
         $0.0 == $0.1 ? true : nil
     }.count
     ball -= strike
-    
+
     print("\(strike) 스트라이크, \(ball) 볼")
     return strike == 3 ? true : false
 }
 
 func gameStart() {
-    let randomNumber = createRandomNumber()
-    var round = 0
-    
-    while round < 9 {
+    let randomNumber = getRandomNumber()
+
+    for round in (0...8).reversed() {
         let userNumber = createUserNumber()
-        
-        if userNumber.count == 3 {
-            if compareStikeBall(randomNumber: randomNumber, userNumber: userNumber) {
-                print("사용자 승리")
-            }
-            round += 1
-            print("남은 기회 : \(9-round)")
+
+        if compareStikeBall(randomNumber: randomNumber, userNumber: userNumber) {
+            print("사용자 승리")
+            return
         }
+        print("남은 기회 : \(round)")
     }
     print("컴퓨터 승리...!")
 }
 
-func createRandomNumber() -> Array<Int> {
+func getRandomNumber() -> Array<Int> {
     var randomNumber = Set<Int>()
-    
+
     while randomNumber.count < 3 {
         randomNumber.insert(Int.random(in: 1...9))
     }
     return Array(randomNumber)
 }
 
+// 3개를 반환하도록 유효성검사
 func createUserNumber() -> Array<Int> {
     print("숫자 3개를 띄어쓰기로 구분해주세요.\n중복숫자는 허용하지 않습니다")
     print("입력 : ", terminator: "")
     var userNumbers = [Int]()
-    
+
     if let userNumber = readLine()?.split(separator: " ") {
         if userNumber.count == 3 {
             if let userNumber1 = Int(userNumber[0]) {
@@ -79,7 +77,7 @@ func createUserNumber() -> Array<Int> {
     if userNumbers.count != 3 {
         print("입력이 잘못되었습니다.")
     }
-    
+
     return userNumbers
 }
 
@@ -87,7 +85,7 @@ func selectMenu() {
     while true {
         print("1. 게임시작"); print("2. 게임종료")
         print("원하는 기능을 선택해주세요 : ", terminator: "")
-        
+
         let menuNumber = readLine()
         if menuNumber == "1" {
             gameStart()
