ff50472bc8777e9363732a729217604e62190446
미니 <cow970814@naver.com>
2022-08-18 21:37:06 +0900

    fix : 사용자 입력 부분 오류 수정

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -1,8 +1,8 @@
-// startGame()
+// MARK: 1. 메뉴 구현 - 우선 클리어
+// MARK: 2. 메뉴 입력 받기 & 오류 처리 - 우선 클리어
+// MARK: 3. 사용자 숫자 입력 받기 - 우선 클리어
+	// TODO: - 오류 처리
 
-// MARK: 1. 메뉴 구현
-// MARK: 2. 메뉴 입력 받기 & 오류 처리
-// MARK: 3. 사용자 숫자 입력 받기 & 오류 처리
 // MARK: 4. 사용자/컴퓨터 승리 시 메뉴 선택으로 이동
 
 private func printMenu() {
@@ -10,14 +10,16 @@ private func printMenu() {
     print("2. 게임종료")
 }
 
-private func getPlayerInput() -> Int {
+private func getPlayerInput() -> Int? {
     print("원하는 기능을 선택해주세요", terminator: " : ")
-	let input = readLine().map { Int($0) ?? 2 }
-    
-    return input ?? 2
+	guard let input = readLine() else {
+		return nil
+	}
+	
+    return Int(input)
 }
 
-private func validateInput(_ playerInput: Int) -> Bool? {
+private func validateInput(_ playerInput: Int?) -> Bool? {
     switch playerInput {
     case 1: return true
     case 2: return false
@@ -30,23 +32,28 @@ private func getPlayerNumbers() -> String? {
 	print("중복 숫자는 허용하지 않습니다.")
 	print("입력", terminator: " : ")
 	let playerNumbers = readLine()
+	validatePlayerNumbers(playerNumbers: playerNumbers)
 	return playerNumbers
 }
 
-private func validatePlayerNumbers(playerNumbers: String) {
-	
+private func validatePlayerNumbers(playerNumbers: String?) {
+	if let playerNumbers = playerNumbers {
+		let inputValues = playerNumbers.split(separator: " ").compactMap { Int($0) }
+		print(inputValues)
+	}
 }
 
 while true {
-    printMenu()
-    let playerInput = getPlayerInput()
-    guard let isCorrectInput = validateInput(playerInput) else {
-        print("입력이 잘못되었습니다.")
-        break
-    }
-    if isCorrectInput {
-        startGame()
-    } else {
-        break
-    }
+	printMenu()
+	
+	if let isCorrectInput = validateInput(getPlayerInput()) {
+		if isCorrectInput {
+			startGame()
+		} else {
+			break
+		}
+	} else {
+		print("입력이 잘못되었습니다.")
+	}
 }
+
