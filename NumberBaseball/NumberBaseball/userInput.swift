ddc35fd8a0cbee47a4fbcf3a0cb9e003eb3bd85f
jeongmin <lj845854@gmail.com>
2022-08-18 23:14:40 +0900

    feat: printMenu 함수 선언

--- NumberBaseball/NumberBaseball/userInput.swift
+++ NumberBaseball/NumberBaseball/userInput.swift
@@ -4,9 +4,18 @@
 //  Copyright © yagom academy. All rights reserved.
 //
 
-func inputMenu() -> String? {
+func inputMenu() -> String {
     guard let menu = readLine() else {
         return ""
     }
     return menu
+}
+
+func printMenu() {
+    let menu = """
+            1. 게임시작
+            2. 게임 종료
+            원하는 기능을 선택해주세요 :
+            """
+    print(menu, terminator: " ")
 }
