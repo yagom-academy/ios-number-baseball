1a0b612f486f9c7ac304d12bbfe03aa44ad65b46
최지혁 <zhzh1x4@gmail.com>
2022-08-18 22:10:41 +0900

    refactor: 임의의 수 관련 변수, 메서드 삭제 및 사용자 입력 변수, 메서드로 교체

--- NumberBaseball/NumberBaseball/NumberBaseballLibrary.swift
+++ NumberBaseball/NumberBaseball/NumberBaseballLibrary.swift
@@ -4,7 +4,6 @@ class NumberBaseballLibrary {
     private var computerRandomNumberArray: [Int] = []
     private var userThreeNumberArray: [Int] = []
     private var numberOfAttempts = 9
-    private var otherComputerRandomNumberArray: [Int] = []
     private var strike: Int = 0
     private var ball: Int = 0
 
@@ -19,10 +18,7 @@ class NumberBaseballLibrary {
 
     private func putNumberInArray() {
         computerRandomNumberArray = generateRandomNumber()
-    }
-
-    private func putTestNumberInArray() {
-        otherComputerRandomNumberArray = generateRandomNumber()
+        print(computerRandomNumberArray)
     }
 
     private func matchIndex(_ x: Int, _ y: Int) -> Bool {
@@ -31,13 +27,13 @@ class NumberBaseballLibrary {
 
     private func matchNumberArray(
         _ computerArray: [Int],
-        _ otherComputerArray: [Int]
+        _ userThreeNumberArray: [Int]
     ) -> (Int, Int)
     {
         for i in 0...2 {
-            if matchIndex(Int(computerArray[i]), Int(otherComputerArray[i])) == true {
+            if matchIndex(Int(computerArray[i]), Int(userThreeNumberArray[i])) == true {
                 strike += 1
-            } else if computerArray.contains(otherComputerArray[i]) {
+            } else if computerArray.contains(userThreeNumberArray[i]) {
                 ball += 1
             }
         }
@@ -51,6 +47,7 @@ class NumberBaseballLibrary {
     
     private func isThreeStrike() {
         if strike == 3 {
+            numberOfAttempts = -1
             return
         } else {
             resetStrikeAndBall()
@@ -59,26 +56,24 @@ class NumberBaseballLibrary {
     }
     
     private func confirmNumberOfAttempts() {
-        if numberOfAttempts != 0 {
+        if numberOfAttempts > 0 {
             print("남은 기회 : \(numberOfAttempts)")
         }
     }
     
     private func judgeWinCondition() {
-        if numberOfAttempts > 0 {
-            print("임의의 수 승리!")
-        } else {
+        if numberOfAttempts == 0 {
             print("컴퓨터 승리...!")
+        } else if numberOfAttempts == -1 {
+            print("사용자 승리!")
         }
     }
     
     private func displayGameStatement() {
         let (outputStrike,outputBall) = matchNumberArray(
             computerRandomNumberArray,
-            otherComputerRandomNumberArray
+            userThreeNumberArray
         )
-        let messeage = otherComputerRandomNumberArray.map { String($0) }.joined(separator: " ")
-        print("임의의 수 : \(messeage)")
         print("\(outputStrike) 스트라이크, \(outputBall) 볼")
     }
     
@@ -86,7 +81,7 @@ class NumberBaseballLibrary {
         putNumberInArray()
         
         while numberOfAttempts > 0 {
-            putTestNumberInArray()
+            userThreeNumberArray = inputUserThreeNumber()
             displayGameStatement()
             isThreeStrike()
             confirmNumberOfAttempts()
@@ -111,7 +106,6 @@ extension NumberBaseballLibrary: NumberBaseballLibraryProtocol {
         let userSelectNumber = inputUserMenuSelect()
         switch userSelectNumber {
         case 1:
-            inputUserThreeNumber()
             playNumberBaseball()
             return true
         case 2:
