ea59b9cfcfec7b06c0ed9852755fd920dcb8789a
jsa0224 <94514250+jsa0224@users.noreply.github.com>
2022-08-18 12:30:57 +0900

    fix: 주석 지우기 & 작성자 추가

--- NumberBaseball/NumberBaseball/NumberBaseballLibrary.swift
+++ NumberBaseball/NumberBaseball/NumberBaseballLibrary.swift
@@ -1,9 +1,4 @@
-//
-//  NumberBaseballLibrary.swift
-//  NumberBaseball
-//
-//  Created by 정선아 on 2022/08/17.
-//
+//  Created by zhilly and som on 2022/08/17.
 
 import Foundation
 
--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -1,3 +1,5 @@
+//  Created by zhilly and som on 2022/08/17.
+
 import Foundation
 
 let numberBaseball:NumberBaseballLibraryProtocol = NumberBaseballLibrary()
