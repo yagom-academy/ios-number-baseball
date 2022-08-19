ace910e0b3246e942de5303a98d0f1a1c66eb8e3
LJ <swtw72@gmail.com>
2022-08-19 23:46:38 +0900

    feat: STEP2 함수 추가 - 함수이름 변경 startGame -> playGame - 함수이름 변경 printRandomNumberArray -> printRandomNumbers - startGame 함수 재정의 - selectMenu 함수 추가 - showMenu 함수 추가 - selectedMenu 변수 추가

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -4,18 +4,16 @@
 //  Copyright © yagom academy. All rights reserved.
 //
 
-import Foundation
-
 var computerNumberArray: Array<Int> = []
 var tryCount: Int = 9
 
 
-func startGame() {
+func playGame() {
     computerNumberArray = createRandomNumbers()
     while tryCount > 0 {
-        let randomNumberArray = createRandomNumbers()
+        let randomNumberArray = createRandomNumbers()   //
+        printRandomNumbers(randomNumberArray)
         
-        printRandomNumberArray(randomNumberArray)
         let strikeAndBall = checkStrikeAndBall(with: randomNumberArray)
         
         print("\n\(strikeAndBall[0]) 스트라이크, \(strikeAndBall[1]) 볼")
@@ -44,11 +42,10 @@ func judgeVictory(by strikeCount: Int){
 }
 
 func printRemainTryCount(strikeCount: Int) {
-    if strikeCount != 3 && tryCount > 0 { //남은기회 출력조건
+    if strikeCount != 3 && tryCount > 0 {
         print("남은 기회 : \(tryCount) \n")
     }
 }
-
 
 func checkStrikeAndBall(with numbers: Array<Int>) -> Array<Int> {
     var strikeCount = 0
@@ -56,20 +53,53 @@ func checkStrikeAndBall(with numbers: Array<Int>) -> Array<Int> {
     
     for (index,number) in numbers.enumerated() {
         guard computerNumberArray.contains(number) else { continue }
+        
         if computerNumberArray[index] == number {
             strikeCount += 1
         } else {
             ballCount += 1
         }
     }
+    
     return [strikeCount, ballCount]
 }
 
-func printRandomNumberArray(_ numbers: Array<Int>) {
+func printRandomNumbers(_ numbers: Array<Int>) {
     print("임의의 수 : ", terminator: "")
     for index in 0...2 {
         print("\(numbers[index])", terminator: " ")
     }
 }
+
+func showMenu() {
+    print("1. 게임 시작")
+    print("2. 게임 종료")
+    print("원하는 기능을 선택해주세요. : ", terminator: "")
+}
+
+func selectMenu() -> String {
+    showMenu()
+    if let input = readLine() {
+        return input
+    }
+    
+    return "3"
+}
+
+func startGame() {
+    var selectedMenu: String
+    repeat {
+        selectedMenu = selectMenu()
+        switch selectedMenu {
+            case "1":
+                playGame()
+            case "2":
+                break
+            default:
+                print("입력이 잘못되었습니다.\n")
+        }
+    } while selectedMenu != "2"
+}
+
 
 startGame()
