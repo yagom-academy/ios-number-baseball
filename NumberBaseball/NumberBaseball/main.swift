9d5f9f001398cc5899004916f73392442058485b
미니 <cow970814@naver.com>
2022-08-19 19:43:32 +0900

    refactor : 게임 함수 재귀함수로 구현 및 메뉴 오류처리 과정 수정

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -1,31 +1,37 @@
-private func receivePlayerInput() -> Int? {
+private func fetchPlayerInput() -> Int? {
 	print("원하는 기능을 선택해주세요", terminator: " : ")
     
-	guard let input = readLine() else {
+	guard let input = readLine(),
+		  let playerInputValue = Int(input) else {
 		return nil
 	}
 	
-	return Int(input)
+	return playerInputValue
 }
 
-private func identify(playerInput: Int?) -> Bool? {
-	switch playerInput {
-	case 1:
-        return true
-	case 2:
-        return nil
-	default:
-        return false
+private func identifyPlayerInput(_ playerInput: Int) {
+	if playerInput == 1 {
+		startGame()
+	} else if playerInput > 2 || playerInput < 1 {
+		errorAction()
 	}
 }
 
-while true {
-    print("1. 게임시작")
-    print("2. 게임종료")
-    
-	if let identifiedInput = identify(playerInput: receivePlayerInput()) {
-		identifiedInput ? startGame() : print("입력이 잘못되었습니다")
-	} else {
-		break
-	}
+private func errorAction() {
+	print("입력이 잘못되었습니다")
+	showMenu()
 }
+
+private func showMenu() {
+	print("1. 게임시작")
+	print("2. 게임종료")
+	
+	guard let value = fetchPlayerInput() else {
+		errorAction()
+		return
+	}
+	
+	identifyPlayerInput(value)
+}
+
+showMenu()
