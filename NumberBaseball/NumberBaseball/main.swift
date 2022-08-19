4faa0902911827d5102d1c52bfe05667aac052c8
lnhk98 <hozzang666@gmai.com>
2022-08-19 12:58:00 +0900

    feat: 사용자 게임 숫자 입력 및 검증하는 함수 getThreeGameNumbers 구현

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -1,5 +1,5 @@
 let targetNumbers: [Int] = generateThreeRandomNumbers()
-showResult()
+showManual()
 
 func generateThreeRandomNumbers() -> [Int] {
     let shuffledNumbers = (1...9).shuffled()
@@ -22,7 +22,7 @@ func countStrikeOrBall(with myNumbers: [Int]) -> (Int, Int) {
 }
 
 func isThreeStrike() -> Bool {
-    let myNumbers: [Int] = generateThreeRandomNumbers()
+    let myNumbers: [Int] = getThreeGameNumbers()
     let convertedMyNumber: String = myNumbers.reduce("") { String($0) + " " + String($1) }
     let (strike, ball) = countStrikeOrBall(with: myNumbers)
     
@@ -53,15 +53,14 @@ func showResult() {
 func showManual() {
     while true {
         print("""
-        1. 게임시작
-        2. 게임종료
-        원하는 기능을 선택해주세요 :
-        """, terminator: " ")
-        
+               1. 게임시작
+               2. 게임종료
+               원하는 기능을 선택해주세요 :
+               """, terminator: " ")
         if let input = readLine(), let menuInput = Int(input) {
             switch menuInput {
             case 1:
-                print("1")
+                showResult()
             case 2:
                 return
             default:
@@ -71,17 +70,38 @@ func showManual() {
     }
 }
 
-//showManual()
-func getThreeGameNumbers() {
+func getThreeGameNumbers() -> [Int] {
+    var threeNumbers = [Int]()
+    
     while true {
         print("""
-        숫자 3개를 띄어쓰기로 구분하여 입력해주세요.
-        중복 숫자는 허용하지 않습니다.
-        입력 :
-        """, terminator: " ")
-        
-        if let input = readLine(), let threeNumbers = Int(input) {
-            
+               숫자 3개를 띄어쓰기로 구분하여 입력해주세요.
+               중복 숫자는 허용하지 않습니다.
+               입력 :
+               """, terminator: " ")
+        guard let userInput = readLine()?.split(separator: " ").map({ String($0) }) else {
+            continue
         }
+        
+        if userInput.count != 3 {
+            print("입력이 잘못되었습니다.")
+            continue
+        }
+        
+        for number in userInput {
+            if number.count == 1, let convertedNumber = Int(number) {
+                threeNumbers.append(convertedNumber)
+            } else {
+                print("입력이 잘못되었습니다.")
+                break
+            }
+        }
+        
+        if threeNumbers.count == 3 {
+            break
+        }
+        
+        threeNumbers.removeAll()
     }
+    return threeNumbers
 }
