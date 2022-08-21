8776caa2afd883d825993f0392a182e26fb29621
LJ <swtw72@gmail.com>
2022-08-21 16:49:01 +0900

    feat: 함수 refactoring - 함수 호출순서대로 코드순서 변경 - getInputNumbers 함수 반환타입 Bool에서 Array<Int>? 로 변경 - playGame 함수의 inputNumbers상수 타입 변경 - checkError 함수 입력값 배열타입으로 변환하는 역할 추가

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -8,21 +8,89 @@ import Foundation
 var computerNumberArray: Array<Int> = []
 var tryCount: Int = 9
 
-func judgeVictory(by strikeCount: Int) -> String {
-    if strikeCount == 3 {
-        print("사용자 승리...!")
-        return "win"
-    } else if tryCount == 0 {
-        print("컴퓨터 승리...!")
-    }
-    
-    return "lose"
+func startGame() {
+    var selectedMenu: String
+    repeat {
+        selectedMenu = selectMenu()
+        
+        switch selectedMenu {
+            case "1":
+                playGame()
+                tryCount = 9
+            case "2":
+                break
+            default:
+                print("입력이 잘못되었습니다.")
+        }
+    } while selectedMenu != "2"
 }
 
-func printRemainTryCount(strikeCount: Int) {
-    if strikeCount != 3 && tryCount >= 0 {
-        print("남은 기회 : \(tryCount) \n")
+func selectMenu() -> String {
+    showMenu()
+    guard let input = readLine() else {
+       return ""
     }
+
+    return input
+}
+
+func showMenu() {
+    print("1. 게임 시작")
+    print("2. 게임 종료")
+    print("원하는 기능을 선택해주세요. : ", terminator: "")
+}
+
+func playGame() {
+    computerNumberArray = createRandomNumbers()
+    while tryCount > 0 {
+        printInputCondition()
+        let inputNumbers: String? = getInputNumbers()
+        guard let intNumbers: Array<Int> = checkError(with: inputNumbers) else { continue }
+        
+        let strikeAndBall = checkStrikeAndBall(with: intNumbers)
+        print("\(strikeAndBall[0]) 스트라이크, \(strikeAndBall[1]) 볼")
+        
+        tryCount -= 1
+        if judgeVictory(by: strikeAndBall[0]) == "win" { return }
+        printRemainTryCount(strikeCount: strikeAndBall[0])
+        
+    }
+}
+
+func createRandomNumbers() -> Array<Int> {
+    var numbersSet: Set<Int> = []
+    while numbersSet.count < 3 {
+        numbersSet.insert(Int.random(in: 1...9))
+    }
+    
+    return Array(numbersSet)
+}
+
+func printInputCondition() {
+    print("숫자 3개를 띄어쓰기로 구분하여 입력해주세요.\n중복 숫자는 허용하지 않습니다.\n입력 : ", terminator: "")
+}
+
+func getInputNumbers() -> String? {
+    guard let input: String = readLine() else {
+        return nil
+    }
+    return input
+}
+
+func checkError(with input: String?) -> Array<Int>? {
+    guard let input = input else { //검증1 잘못입력한 경우, 아무것도 입력 안했을경우
+        return nil
+    }
+    let inputArray: Array<String> = input.components(separatedBy: " ") //string타입 input값을 배열로 반환
+    let convertToNumbers: Array<Int> = inputArray.map { Int($0) }.compactMap { $0 } //string배열을 int배열로 변환, 숫자가 아닌 문자는 nil로 리턴된 후 compactMap에 의해 제거됨
+    let convertToSetNumbers = Set(convertToNumbers)
+    guard convertToSetNumbers.count == 3, //검증2 입력숫자 갯수가 잘못될 경우(중복 제거 후 갯수가 3개가 아니면 false)
+          convertToSetNumbers.filter({ 1...9 ~= $0 }).count == 3 else {
+        print("입력이 잘못되었습니다.")
+        return nil
+    } //검증3 숫자를 잘못입력하거나 범위를 벗어난 경우
+    
+    return convertToNumbers
 }
 
 func checkStrikeAndBall(with numbers: Array<Int>) -> Array<Int> {
@@ -42,83 +110,21 @@ func checkStrikeAndBall(with numbers: Array<Int>) -> Array<Int> {
     return [strikeCount, ballCount]
 }
 
-func checkError(with input: Array<String>) -> Bool {
-    let convertToNumbers: Array<Int> = input.map { Int($0) }.compactMap { $0 }
-    guard input[0] != "", Set(input).count == 3,
-          convertToNumbers.count == 3,
-          convertToNumbers.filter { 1...9 ~= $0 }.count == 3 else { return false }
-    
-    return true
-}
-
-func getInputNumbers() -> Array<String> {
-    if let input: Array<String> = readLine()?.components(separatedBy: " ") {
-        return input
-    }
-    
-    return ["nil input"]
-}
-
-func printInputCondition() {
-    print("숫자 3개를 띄어쓰기로 구분하여 입력해주세요.\n중복 숫자는 허용하지 않습니다.\n입력 : ", terminator: "")
-}
-
-func createRandomNumbers() -> Array<Int> {
-    var numbersSet: Set<Int> = []
-    while numbersSet.count < 3 {
-        numbersSet.insert(Int.random(in: 1...9))
-    }
-    
-    return Array(numbersSet)
-}
-
-func playGame() {
-    computerNumberArray = createRandomNumbers()
-    while tryCount > 0 {
-        printInputCondition()
-        
-        let inputNumbers: Array<String> = getInputNumbers()
-        guard checkError(with: inputNumbers) else { continue }
-        
-        let intNumbers = inputNumbers.map { Int($0) }.compactMap{ $0 }
-        let strikeAndBall = checkStrikeAndBall(with: intNumbers)
-        print("\n\(strikeAndBall[0]) 스트라이크, \(strikeAndBall[1]) 볼")
-        
-        tryCount -= 1
-        if judgeVictory(by: strikeAndBall[0]) == "win" { return }
-        printRemainTryCount(strikeCount: strikeAndBall[0])
+func printRemainTryCount(strikeCount: Int) {
+    if strikeCount != 3 && tryCount >= 0 {
+        print("남은 기회 : \(tryCount)")
     }
 }
 
-func showMenu() {
-    print("1. 게임 시작")
-    print("2. 게임 종료")
-    print("원하는 기능을 선택해주세요. : ", terminator: "")
-}
-
-func selectMenu() -> String {
-    showMenu()
-    if let input = readLine() {
-        return input
+func judgeVictory(by strikeCount: Int) -> String {
+    if strikeCount == 3 {
+        print("사용자 승리...!")
+        return "win"
+    } else if tryCount == 0 {
+        print("컴퓨터 승리...!")
     }
     
-    return " "
-}
-
-func startGame() {
-    var selectedMenu: String
-    repeat {
-        selectedMenu = selectMenu()
-        switch selectedMenu {
-            case "1":
-                playGame()
-                tryCount = 9
-            case "2":
-                break
-            default:
-                print("입력이 잘못되었습니다.\n")
-        }
-    } while selectedMenu != "2"
+    return "lose"
 }
 
 startGame()
