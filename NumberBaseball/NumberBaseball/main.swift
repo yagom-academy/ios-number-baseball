1b50d74d5325a7e49c179607d523fd35612546a8
minsu3007 <minsu3007@gmail.com>
2022-12-19 20:19:11 +0900

    Refactor: 컨벤션 수정, 값 비교기능 수정

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -14,6 +14,7 @@ func createRandomNumber() -> Array<Int> {
     
     while numbers.count < 3 {
         let number = Int.random(in: 1...9)
+        
         if !numbers.contains(number) {
             numbers.append(number)
         }
@@ -25,14 +26,15 @@ func createRandomNumber() -> Array<Int> {
 func compareBall(userBall: Array<Int>, answerBall: Array<Int>) -> [Int] {
     var ball = 0
     var strike = 0
+    
     for index in 0..<userBall.count {
         if userBall[index] == answerBall[index] {
             strike += 1
-        }
-        if userBall[index] != answerBall[index], answerBall.contains(userBall[index]) {
+        } else if userBall[index] != answerBall[index], answerBall.contains(userBall[index]) {
             ball += 1
         }
     }
+    
     return [ball, strike]
 }
 
@@ -41,17 +43,21 @@ func startGame() {
     
     while chance > 0 {
         let userBall = createRandomNumber()
-        chance -= 1
-        print("임의의 수 : \(userBall.map{ String($0) }.joined(separator: " "))")
         let ballStrike = compareBall(userBall: userBall, answerBall: answerBall)
         let ball = ballStrike[0]
         let strike = ballStrike[1]
+        
+        chance -= 1
+        print("임의의 수 : \(userBall.map{ String($0) }.joined(separator: " "))")
         print("\(strike) 스트라이크, \(ball) 볼")
+        
         if strike == 3 {
             print("사용자 승리...!")
             break
         }
+        
         print("남은 기회 : \(chance)")
+        
         if chance == 0 {
             print("컴퓨터 승리...!")
         }
