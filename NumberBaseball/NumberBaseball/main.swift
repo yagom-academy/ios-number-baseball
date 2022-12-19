23cbc6baec03fc8f73a4533653ef5ac7610c40a2
yijiye <jiyee9932@daum.net>
2022-12-19 18:25:09 +0900

    feat: 임의의 정수 3개를 생성하는 함수 구현

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -6,5 +6,18 @@
 
 import Foundation
 
-print("Hello, World!")
+
+
+while true {
+    print(makeThreeBalls())
+    break
+}
+
+func makeThreeBalls() -> Set<Int> {
+    var balls: Set<Int> = []
+    while balls.count < 3 {
+        balls.insert(Int.random(in: 1...9))
+    }
+    return balls
+}
 
