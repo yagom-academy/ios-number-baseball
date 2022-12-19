483c468398dff8215aea417fd9a61c19aae4ec89
kimseongj <ksj8277@naver.com>
2022-12-19 16:44:20 +0900

    1. 드라이버 : 송준, 네비게이터 : Andrew

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -1,10 +1,28 @@
-//
-//  NumberBaseball - main.swift
-//  Created by yagom. 
-//  Copyright © yagom academy. All rights reserved.
-// 
-
 import Foundation
 
-print("Hello, World!")
+func menu() {
+    print("1. 게임시작")
+    print("2. 게임종료")
+    print("원하는 기능을 선택해주세요", terminator: " : ")
+}
 
+func gameStart() {
+    print("숫자 세개를 띄어쓰기로 구분하여 입력해주세요.")
+    print("중복 숫자는 허용하지 않습니다.")
+    print("입력", terminator: " : ")
+    
+//    let inputNumbers:
+}
+
+while true {
+    menu()
+    let inputMenu: String = readLine()!
+    
+    if inputMenu == "1" {
+        gameStart()
+    } else if inputMenu == "2" {
+        break
+    } else {
+        print("입력이 잘못되었습니다.")
+    }
+}
