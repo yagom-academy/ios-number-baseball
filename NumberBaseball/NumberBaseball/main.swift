b53b4295293571b049c06a5a41f98f7c7c81f2a7
ROWAN <kimkj93@naver.com>
2022-12-19 16:37:07 +0900

    feat: 랜덤 정수 생성, 값 비교 기능 추가

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -6,5 +6,33 @@
 
 import Foundation
 
-print("Hello, World!")
+var answerBall : Array<Int> = []
+var chance = 9
 
+func createRandomNumber() -> Array<Int> {
+    var numbers : Array<Int> = []
+    
+    while numbers.count < 3 {
+        let number = Int.random(in: 1...9)
+        if !numbers.contains(number) {
+            numbers.append(number)
+        }
+    }
+    
+    return numbers
+}
+
+func compareBall(userBall: Array<Int>, answerBall: Array<Int>) -> Array<Int> {
+    var ballStrike : Array<Int> = [0, 0]
+    for index in 0..<userBall.count {
+        if userBall[index] == answerBall[index] {
+            ballStrike[1] += 1
+        } else {
+            if answerBall.contains(userBall[index]) {
+                ballStrike[0] += 1
+            }
+        }
+    }
+    
+    return ballStrike
+}
