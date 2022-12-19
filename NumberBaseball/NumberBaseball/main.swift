3668bc717d687b17ebfb76f5d45b38da6624b56b
parkmuri <smxhmu@naver.com>
2022-12-19 18:31:19 +0900

    짝프로그래밍: 베토 - 1

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -6,3 +6,31 @@
 
 import Foundation
 
+let computerNumberArray: [Int]
+var numberOfTry = 9
+func makeRandomNumberArray() -> [Int] {
+    var setNumber: Set<Int> = Set<Int>()
+    while setNumber.count != 3 {
+        setNumber.insert(Int.random(in: 1...9))
+    }
+    return Array(setNumber)
+}
+func countBallStrike(computerArray: [Int], numberArray: [Int]) -> (Int, Int) {
+    var countStrike = 0
+    var countBall = 0
+    for index in 0..<3 {
+        guard let findIndex = numberArray.firstIndex(of: computerArray[index]) else {
+            continue
+        }
+        if index == findIndex {
+            countStrike += 1
+        } else {
+            countBall += 1
+        }
+    }
+    return (countStrike, countBall)
+}
+computerNumberArray = makeRandomNumberArray()
+while numberOfTry != 0 {
+    let randomNumberArray
+}
