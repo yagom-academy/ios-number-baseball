47259f93d01ff2647db0c101f1a99e5d6d63d1cf
whk1140 <whk1140@gmail.com>
2022-12-19 18:12:03 +0900

    4.드라이버:Andrew,네비게이터:송중

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -1,8 +1,8 @@
 import Foundation
 
 var chances: Int = 9
-
 var randomNumbers = [String]()
+var selectedNumbers = [String]()
 
 func presentMenu() {
     print("1. 게임시작")
@@ -13,11 +13,14 @@ func presentMenu() {
 func startGame() {
     print("숫자 세개를 띄어쓰기로 구분하여 입력해주세요.")
     print("중복 숫자는 허용하지 않습니다.")
-    print("입력", terminator: " : ")
-    
-    var inputNumbers = readLine()!.split(separator: " ")
+}
 
-    if inputNumbers.count == 3 {
+func inputNumbers() {
+    print("입력", terminator: " : ")
+
+    selectedNumbers = readLine()!.components(separatedBy: " ")
+    
+    if selectedNumbers.count == 3 {
         
     } else {
         print("입력이 잘못되었습니다.")
@@ -25,20 +28,36 @@ func startGame() {
 }
 
 
-
 func makeRandomNumbers() {
     randomNumbers.append(String(Int.random(in: 1...9)))
+    randomNumbers.append(String(Int.random(in: 1...9)))
+    
+    while randomNumbers[0] == randomNumbers[1] {
+        randomNumbers[1] = String(Int.random(in: 1...9))
+    }
+    
+    randomNumbers.append(String(Int.random(in: 1...9)))
+    
+    while randomNumbers[0] == randomNumbers[2] || randomNumbers[1] == randomNumbers[2] {
+        randomNumbers[2] = String(Int.random(in: 1...9))
+    }
+    
 }
 
-while true {
-    presentMenu()
-    let inputMenu: String = readLine()!
-    
-    if inputMenu == "1" {
-        startGame()
-    } else if inputMenu == "2" {
-        break
-    } else {
-        print("입력이 잘못되었습니다.")
-    }
+func checkStrike() {
+    randomNumbers
+    selectedNumbers
 }
+
+//while true {
+//    presentMenu()
+//    let inputMenu: String = readLine()!
+//
+//    if inputMenu == "1" {
+//        startGame()
+//    } else if inputMenu == "2" {
+//        break
+//    } else {
+//        print("입력이 잘못되었습니다.")
+//    }
+//}
