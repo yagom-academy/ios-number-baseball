71b328d4a0ba3f570bf239d6292fbe57c5622fa7
kimseongj <ksj8277@naver.com>
2022-12-19 17:21:03 +0900

    3. 드라이버: 송중, 네비게이터: Andrew

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -1,31 +1,41 @@
 import Foundation
 
 var chances: Int = 9
-var inputNumbers = [String]()
+
 var randomNumbers = [String]()
 
-func menu() {
+func presentMenu() {
     print("1. 게임시작")
     print("2. 게임종료")
     print("원하는 기능을 선택해주세요", terminator: " : ")
 }
 
-func gameStart() {
+func startGame() {
     print("숫자 세개를 띄어쓰기로 구분하여 입력해주세요.")
     print("중복 숫자는 허용하지 않습니다.")
     print("입력", terminator: " : ")
+    
+    var inputNumbers = readLine()!.split(separator: " ")
+
+    if inputNumbers.count == 3 {
+        
+    } else {
+        print("입력이 잘못되었습니다.")
+    }
 }
+
+
 
 func makeRandomNumbers() {
     randomNumbers.append(String(Int.random(in: 1...9)))
 }
 
 while true {
-    menu()
+    presentMenu()
     let inputMenu: String = readLine()!
     
     if inputMenu == "1" {
-        gameStart()
+        startGame()
     } else if inputMenu == "2" {
         break
     } else {
