1aaa20f32f4cb9d1311f0367d740133514a2962e
이주명 <gzzjk159@naver.com>
2022-12-20 15:38:49 +0900

    메뉴 선택 함수 추가

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -33,29 +33,56 @@ func countBallStrike(computerArray: [Int], numberArray: [Int]) -> (Int, Int) {
     return (countStrike, countBall)
 }
 
-computerNumberArray = makeRandomNumberArray()
+//computerNumberArray = makeRandomNumberArray()
+//
+//while numberOfTry != 0 {
+//    let randomNumberArray: [Int]
+//    let result: (Int, Int)
+//    randomNumberArray = makeRandomNumberArray()
+//    result = countBallStrike(computerArray: computerNumberArray, numberArray: randomNumberArray)
+//    let countStrike = result.0
+//    let countBall = result.1
+//    print("임의의 수 : ", terminator: "")
+//    for number in randomNumberArray {
+//        print(number, terminator: " ")
+//    }
+//    print("\n\(countStrike) 스트라이크, \(countBall) 볼")
+//    if countStrike == 3 {
+//        break
+//    }
+//    numberOfTry -= 1
+//    print("남은 기회 \(numberOfTry)")
+//}
+//
+//if numberOfTry > 0 {
+//    print("사용자 승리!")
+//} else {
+//    print("컴퓨터 승리...")
+//}
 
-while numberOfTry != 0 {
-    let randomNumberArray: [Int]
-    let result: (Int, Int)
-    randomNumberArray = makeRandomNumberArray()
-    result = countBallStrike(computerArray: computerNumberArray, numberArray: randomNumberArray)
-    let countStrike = result.0
-    let countBall = result.1
-    print("임의의 수 : ", terminator: "")
-    for number in randomNumberArray {
-        print(number, terminator: " ")
+func selectMenu() {
+    print("원하는 기능을 선택해주세요 : ", terminator: "")
+
+    if let userInput = readLine() {
+        if userInput == "1" {
+            startGame()
+        } else if userInput == "2" {
+            print("게임 종료")
+        } else {
+            print("입력이 잘못 되었습니다.")
+        }
     }
-    print("\n\(countStrike) 스트라이크, \(countBall) 볼")
-    if countStrike == 3 {
-        break
-    }
-    numberOfTry -= 1
-    print("남은 기회 \(numberOfTry)")
 }
 
-if numberOfTry > 0 {
-    print("사용자 승리!")
-} else {
-    print("컴퓨터 승리...")
+func startGame() {
+    print("숫자 3개를 띄어쓰기로 구분하여 입력해주세요.\n중복숫자는 허용하지 않습니다.")
+    print("입력 : ", terminator: "")
+    var userNumberArray: [Int]
+    if let inputValue = readLine() {
+        let numbers = inputValue.split(separator: " ")
+//        userNumberArray[0] = numbers
+        print(numbers)
+    }
 }
+
+selectMenu()
