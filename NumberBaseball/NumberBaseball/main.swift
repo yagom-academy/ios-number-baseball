830cdee12248855db9de927edfbc7761eff0371c
HarryHyeon <hharry0728@gmail.com>
2022-12-20 17:34:33 +0900

    feat: checkStrike 함수 추가

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -6,12 +6,25 @@
 
 import Foundation
 
-var comNums = Set<Int>()
 var chance = 9
 
-func createRandNums() {
-    while comNums.count < 3 {
-        let randNum = Int.random(in: 1...9)
-        comNums.insert(randNum)
+func createRandomNumbers() -> [Int] {
+    var randomNumbers = Set<Int>()
+    while randomNumbers.count < 3 {
+        randomNumbers.insert(Int.random(in: 1...9))
     }
+    return Array(randomNumbers)
+}
+
+func checkStrike(_ computerNumbers: [Int], _ userNumbers: [Int]) -> (Int, Int) {
+    var strike = 0
+    var ball = 0
+    for index in 0..<3 {
+        if computerNumbers[index] == userNumbers[index] {
+            strike += 1
+        } else {
+            ball += 1
+        }
+    }
+    return (strike, ball)
 }
