c26edc2c62be0688904f8ecb2c749fb91416e3da
minsu3007 <minsu3007@gmail.com>
2022-12-20 15:48:01 +0900

    feat: displayMenu, refactor: readMenu

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -6,82 +6,99 @@
 
 import Foundation
 
-var answerBall: Array<Int> = []
-var chance = 9
-
-func createRandomNumber() -> Array<Int> {
-    var numbers: Array<Int> = []
+class baseballGame {
+    var answerBall: Array<Int> = []
+    var chance = 9
     
-    while numbers.count < 3 {
-        let number = Int.random(in: 1...9)
+    func createRandomNumber() -> Array<Int> {
+        var numbers: Array<Int> = []
         
-        if numbers.contains(number) == false {
-            numbers.append(number)
+        while numbers.count < 3 {
+            let number = Int.random(in: 1...9)
+            
+            if numbers.contains(number) == false {
+                numbers.append(number)
+            }
         }
+        
+        return numbers
     }
     
-    return numbers
-}
-
-func compareBall(userBall: Array<Int>, answerBall: Array<Int>) -> (Int, Int) {
-    var ball = 0
-    var strike = 0
-    
-    for index in 0..<userBall.count {
-        if userBall[index] == answerBall[index] {
-            strike += 1
-        } else if userBall[index] != answerBall[index], answerBall.contains(userBall[index]) {
-            ball += 1
+    func compareBall(userBall: Array<Int>, answerBall: Array<Int>) -> (Int, Int) {
+        var ball = 0
+        var strike = 0
+        
+        for index in 0..<userBall.count {
+            if userBall[index] == answerBall[index] {
+                strike += 1
+            } else if userBall[index] != answerBall[index], answerBall.contains(userBall[index]) {
+                ball += 1
+            }
         }
+        
+        return (ball, strike)
     }
     
-    return (ball, strike)
-}
-
-func readMenu() -> Bool {
-    let inputValue = readLine()
-    
-    guard let menu = inputValue else {
+    func readMenu() throws -> Bool {
+        let inputValue = readLine()
+        
+        guard let menu = inputValue else {
+            return false
+        }
+        
+        if menu == "1" {
+            return true
+        }
+        
         return false
     }
     
-    if menu == "1" {
-        return true
+    func displayMenu() {
+        print("""
+            1. 게임시작
+            2. 게임종료
+            원하는 기능을 선택해주세요 :
+            """)
+        
+        do {
+            try self.readMenu()
+        } catch InputError.wrongInput {
+            print("입력이 잘못되었습니다")
+        } catch {
+            print(error)
+        }
     }
     
-    return false
-}
-
-func startGame() {
-    answerBall = createRandomNumber()
+    func startGame() {
+        answerBall = createRandomNumber()
+        
+        while chance > 0 {
+            let userBall = createRandomNumber()
+            let (ball, strike) = compareBall(userBall: userBall, answerBall: answerBall)
+            
+            chance -= 1
+            print("임의의 수 : \(userBall.map{ String($0) }.joined(separator: " "))")
+            print("\(strike) 스트라이크, \(ball) 볼")
+            
+            if let winner = decideWinner(strike: strike, chance: chance) {
+                print("\(winner) 승리...!")
+                break
+            }
+            
+            print("남은 기회 : \(chance)")
+        }
+    }
     
-    while chance > 0 {
-        let userBall = createRandomNumber()
-        let (ball, strike) = compareBall(userBall: userBall, answerBall: answerBall)
+    func decideWinner(strike: Int, chance: Int) -> String? {
+        var winner: String?
         
-        chance -= 1
-        print("임의의 수 : \(userBall.map{ String($0) }.joined(separator: " "))")
-        print("\(strike) 스트라이크, \(ball) 볼")
-        
-        if let winner = decideWinner(strike: strike, chance: chance) {
-            print("\(winner) 승리...!")
-            break
+        if strike == 3 {
+            winner = "사용자"
+        } else if chance == 0 {
+            winner = "컴퓨터"
         }
         
-        print("남은 기회 : \(chance)")
+        return winner
     }
 }
 
-func decideWinner(strike: Int, chance: Int) -> String? {
-    var winner: String?
-    
-    if strike == 3 {
-        winner = "사용자"
-    } else if chance == 0 {
-        winner = "컴퓨터"
-    }
-    
-    return winner
-}
-
-startGame()
