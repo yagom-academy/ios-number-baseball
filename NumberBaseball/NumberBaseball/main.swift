d7e3d85fd0ac9218bc76e5503b8651ebc41fff48
Jun <sa9343@naver.com>
2022-12-21 21:56:12 +0900

    Refactor : Step2 코드로 전환
    기존 메인 코드 Step2 코드로 전환과정

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -23,28 +23,40 @@ func makeRandomNumbers() -> [Int] {
 
 func playGame() {
     var leftChances = 9
+    var inputNumbers : [Int] = []
     var result : [Int] = []
 
     while leftChances > 0 {
-        let randomNumbers = makeRandomNumbers()
-        result = checkResult(numbers: randomNumbers)
+        print("숫자 3개를 띄어쓰기로 구분하여 입력해주세요.")
+        print("중복 숫자는 허용하지 않습니다.")
+        print("입력 : ", terminator: " ")
+        
+        let inputData = readLine()
+        inputNumbers = checkAvailability(inputData: inputData)
+        guard inputNumbers.isEmpty == false else {
+            continue
+        }
+        
+        result = checkStrikeBall(numbers: inputNumbers)
+        
         let strike = result[0]
         let ball = result[1]
-
+        
         if strike == 3 {
-            print("정답 입니다!")
+            print("사용자 승리!")
             return
         }
+        
         leftChances -= 1
-
-        print("임의의 수 : \(randomNumbers[0]), \(randomNumbers[1]), \(randomNumbers[2])")
+        
         print("\(strike) 스트라이크, \(ball) 볼")
         print("남은 기회 : \(leftChances)")
+        print(" ")
     }
     print("컴퓨터의 승리입니다.")
 }
 
-func checkResult(numbers: [Int]) -> [Int] {
+func checkStrikeBall(numbers: [Int]) -> [Int] {
     var strike = 0
     var ball = 0
     
@@ -58,4 +70,63 @@ func checkResult(numbers: [Int]) -> [Int] {
     return [strike,ball]
 }
 
-playGame()
+func checkAvailability(inputData: String?) -> [Int] {
+    
+    guard let inputData = inputData?.split(separator: " ") else {
+        print("입력 형식이 잘못 되었습니다! 띄어쓰기를 준수해 주세요.")
+        return []
+    }
+    
+    let numberPattern = "^([1-9])$"
+    for data in inputData {
+        guard let _ = data.range(of: numberPattern, options: .regularExpression) else {
+            print("입력 형식이 잘못 되었습니다! 숫자만 입력해 주세요.")
+            return[]
+        }
+    }
+    
+    guard inputData.count == 3 else {
+        print("입력 갯수가 잘못 되었습니다!")
+        return []
+    }
+    
+    let inputNumbers = inputData.compactMap { str in Int(str) }
+    
+    for number in inputNumbers {
+        guard number > 0 && number < 10 else {
+            print("범위를 벗어 났습니다!")
+            return []
+        }
+    }
+    
+    guard Set(inputNumbers).count == 3 else {
+        print("중복된 숫자가 있습니다!")
+        return []
+    }
+    return inputNumbers
+}
+
+func menuPrint() {
+    while true {
+        print("1. 게임시작")
+        print("2. 게임종료")
+        print("원하는 기능을 선택해주세요 : ", terminator: " ")
+        guard let inputData = readLine() else {
+            print("입력이 잘못되었습니다")
+            continue
+        }
+        
+        switch inputData {
+        case "1" :
+            playGame()
+        case "2" :
+            print("게임을 종료합니다")
+            return
+        default :
+            print("입력이 잘못되었습니다")
+            continue
+        }
+    }
+}
+
+menuPrint()
