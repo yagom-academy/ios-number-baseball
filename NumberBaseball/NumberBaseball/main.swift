9aaec43db580a1a166aaf2f834f52fd56569dc72
yijiye <jiyee4013@naver.com>
2022-12-21 13:23:34 +0900

    fix: STEP1 PR 리뷰 수정사항 반영

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -6,50 +6,59 @@
 
 import Foundation
 
-var computerBalls = makeThreeBalls()
-var userBalls: Set<Int> = []
-var restOfTimes: Int = 9
+var computerNumbers = makeThreeNumbers()
+var userNumbers: [Int] = []
+var leftCount: Int = 9
+var isUserWin: Bool = false
 
-while restOfTimes > 0 {
-    userBalls = makeThreeBalls()
-    checkBalls(userBalls)
-}
-
-func makeThreeBalls() -> Set<Int> {
-    var balls: Set<Int> = []
-    while balls.count < 3 {
-        balls.insert(Int.random(in: 1...9))
-    }
-    return balls
-}
-
-func checkBalls(_ userBalls: Set<Int>) {
-    var strike = 0
-    var ball = 0
-    let computerBallsArray = Array(computerBalls)
-    let userBallsArray = Array(userBalls)
-    
-    print("임의의 수 : ", terminator: "")
-    userBalls.forEach{print("\($0)", terminator: " ")}
-    
-    (0..<computerBallsArray.count).forEach{ index in
-        if computerBallsArray[index] == userBallsArray[index] {
-            strike += 1
-            return
+func playBaseBallGame() {
+    while leftCount > 0 {
+        var strikeCount: Int = 0
+        var ballCount: Int = 0
+        userNumbers = makeThreeNumbers()
+        (strikeCount, ballCount) = compareNumbers(userNumbers, with: computerNumbers)
+        leftCount -= 1
+        print("\n\(strikeCount) 스트라이크, \(ballCount) 볼")
+        if leftCount != 0 {
+            print("남은 기회 : \(leftCount)")
         }
-        guard computerBallsArray.contains(userBallsArray[index]) else {
-            return
+        if strikeCount == 3 {
+            isUserWin = true
+            break
         }
-        ball += 1
     }
-    restOfTimes -= 1
-    print("\n\(strike) 스트라이크, \(ball) 볼")
-    if restOfTimes != 0 {
-        print("남은 기회 : \(restOfTimes)")
+    if isUserWin {
+        print("사용자 승리")
     } else {
         print("컴퓨터 승리...!")
     }
-    if strike == 3 {
-        print("사용자 승리!")
-    }    
 }
+
+func makeThreeNumbers() -> Array<Int> {
+    var numbers: Set<Int> = []
+    while numbers.count < 3 {
+        numbers.insert(Int.random(in: 1...9))
+    }
+    return Array(numbers)
+}
+
+func compareNumbers(_ userNumbers: [Int], with computerNumbers: [Int]) -> (Int, Int) {
+    var strikeCount: Int = 0
+    var ballCount: Int = 0
+    print("임의의 수 : ", terminator: "")
+    userNumbers.forEach{print("\($0)", terminator: " ")}
+    
+    (0..<computerNumbers.count).forEach{ index in
+        guard computerNumbers[index] != userNumbers[index] else {
+            strikeCount += 1
+            return
+        }
+        guard !computerNumbers.contains(userNumbers[index]) else {
+            ballCount += 1
+            return
+        }
+    }
+    return (strikeCount, ballCount)
+}
+
+playBaseBallGame()
