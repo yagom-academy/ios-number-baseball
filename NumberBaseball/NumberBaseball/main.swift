377cfe682e0ae3caf7ee5ab442bb9183ead5e0f2
이주명 <gzzjk159@naver.com>
2022-12-21 11:49:52 +0900

    feat: 임의의 정수 3개 반환하는 함수, 컴퓨터의 정수 3개 저장하는 변수

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -1,10 +1,34 @@
-//
-//  NumberBaseball - main.swift
-//  Created by yagom. 
-//  Copyright © yagom academy. All rights reserved.
-// 
+
+//  Created by vetto, 무리
+//  2022.12.19 ~ 2022.12.23
 
 import Foundation
 
-print("Hello, World!")
+func startGame() {
+    let computerNumberArray: [Int] = makeRandomNumberArray()
+    var numberOfTry: Int = 9
+    
+    while numberOfTry > 0 {
+        let randomNumberArray = makeRandomNumberArray()
+        print("임의의 수 : \(randomNumberArray)")
+    }
+}
 
+func makeRandomNumberArray() -> [Int] {
+    var randomSetNumber: Set<Int> = Set<Int>()
+    
+    while randomSetNumber.count < 3 {
+        randomSetNumber.insert(Int.random(in: 1...9))
+    }
+    
+    return Array(randomSetNumber)
+}
+
+func countBallStrike(computerNumberArray: [Int], randomNumberArray: [Int]) -> (Int, Int) {
+    var numberOfStrike: Int = 0
+    var numberOfBall: Int = 0
+    
+    
+    
+    return (numberOfStrike, numberOfBall)
+}
