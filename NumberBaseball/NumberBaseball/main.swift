6ed5a071950de7503666aaf99bf56d7da0f144b6
whk1140 <whk1140@gmail.com>
2022-12-22 14:52:26 +0900

    refactor: 전역변수명, fix: makerandomNumber 함수, feat: initializeData함수

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -3,55 +3,55 @@ import Foundation
 var chances: Int = 9
 var userNumbers = [String]()
 var computerNumbers = [String]()
-var userWin: Bool = false
-var checkingError: Bool = false
+var isUserWin: Bool = false
+var hasError: Bool = false
 
-//MARK: 랜덤 숫자 만들기
 func makeRandomNumbers() -> [String] {
-    var randomNumbers = [String]()
+    var randomNumbers = Set<String>()
     
-    while Set(randomNumbers).count != 3 {
-        randomNumbers.append(String(Int.random(in: 1...9)))
+    while randomNumbers.count != 3 {
+        randomNumbers.insert(String(Int.random(in: 1...9)))
     }
     
-    return randomNumbers
+    let randomNumberArray = Array(randomNumbers)
+    
+    return randomNumberArray
 }
 
-// MARK: 숫자 입력받기
 func inputUserNumbers() {
+    print(computerNumbers)
+    print(chances)
     print("숫자 3개를 띄어쓰기로 구분하여 입력해주세요.")
     print("중복 숫자는 허용하지 않습니다.")
     print("입력", terminator: " : ")
-    
-    guard let inputtedUserNumber = readLine() else {
+    guard let inputtedUserNumbers = readLine() else {
         print("you entered nil")
-        
+
         return
     }
     
-    let inputNumbers = inputtedUserNumber.components(separatedBy: " ")
-    let checkingNumberRange = inputNumbers.joined().range(of: "^[1-9]{3,3}$", options: .regularExpression) != nil
+    let userNumberArray = inputtedUserNumbers.components(separatedBy: " ")
+    let checkingNumberRange = userNumberArray.joined().range(of: "^[1-9]{3,3}$", options: .regularExpression) != nil
     
-    if Set(inputNumbers).count != 3 || checkingNumberRange == false {
+    if Set(userNumberArray).count != 3 || checkingNumberRange == false {
         print("입력이 잘못되었습니다.")
-        checkingError = true
+        hasError = true
         
         return
     }
     
-    userNumbers = inputNumbers
+    userNumbers = userNumberArray
 }
 
-//MARK: 스트라이크 볼 확인하기
 func checkStrikeAndBall() {
     var ballCounter: Int = 0
     var strikeCounter: Int = 0
     chances -= 1
     
-    for number in 0...2 {
-        if computerNumbers[number] == userNumbers[number] {
+    for index in 0...2 {
+        if computerNumbers[index] == userNumbers[index] {
             strikeCounter += 1
-        } else if computerNumbers.contains(userNumbers[number]) == true {
+        } else if computerNumbers.contains(userNumbers[index]) == true {
             ballCounter += 1
         }
     }
@@ -60,29 +60,36 @@ func checkStrikeAndBall() {
     print("남은 기회 : \(chances)")
     
     if strikeCounter == 3 {
-        userWin = true
+        isUserWin = true
     }
+    
 }
 
-// MARK: 승리자 프린트
 func printWinner() {
     if chances == 0 {
         print("컴퓨터 승리...!")
     }
-    
-    if  userWin == true {
-        chances = -1
+    if isUserWin == true {
+        chances = 0
         print("사용자 승리...!")
     }
+}
+
+func initializeData() {
+    chances = 9
+    userNumbers = [String]()
+    computerNumbers = [String]()
+    isUserWin = false
+    hasError = false
 }
 
 func startGame() {
     computerNumbers = makeRandomNumbers()
     
     repeat {
-        checkingError = false
+        hasError = false
         inputUserNumbers()
-        if checkingError == true {
+        if hasError == true {
             continue
         }
         checkStrikeAndBall()
@@ -117,6 +124,7 @@ func startBaseBallGame() {
     var inputedMenuNumber: String
     
     repeat {
+        initializeData()
         presentMenu()
         inputedMenuNumber = inputMenuNumber()
         selectMenu(selectedMenuNunber: inputedMenuNumber)
