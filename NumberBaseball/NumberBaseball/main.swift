99b9f36a18ea109391e37ad2715fde1102492bd1
HarryHyeon <hharry0728@gmail.com>
2022-12-22 12:39:31 +0900

    refactor: 개행 수정 및 게임시작함수 승리 출력 코드 위치 변경

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -7,7 +7,9 @@
 import Foundation
 
 func createRandomNumbers() -> [Int] {
+    
     var randomNumbers = Set<Int>()
+    
     while randomNumbers.count < 3 {
         randomNumbers.insert(Int.random(in: 1...9))
     }
@@ -15,19 +17,23 @@ func createRandomNumbers() -> [Int] {
 }
 
 func setUserNums() -> [Int] {
+    
     while true {
         print("""
         숫자 3개를 띄어쓰기로 구분하여 입력해주세요.
         중복 숫자는 허용하지 않습니다.
         """)
         print("입력 : ", terminator: "")
+        
         guard let input = readLine() else { return Array<Int>() }
         if input == "" {
             print("입력이 잘못되었습니다.")
             continue
         }
+        
         let numArray = input.split(separator: " ").map{ Int($0) ?? 0 }
         let filteredNums = numArray.filter{ $0 > 0 && $0 < 10 }
+        
         if Set(filteredNums).count != 3 {
             print("입력이 잘못되었습니다.")
             continue
@@ -36,9 +42,11 @@ func setUserNums() -> [Int] {
     }
 }
 
-func checkStrike(_ computerNumbers: [Int], _ userNumbers: [Int]) -> (Int, Int) {
+func checkStrike(computerNumbers: [Int], userNumbers: [Int]) -> (Int, Int) {
+    
     var strike = 0
     var ball = 0
+    
     for index in 0..<3 {
         if computerNumbers[index] == userNumbers[index] {
             strike += 1
@@ -52,27 +60,30 @@ func checkStrike(_ computerNumbers: [Int], _ userNumbers: [Int]) -> (Int, Int) {
 func startGame() {
     
     var chance = 9
+    
     while chance > 0 {
         let comNums = createRandomNumbers()
         let userNums = setUserNums()
         
-        let (strike, ball) = checkStrike(comNums, userNums)
+        let (strike, ball) = checkStrike(computerNumbers: comNums, userNumbers: userNums)
+        
         chance -= 1
         
         print("\(strike) 스트라이크, \(ball) 볼")
-        print("남은 기회 : \(chance)")
-        
         
         if strike == 3 {
             print("사용자 승리...!")
             break
         } else if chance == 0 {
             print("컴퓨터 승리...!")
+            break
         }
+        print("남은 기회 : \(chance)")
     }
 }
 
 func selectMenu() -> Bool {
+    
     print("""
           1. 게임시작
           2. 게임종료
@@ -83,6 +94,7 @@ func selectMenu() -> Bool {
         print("입력이 잘못되었습니다")
         return selectMenu()
     }
+    
     switch Int(selectedMenu) {
     case 1:
         return true
