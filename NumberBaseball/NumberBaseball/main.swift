8ff85079a6d7c55a941d7d7cc701fd0b6cab9844
kaki <ycg02109@naver.com>
2022-12-22 15:08:05 +0900

    feat: STEP 1 수정사항 반영

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -1,127 +1,124 @@
+//
+//  NumberBaseball - main.swift
+//  Created by yagom.
+//  Copyright © yagom academy. All rights reserved.
+//
+
 import Foundation
 
 enum InputError: Error {
-    case invalidFuncNum
+    case invalidNumber
 }
 
-var winNumbers: Set<Int> = []
-var funcInt = 0
+var winNumberArray: [UInt] = []
+var userNumberArray: [UInt] = []
+var strikeNumber = 0
+var ballNumber = 0
 var chance = 9
-var winNumArr: [Int] = []
-var userNumArr: [UInt] = []
-var strikeNum = 0
-var ballNum = 0
+var menuNumber = 0
 
-while winNumbers.count < 3 {
-    winNumbers.insert(Int.random(in: 1...9))
+func createRandomNumbers() -> [UInt] {
+    var randomNumbers: Set<UInt> = []
+    
+    while randomNumbers.count < 3 {
+        randomNumbers.insert(UInt.random(in: 1...9))
+    }
+    
+    return Array(randomNumbers)
 }
 
-winNumArr = Array(winNumbers)
-
-func countStrike() {
-    if winNumArr[0] == userNumArr[0] {
-        strikeNum += 1
-    }
-    if winNumArr[1] == userNumArr[1] {
-        strikeNum += 1
-    }
-    if winNumArr[2] == userNumArr[2] {
-        strikeNum += 1
-    }
-}
-
-func countBall() {
-    if winNumArr[0] == userNumArr[1]  {
-        ballNum += 1
-    }
-    if winNumArr[0] == userNumArr[2]  {
-        ballNum += 1
-    }
-    if winNumArr[1] == userNumArr[0]  {
-        ballNum += 1
-    }
-    if winNumArr[2] == userNumArr[0]  {
-        ballNum += 1
-    }
-    if winNumArr[1] == userNumArr[2]  {
-        ballNum += 1
-    }
-    if winNumArr[2] == userNumArr[1]  {
-        ballNum += 1
+func countStrikeBall() {
+    for index in 0 ..< winNumberArray.count {
+        if winNumberArray[index] == userNumberArray[index] {
+            strikeNumber += 1
+        } else if winNumberArray.contains(userNumberArray[index]) {
+            ballNumber += 1
+        }
     }
 }
 
 func menuInputOutput() throws {
     print("1. 게임시작\n2. 게임종료\n원하는 기능을 선택해주세요 : ", terminator: "")
-    let funcStr = readLine()
-    guard let funcString = funcStr else {
-        throw InputError.invalidFuncNum
+    
+    let inputMenu = readLine()
+    
+    guard let menu = inputMenu else {
+        throw InputError.invalidNumber
     }
-    guard funcString == "1" || funcString == "2" else {
-        throw InputError.invalidFuncNum
+    
+    guard menu == "1" || menu == "2" else {
+        throw InputError.invalidNumber
     }
-    let funcIntOptional = Int(funcString)
-    if let funcInt1 = funcIntOptional {
-        funcInt = funcInt1
+    
+    let menuIntOptional = Int(menu)
+    
+    if let menuInt = menuIntOptional {
+        menuNumber = menuInt
     }
 }
 
 func inputNumbers() {
     print("숫자 3개를 띄어쓰기로 구분하여 입력해주세요.\n중복 숫자는 허용하지 않습니다.\n입력 : ", terminator: "")
-    guard let inputNum = readLine() else {
+    
+    guard let inputNumber = readLine() else {
         print("입력이 잘못되었습니다")
         return inputNumbers()
     }
-    let inputNumArr = inputNum.components(separatedBy: " ").map{ UInt($0) }
     
-    for num in inputNumArr {
-        if let number = num {
-            userNumArr.append(number)
+    let inputNumberArray = inputNumber.components(separatedBy: " ").map{ UInt($0) }
+    
+    for number in inputNumberArray {
+        if let number = number {
+            userNumberArray.append(number)
         }
     }
     
-    guard userNumArr.count == 3 else {
+    guard userNumberArray.count == 3 else {
         print("입력이 잘못되었습니다")
-        userNumArr = []
+        userNumberArray = []
         return inputNumbers()
     }
     
-    if userNumArr[0] > 9 {
+    if userNumberArray[0] > 9 {
         print("입력이 잘못되었습니다")
-        userNumArr = []
+        userNumberArray = []
         return inputNumbers()
-    } else if userNumArr[1] > 9 {
+    } else if userNumberArray[1] > 9 {
         print("입력이 잘못되었습니다")
-        userNumArr = []
+        userNumberArray = []
         return inputNumbers()
-    } else if userNumArr[2] > 9 {
+    } else if userNumberArray[2] > 9 {
         print("입력이 잘못되었습니다")
-        userNumArr = []
+        userNumberArray = []
         return inputNumbers()
     }
     
-    let userNumSet = Set(userNumArr)
-    let userNumArray = Array(userNumSet)
+    let userNumberSet = Set(userNumberArray)
+    let userNumbers = Array(userNumberSet)
     
-    guard userNumArray.count == 3 else {
+    guard userNumbers.count == 3 else {
         print("입력이 잘못되었습니다")
-        userNumArr = []
+        userNumberArray = []
         return inputNumbers()
     }
 }
 
 func playGame() throws {
+    winNumberArray = createRandomNumbers()
+    
     for _ in 1...9 {
         inputNumbers()
-        countStrike()
-        if strikeNum == 3 {
+        countStrikeBall()
+        chance -= 1
+        
+        print("\(strikeNumber) 스트라이크, \(ballNumber) 볼")
+        
+        if strikeNumber == 3 {
             print("사용자 승리!")
             try startGame()
             break
         }
-        countBall()
-        chance -= 1
-        print("\(strikeNum) 스트라이크, \(ballNum) 볼")
+        
         if chance != 0 {
             print("남은기회 : \(chance)")
         }
@@ -129,21 +126,27 @@ func playGame() throws {
             print("컴퓨터 승리...!")
             try startGame()
         }
-        strikeNum = 0
-        ballNum = 0
-        userNumArr = []
+        strikeNumber = 0
+        ballNumber = 0
+        userNumberArray.removeAll()
     }
 }
 
 func startGame() throws{
     do {
         try menuInputOutput()
-        if funcInt == 2 {
+        
+        chance = 9
+        strikeNumber = 0
+        ballNumber = 0
+        userNumberArray.removeAll()
+        
+        if menuNumber == 2 {
             return
         }
         try playGame()
     }
-    catch InputError.invalidFuncNum {
+    catch InputError.invalidNumber {
         print("입력이 잘못되었습니다")
         try startGame()
     }
