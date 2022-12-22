e291bf54ff55ef803c1442373ac4605546f38a7c
HarryHyeon <hharry0728@gmail.com>
2022-12-22 12:26:41 +0900

    refactor: 개행 수정 및 게임시작함수 승리 출력 코드 위치 변경

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -9,16 +9,20 @@ import Foundation
 var chance = 9
 
 func createRandomNumbers() -> [Int] {
+    
     var randomNumbers = Set<Int>()
+    
     while randomNumbers.count < 3 {
         randomNumbers.insert(Int.random(in: 1...9))
     }
     return Array(randomNumbers)
 }
 
-func checkStrike(_ computerNumbers: [Int], _ userNumbers: [Int]) -> (Int, Int) {
+func checkStrike(computerNumbers: [Int], userNumbers: [Int]) -> (Int, Int) {
+    
     var strike = 0
     var ball = 0
+    
     for index in 0..<3 {
         if computerNumbers[index] == userNumbers[index] {
             strike += 1
@@ -35,20 +39,21 @@ func startGame() {
         let comNums = createRandomNumbers()
         let userNums = createRandomNumbers()
         
-        let (strike, ball) = checkStrike(comNums, userNums)
+        let (strike, ball) = checkStrike(computerNumbers: comNums, userNumbers: userNums)
+        
         chance -= 1
         
         print("임의의 수 : \(comNums.map{ String($0) }.joined(separator: " "))")
         print("\(strike) 스트라이크, \(ball) 볼")
-        print("남은 기회 : \(chance)")
-        
         
         if strike == 3 {
             print("사용자 승리...!")
             break
         } else if chance == 0 {
             print("컴퓨터 승리...!")
+            break
         }
+        print("남은 기회 : \(chance)")
     }
 }
 
