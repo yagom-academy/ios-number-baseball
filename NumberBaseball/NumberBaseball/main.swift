ea7042a0fcdaf5f5bab6746e3ebe925c2c6d2bbf
whk1140 <whk1140@gmail.com>
2022-12-22 14:54:59 +0900

    chore: 테스트프린트 삭제

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -19,8 +19,6 @@ func makeRandomNumbers() -> [String] {
 }
 
 func inputUserNumbers() {
-    print(computerNumbers)
-    print(chances)
     print("숫자 3개를 띄어쓰기로 구분하여 입력해주세요.")
     print("중복 숫자는 허용하지 않습니다.")
     print("입력", terminator: " : ")
