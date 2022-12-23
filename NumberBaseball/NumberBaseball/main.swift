0aeabb24aa483b0fcc9685cb5af23f1ad52fa2c4
yijiye <jiyee4013@naver.com>
2022-12-23 21:09:05 +0900

    feat: 메뉴출력 함수 구현

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -113,9 +113,7 @@ func choiceGameMenu() throws {
 
 func playBaseBallGame() {
     while !isGameEnd {
-        print("1. 게임시작")
-        print("2. 게임종료")
-        print("원하는 기능을 선택해주세요 : ", terminator: "")
+        printMenu()
         do {
             try choiceGameMenu()
         } catch BaseBallGameError.invalidFunction {
@@ -127,6 +125,13 @@ func playBaseBallGame() {
         isUserWin = false
         computerNumbers = makeThreeNumbers()
     } 
+}
+func printMenu() {
+    print("""
+          1. 게임시작
+          2. 게임종료
+          원하는 기능을 선택해주세요 :
+          """, terminator:" ")
 }
 
 playBaseBallGame()
