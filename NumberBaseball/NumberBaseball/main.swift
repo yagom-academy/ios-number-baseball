422989d8243d2e9f578ccd21a0b157388cd28761
Hoon94 <dleognsdl1@naver.com>
2023-04-25 02:29:58 +0900

    feat: 중복되지 않는 3개의 숫자만드는 함수 추가

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -8,8 +8,19 @@ import Foundation
 
 
 var computerRandomNumbers: Set<Int> = []
+var userRandomNumbers: Set<Int> = []
 var remainCount: Int = 9
 
 func randomNumberGenerator() -> Int {
     return Int.random(in: 1...9)
+}
+
+func addNumbers() -> Set<Int> {
+    var randomNumbers: Set<Int> = []
+    
+    while randomNumbers.count < 3 {
+        randomNumbers.insert(randomNumberGenerator())
+    }
+    
+    return randomNumbers
 }
