854bfbadfdf4eb4891182fb25cfec2b164f87294
mint3382 <mskim0926@naver.com>
2023-04-25 00:01:33 +0900

    feat: answerCountCheck에 요소 추가하는 while문 구현, answerNumber에 요소 추가하는 for문 구현

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -13,6 +13,13 @@ var strike: Int
 var answerCountCheck: Set<Int> = Set<Int>()
 
 print("임의의 수 : ",terminator: "")
+while answerCountCheck.count < 3 {
+    answerCountCheck.insert(Int.random(in: 1...9))
+}
+
+for randomNumber in answerCountCheck {
+    answerNumber.append(randomNumber)
+}
 
 guard let playerNumber = readLine() else {
     return ""
