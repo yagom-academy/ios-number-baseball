5d21169ad0980e261bae2ccd6a05ffaa8869b26a
YaRkyungmin <kkgm94@naver.com>
2023-04-25 02:08:11 +0900

    feat: checkBallStrike, initRandomNumber, startBaseballGame 함수 구현

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -6,14 +6,44 @@
 
 import Foundation
 
-func checkBallStrikeCount(to: [Int], and: [Int]){
-    var ball: Int
-    var strike: Int
+func checkBallStrikeCount(to: [Int], and: [Int]) -> Int {
+    var ball = 0
+    var strike = 0
+    let answerNumbers = to
+    let randomNumbers = and
+    
+    for number in randomNumbers {
+        if answerNumbers.contains(number) {
+            ball += 1
+        }
+    }
+
+    for index in 0...2 {
+        if answerNumbers[index] == randomNumbers[index] {
+            ball -= 1
+            strike += 1
+        }
+    }
+    
+    print("\(strike) 스트라이크, \(ball) 볼")
+    return strike
 }
 
-func initRandomNumber() -> from: [Int] {
+func initRandomNumber() -> [Int] {
     var randomNumbers: Array<Int> = Array<Int>()
     var randomNumberSet: Set<Int> = Set<Int>()
+    
+    while randomNumberSet.count < 3 {
+        randomNumberSet.insert(Int.random(in: 1...9))
+    }
+
+    for randomNumber in randomNumberSet {
+        randomNumbers.append(randomNumber)
+    }
+
+    print("임의의 수 : \(randomNumbers)")
+    
+    return randomNumbers
 }
 
 func startBaseballGame() {
@@ -30,41 +60,21 @@ func startBaseballGame() {
     }
     
     while lastChance > 0 {
-        checkBallStrikeCount(to: answerNumbers, and: initRandomNumber())
+        let strike = checkBallStrikeCount(to: answerNumbers, and: initRandomNumber())
         
-        
-        
+        if strike == 3 {
+            break
+        }
         lastChance -= 1
+        print("남은 기회 : \(lastChance)")
+    }
+    if lastChance == 0 {
+        print("컴퓨터 승리...!")
+    } else {
+        print("사용자 승리!")
     }
 }
 
-while randomNumberSet.count < 3 {
-    randomNumberSet.insert(Int.random(in: 1...9))
-}
+startBaseballGame()
 
-for randomNumber in randomNumberSet {
-    randomNumbers.append(randomNumber)
-}
 
-print("임의의 수 : \(randomNumbers)")
-
-for number in inputNumber {
-    if answerNumber.contains(Int(number) ?? 0) {
-        ball += 1
-    }
-}
-
-for index in 0...2 {
-    if answerNumbers[index] == Int(inputNumber[index]) {
-        ball -= 1
-        strike += 1
-    }
-}
-lastChance -= 1
-
-print("\(strike) 스트라이크, \(ball) 볼")
-print("남은 기회 : \(lastChance)")
-
-if strike == 3 {
-    print("사용자 승리!")
-}
