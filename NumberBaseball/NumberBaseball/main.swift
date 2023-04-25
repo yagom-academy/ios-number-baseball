83b6304eedd40d56756dfeafbd6f639eba74335e
JusBug <pksgwo123@gmail.com>
2023-04-25 12:23:38 +0900

    feat 숫자 비교 함수
    네비게이터 : 맥스
    드라이버 : jusbug

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -12,12 +12,32 @@ var input3: Int
 
 var chance: Int = 9
 
-func randomNumbers() -> Int {
+func randomNumbers() -> Array<Int> {
     var computerNumbers: Array<Int> = []
     
-    if computerNumbers.count != 3 {
-        var randomNumber1 = Int.random(in:1...9)
-        computerNumbers.append(randomNumber1)
+    while computerNumbers.count < 3 {
+        if computerNumbers.count == 3 {
+            break
+        }
+        let randomNumber = Int.random(in:1...9)
+        if computerNumbers.contains(randomNumber) {
+            continue
+        }
+        computerNumbers.append(randomNumber)
     }
+    return computerNumbers
 }
+
+func compareNumbers(to computerNumbers: Array<Int>) -> (Int, Int) {
+    let userNumbers = randomNumbers()
+    let strikes = userNumbers.enumerated().filter {userNumbers[$0.offset] == computerNumbers[$0.offset]}.map {$0.element}
+    
+}
+
+feat 숫자 비교 함수
+
+네비게이터 : 맥스
+드라이버 : jusbug
+
+
 
