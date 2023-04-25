55d84b3e698dd270a82491f32d810dacab41fa0c
Hoon94 <dleognsdl1@naver.com>
2023-04-25 21:42:16 +0900

    refactor: 메세지 출력 함수 분리

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -38,6 +38,23 @@ func checkBallCount(matching matchCount: Int, strike strikeCount: Int) -> Int {
     return matchCount - strikeCount
 }
 
+func printMessages(userRandomNumbers: [Int], strikeCount: Int, ballCount: Int, remainCount: Int) -> Bool {
+    print("임의의 수 : \(userRandomNumbers.map { String($0) }.joined(separator: " "))")
+    print("\(strikeCount) 스트라이크, \(ballCount) 볼")
+    
+    if strikeCount != 3 && remainCount == 0 {
+        print("컴퓨터 승리...!")
+        return false
+    } else if strikeCount == 3 {
+        print("사용자 승리!!!!")
+        return false
+    } else {
+        print("남은 기회 : \(remainCount)")
+    }
+    
+    return true
+}
+
 func playBaseballGame() {
     let computerRandomNumbers: [Int] = drawRandomNumbers()
     var remainCount: Int = 9
@@ -49,18 +66,7 @@ func playBaseballGame() {
         let ballCount = checkBallCount(matching: matchCount, strike: strikeCount)
         remainCount -= 1
         
-        print("임의의 수 : \(userRandomNumbers.map { String($0) }.joined(separator: " "))")
-        print("\(strikeCount) 스트라이크, \(ballCount) 볼")
-        
-        if strikeCount != 3 && remainCount == 0 {
-            print("컴퓨터 승리...!")
-            return
-        } else if strikeCount == 3 {
-            print("사용자 승리!!!!")
-            return
-        } else {
-            print("남은 기회 : \(remainCount)")
-        }
+        guard printMessages(userRandomNumbers: userRandomNumbers, strikeCount: strikeCount, ballCount: ballCount, remainCount: remainCount) else { return }
     }
 }
 
