0d3640f89b9fdee4166194fbfc78f79d614acfc7
Hyun A Song <hyuna@Hyunui-MacBookPro.local>
2023-04-25 20:19:26 +0900

    feat: 전역변수 설정, 랜덤한 숫자 생성 함수 정의

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -6,5 +6,19 @@
 
 import Foundation
 
-print("Hello, World!")
+var randomNumbers: [Int] = []
+var userNumbers: [Int] = []
+var strike = 0
+var ball = 0
 
+func createRandomNumbers() {
+    while randomNumbers.count < 3 {
+        let randomNumber = Int.random(in: 1...9)
+        if !randomNumbers.contains(randomNumber) {
+            randomNumbers.append(randomNumber)
+        }
+    }
+    print("RandomNumber : \(randomNumbers)")
+}
+
+createRandomNumbers()
