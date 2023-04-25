7b0426566df26b6ae60dcab6629f3249a4bfc6ca
Min Hyun <minnimida@gmail.com>
2023-04-25 11:52:14 +0900

    feat 랜덤 배열 함수 작성(1)
    내비게이터 : JusBug
    드라이버 : Max

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -6,5 +6,18 @@
 
 import Foundation
 
-print("Hello, World!")
+var input1: Int
+var input2: Int
+var input3: Int
+
+var chance: Int = 9
+
+func randomNumbers() -> Int {
+    var computerNumbers: Array<Int> = []
+    
+    if computerNumbers.count != 3 {
+        var randomNumber1 = Int.random(in:1...9)
+        computerNumbers.append(randomNumber1)
+    }
+}
 
