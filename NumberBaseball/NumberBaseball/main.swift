d7f8b5c0f23c6554d9447782a6ac71e3614b95c0
bubblecocoa <bubblecocoa@gmail.com>
2023-04-25 11:09:35 +0900

    [STEP 1] feat: 숫자야구 게임 1차 구현

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -6,5 +6,34 @@
 
 import Foundation
 
-print("Hello, World!")
+var number: [Int] = []
+var guess: [Int] = []
+var turnNumber = 0
 
+while number.count < 3 {
+    let randomNumber = Int.random(in: 1...9)
+    if !number.contains(randomNumber) {
+        number.append(randomNumber)
+    }
+}
+
+while turnNumber < 9 {
+    var ball = 0
+    var strike = 0
+    while guess.count < 3 {
+        let randomNumber = Int.random(in: 1...9)
+        if !guess.contains(randomNumber) {
+            guess.append(randomNumber)
+        }
+    }
+    for g in 0...2 {
+        if guess[g] == number[g] {
+            strike += 1
+        } else if number.contains(guess[g]) {
+            ball += 1
+        }
+    }
+    print("임의의 수 : \(number[0]) \(number[1]) \(number[2])")
+    print("\(strike) 스트라이크, \(ball) 볼")
+    turnNumber += 1
+}
