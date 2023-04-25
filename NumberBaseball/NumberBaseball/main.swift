3e40c2f4834b7d8a85a7039c8cc1c8e088ed15a0
Hayeon Kim <qoocrab@gmail.com>
2023-04-25 19:11:02 +0900

    [STEP 1] feat: 숫자야구 게임 2차 구현

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -1,39 +1,51 @@
-//
 //  NumberBaseball - main.swift
 //  Created by yagom. 
 //  Copyright © yagom academy. All rights reserved.
-// 
-
 import Foundation
 
-var number: [Int] = []
-var guess: [Int] = []
-var turnNumber = 0
+var computerNumbers: [Int] = []
+var playerNumbers: [Int] = []
+var remainingChance = 9
 
-while number.count < 3 {
+while computerNumbers.count < 3 {
     let randomNumber = Int.random(in: 1...9)
-    if !number.contains(randomNumber) {
-        number.append(randomNumber)
+    if !computerNumbers.contains(randomNumber) {
+        computerNumbers.append(randomNumber)
     }
 }
 
-while turnNumber < 9 {
+while remainingChance > 0 {
+    remainingChance -= 1
+    
     var ball = 0
     var strike = 0
-    while guess.count < 3 {
+    
+    playerNumbers = []
+    
+    while playerNumbers.count < 3 {
         let randomNumber = Int.random(in: 1...9)
-        if !guess.contains(randomNumber) {
-            guess.append(randomNumber)
+        if !playerNumbers.contains(randomNumber) {
+            playerNumbers.append(randomNumber)
         }
     }
-    for g in 0...2 {
-        if guess[g] == number[g] {
+    
+    for index in 0...2 {
+        if playerNumbers[index] == computerNumbers[index] {
             strike += 1
-        } else if number.contains(guess[g]) {
+        } else if computerNumbers.contains(playerNumbers[index]) {
             ball += 1
         }
     }
-    print("임의의 수 : \(number[0]) \(number[1]) \(number[2])")
+    
+    print("임의의 수 : \(computerNumbers[0]) \(computerNumbers[1]) \(computerNumbers[2])")
     print("\(strike) 스트라이크, \(ball) 볼")
-    turnNumber += 1
+    
+    if strike == 3 {
+        print("사용자 승리!")
+        break
+    } else if remainingChance > 0 {
+        print("남은 기회 : \(remainingChance)")
+    } else {
+        print("컴퓨터 승리...!")
+    }
 }
