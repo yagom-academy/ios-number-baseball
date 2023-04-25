f4571c9fc7eef7aa87aacf9b8670efb42cfb02b5
mint3382 <mskim0926@naver.com>
2023-04-25 11:22:56 +0900

    fix: 네이밍 수정

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -6,40 +6,36 @@
 
 import Foundation
 
-func checkBallStrikeCount(to: [Int], and: [Int]) -> Int {
-    var ball = 0
-    var strike = 0
-    let answerNumbers = to
-    let randomNumbers = and
+func checkBallStrikeCount(to answerNumbersOfBaseballGame: [Int], and randomNumbersOfUser: [Int]) -> Int {
+    var ballCounts = 0
+    var strikeCounts = 0
     
-    for number in randomNumbers {
-        if answerNumbers.contains(number) {
-            ball += 1
+    for randomNumber in randomNumbersOfUser {
+        if answerNumbersOfBaseballGame.contains(randomNumber) {
+            ballCounts += 1
         }
     }
 
     for index in 0...2 {
-        if answerNumbers[index] == randomNumbers[index] {
-            ball -= 1
-            strike += 1
+        if answerNumbersOfBaseballGame[index] == randomNumbersOfUser[index] {
+            ballCounts -= 1
+            strikeCounts += 1
         }
     }
     
-    print("\(strike) 스트라이크, \(ball) 볼")
-    return strike
+    print("\(strikeCounts) 스트라이크, \(ballCounts) 볼")
+    return strikeCounts
 }
 
 func initRandomNumber() -> [Int] {
-    var randomNumbers: Array<Int> = Array<Int>()
     var randomNumberSet: Set<Int> = Set<Int>()
+    var randomNumbers = Array(randomNumberSet)
     
     while randomNumberSet.count < 3 {
         randomNumberSet.insert(Int.random(in: 1...9))
     }
-
-    for randomNumber in randomNumberSet {
-        randomNumbers.append(randomNumber)
-    }
+    
+    randomNumbers = Array(randomNumberSet)
 
     print("임의의 수 : \(randomNumbers.map{(randomNumber: Int) -> String in return String(randomNumber)}.joined(separator: " "))")
     
@@ -47,35 +43,31 @@ func initRandomNumber() -> [Int] {
 }
 
 func startBaseballGame() {
-    var answerCountCheck: Set<Int> = Set<Int>()
+    var answerNumbersSet: Set<Int> = Set<Int>()
     var answerNumbers: Array<Int> = Array<Int>()
-    var lastChance = 9
-    var startLoop: Bool = true
+    var leftChance = 9
+    var isWorking: Bool = true
     
-    while answerCountCheck.count < 3 {
-        answerCountCheck.insert(Int.random(in: 1...9))
-    }
-
-    for number in answerCountCheck {
-        answerNumbers.append(number)
+    while answerNumbersSet.count < 3 {
+        answerNumbersSet.insert(Int.random(in: 1...9))
     }
     
-    while startLoop {
+    answerNumbers = Array(answerNumbersSet)
+    
+    while isWorking {
         let strike = checkBallStrikeCount(to: answerNumbers, and: initRandomNumber())
         
         if strike == 3 {
             print("사용자 승리!")
-            startLoop = false
-        } else if lastChance == 0 {
+            isWorking = false
+        } else if leftChance == 0 {
             print("컴퓨터 승리...!")
-            startLoop = false
+            isWorking = false
         } else {
-            print("남은 기회 : \(lastChance)")
+            print("남은 기회 : \(leftChance)")
         }
-        lastChance -= 1
+        leftChance -= 1
     }
 }
 
 startBaseballGame()
-
-
