f1eba3d317ca2630fc10156ccffff9eecc81caa3
Zion <lion2212@naver.com>
2023-04-25 11:17:18 +0900

    feat: 볼과 스트라이크 결과 반환함수 추가 및 게임시작 함수 추가

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -7,9 +7,8 @@
 import Foundation
 
 var computerNumberList: [Int] = [8, 2, 4]
-var countOfTurn = 9
 
-func generateRandomIntegerList() -> [Int] {
+func generateRandomNumberList() -> [Int] {
     var randomIntegerList: Set<Int> = []
     
     while randomIntegerList.count != 3 {
@@ -19,4 +18,44 @@ func generateRandomIntegerList() -> [Int] {
     return Array(randomIntegerList)
 }
 
+func checkStrikeAndBall(_ randomNumberList: [Int]) -> (strike: Int, ball: Int) {
+    var strikeCount = 0
+    var ballCount = 0
+
+    for index in 0..<3 {
+        strikeCount += computerNumberList[index] == randomNumberList[index] ? 1 : 0
+        ballCount += computerNumberList.contains(randomNumberList[index]) ? 1 : 0
+    }
+    ballCount -= strikeCount
+    
+    return (strikeCount, ballCount)
+}
+
+func startNumberBaseball() {
+    var turnCount = 9
+    
+    while turnCount != 0 {
+        let randomNumberList = generateRandomNumberList()
+        let strikeAndBallCount = checkStrikeAndBall(randomNumberList)
+        turnCount -= 1
+        
+        print("""
+              임의의 수 : \(randomNumberList[0]) \(randomNumberList[1]) \(randomNumberList[2])
+              \(strikeAndBallCount.strike) 스트라이크, \(strikeAndBallCount.ball) 볼
+              """)
+        
+        if strikeAndBallCount.strike == 3 {
+            print("사용자 승리!")
+            break
+        }
+        
+        if turnCount == 0 {
+            print("컴퓨터 승리...!")
+            break
+        }
+        print("남은 기회 : \(turnCount)")
+    }
+}
+
+startNumberBaseball()
 
