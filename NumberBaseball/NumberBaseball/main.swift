4f4d8becb1bd743eda75a35838a1bdbdc717ea65
idinaloq <idinaloq@icloud.com>
2023-04-25 12:12:45 +0900

    feat: 랜덤숫자 변수 추가, 시도횟수 변수 추가, 랜덤변수 생성 함수 추가

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -6,5 +6,21 @@
 
 import Foundation
 
-print("Hello, World!")
+var randomNumbers: [Int] = []
+var tryCount: Int = 9
 
+func getRandomNumber() -> [Int] {
+    
+    var randomNumbers: [Int] = []
+    
+    while randomNumbers.count != 3 {
+        
+        let randomNumber = Int.random(in: 1...9)
+        
+        if !randomNumbers.contains(randomNumber) {
+            randomNumbers.append(randomNumber)
+        }
+    }
+    
+    return randomNumbers
+}
