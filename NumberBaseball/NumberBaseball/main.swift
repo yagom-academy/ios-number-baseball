6f2e07aaed3fcf646acd853f9d297750907c3a45
DasanKim <tempzipp@gmail.com>
2023-04-25 20:00:40 +0900

    refactor: 네이밍 및 startGame() 함수 수정

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -6,35 +6,32 @@
 
 import Foundation
 
-var computerRandomInt = [Int]()
-var userRandomInt = [Int]()
-var count: Int = 9
+var computerRandomNumbers = [Int]()
+var userRandomNumbers = [Int]()
+var remainingChance: Int = 9
 
-func creatRandomNumber() -> [Int] {
-    var outputRandomInput = [Int]()
+func creatRandomNumbers() -> [Int] {
+    var randomNumbers = [Int]()
     
-    while outputRandomInput.count < 3 {
+    while randomNumbers.count < 3 {
         let randomNumber = Int.random(in: 1...9)
-        if !outputRandomInput.contains(randomNumber) {
-            outputRandomInput.append(randomNumber)
+        if !randomNumbers.contains(randomNumber) {
+            randomNumbers.append(randomNumber)
         }
     }
-    return outputRandomInput
+    return randomNumbers
 }
 
-computerRandomInt = creatRandomNumber()
-//userRandomInt = creatRandomNumber()
-
-func compare(to computerNumbers: [Int], from userNumbers: [Int]) -> (strikeCount: Int, ballCount: Int) {
+func compareComputerNumbers(and userNumbers: [Int]) -> (strikeCount: Int, ballCount: Int) {
     var strikeCount: Int = 0
     var ballCount: Int = 0
     
     for i in 0...2 {
-        if computerNumbers.contains(userNumbers[i]) {
+        if computerRandomNumbers.contains(userNumbers[i]) {
             ballCount += 1
         }
         
-        if computerNumbers[i] == userNumbers[i] {
+        if computerRandomNumbers[i] == userNumbers[i] {
             strikeCount += 1
             ballCount -= 1
         }
@@ -42,33 +39,29 @@ func compare(to computerNumbers: [Int], from userNumbers: [Int]) -> (strikeCount
     return (strikeCount: strikeCount, ballCount: ballCount)
 }
 
-//var one: Int = compare(to: computerRandomInt, from: userRandomInt).strikeCount
-//var two: Int = compare(to: computerRandomInt, from: userRandomInt).ballCount
-//test용
-//print(computerRandomInt)
-//print(userRandomInt)
-//print("strike: \(one), ball: \(two)")
-
-func startGame() -> String {
-    while count > 1 {
-        userRandomInt = creatRandomNumber()
-        let one: Int = compare(to: computerRandomInt, from: userRandomInt).strikeCount
-        let two: Int = compare(to: computerRandomInt, from: userRandomInt).ballCount
+func startGame() {
+    while remainingChance > 0 {
+        userRandomNumbers = creatRandomNumbers()
+        let strikeCount: Int = compareComputerNumbers(and: userRandomNumbers).strikeCount
+        let ballCount: Int = compareComputerNumbers(and: userRandomNumbers).ballCount
         
-        count -= 1
+        remainingChance -= 1
         
-        print(computerRandomInt)
-        print(userRandomInt)
-        print("남은 기회: \(count)")
-        print("\(one) 스트라이크, \(two) 볼")
+        print("임의의 수 : \(userRandomNumbers[0]) \(userRandomNumbers[1]) \(userRandomNumbers[2])")
+        print("\(strikeCount) 스트라이크, \(ballCount) 볼")
         
-        if one == 3 {
-            return "USER 승리"
-            //break
+        if remainingChance > 0 {
+            print("남은 기회: \(remainingChance)")
+        }
+        if strikeCount == 3 {
+            print("USER WIN!!")
+            break
+        }
+        if remainingChance == 0 {
+            print("COMPUTER WIN!!")
         }
     }
-    return "COMPUTER WIN"
 }
 
-var result = startGame()
-print(result)
+computerRandomNumbers = creatRandomNumbers()
+startGame()
