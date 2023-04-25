5e35cb4378d06b712056587fe46c82ed00756aad
bubblecocoa <bubblecocoa@gmail.com>
2023-04-25 19:59:05 +0900

    [STEP 1] fix: 숫자야구 게임 리팩토링

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -3,49 +3,53 @@
 //  Copyright © yagom academy. All rights reserved.
 import Foundation
 
-var computerNumbers: [Int] = []
-var playerNumbers: [Int] = []
+func generateNumbers(numbers: inout [Int]) -> [Int] {
+    while numbers.count < 3 {
+        let randomNumber = Int.random(in: 1...9)
+        if !numbers.contains(randomNumber) {
+            numbers.append(randomNumber)
+        }
+    }
+    
+    return numbers
+}
+
+func playNumberBaseballGame(remainingChance: inout Int) {
+    var computerNumbers: [Int] = []
+    var playerNumbers: [Int] = []
+    
+    computerNumbers = generateNumbers(numbers: &computerNumbers)
+    
+    while remainingChance > 0 {
+        remainingChance -= 1
+        
+        var ball = 0
+        var strike = 0
+        
+        playerNumbers = generateNumbers(numbers: &playerNumbers)
+        
+        for index in 0...2 {
+            if playerNumbers[index] == computerNumbers[index] {
+                strike += 1
+            } else if computerNumbers.contains(playerNumbers[index]) {
+                ball += 1
+            }
+        }
+        
+        print("임의의 수 : \(computerNumbers[0]) \(computerNumbers[1]) \(computerNumbers[2])")
+        print("\(strike) 스트라이크, \(ball) 볼")
+        
+        if strike == 3 {
+            print("사용자 승리!")
+            break
+        } else if remainingChance > 0 {
+            print("남은 기회 : \(remainingChance)")
+        } else {
+            print("컴퓨터 승리...!")
+        }
+    }
+}
+
 var remainingChance = 9
 
-while computerNumbers.count < 3 {
-    let randomNumber = Int.random(in: 1...9)
-    if !computerNumbers.contains(randomNumber) {
-        computerNumbers.append(randomNumber)
-    }
-}
-
-while remainingChance > 0 {
-    remainingChance -= 1
-    
-    var ball = 0
-    var strike = 0
-    
-    playerNumbers = []
-    
-    while playerNumbers.count < 3 {
-        let randomNumber = Int.random(in: 1...9)
-        if !playerNumbers.contains(randomNumber) {
-            playerNumbers.append(randomNumber)
-        }
-    }
-    
-    for index in 0...2 {
-        if playerNumbers[index] == computerNumbers[index] {
-            strike += 1
-        } else if computerNumbers.contains(playerNumbers[index]) {
-            ball += 1
-        }
-    }
-    
-    print("임의의 수 : \(computerNumbers[0]) \(computerNumbers[1]) \(computerNumbers[2])")
-    print("\(strike) 스트라이크, \(ball) 볼")
-    
-    if strike == 3 {
-        print("사용자 승리!")
-        break
-    } else if remainingChance > 0 {
-        print("남은 기회 : \(remainingChance)")
-    } else {
-        print("컴퓨터 승리...!")
-    }
-}
+playNumberBaseballGame(remainingChance: &remainingChance)
