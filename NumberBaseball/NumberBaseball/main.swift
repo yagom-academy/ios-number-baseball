c1cc016004a6dd4a9e8cf006c4774f12968e52b9
mint3382 <mskim0926@naver.com>
2023-04-26 09:32:34 +0900

    refactor: 네이밍 수정 및 전역변수로 전환

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -4,27 +4,28 @@
 //  Copyright © yagom academy. All rights reserved.
 // 
 
-import Foundation
+let answerNumbers = generateRandomNumber()
+var chance = 9
 
-func checkBallStrikeCount(from answerNumbersOfBaseballGame: [Int], with matchNumbersOfUser: [Int]) -> (strike: Int, ball: Int) {
-    var ballCounts = 0
-    var strikeCounts = 0
+func checkBallStrikeCount(from answerNumbers: [Int], with matchNumbersOfUser: [Int]) -> (strike: Int, ball: Int) {
+    var ballCount = 0
+    var strikeCount = 0
     
     for index in 0...2 {
-        if answerNumbersOfBaseballGame.contains(matchNumbersOfUser[index]) {
-            ballCounts += 1
+        if answerNumbers.contains(matchNumbersOfUser[index]) {
+            ballCount += 1
         }
         
-        if answerNumbersOfBaseballGame[index] == matchNumbersOfUser[index] {
-            ballCounts -= 1
-            strikeCounts += 1
+        if answerNumbers[index] == matchNumbersOfUser[index] {
+            ballCount -= 1
+            strikeCount += 1
         }
     }
     
-    return (strikeCounts, ballCounts)
+    return (strikeCount, ballCount)
 }
 
-func initializeRandomNumber() -> [Int] {
+func generateRandomNumber() -> [Int] {
     var randomNumbers: Set<Int> = Set<Int>()
     
     while randomNumbers.count < 3 {
@@ -35,25 +36,23 @@ func initializeRandomNumber() -> [Int] {
 }
 
 func startBaseballGame() {
-    let answerNumbers = initializeRandomNumber()
-    var chance = 9
-    var isLeftChance: Bool = true
+    var isRemainingChance: Bool = true
     
-    while isLeftChance {
+    while isRemainingChance {
         chance -= 1
         
-        var randomNumbers = initializeRandomNumber()
+        let randomNumbers = generateRandomNumber()
         let matchCount = checkBallStrikeCount(from: answerNumbers, with: randomNumbers)
         
-        print("임의의 수 : \(Array(randomNumbers).map{String($0)}.joined(separator: " "))")
+        print("임의의 수 : \(Array(randomNumbers).map { String($0) }.joined(separator: " "))")
         print("\(matchCount.strike) 스트라이크, \(matchCount.ball) 볼")
         
         if matchCount.strike == 3 {
             print("사용자 승리!")
-            isLeftChance = false
+            isRemainingChance = false
         } else if chance == 0 {
             print("컴퓨터 승리...!")
-            isLeftChance = false
+            isRemainingChance = false
         } else {
             print("남은 기회 : \(chance)")
         }
