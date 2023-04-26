d903821a5f156d569a09941b49cb64e13965406d
DasanKim <tempzipp@gmail.com>
2023-04-26 23:15:27 +0900

    refactor: 승부판정 기능 분리
    startBaseballGame함수에서 승부판정 기능을 분리하여 decideWinner 함수 추가

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -4,8 +4,6 @@
 //  Copyright © yagom academy. All rights reserved.
 //
 
-import Foundation
-
 func createRandomNumbers() -> [Int] {
     var uniqueRandomNumbers = Set<Int>()
     
@@ -16,43 +14,54 @@ func createRandomNumbers() -> [Int] {
     return Array(uniqueRandomNumbers)
 }
 
-func compare(_ hiddenRandomNumbers: [Int], and userNumbers: [Int]) -> (strikeCount: Int, ballCount: Int) {
+func compare(_ computerRandomNumbers: [Int], and userNumbers: [Int]) -> (strikeCount: Int, ballCount: Int) {
     var strikeCount: Int = 0
     var ballCount: Int = 0
     
-    for index in 0..<hiddenRandomNumbers.count {
-        if hiddenRandomNumbers[index] == userNumbers[index] {
+    for index in 0..<computerRandomNumbers.count {
+        if computerRandomNumbers[index] == userNumbers[index] {
             strikeCount += 1
-        } else if hiddenRandomNumbers.contains(userNumbers[index]){
+        } else if computerRandomNumbers.contains(userNumbers[index]){
             ballCount += 1
         }
     }
     return (strikeCount: strikeCount, ballCount: ballCount)
 }
 
+func decideWinner(_ strikeCount: Int, _ remainingChance: Int) -> Bool {
+    var isContinue: Bool
+    
+    if strikeCount == 3 {
+        print("USER WIN!!")
+        isContinue = false
+    } else if remainingChance == 0 {
+        print("COMPUTER WIN!!")
+        isContinue = false
+    } else {
+        print("남은 기회: \(remainingChance)")
+        isContinue = true
+    }
+    return isContinue
+}
+
 func startBaseballGame() {
-    let hiddenRandomNumbers = createRandomNumbers()
+    let computerRandomNumbers: [Int] = createRandomNumbers()
     var userRandomNumbers = [Int]()
     var remainingChance: Int = 9
+    var isContinue: Bool = true
     
-    while remainingChance > 0 {
+    repeat {
         remainingChance -= 1
         userRandomNumbers = createRandomNumbers()
         
-        let gameResult: (strikeCount: Int, ballCount: Int) = compare(hiddenRandomNumbers, and: userRandomNumbers)
+        let gameResult: (strikeCount: Int, ballCount: Int) = compare(computerRandomNumbers, and: userRandomNumbers)
         
         print("임의의 수 : \(userRandomNumbers.map{ String($0) }.joined(separator: " "))")
         print("\(gameResult.strikeCount) 스트라이크, \(gameResult.ballCount) 볼")
         
-        if gameResult.strikeCount == 3 {
-            print("USER WIN!!")
-            break
-        } else if remainingChance == 0 {
-            print("COMPUTER WIN!!")
-        } else {
-            print("남은 기회: \(remainingChance)")
-        }
-    }
+        isContinue = decideWinner(gameResult.strikeCount, remainingChance)
+
+    } while isContinue
 }
 
 startBaseballGame()
