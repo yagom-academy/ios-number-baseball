bdc7362f399d24b12525dfecfb7f9c3e126ff178
김광준 <mskang@xingxingmobility.com>
2023-04-26 15:57:44 +0900

    feat: 임의의 숫자 생성 함수 구현

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -2,3 +2,12 @@ import Foundation
 
 var remainCount: Int = 9
 var randomNumbers: Set<Int> = []
+
+func createRandomNumbers() -> Set<Int> {
+	while randomNumbers.count != 3 {
+		randomNumbers.insert(Int.random(in: 1...9))
+	}	
+	return randomNumbers
+}
+
+
