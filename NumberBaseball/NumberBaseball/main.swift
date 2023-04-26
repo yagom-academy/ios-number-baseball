723eeecfa572367489eb0dec66657012dd80024c
김광준 <mskang@xingxingmobility.com>
2023-04-26 18:06:30 +0900

    feat: 볼과 스트라이크 결과를 반환하는 함수 구현중

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -16,14 +16,32 @@ func createRandomNumbers() -> [Int] {
     while randomNumbers.count != 3 {
         randomNumbers.insert(Int.random(in: 1...9))
     }
-    let randomNumbers = Array(randomNumbers)
+	let randomNumbers = Array(randomNumbers)
     return randomNumbers
 }
+
+let computerRandomNumbersArray = createComputerRandomNumbers()
+var randomNumbersArray = createRandomNumbers()
+
 
 func judgeResult(randomNumbers: [Int]) -> [Int] {
     var ball: Int = 0
     var strike: Int = 0
+	var resultOfStrikeAndBall: [Int] = []
     
-    
-    return [0]
+	for i in 0...2 {
+		if computerRandomNumbersArray[i] == randomNumbersArray[i] {
+			strike += 1
+		} else if computerRandomNumbersArray[i] != randomNumbersArray[i] && randomNumbersArray.contains(computerRandomNumbersArray[i]) {
+			ball += 1
+		}
+	}
+	remainCount -= 1
+	print("임의의 수 : \(randomNumbersArray)")
+	print("\(strike) 스트라이크, \(ball) 볼")
+	print("남은 기회 : \(remainCount)")
+	resultOfStrikeAndBall.append(strike)
+	resultOfStrikeAndBall.append(ball)
+    return resultOfStrikeAndBall
 }
+
