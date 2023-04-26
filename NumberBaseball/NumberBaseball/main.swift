6424889d3908c1679b138bec384697a469709d84
iOS-Yetti <yechan123456@gmail.net>
2023-04-26 17:02:38 +0900

    feat: createRandomNumbers함수 수정 중

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -1,13 +1,29 @@
 import Foundation
 
 var remainCount: Int = 9
+var computerRandomNumbers: Set<Int> = []
 var randomNumbers: Set<Int> = []
 
-func createRandomNumbers() -> Set<Int> {
-	while randomNumbers.count != 3 {
-		randomNumbers.insert(Int.random(in: 1...9))
-	}	
-	return randomNumbers
+func createComputerRandomNumbers() -> [Int] {
+	while computerRandomNumbers.count != 3 {
+        computerRandomNumbers.insert(Int.random(in: 1...9))
+	}
+    let computerRandomNumbers = Array(computerRandomNumbers)
+	return computerRandomNumbers
 }
 
+func createRandomNumbers() -> [Int] {
+    while randomNumbers.count != 3 {
+        randomNumbers.insert(Int.random(in: 1...9))
+    }
+    let randomNumbers = Array(randomNumbers)
+    return randomNumbers
+}
 
+func judgeResult(randomNumbers: [Int]) -> [Int] {
+    var ball: Int = 0
+    var strike: Int = 0
+    
+    
+    return [0]
+}
