fe4224ad690666a1d0a33cd47c9d2d2f87949672
ysj <yyssjj9999@gmail.com>
2023-04-26 16:55:41 +0900

    feat: 사용자가 입력한 임의의 수를 검증하는 함수 구현

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -17,7 +17,7 @@ func makeUniqueRandomNumbers() -> [Int] {
 }
 
 func getBallAndStrikeResult(of userNumbers: [Int]) -> (Int, Int) {
-    let sameNumbers = computerNumbers.filter{ userNumbers.contains($0) }
+    let sameNumbers = computerNumbers.filter { userNumbers.contains($0) }
     var ballCount = 0
     var strikeCount = 0
     
@@ -26,35 +26,33 @@ func getBallAndStrikeResult(of userNumbers: [Int]) -> (Int, Int) {
             strikeCount += 1
         }
     }
-    
     ballCount = sameNumbers.count - strikeCount
     
     return (ballCount, strikeCount)
 }
 
 func startGame() {
-    while tryCounts > 0 {
+    while count > 0 {
         let userNumbers = makeUniqueRandomNumbers()
         let (ballCount, strikeCount) = getBallAndStrikeResult(of: userNumbers)
-        
-        print("임의의 수 : \(userNumbers.map { String($0)}.joined(separator: " "))")
+
+        print("임의의 수 : \(userNumbers.map { String($0) }.joined(separator: " "))")
         print("\(strikeCount) 스트라이크, \(ballCount) 볼")
-        
-        tryCounts -= 1
-        
-        if tryCounts > 0 {
-            print("남은 기회 : \(tryCounts)")
+
+        count -= 1
+
+        if count > 0 {
+            print("남은 기회 : \(count)")
         }
-        
+
         if strikeCount == 3 {
             print("사용자 승리!")
             break
         }
-        
-        if tryCounts == 0 {
-            print("컴퓨터 승리...!")
-        }
         print()
+    }
+    if count == 0 {
+        print("컴퓨터 승리...!")
     }
 }
 
@@ -76,7 +74,31 @@ func showMenu() {
     }
 }
 
+func checkNumbers(for userInput: String) -> Bool {
+    let invaildInput = -1
+    let vaildNumberLength = 1
+    let vaildNumberCount = 3
+    let separatedInput = userInput.split(separator: " ").map { Int($0) ?? invaildInput }
+    
+    if separatedInput.contains(invaildInput)
+        || separatedInput.filter({ String($0).count == vaildNumberLength }).count != vaildNumberCount {
+        return false
+    }
+    return true
+}
+
+func inputUserGameNumber() {
+    while true {
+        print("숫자 3개를 띄어쓰기로 구분하여 입력해주세요.\n중복 숫자는 허용하지 않습니다.\n입력 : ", terminator: "")
+        if let userInput = readLine(), checkNumbers(for: userInput) {
+                
+        } else {
+            print("입력이 잘못되었습니다")
+        }
+    }
+}
+
 let computerNumbers = makeUniqueRandomNumbers()
-var tryCounts = 9
+var count = 9
 
 showMenu()
