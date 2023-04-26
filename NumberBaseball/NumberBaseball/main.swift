5ad536d9400bce63efc2ad31b5e3e02e9867de7b
iOS-Yetti <yechan123456@gmail.net>
2023-04-26 15:36:02 +0900

    feat: 전역변수 추가

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -1,13 +1,4 @@
 import Foundation
 
 var remainCount: Int = 9
-var randomNumber: Set<Int> = []
-if let selectMenuNumber = readLine()
-
-func excuteGameMenu() {
-	print("1. 게임시작 \n2. 게임종료")
-	print("원하는 기능을 선택해주세요:", terminator: " ")
-	
-		
-	
-}
+var randomNumbers: Set<Int> = []
