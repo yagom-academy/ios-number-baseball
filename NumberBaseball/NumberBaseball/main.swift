b5a1628d72a6851aea1b31df6712a343cd4f8837
조호준 <johojun@johojun-ui-MacBookAir.local>
2023-04-26 21:43:49 +0900

    feat: 입력받은 세 숫자 유효성 검증

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -4,6 +4,8 @@
 //  Copyright © yagom academy. All rights reserved.
 //
 
+import Foundation
+
 func createThreeNumbers() -> [Int] {
     return Array((1...9).shuffled()[0...2])
 }
@@ -60,14 +62,15 @@ func startGame() {
     }
 }
 
-func menu() {
+func showMenu() {
     while true {
         print("1. 게임시작")
         print("2. 게임종료")
         print("원하는 기능을 선택해주세요 : ", terminator: "")
         let selectingMenu = readLine()
 
-        guard let selectingMenu, let selectingMenu = Int(selectingMenu) else {
+        guard let selectingMenu,
+              let selectingMenu = Int(selectingMenu) else {
             print("입력이 잘못되었습니다.")
             continue
         }
@@ -84,12 +87,29 @@ func menu() {
 }
 
 func receiveGuessNumber() {
-    print("숫자 3개를 띄어쓰기로 구분하여 입력해주세요.")
-    print("중복 숫자는 허용하지 않습니다.\n입력 : ", terminator: "")
-    let guessNumber = readLine()
-    if let guessNumber {
-        print(guessNumber)
+    while true {
+        print("숫자 3개를 띄어쓰기로 구분하여 입력해주세요.")
+        print("중복 숫자는 허용하지 않습니다.\n입력 : ", terminator: "")
+        guard let guessNumber = readLine(),
+              isValidGuessNumber(guessNumber: guessNumber) else {
+            print("입력이 잘못되었습니다.")
+            continue
+        }
+        break
     }
 }
 
-menu()
+func isValidGuessNumber(guessNumber: String) -> Bool {
+    let numbersArray = guessNumber.components(separatedBy: " ")
+    let numbersIntArray = numbersArray.compactMap({ Int($0) })
+    
+    guard numbersArray.count == 3,
+          numbersIntArray.count == 3,
+          numbersIntArray.filter({ 1...9 ~= $0 }).count == 3,
+          Set(numbersIntArray).count == 3
+    else { return false }
+    
+    return true
+}
+
+receiveGuessNumber()
