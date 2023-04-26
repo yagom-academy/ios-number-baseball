c03e478a41b939c08d2ba21d2b71eac4e991769a
Hayeon Kim <qoocrab@gmail.com>
2023-04-26 10:56:52 +0900

    refactor: [Step 1] 숫자 야구게임 주석 변경 및 개행 추가, 스트라이크 볼 판별 함수 생성

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -1,6 +1,7 @@
 //  NumberBaseball - main.swift
-//  Created by yagom. 
+//  Created by qoocrab, bmo.
 //  Copyright © yagom academy. All rights reserved.
+
 import Foundation
 
 func generateNumbers() -> [Int] {
@@ -16,12 +17,23 @@ func generateNumbers() -> [Int] {
     return numbers
 }
 
+func evaluateStrikeBall(computer computerNumbers: [Int], player playerNumbers: [Int]) -> (Int, Int) {
+    var ball = 0
+    var strike = 0
+    
+    for index in 0...2 {
+        if playerNumbers[index] == computerNumbers[index] {
+            strike += 1
+        } else if computerNumbers.contains(playerNumbers[index]) {
+            ball += 1
+        }
+    }
+    
+    return (strike, ball)
+}
+
 func playNumberBaseballGame() {
     var remainingChance = 9
-    var computerNumbers: [Int] = []
-    var playerNumbers: [Int] = []
-    
-    computerNumbers = generateNumbers()
     
     while remainingChance > 0 {
         remainingChance -= 1
@@ -29,15 +41,10 @@ func playNumberBaseballGame() {
         var ball = 0
         var strike = 0
         
-        playerNumbers = generateNumbers()
+        let computerNumbers = generateNumbers()
+        let playerNumbers = generateNumbers()
         
-        for index in 0...2 {
-            if playerNumbers[index] == computerNumbers[index] {
-                strike += 1
-            } else if computerNumbers.contains(playerNumbers[index]) {
-                ball += 1
-            }
-        }
+        (strike, ball) = evaluateStrikeBall(computer: computerNumbers, player: playerNumbers)
         
         print("임의의 수 : \(computerNumbers[0]) \(computerNumbers[1]) \(computerNumbers[2])")
         print("\(strike) 스트라이크, \(ball) 볼")
