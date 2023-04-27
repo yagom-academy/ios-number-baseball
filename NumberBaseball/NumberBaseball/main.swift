8e54817abb21809a8dd53872280e2eb4bd6a6b92
Hoon94 <dleognsdl1@naver.com>
2023-04-27 12:13:57 +0900

    feat: 사용자 메뉴 입력 함수 추가

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -66,5 +66,25 @@ func playBaseballGame() {
     }
 }
 
+func inputUserMenu() {
+    var isRun: Bool = true
+    
+    while isRun {
+        print("1. 게임시작 \n2. 게임종료")
+        print("원하는 기능을 선택해주세요 :", terminator: " ")
+        
+        guard let userMenu = readLine() else { return }
+        
+        if userMenu == "1" {
+            playBaseballGame()
+        } else if userMenu == "2" {
+            print("게임종료")
+            isRun = false
+        } else {
+            print("입력이 잘못되었습니다")
+        }
+    }
+}
+
 //MARK: 실행
-playBaseballGame()
+inputUserMenu()
