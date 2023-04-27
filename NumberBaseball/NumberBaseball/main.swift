580745c6debebad75484a46e1e9e777e4b11c71a
myungsun <audtjs9987@naver.com>
2023-04-27 16:28:57 +0900

    feat: checkNumbers 함수에서 0과 중복되는 값 제거하는 로직 추가

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -2,9 +2,7 @@
 //  NumberBaseball - main.swift
 //  Created by myungsun, yyss99.
 //  Copyright © yagom academy. All rights reserved.
-// 
-
-import Foundation
+//
 
 func makeUniqueRandomNumbers() -> [Int] {
     var randomNumbers = Set<Int>()
@@ -31,42 +29,16 @@ func getBallAndStrikeResult(of userNumbers: [Int]) -> (Int, Int) {
     return (ballCount, strikeCount)
 }
 
-func startGame() {
-    while count > 0 {
-        let userNumbers = makeUniqueRandomNumbers()
-        let (ballCount, strikeCount) = getBallAndStrikeResult(of: userNumbers)
-
-        print("임의의 수 : \(userNumbers.map { String($0) }.joined(separator: " "))")
-        print("\(strikeCount) 스트라이크, \(ballCount) 볼")
-
-        count -= 1
-
-        if count > 0 {
-            print("남은 기회 : \(count)")
-        }
-
-        if strikeCount == 3 {
-            print("사용자 승리!")
-            break
-        }
-        print()
-    }
-    if count == 0 {
-        print("컴퓨터 승리...!")
-    }
-}
-
 func showMenu() {
     while true {
         print("1. 게임시작 \n2. 게임종료 \n원하는 기능을 선택해주세요 : ", terminator: "")
-        let menuChoice = readLine() ?? ""
+        guard let menuChoice = readLine() else { continue }
         let startOption = "1"
         let endOption = "2"
         
         if menuChoice == startOption {
-            print("start Game")
+            inputUserGameNumber()
         } else if menuChoice == endOption {
-            print("end Game")
             break
         } else {
             print("입력이 잘못되었습니다.")
@@ -74,31 +46,37 @@ func showMenu() {
     }
 }
 
-func checkNumbers(for userInput: String) -> Bool {
+
+
+func checkNumbers(for userInput: String) -> [Int]? {
     let invaildInput = -1
     let vaildNumberLength = 1
     let vaildNumberCount = 3
-    let separatedInput = userInput.split(separator: " ").map { Int($0) ?? invaildInput }
+    let separatedInput = userInput.split(separator: " ")
+                                  .map { Int($0) ?? invaildInput }
+                                  .filter { $0 != 0 }
     
     if separatedInput.contains(invaildInput)
-        || separatedInput.filter({ String($0).count == vaildNumberLength }).count != vaildNumberCount {
-        return false
+        || Set(separatedInput.filter({ String($0).count == vaildNumberLength })).count != vaildNumberCount {
+        return nil
     }
-    return true
+    return separatedInput
 }
 
 func inputUserGameNumber() {
-    while true {
-        print("숫자 3개를 띄어쓰기로 구분하여 입력해주세요.\n중복 숫자는 허용하지 않습니다.\n입력 : ", terminator: "")
-        if let userInput = readLine(), checkNumbers(for: userInput) {
-                
-        } else {
-            print("입력이 잘못되었습니다")
+    var isGameDone = true
+    while isGameDone {
+        print("\n숫자 3개를 띄어쓰기로 구분하여 입력해주세요.\n중복 숫자는 허용하지 않습니다.\n입력 : ", terminator: "")
+        
+        guard let userInput = readLine(), let validNumber = checkNumbers(for: userInput) else {
+            print("입력이 잘못되었습니다. \n")
+            continue
         }
+        let strikeCount = getBallAndStrikeResult(of: validNumber).1
     }
 }
 
-let computerNumbers = makeUniqueRandomNumbers()
+var computerNumbers = makeUniqueRandomNumbers()
 var count = 9
 
 showMenu()
