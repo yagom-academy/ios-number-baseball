42815f2cdafeed97688c2a9c2f4c713fc7b4bc75
Kobe <mskang@xingxingmobility.com>
2023-04-29 16:04:36 +0900

    fix: 임의의 숫자 생성 함수 수정, 스트라이크와 볼 판정 함수 수정, 게임 시작 함수 내부 로직 수정.

--- NumberBaseball/NumberBaseball/Step1.swift
+++ NumberBaseball/NumberBaseball/Step1.swift
@@ -4,56 +4,49 @@
 //  Copyright © yagom academy. All rights reserved.
 //
 
-import Foundation
 
 var computerRandomNumbers: Set<Int> = []
 var randomNumbers: Set<Int> = []
 
-func createComputerRandomNumbers() -> [Int] {
-    while computerRandomNumbers.count != 3 {
-        computerRandomNumbers.insert(Int.random(in: 1...9))
-    }
-    let computerRandomNumbers = Array(computerRandomNumbers)
-    return computerRandomNumbers
-}
-
-func createRandomNumbers() -> [Int] {
+func createRandomNumbers() -> (userRandomNumbers: [Int], computerRandomNumbers: [Int]) {
     randomNumbers.removeAll()
     while randomNumbers.count != 3 {
         randomNumbers.insert(Int.random(in: 1...9))
     }
-    let randomNumbers = Array(randomNumbers)
-    return randomNumbers
+	while computerRandomNumbers.count != 3 {
+		computerRandomNumbers.insert(Int.random(in: 1...9))
+	}
+	let randomNumbers = Array(randomNumbers)
+	let computerRandomNumbers = Array(computerRandomNumbers)
+	let returnValue: ([Int], [Int]) = (randomNumbers, computerRandomNumbers)
+	
+    return returnValue
 }
 
-func judgeResult(_ computerRandomNumbersArray: [Int],_ randomNumbersArray: [Int]) -> [Int] {
-    var (strike, ball) = (0, 0)
-    var resultOfStrikeAndBall: [Int] = []
-
+func judgeStrikeAndBall(_ computerRandomNumbers: [Int],_ randomNumbers: [Int]) -> (strike: Int, ball: Int) {
+	var strikeAndBall: (strike: Int, ball: Int) = (0, 0)
     for i in 0...2 {
-        if computerRandomNumbersArray[i] == randomNumbersArray[i] {
-            strike += 1
-        } else if computerRandomNumbersArray[i] != randomNumbersArray[i] && randomNumbersArray.contains(computerRandomNumbersArray[i]) {
-            ball += 1
+        if computerRandomNumbers[i] == randomNumbers[i] {
+			strikeAndBall.strike += 1
+        } else if computerRandomNumbers[i] != randomNumbers[i] && randomNumbers.contains(computerRandomNumbers[i]) {
+			strikeAndBall.ball += 1
         }
     }
-    resultOfStrikeAndBall.append(strike)
-    resultOfStrikeAndBall.append(ball)
-    return resultOfStrikeAndBall
+    return strikeAndBall
 }
 
 func startGame() {
     var remainCount: Int = 9
-    let computerRandomNumbersArray = createComputerRandomNumbers()
+	let computerRandomNumbers = createRandomNumbers().computerRandomNumbers
 
     while remainCount > 0 {
-        let randomNumbersArray = createRandomNumbers()
-        let judgeResult = judgeResult(computerRandomNumbersArray, randomNumbersArray)
-        let strike = judgeResult[0]
-        let ball = judgeResult[1]
+		let randomNumbers = createRandomNumbers().userRandomNumbers
+        let judgeResult = judgeStrikeAndBall(computerRandomNumbers, randomNumbers)
+		let strike = judgeResult.strike
+		let ball = judgeResult.ball
         remainCount -= 1
 
-        print("임의의 수 : \(randomNumbersArray[0]) \(randomNumbersArray[1]) \(randomNumbersArray[2])")
+        print("임의의 수 : \(randomNumbers[0]) \(randomNumbers[1]) \(randomNumbers[2])")
         print("\(strike) 스트라이크, \(ball) 볼")
 
         if remainCount > 0 && strike < 3 {
