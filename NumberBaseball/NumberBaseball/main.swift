4afbace596625cb7349499b8cca593c05530baed
kiseok <carti1108@naver.com>
2023-08-28 16:39:03 +0900

    feat: step1 랜덤값 생성 함수, 비교 함수 추가:

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -6,5 +6,33 @@
 
 import Foundation
 
-print("Hello, World!")
+func mainLoop() {
+    var randomNumbers: Set<Int> = []
+    var userRandomNumbers: Set<Int> = []
+    var chance = 9
+    randomNumbers = makeRandomNumber()
+    userRandomNumbers = makeUserRandomNumber()
+    while chance != 0 {
+        
+    }
+}
 
+func makeRandomNumber() -> Set<Int> {
+    var numbers: Set<Int> = []
+    while numbers.count < 3 {
+        numbers.insert(Int.random(in: 1...9))
+    }
+    return numbers
+}
+
+func compareNumbers(userNumbers: Set<Int>, randomNumbers: Set<Int>) -> Int {
+    return userNumbers.intersection(randomNumbers).count
+}
+
+func makeUserRandomNumber() -> Set<Int> {
+    var numbers: Set<Int> = []
+    while numbers.count < 3 {
+        numbers.insert(Int.random(in: 1...9))
+    }
+    return numbers
+}
