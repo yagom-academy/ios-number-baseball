de74891fc39ef8f9d82c405c7306401ed0af189c
Hisop <hisop@Hisopui-MacBookPro.local>
2023-08-28 17:35:25 +0900

    feat: 스트라이크 판별 기능 추가

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -7,32 +7,49 @@
 import Foundation
 
 func mainLoop() {
-    var randomNumbers: Set<Int> = []
-    var userRandomNumbers: Set<Int> = []
+    var randomNumbers: [Int] = []
+    var userRandomNumbers: [Int] = []
     var chance = 9
     randomNumbers = makeRandomNumber()
     userRandomNumbers = makeUserRandomNumber()
-    while chance != 0 {
+    print(randomNumbers)
+    print("임의의 수 : \(userRandomNumbers)")
+    compareNumbers(userNumbers: randomNumbers, randomNumbers: userRandomNumbers)
+    
+    /*while chance != 0 {
         
-    }
+    }*/
 }
 
-func makeRandomNumber() -> Set<Int> {
+func makeRandomNumber() -> [Int] {
     var numbers: Set<Int> = []
     while numbers.count < 3 {
         numbers.insert(Int.random(in: 1...9))
     }
-    return numbers
+    return numbers.map { Int($0) }
 }
 
-func compareNumbers(userNumbers: Set<Int>, randomNumbers: Set<Int>) -> Int {
-    return userNumbers.intersection(randomNumbers).count
-}
-
-func makeUserRandomNumber() -> Set<Int> {
+func makeUserRandomNumber() -> [Int] {
     var numbers: Set<Int> = []
     while numbers.count < 3 {
         numbers.insert(Int.random(in: 1...9))
     }
-    return numbers
+    return numbers.map { Int($0) }
 }
+
+func compareNumbers(userNumbers: [Int], randomNumbers: [Int]) -> Int {
+    var strike = 0
+    if userNumbers[0] == randomNumbers[0] {
+        strike += 1
+    }
+    if userNumbers[1] == randomNumbers[1] {
+        strike += 1
+    }
+    if userNumbers[2] == randomNumbers[2] {
+        strike += 1
+    }
+    print(strike)
+    return 0
+}
+
+mainLoop()
