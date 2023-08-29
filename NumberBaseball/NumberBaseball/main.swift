8ae03e5aa677c8da81445d451b6900e7d62df725
ue-mu <ym14170@gmail.com>
2023-08-29 16:50:02 +0900

    Feat: 랜덤정수를 생성하는 함수
    1~9 사이의 중복된 수가 없는 세 개의 임의의 정수를 생성하여 반환하는 함수 생성

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -9,3 +9,13 @@ import Foundation
 var randomComputerNumbers: [Int] = []
 var remainsChance = 9
 
+func generateRandomNumbers() -> [Int] {
+    var numbers = [Int]()
+    while numbers.count < 3 {
+        let randomNumber = Int.random(in:1...9)
+        if numbers.contains(randomNumber) == false {
+            numbers.append(randomNumber)
+        }
+    }
+    return numbers
+}
