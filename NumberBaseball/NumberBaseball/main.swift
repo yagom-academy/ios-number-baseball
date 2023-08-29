4e77702ecff26868e3d49e1424c9c49b71ece7b3
woohyeon3382 <woohyeon3382@naver.com>
2023-08-29 17:15:08 +0900

    STEP1기본기능구현

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -6,11 +6,16 @@
 
 import Foundation
 
-var tryCount: Int = 9
+var tryCount = 9
 
+<<<<<<< Updated upstream
 
 func makeUserRandomNumber() -> [Int] {
     var randomNum: Set<Int> = Set<Int>()
+=======
+func makeUserRandomNumber() -> [Int] {
+    var randomNum = Set<Int>()
+>>>>>>> Stashed changes
     while randomNum.count < 3 {
         randomNum.insert(Int.random(in: 1...9))
     }
@@ -18,7 +23,11 @@ func makeUserRandomNumber() -> [Int] {
 }
 
 func makeComputerRandomNumber() -> [Int] {
+<<<<<<< Updated upstream
     var randomNum: Set<Int> = Set<Int>()
+=======
+    var randomNum = Set<Int>()
+>>>>>>> Stashed changes
     while randomNum.count < 3 {
         randomNum.insert(Int.random(in: 1...9))
     }
@@ -27,22 +36,36 @@ func makeComputerRandomNumber() -> [Int] {
 
 var computerChoice = makeComputerRandomNumber()
 
+<<<<<<< Updated upstream
 while true {
     let userNumber = makeUserRandomNumber()
     
     func comparisonRanmdomNumber() {
+=======
+
+
+while true {
+    let userNumber = makeUserRandomNumber()
+    
+    func comparisonRandomNumber() {
+>>>>>>> Stashed changes
         var strike = 0
         var ball = 0
         
         for (index, number) in userNumber.enumerated() {
             if index == computerChoice.firstIndex(of: number) {
                 strike += 1
+<<<<<<< Updated upstream
             } else if computerChoice.contains(number) {
+=======
+            } else if computerChoice.contains(number){
+>>>>>>> Stashed changes
                 ball += 1
             }
         }
         print("\(strike) 스트라이크, \(ball) 볼")
     }
+<<<<<<< Updated upstream
     if tryCount < 0 {
         print("컴퓨터 승리")
         break
@@ -55,4 +78,18 @@ while true {
         tryCount -= 1
     }
     
+=======
+    
+    if tryCount < 0 {
+        print("컴퓨터 승리...!")
+        break
+    } else {
+        print("임의의 수 : \(userNumber)")
+        comparisonRandomNumber()
+        if tryCount > 0 {
+            print("남은 기회 : \(tryCount)")
+        }
+        tryCount -= 1
+    }
+>>>>>>> Stashed changes
 }
