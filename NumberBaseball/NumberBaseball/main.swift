a1cfb0540eee56c5cbb566442b6e1c01a27810f4
morgan <devjoon@naver.com>
2023-08-29 16:50:06 +0900

    feat: 전역변수선언 및 임의의숫자 반환하는 함수 생성

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -1,10 +1,25 @@
 //
 //  NumberBaseball - main.swift
-//  Created by yagom. 
+//  Created by jyubong, morgan.
 //  Copyright © yagom academy. All rights reserved.
 // 
 
 import Foundation
 
-print("Hello, World!")
+var computerNumbers: [Int] = []
+var remainingCount: Int = 9
+
+func getRandomNumbers() -> [Int] {
+    var numbers: [Int] = []
+    
+    while numbers.count < 3 {
+        let randomNumber = Int.random(in: 1...9)
+        
+        if !numbers.contains(randomNumber){
+            numbers.append(randomNumber)
+        }
+    }
+    
+    return numbers
+}
 
