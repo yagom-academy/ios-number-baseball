7e658dfd000812800c09edeb4daf47e309a53ac4
mireu79 <mireu79@github.com>
2023-08-29 17:21:35 +0900

    STEP1 기능구현수정

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -8,88 +8,84 @@ import Foundation
 
 var tryCount = 9
 
-<<<<<<< Updated upstream
 
 func makeUserRandomNumber() -> [Int] {
     var randomNum: Set<Int> = Set<Int>()
-=======
-func makeUserRandomNumber() -> [Int] {
-    var randomNum = Set<Int>()
->>>>>>> Stashed changes
-    while randomNum.count < 3 {
-        randomNum.insert(Int.random(in: 1...9))
-    }
-    return Array(randomNum)
-}
-
-func makeComputerRandomNumber() -> [Int] {
-<<<<<<< Updated upstream
-    var randomNum: Set<Int> = Set<Int>()
-=======
-    var randomNum = Set<Int>()
->>>>>>> Stashed changes
-    while randomNum.count < 3 {
-        randomNum.insert(Int.random(in: 1...9))
-    }
-    return Array(randomNum)
-}
-
-var computerChoice = makeComputerRandomNumber()
-
-<<<<<<< Updated upstream
-while true {
-    let userNumber = makeUserRandomNumber()
     
-    func comparisonRanmdomNumber() {
-=======
-
-
-while true {
-    let userNumber = makeUserRandomNumber()
-    
-    func comparisonRandomNumber() {
->>>>>>> Stashed changes
-        var strike = 0
-        var ball = 0
+    func makeUserRandomNumber() -> [Int] {
+        var randomNum = Set<Int>()
         
-        for (index, number) in userNumber.enumerated() {
-            if index == computerChoice.firstIndex(of: number) {
-                strike += 1
-<<<<<<< Updated upstream
-            } else if computerChoice.contains(number) {
-=======
-            } else if computerChoice.contains(number){
->>>>>>> Stashed changes
-                ball += 1
+        while randomNum.count < 3 {
+            randomNum.insert(Int.random(in: 1...9))
+        }
+        return Array(randomNum)
+    }
+    
+    func makeComputerRandomNumber() -> [Int] {
+        var randomNum: Set<Int> = Set<Int>()
+        var randomNum = Set<Int>()
+        
+        while randomNum.count < 3 {
+            randomNum.insert(Int.random(in: 1...9))
+        }
+        return Array(randomNum)
+    }
+    
+    var computerChoice = makeComputerRandomNumber()
+    
+    
+    while true {
+        let userNumber = makeUserRandomNumber()
+        
+        func comparisonRanmdomNumber() {
+            
+            
+            
+            while true {
+                let userNumber = makeUserRandomNumber()
+                
+                func comparisonRandomNumber() {
+                    
+                    var strike = 0
+                    var ball = 0
+                    
+                    for (index, number) in userNumber.enumerated() {
+                        if index == computerChoice.firstIndex(of: number) {
+                            strike += 1
+                            
+                        } else if computerChoice.contains(number) {
+                            
+                        } else if computerChoice.contains(number){
+                            
+                            ball += 1
+                        }
+                    }
+                    print("\(strike) 스트라이크, \(ball) 볼")
+                }
+                
+                if tryCount < 0 {
+                    print("컴퓨터 승리")
+                    break
+                } else {
+                    print("임의의 수 : \(userNumber)")
+                    comparisonRanmdomNumber()
+                    if tryCount > 0 {
+                        print("남은기회: \(tryCount)")
+                    }
+                    tryCount -= 1
+                }
+                
+                
+                if tryCount < 0 {
+                    print("컴퓨터 승리...!")
+                    break
+                } else {
+                    print("임의의 수 : \(userNumber)")
+                    comparisonRandomNumber()
+                    if tryCount > 0 {
+                        print("남은 기회 : \(tryCount)")
+                    }
+                    tryCount -= 1
+                }
+                
             }
-        }
-        print("\(strike) 스트라이크, \(ball) 볼")
-    }
-<<<<<<< Updated upstream
-    if tryCount < 0 {
-        print("컴퓨터 승리")
-        break
-    } else {
-        print("임의의 수 : \(userNumber)")
-        comparisonRanmdomNumber()
-        if tryCount > 0 {
-            print("남은기회: \(tryCount)")
-        }
-        tryCount -= 1
-    }
-    
-=======
-    
-    if tryCount < 0 {
-        print("컴퓨터 승리...!")
-        break
-    } else {
-        print("임의의 수 : \(userNumber)")
-        comparisonRandomNumber()
-        if tryCount > 0 {
-            print("남은 기회 : \(tryCount)")
-        }
-        tryCount -= 1
-    }
->>>>>>> Stashed changes
-}
