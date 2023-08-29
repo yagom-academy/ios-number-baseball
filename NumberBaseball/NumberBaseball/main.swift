f854d188dcb2ff02c2d66340f8bd3734ee5a1bd5
woohyeon3382 <woohyeon3382@naver.com>
2023-08-29 18:05:34 +0900

    feat: 게임 진행 구현

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -9,83 +9,61 @@ import Foundation
 var tryCount = 9
 
 
+
 func makeUserRandomNumber() -> [Int] {
     var randomNum: Set<Int> = Set<Int>()
     
-    func makeUserRandomNumber() -> [Int] {
-        var randomNum = Set<Int>()
-        
-        while randomNum.count < 3 {
-            randomNum.insert(Int.random(in: 1...9))
-        }
-        return Array(randomNum)
+    while randomNum.count < 3 {
+        randomNum.insert(Int.random(in: 1...9))
     }
+    return Array(randomNum)
+}
+
+func makeComputerRandomNumber() -> [Int] {
+    var randomNum: Set<Int> = Set<Int>()
     
-    func makeComputerRandomNumber() -> [Int] {
-        var randomNum: Set<Int> = Set<Int>()
-        var randomNum = Set<Int>()
-        
-        while randomNum.count < 3 {
-            randomNum.insert(Int.random(in: 1...9))
-        }
-        return Array(randomNum)
+    while randomNum.count < 3 {
+        randomNum.insert(Int.random(in: 1...9))
     }
+    return Array(randomNum)
+}
+
+var computerChoice = makeComputerRandomNumber()
+
+
+
     
-    var computerChoice = makeComputerRandomNumber()
-    
-    
-    while true {
-        let userNumber = makeUserRandomNumber()
+    func compariseRandomNumber(answer: [Int]) -> [Int] {
+        var strike = 0
+        var ball = 0
         
-        func comparisonRanmdomNumber() {
-            
-            
-            
-            while true {
-                let userNumber = makeUserRandomNumber()
-                
-                func comparisonRandomNumber() {
-                    
-                    var strike = 0
-                    var ball = 0
-                    
-                    for (index, number) in userNumber.enumerated() {
-                        if index == computerChoice.firstIndex(of: number) {
-                            strike += 1
-                            
-                        } else if computerChoice.contains(number) {
-                            
-                        } else if computerChoice.contains(number){
-                            
-                            ball += 1
-                        }
-                    }
-                    print("\(strike) 스트라이크, \(ball) 볼")
-                }
-                
-                if tryCount < 0 {
-                    print("컴퓨터 승리")
-                    break
-                } else {
-                    print("임의의 수 : \(userNumber)")
-                    comparisonRanmdomNumber()
-                    if tryCount > 0 {
-                        print("남은기회: \(tryCount)")
-                    }
-                    tryCount -= 1
-                }
-                
-                
-                if tryCount < 0 {
-                    print("컴퓨터 승리...!")
-                    break
-                } else {
-                    print("임의의 수 : \(userNumber)")
-                    comparisonRandomNumber()
-                    if tryCount > 0 {
-                        print("남은 기회 : \(tryCount)")
-                    }
-                    tryCount -= 1
-                }
-                
+        for (index, number) in answer.enumerated() {
+            if index == computerChoice.firstIndex(of: number) {
+                strike += 1
+            } else if computerChoice.contains(number){
+                ball += 1
             }
+        }
+        return [ball, strike]
+    }
+    
+
+    func startGame() {
+        while tryCount > 0 {
+            let userNumber = makeUserRandomNumber()
+            let result = compariseRandomNumber(answer: userNumber)
+        print("임의의 수 :\(userNumber.map{String($0)}.joined(separator: " "))")
+        print("\(result[1]) 스트라이크, \(result[0]) 볼")
+        print("남은 기회 : \(tryCount)")
+        
+        if result[1] == 3 {
+            print("사용자 승리")
+            break
+        }
+        tryCount -= 1
+        if tryCount == 0 {
+            print("컴퓨터 승리")
+        }
+    }
+}
+startGame()
