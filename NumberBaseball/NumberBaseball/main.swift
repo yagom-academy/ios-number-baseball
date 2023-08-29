14c6d2fca638b2df02902ac595318b534f227aed
mireu79 <mireu79@github.com>
2023-08-29 18:22:19 +0900

    feat: STEP1 기본기능구현완료

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -32,38 +32,39 @@ var computerChoice = makeComputerRandomNumber()
 
 
 
-    
-    func compariseRandomNumber(answer: [Int]) -> [Int] {
-        var strike = 0
-        var ball = 0
-        
-        for (index, number) in answer.enumerated() {
-            if index == computerChoice.firstIndex(of: number) {
-                strike += 1
-            } else if computerChoice.contains(number){
-                ball += 1
-            }
-        }
-        return [ball, strike]
-    }
-    
 
-    func startGame() {
-        while tryCount > 0 {
-            let userNumber = makeUserRandomNumber()
-            let result = compariseRandomNumber(answer: userNumber)
+func compariseRandomNumber(answer: [Int]) -> [Int] {
+    var strike = 0
+    var ball = 0
+    
+    for (index, number) in answer.enumerated() {
+        if index == computerChoice.firstIndex(of: number) {
+            strike += 1
+        } else if computerChoice.contains(number){
+            ball += 1
+        }
+    }
+    return [ball, strike]
+}
+
+
+func startGame() {
+    while tryCount >= 0 {
+        let userNumber = makeUserRandomNumber()
+        let result = compariseRandomNumber(answer: userNumber)
         print("임의의 수 :\(userNumber.map{String($0)}.joined(separator: " "))")
         print("\(result[1]) 스트라이크, \(result[0]) 볼")
-        print("남은 기회 : \(tryCount)")
-        
+        if tryCount != 0 {
+            print("남은 기회 : \(tryCount)")
+        } else {
+            print("컴퓨터 승리")
+        }
         if result[1] == 3 {
             print("사용자 승리")
             break
         }
         tryCount -= 1
-        if tryCount == 0 {
-            print("컴퓨터 승리")
-        }
+        
     }
 }
 startGame()
