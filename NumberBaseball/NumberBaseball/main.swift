fd21f6894031c013cc9354a4e1aa8bf8bba1c4c9
ue-mu <ym14170@gmail.com>
2023-08-29 20:56:45 +0900

    Feat: 게임시작 함수 추가
    사용자 입력없이 임의의 수를 생성하여 게임을 시작하는 함수 구현

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -8,21 +8,25 @@ import Foundation
 
 var randomComputerNumbers = generateRandomNumbers()
 var remainsChance = 9
+var userNumbers = [Int]()
 
 func generateRandomNumbers() -> [Int] {
     var numbers = [Int]()
+    
     while numbers.count < 3 {
         let randomNumber = Int.random(in:1...9)
+        
         if numbers.contains(randomNumber) == false {
             numbers.append(randomNumber)
         }
     }
+    
     return numbers
 }
 
 func compareNumbersAndReturnResult() -> [Int] {
-    let userNumbers = generateRandomNumbers()
     var ballAndStrikeCount: [Int] = []
+    userNumbers = generateRandomNumbers()
     
     var ballCount = 0
     var strikeCount = 0
@@ -43,3 +47,27 @@ func compareNumbersAndReturnResult() -> [Int] {
     
     return ballAndStrikeCount
 }
+
+func playGame() {
+    while true {
+        let resultGameNumbers = compareNumbersAndReturnResult()
+        print("임의의 수 : \(userNumbers[0]) \(userNumbers[1]) \(userNumbers[2])")
+        print("\(resultGameNumbers[1]) 스트라이크, \(resultGameNumbers[0]) 볼")
+        
+        if resultGameNumbers[1] == 3 {
+            print("사용자 승리!")
+            break
+        } else {
+            remainsChance -= 1
+        }
+        
+        if remainsChance == 0 {
+            print("컴퓨터 승리...!")
+            break
+        }
+
+        print("남은 기회 : \(remainsChance) ")
+    }
+}
+
+playGame()
