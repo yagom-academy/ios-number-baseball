bdf141c3ffeeddf3971ad053fb4dd58825d78313
morgan <devjoon@naver.com>
2023-08-29 17:33:10 +0900

    feat: 결과를 출력해주는 함수추가, 전역변수 수정

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -6,16 +6,13 @@
 
 import Foundation
 
-var computerNumbers: [Int] = []
-var remainingCount: Int = 9
-
 func getRandomNumbers() -> [Int] {
     var numbers: [Int] = []
     
     while numbers.count < 3 {
         let randomNumber = Int.random(in: 1...9)
         
-        if !numbers.contains(randomNumber){
+        if !numbers.contains(randomNumber) {
             numbers.append(randomNumber)
         }
     }
@@ -23,14 +20,49 @@ func getRandomNumbers() -> [Int] {
     return numbers
 }
 
-func getResult(userNumbers: [Int]) -> (Int, Int) {
+func getResult(computerNumbers:[Int], userNumbers: [Int]) -> (Int, Int) {
     var (strike, ball) = (0,0)
-    for index in 0..<userNumbers.count where computerNumbers.contains(userNumbers[index]){
-        if computerNumbers[index] == userNumbers[index]{
+    
+    for index in 0..<userNumbers.count where computerNumbers.contains(userNumbers[index]) {
+        if computerNumbers[index] == userNumbers[index] {
             strike += 1
-        }else{
+        } else {
             ball += 1
         }
     }
+    
     return (strike, ball)
 }
+
+func playNumberBaseBall() {
+    var remainingCount: Int = 9
+    var computerNumbers = getRandomNumbers()
+    
+    while remainingCount > 0 {
+        let userNumbers = getRandomNumbers()
+        let (strike, ball) = getResult(computerNumbers: computerNumbers, userNumbers: userNumbers)
+        
+        print("임의의 수 : ", terminator: "")
+        for number in userNumbers {
+            print("\(number) ", terminator: "")
+        }
+        print("")
+        print("\(strike) 스트라이크, \(ball) 볼")
+        
+        if strike == 3 {
+            print("유저의 승리...!")
+            return
+        } else {
+            remainingCount -= 1
+        }
+        
+        if remainingCount == 0 {
+            print("컴퓨터의 승리...!")
+            return
+        } else {
+            print("남은 기회 : \(remainingCount)")
+        }
+    }
+}
+
+playNumberBaseBall()
