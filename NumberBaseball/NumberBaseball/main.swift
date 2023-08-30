a556b92cc5b22374c24a7b4fe9075cad12d1ca8e
charles <kch1759@naver.com>
2023-08-30 22:27:56 +0900

    Feat: 유효성 검증 함수 구현
    사용자가 입력하는 세 개의 숫자의 유효성을 검증하는 함수를 구현

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -6,8 +6,7 @@
 
 import Foundation
 
-let randomComputerNumbers = generateRandomNumbers()
-var remainsChance = 9
+var randomComputerNumbers = generateRandomNumbers()
 var userNumbers = [Int]()
 
 func generateRandomNumbers() -> [Int] {
@@ -49,25 +48,34 @@ func calculateStrikeCount(userNumbers: [Int]) -> Int {
 }
 
 func playGame() {
+    var remainsChance = 3
     
     while remainsChance > 0 {
-        userNumbers = generateRandomNumbers()
-        print("임의의 수 : \(userNumbers[0]) \(userNumbers[1]) \(userNumbers[2])")
+        
         print("\(calculateStrikeCount(userNumbers: userNumbers)) 스트라이크, \(calculateBallCount(userNumbers: userNumbers)) 볼")
         
         if calculateStrikeCount(userNumbers: userNumbers) == 3 {
             print("사용자 승리!")
-            break
+            
+            randomComputerNumbers = generateRandomNumbers()
+            conductMenu()
+            
+            return
         } else {
             remainsChance -= 1
         }
         
         if remainsChance == 0 {
             print("컴퓨터 승리...!")
-            break
+            
+            randomComputerNumbers = generateRandomNumbers()
+            conductMenu()
+            
+            return
         }
-
+        
         print("남은 기회 : \(remainsChance) ")
+        inputGameNumbers()
     }
 }
 
@@ -76,10 +84,11 @@ func conductMenu() {
     print("2. 게임종료")
     print("원하는 기능을 선택해주세요 : ", terminator: "")
     
-    if let userInputMenu = readLine(){
+    if let userInputMenu = readLine() {
         switch userInputMenu{
         case "1":
             inputGameNumbers()
+            playGame()
         case "2":
             return
         default:
@@ -90,18 +99,32 @@ func conductMenu() {
 }
 
 func inputGameNumbers() {
-    print("숫자 3개를 띄어쓰기로 구분하여 입력해주세요.")
-    print("중복 숫자는 허용하지 않습니다.")
-    print("입력 : ", terminator: "")
-    
-    if let userInputNumbers = readLine(){
-        userNumbers = userInputNumbers.split(separator:" ").compactMap { Int($0) }
+    while true {
+        print("숫자 3개를 띄어쓰기로 구분하여 입력해주세요.")
+        print("중복 숫자는 허용하지 않습니다.")
+        print("입력 : ", terminator: "")
+        
+        if let userInputNumbers = readLine() {
+            validateInput(userInput: userInputNumbers)
+            userNumbers = userInputNumbers.split(separator:" ").compactMap { Int($0) }
+        }
+        
+        if userNumbers.count == 3 {
+            break
         }
     }
+}
 
+func validateInput(userInput: String) {
+    guard userInput.isEmpty == false else {
+        print("입력이 잘못되었습니다.")
+        return
+    }
+    
+    guard userInput.split(separator: " ").compactMap({ Int($0) }).count == 3 else {
+        print("입력이 잘못되었습니다.")
+        return
+    }
+}
 
 conductMenu()
-
-
-
-
