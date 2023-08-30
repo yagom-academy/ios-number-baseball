4156a84f944b12b4d07bb00c84cd2a4c4af91cc3
ue-mu <ym14170@gmail.com>
2023-08-30 20:00:28 +0900

    Feat: 사용자 메뉴를 출력하고 입력받는 함수와 게임 숫자를 입력받는 함수 구현

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -49,9 +49,9 @@ func calculateStrikeCount(userNumbers: [Int]) -> Int {
 }
 
 func playGame() {
+    
     while remainsChance > 0 {
         userNumbers = generateRandomNumbers()
-        
         print("임의의 수 : \(userNumbers[0]) \(userNumbers[1]) \(userNumbers[2])")
         print("\(calculateStrikeCount(userNumbers: userNumbers)) 스트라이크, \(calculateBallCount(userNumbers: userNumbers)) 볼")
         
@@ -71,4 +71,37 @@ func playGame() {
     }
 }
 
-playGame()
+func conductMenu() {
+    print("1. 게임시작")
+    print("2. 게임종료")
+    print("원하는 기능을 선택해주세요 : ", terminator: "")
+    
+    if let userInputMenu = readLine(){
+        switch userInputMenu{
+        case "1":
+            inputGameNumbers()
+        case "2":
+            return
+        default:
+            print("입력이 잘못되었습니다")
+            conductMenu()
+        }
+    }
+}
+
+func inputGameNumbers() {
+    print("숫자 3개를 띄어쓰기로 구분하여 입력해주세요.")
+    print("중복 숫자는 허용하지 않습니다.")
+    print("입력 : ", terminator: "")
+    
+    if let userInputNumbers = readLine(){
+        userNumbers = userInputNumbers.split(separator:" ").compactMap { Int($0) }
+        }
+    }
+
+
+conductMenu()
+
+
+
+
