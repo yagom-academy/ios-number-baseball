a10628eb70ce5c9305b4b6f79ca69c4a40c9fde6
kiseok <carti1108@naver.com>
2023-08-30 15:59:36 +0900

    refactor: 코멘트 받은 사항 수정
    스트라이크가 3일 때 함수가 종료될 수 있도록 수정
    남은 기회가 0일 때 출력하지 않도록 변경
    import Foundation 제거
    randomNumber 상수명을 computerRandomNumber로 변경
    let 변수 선언 후 개행하는 컨벤션으로 정리

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -1,44 +1,48 @@
 //
 //  NumberBaseball - main.swift
-//  Created by yagom. 
+//  Created by yagom.
 //  Copyright © yagom academy. All rights reserved.
-// 
+//
 
-//import Foundation
 
 func playNumberBaseBallGame() {
-    let randomNumbers: [Int] = generateRandomNumber()
+    let computerRandomNumbers: [Int] = generateRandomNumber()
     var userRandomNumbers: [Int] = []
     var chance: Int = 9
-    
+
     while chance != 0 {
         userRandomNumbers = generateRandomNumber()
         print("임의의 수 : \(userRandomNumbers.map { String($0) }.joined(separator: " "))")
-        let (strike, ball) = checkStrikeAndBall(userNumbers: userRandomNumbers, randomNumbers: randomNumbers)
+        let (strike, ball) = checkStrikeAndBall(userNumbers: userRandomNumbers, randomNumbers: computerRandomNumbers)
+
         print("\(strike) 스트라이크, \(ball) 볼")
         if strike == 3 {
             print("사용자 승리!")
+
+            return
         }
         chance -= 1
-        print("남은 기회 : \(chance)")
+        if chance > 0 {
+            print("남은 기회 : \(chance)")
+        }
     }
     print("컴퓨터 승리...!")
 }
 
 func generateRandomNumber() -> [Int] {
     var numbers: Set<Int> = []
-    
+
     while numbers.count < 3 {
         numbers.insert(Int.random(in: 1...9))
     }
-    
+
     return numbers.map { Int($0) }
 }
 
 func checkStrikeAndBall(userNumbers: [Int], randomNumbers: [Int]) -> (Int, Int) {
     var strike: Int = 0
     var ball: Int = 0
-    
+
     for (index, number) in userNumbers.enumerated() {
         if number == randomNumbers[index] {
             strike += 1
