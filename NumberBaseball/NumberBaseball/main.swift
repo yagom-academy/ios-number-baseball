0cf7efa022ebb2b8b9995aeda34cde0ef2e3b693
morgan <devjoon@naver.com>
2023-08-31 15:16:26 +0900

    refactor: 잘못된 입력에 대한 출력문 통일, 코딩컨벤션 수정, 함수배치변경, for문 네이밍수정

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -2,7 +2,48 @@
 //  NumberBaseball - main.swift
 //  Created by jyubong, morgan.
 //  Copyright © yagom academy. All rights reserved.
-// 
+//
+
+func selectMenu() {
+    var isPlaying: Bool = true
+    
+    while isPlaying {
+        print("""
+        1. 게임 시작
+        2. 게임 종료
+        원하는 기능을 선택해주세요 :
+        """, terminator: " ")
+
+        guard let selectNumber = readLine() else {
+            print("입력이 잘못되었습니다.")
+            continue
+        }
+        
+        switch selectNumber {
+        case "1":
+            playNumberBaseBall()
+        case "2":
+            isPlaying = false
+        default:
+            print("입력이 잘못되었습니다.")
+            continue
+        }
+    }
+}
+
+func playNumberBaseBall() {
+    var remainingCount: Int = 9
+    let computerNumbers = makeRandomNumbers()
+    
+    while remainingCount > 0 {
+        let userNumbers = selectUserNumbers()
+        let strike = countStrike(computerNumbers: computerNumbers, userNumbers: userNumbers)
+        let ball = countBall(computerNumbers: computerNumbers, userNumbers: userNumbers)
+        
+        showRandomNumber(userNumbers: userNumbers)
+        showResult(strike: strike, ball: ball, remainingCount: &remainingCount)
+    }
+}
 
 func makeRandomNumbers() -> [Int] {
     var numbers: Set<Int> = []
@@ -13,6 +54,54 @@ func makeRandomNumbers() -> [Int] {
         numbers.insert(randomNumber)
     }
     return Array(numbers)
+}
+
+func selectUserNumbers() -> [Int] {
+    var isIncorrect: Bool = true
+    
+    while isIncorrect {
+        print("""
+        숫자 3개를 띄어쓰기로 구분하여 입력해주세요.
+        중복 숫자는 허용하지 않습니다.
+        입력 :
+        """, terminator: " ")
+        
+        guard let userInput = readLine() else {
+            print("입력이 잘못되었습니다.")
+            continue
+        }
+        
+        let userNumbers: [Int] = userInput.split(separator: " ").map{Int($0) ?? 10}
+        
+        if validate(userNumbers: userNumbers) {
+            isIncorrect = false
+        } else {
+            continue
+        }
+        return userNumbers
+    }
+}
+
+func validate(userNumbers: [Int]) -> Bool {
+    if userNumbers.count != 3 {
+        print("입력이 잘못되었습니다.")
+        return false
+    }
+    
+    let numbersSetList = Array(Set(userNumbers))
+    
+    if numbersSetList.count != 3 {
+        print("입력이 잘못되었습니다.")
+        return false
+    }
+    
+    for number in numbersSetList{
+        if number > 9 || number < 1 {
+            print("입력이 잘못되었습니다.")
+            return false
+        }
+    }
+    return true
 }
 
 func countStrike(computerNumbers: [Int], userNumbers: [Int]) -> Int {
@@ -62,94 +151,6 @@ func showResult(strike: Int, ball: Int, remainingCount: inout Int) {
         return
     } else {
         print("남은 기회 : \(remainingCount)")
-    }
-}
-
-func selectMenu() {
-    var isPlaying: Bool = true
-    
-    while isPlaying {
-        print("""
-        1. 게임 시작
-        2. 게임 종료
-        원하는 기능을 선택해주세요 :\(" ")
-        """, terminator: "")
-
-        guard let selectNumber = readLine() else {
-            print("입력이 잘못되었습니다.")
-            continue
-        }
-        
-        switch selectNumber {
-        case "1":
-            playNumberBaseBall()
-        case "2":
-            isPlaying = false
-        default:
-            print("입력이 잘못되었습니다.")
-            continue
-        }
-    }
-}
-
-func selectUserNumbers() -> [Int] {
-    var isIncorrect: Bool = true
-    while isIncorrect {
-        print("""
-        숫자 3개를 띄어쓰기로 구분하여 입력해주세요.
-        중복 숫자는 허용하지 않습니다.
-        입력 :\(" ")
-        """, terminator: "")
-        
-        guard let userInput = readLine() else {
-            print("입력이 잘못되었습니다.")
-            continue
-        }
-        let userNumbers: [Int] = userInput.split(separator: " ").map{Int($0) ?? 10}
-        
-        if validate(userNumbers: userNumbers) {
-            isIncorrect = false
-        } else {
-            continue
-        }
-        return userNumbers
-    }
-}
-
-func validate(userNumbers: [Int]) -> Bool {
-    if userNumbers.count != 3 {
-        print("입력이 잘못되었습니다.")
-        return false
-    }
-    
-    let numbersSetList = Array(Set(userNumbers))
-    
-    if numbersSetList.count != 3 {
-        print("중복인 숫자가 존재합니다.")
-        return false
-    }
-    
-    for index in 0..<userNumbers.count {
-        if numbersSetList[index] > 9 || numbersSetList[index] < 1 {
-            print("입력이 잘못되었습니다.")
-            return false
-        }
-    }
-    return true
-}
-
-func playNumberBaseBall() {
-    
-    var remainingCount: Int = 9
-    let computerNumbers = makeRandomNumbers()
-    
-    while remainingCount > 0 {
-        let userNumbers = selectUserNumbers()
-        let strike = countStrike(computerNumbers: computerNumbers, userNumbers: userNumbers)
-        let ball = countBall(computerNumbers: computerNumbers, userNumbers: userNumbers)
-        
-        showRandomNumber(userNumbers: userNumbers)
-        showResult(strike: strike, ball: ball, remainingCount: &remainingCount)
     }
 }
 
