1ffeb569ba6d223b3e91f39cf4e629e76a67e4c0
charles <kch1759@naver.com>
2023-09-01 20:45:10 +0900

    Feat: conductMenu 네이밍 변경, isRunning 변수 추가, 컨벤션 통일, 코드 간결화
    conductMenu -> startMenu 네이밍 변경.
    while문 조건 토글을 위한 isRunning 변수 추가.
    조건문 컨벤션 통일, print 함수 간결화

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -54,62 +54,70 @@ func playGame() {
         
         print("\(calculateStrikeCount(userNumbers: userNumbers)) 스트라이크, \(calculateBallCount(userNumbers: userNumbers)) 볼")
         
-        if calculateStrikeCount(userNumbers: userNumbers) == 3 {
+        guard calculateStrikeCount(userNumbers: userNumbers) != 3 else {
             print("사용자 승리!")
             
             randomComputerNumbers = generateRandomNumbers()
-            conductMenu()
+            startMenu()
             
             return
-        } else {
-            remainsChance -= 1
         }
         
-        if remainsChance == 0 {
+        remainsChance -= 1
+        
+        print("남은 기회 : \(remainsChance) ")
+        
+        guard remainsChance != 0 else {
             print("컴퓨터 승리...!")
             
             randomComputerNumbers = generateRandomNumbers()
-            conductMenu()
+            startMenu()
             
             return
         }
         
-        print("남은 기회 : \(remainsChance) ")
         inputGameNumbers()
     }
 }
 
-func conductMenu() {
-    print("1. 게임시작")
-    print("2. 게임종료")
-    print("원하는 기능을 선택해주세요 : ", terminator: "")
+func startMenu() {
+    print("""
+          1. 게임시작
+          2. 게임종료
+          원하는 기능을 선택해주세요 :
+          """, terminator: " ")
     
-    if let userInputMenu = readLine() {
-        switch userInputMenu{
-        case "1":
-            inputGameNumbers()
-            playGame()
-        case "2":
-            return
-        default:
-            print("입력이 잘못되었습니다")
-            conductMenu()
-        }
+    guard let userInputMenu = readLine() else { return }
+    
+    switch userInputMenu {
+    case "1":
+        inputGameNumbers()
+        playGame()
+    case "2":
+        return
+    default:
+        print("입력이 잘못되었습니다")
+        startMenu()
     }
 }
 
 func inputGameNumbers() {
-    while true {
-        print("숫자 3개를 띄어쓰기로 구분하여 입력해주세요.")
-        print("중복 숫자는 허용하지 않습니다.")
-        print("입력 : ", terminator: "")
+    var isRunning = true
+    
+    while isRunning {
+        print("""
+              숫자 3개를 띄어쓰기로 구분하여 입력해주세요.
+              중복 숫자는 허용하지 않습니다.
+              입력 :
+              """, terminator: " ")
         
-        if let userInputNumbers = readLine() {
-            validateInput(userInput: userInputNumbers)
-        }
+        guard let userInputNumbers = readLine() else { continue }
         
-        if userNumbers.count == 3 {
-            break
+        validateInput(userInput: userInputNumbers)
+        
+        guard userNumbers.count != 3 else {
+            isRunning = false
+            continue
         }
     }
 }
@@ -118,12 +126,12 @@ func validateInput(userInput: String) {
     userNumbers = userInput.split(separator: " ").compactMap({ Int($0) }).filter {$0 < 10}
     
     guard userInput.isEmpty == false,
-         userInput.split(separator: " ").compactMap({ Int($0) }).count == 3,
-         Set(userNumbers).count > 2 else {
-       userNumbers = []
-       print("입력이 잘못되었습니다.")
-       return
+          userInput.split(separator: " ").compactMap({ Int($0) }).count == 3,
+          Set(userNumbers).count > 2 else {
+        userNumbers = []
+        print("입력이 잘못되었습니다.")
+        return
     }
 }
 
-conductMenu()
+startMenu()
