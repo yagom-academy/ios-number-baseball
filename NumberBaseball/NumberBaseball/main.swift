3e734ce3d01046585dd1a468586d9028684766ab
PrismSpirit <yeolkeut@gmail.com>
2024-01-03 23:57:55 +0900

    feat: 세 개의 정수를 전달받아 [1-1]의 수와 비교하여 볼과 스트라이크 결과를 반환하는 함수

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -12,3 +12,20 @@ var remainedInnings = 9
 func getRandomNumbers() -> [Int] {
     return Array(Array(1...9).shuffled()[0..<3])
 }
+
+func calcRoundResult(_ playerNumbers: [Int]) -> (Int, Int) {
+    var strikeNumbers = 0
+    var ballNumbers = 0
+    
+    for i in 0..<3 {
+        for j in 0..<3 where playerNumbers[i] == computerNumbers[j] {
+            if (i == j) {
+                strikeNumbers += 1
+            } else {
+                ballNumbers += 1
+            }
+        }
+    }
+    
+    return (strikeNumbers, ballNumbers)
+}
