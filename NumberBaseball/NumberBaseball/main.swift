1ff9c0608b50fdb16bfc3e23e16d802acafe2f0f
forseaest <u3z@naver.com>
2024-01-04 10:29:42 +0900

    feat: 게임시작 함수 생성

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -29,3 +29,29 @@ func calcRoundResult(_ playerNumbers: [Int]) -> (Int, Int) {
     
     return (strikeNumbers, ballNumbers)
 }
+
+func runGame() {
+    computerNumbers = getRandomNumbers()
+    
+    while remainedInnings > 0 {
+        let playerNumbers = getRandomNumbers()
+        
+        print("임의의 수 : \(playerNumbers[0]) \(playerNumbers[1]) \(playerNumbers[2])")
+        
+        let roundResult = calcRoundResult(playerNumbers)
+        let strikeCount = roundResult.0
+        let ballCount = roundResult.1
+        
+        print("\(strikeCount) 스트라이크, \(ballCount) 볼")
+        
+        if strikeCount == 3 {
+            print("사용자 승리!")
+            return
+        }
+        
+        remainedInnings -= 1
+        print("남은 기회 : \(remainedInnings)")
+    }
+    
+    print("컴퓨터 승리...!")
+}
