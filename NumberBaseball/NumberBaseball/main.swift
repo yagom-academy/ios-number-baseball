7cc9054842c5ac6b3b662b51569151588670b7f7
KKOMGI <kkomgi90@gmail.com>
2024-01-04 12:32:02 +0900

    feat: 랜덤 숫자 3개 생성 및 카운트, 볼 판정

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -8,19 +8,57 @@ import Foundation
 
 var goalNumbers = [Int]()
 var attemp = 9
+var userNumbers = [1,2,3]
 
 func generateRandomNumbers() {
-    for _ in 0...2 {
+    var setNumber = Set<Int> ()
+    
+    while setNumber.count < 3{
         let randomNumber = Int.random(in: 1...9)
         
-        if goalNumbers.contains(randomNumber){
-            
-        } else {
-            goalNumbers.append(randomNumber)
-        }
-        
+        setNumber.insert(randomNumber)
     }
+    goalNumbers = [Int](setNumber)
     print(goalNumbers)
 }
 
+func judgeResult() {
+    var countStrike = 0
+    var countBall = 0
+    
+    if(userNumbers[0] == goalNumbers[0]){
+        countStrike += 1
+    }
+    else if(userNumbers[0] == goalNumbers[1]){
+        countBall += 1
+    }
+    else if(userNumbers[0] == goalNumbers[2]){
+        countBall += 1
+    }
+    
+    if(userNumbers[1] == goalNumbers[0]){
+        countBall += 1
+    }
+    else if(userNumbers[1] == goalNumbers[1]){
+        countStrike += 1
+    }
+    else if(userNumbers[1] == goalNumbers[2]){
+        countBall += 1
+    }
+    
+    if(userNumbers[2] == goalNumbers[0]){
+        countBall += 1
+    }
+    else if(userNumbers[2] == goalNumbers[1]){
+        countBall += 1
+    }
+    else if(userNumbers[2] == goalNumbers[2]){
+        countStrike += 1
+    }
+    
+    print("Strike = \(countStrike), ball = \(countBall)")
+}
+
 generateRandomNumbers()
+print("user = \(userNumbers)")
+judgeResult()
