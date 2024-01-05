8291401bc8e4a420298d331575b7ad5f449f50b2
dannykim1215 <minzami1215@gmail.com>
2024-01-05 11:38:58 +0900

    refactor: userInputNumber 함수 코드 리팩토링

--- NumberBaseball/NumberBaseball/UserInputNumber.swift
+++ NumberBaseball/NumberBaseball/UserInputNumber.swift
@@ -14,16 +14,39 @@ extension NumberBaseBall {
         var flag: Bool = true
         
         while flag {
+            
+            print("입력해주세요 : ", terminator: "")
+            
             guard let randomNum = readLine() else {
                 return
             }
             
             let splitUserInput = randomNum.components(separatedBy: " ")
             
+            if splitUserInput.count > 3 {
+                print("숫자 3개를 띄어쓰기로 구분하여 입력해주세요.")
+                continue
+            } 
+            
+            let deleteSameNumber: Array<String> = []
+            
+            for i in 0...2 {
+                let deleteSameNumber = splitUserInput.filter({$0 == splitUserInput[i]})
+            }
+            
+            if deleteSameNumber.count == 3 {
+                flag = false
+                userNumList = splitUserInput
+            } else if deleteSameNumber.count > 3{
+                print("중복 숫자는 허용하지 않습니다.")
+            }
+            
+            /*
             if splitUserInput[0] != splitUserInput[1] && splitUserInput[0] != splitUserInput[2] && splitUserInput[1] != splitUserInput[2] {
                 flag = false
                 userNumList = splitUserInput
             }
+            */
         }
         compare()
     }
